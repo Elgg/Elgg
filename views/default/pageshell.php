68d22133882d5af341ce756b5cb6cebd156084bd
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-02-14 16:41:20 +0000

    Changes to site structure
    git-svn-id: https://code.elgg.org/elgg/trunk@32 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/pageshell.php
+++ views/default/pageshell.php
@@ -11,19 +11,27 @@
 	 * @copyright Curverider Ltd 2008
 	 * @link http://elgg.org/
 	 * 
+	 * @uses $vars['config'] The site configuration settings, imported
 	 * @uses $vars['title'] The page title
 	 * @uses $vars['body'] The main content of the page
 	 * @uses $vars['messages'] A 2d array of various message registers, passed from system_messages()
 	 */
 
+	// Set title
+		if (empty($vars['title'])) {
+			$title = $vars['config']->sitename;
+		} else {
+			$title = $vars['config']->sitename . ": " . $vars['title'];
+		}
+
 ?>
 
 <html>
 	<head>
-		<title><?php echo $vars['title']; ?></title>
+		<title><?php echo $title; ?></title>
 	</head>
 	<body>
-		<h1><?php echo $vars['title']; ?></h1>
+		<h1><?php echo $title; ?></h1>
 		<?php
 
 			echo elgg_view('messages/list', array('object' => $vars['messages']));
