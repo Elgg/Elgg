a06ecffa834ac26fd4e3d353c8216c9ee3525fca
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-02-15 13:58:52 +0000

    Added a CSS file
    git-svn-id: https://code.elgg.org/elgg/trunk@42 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/css.php
@@ -0,0 +1,17 @@
+<?php
+
+	/**
+	 * Elgg CSS
+	 * The standard CSS file
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 * 
+	 * @uses $vars['wwwroot'] The site URL
+	 */
+
+?>
