743f20d52943887732ea4beaefc67f27b0b4c537
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-03-04 21:18:07 +0000

    Commit so i can switch to the laptop...
    git-svn-id: https://code.elgg.org/elgg/trunk@72 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ engine/lib/metadata.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * Elgg metadata
+	 * Functions to manage object metadata.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey <marcus@dushka.co.uk
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+	
+	
+	// metadata object
+
+
+	// get / sets
+?>
