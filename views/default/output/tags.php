8d001be6fbce495a541175e5c430813fc5f7ccbb
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-04-04 13:24:50 +0000

    Tweaking the output/tags view
    git-svn-id: https://code.elgg.org/elgg/trunk@403 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/output/tags.php
+++ views/default/output/tags.php
@@ -14,7 +14,7 @@
            if (is_string($tag)) {
                $tagstr .= "<a href=\"{$vars['url']}search/?tag=".urlencode($tag) . "{$type}\">{$tag}</a>";
            } else {
-               $tagstr .= "<a href=\"{$vars['url']}search/?tag=".urlencode($tag->tag) . "{$type}\">{$tag->tag}</a>";
+               $tagstr .= "<a href=\"{$vars['url']}search/?tag=".urlencode($tag->value) . "{$type}\">{$tag->value}</a>";
            }
         }
         echo $tagstr;
