5273ac60e0f0490f3d3f7e2126e051ba386ff058
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-04-09 15:14:33 +0000

    Marcus Povey <marcus@dushka.co.uk> * Package header added
    git-svn-id: https://code.elgg.org/elgg/trunk@427 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/guidbrowser/views/default/guidbrowser/entity.php
+++ mod/guidbrowser/views/default/guidbrowser/entity.php
@@ -1,4 +1,14 @@
 <?php
+	/**
+	 * Elgg GUID browser
+	 * 
+	 * @package ElggDevTools
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey <marcus@marcus-povey.co.uk>
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
 	$guid = $vars['entity_guid'];
 	$type = $vars['type'];
 	$subtype = $vars['subtype'];
