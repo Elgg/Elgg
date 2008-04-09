4943b4e05e41d270a8c1448bcf82780ef5103c06
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-04-09 15:20:33 +0000

    Marcus Povey <marcus@dushka.co.uk> * Package header added
    git-svn-id: https://code.elgg.org/elgg/trunk@430 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/apitest/views/default/apitest/configform.php
+++ mod/apitest/views/default/apitest/configform.php
@@ -1,3 +1,14 @@
+<?php
+	/**
+	 * Elgg API Tester
+	 * 
+	 * @package ElggDevTools
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey <marcus@marcus-povey.co.uk>
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+?>
 <form method="post">
 	<input type="hidden" name="action" value="configure" />
 	<p>API Key: <input type="text" name="apikey" value="<?php echo $vars['apikey'];?>" /></p>
--- mod/apitest/views/default/apitest/main.php
+++ mod/apitest/views/default/apitest/main.php
@@ -1,3 +1,14 @@
+<?php
+	/**
+	 * Elgg API Tester
+	 * 
+	 * @package ElggDevTools
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey <marcus@marcus-povey.co.uk>
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+?>
 <script type="text/javascript" language="javascript">
 <!--
 function showhide(oid)
