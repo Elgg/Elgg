c0144b57ce21b4c4be3d0c70358863e534ec5ab5
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-04-18 10:26:45 +0000

    Action debug
    git-svn-id: https://code.elgg.org/elgg/trunk@482 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/actions.php
+++ engine/lib/actions.php
@@ -35,6 +35,7 @@
             
             if (isset($CONFIG->actions[$action])) {
                 if ($CONFIG->actions[$action]['public'] || $_SESSION['id'] != -1) {
+                	system_message("DEBUG ACTION ({$action}): " . $CONFIG->actions[$action]['file']);
 	                if (@include($CONFIG->actions[$action]['file'])) {
 	                } else {
 	                    register_error(sprintf(elgg_echo('actionundefined'),$action));
