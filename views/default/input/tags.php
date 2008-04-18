c5c299f91dc9307b2fd9a232981e49dd74ed611b
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-04-18 11:16:24 +0000

    Updated input tag field to take in strings as well as arrays
    git-svn-id: https://code.elgg.org/elgg/trunk@491 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/input/tags.php
+++ views/default/input/tags.php
@@ -19,21 +19,24 @@
 	 */
 
     $tags = "";
-    
-    if (!empty($vars['value']) && is_array($vars['value'])) {
-        foreach($vars['value'] as $tag) {
-            
-            if (!empty($tags)) {
-                $tags .= ", ";
-            }
-            if (is_string($tag)) {
-            	$tags .= $tag;
-            } else {
-            	$tags .= $tag->value;
-            }
-            
-        }
+    if (!empty($vars['value'])) {
+    	if (is_array($vars['value'])) {
+	        foreach($vars['value'] as $tag) {
+	            
+	            if (!empty($tags)) {
+	                $tags .= ", ";
+	            }
+	            if (is_string($tag)) {
+	            	$tags .= $tag;
+	            } else {
+	            	$tags .= $tag->value;
+	            }
+	            
+	        }
+    	} else {
+    		$tags = $vars['value'];
+    	}
     }
     
 ?>
-<input type="text" <?php echo $vars['js']; ?> name="<?php echo $vars['internalname']; ?>" value="<?php echo $tags; ?>" class="input-tags"/> 
+<input type="text" <?php echo $vars['js']; ?> name="<?php echo $vars['internalname']; ?>" value="<?php echo htmlentities($tags); ?>" class="input-tags"/> 
