dc1b1a48749f441b96cdb7ca67ab35dda0ba895e
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-05-09 17:12:21 +0000

    Added version
    git-svn-id: https://code.elgg.org/elgg/trunk@642 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/opendd.php
+++ engine/lib/opendd.php
@@ -6,6 +6,7 @@
 	 * @subpackage Core
 	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
 	 * @author Marcus Povey
+	 * @version 0.1
 	 * @copyright Curverider Ltd 2008
 	 * @link http://elgg.org/
 	 */
