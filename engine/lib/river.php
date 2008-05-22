2be0ea00c4f2275bec1fe87f369f5e5d7a0ebea5
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-05-22 12:39:39 +0000

    Marcus Povey <marcus@dushka.co.uk> * River stub for the pub.
    git-svn-id: https://code.elgg.org/elgg/trunk@679 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ engine/lib/river.php
@@ -0,0 +1,13 @@
+<?php
+	/**
+	 * Elgg river.
+	 * Functions for listening for and generating the river.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+?>
