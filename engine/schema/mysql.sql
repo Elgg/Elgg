dcda9c67353233750d2552cafcb4f65abe78e5c3
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-05-22 15:28:40 +0000

    Marcus Povey <marcus@dushka.co.uk> * Added system_log table
    git-svn-id: https://code.elgg.org/elgg/trunk@684 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/schema/mysql.sql
+++ engine/schema/mysql.sql
@@ -234,3 +234,17 @@ CREATE TABLE `prefix_datalists` (
   KEY `name` (`name`)
 );
 
+-- System log
+CREATE TABLE `prefix_system_log` (
+	`id` int(11) NOT NULL auto_increment,
+	
+	`object_id` int(11) NOT NULL,
+	`object_class` varchar(128) NOT NULL,
+	
+	`event` varchar(128) NOT NULL,
+	
+	`time_created` int(11) NOT NULL,
+	
+	PRIMARY KEY  (`id`)
+);
+
