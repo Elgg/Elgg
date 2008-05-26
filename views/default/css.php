7590eaa22195821f6cac0177acededcf26284227
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-05-26 19:25:53 +0000

    fixed width page
    git-svn-id: https://code.elgg.org/elgg/trunk@718 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/css.php
+++ views/default/css.php
@@ -75,7 +75,7 @@ small {
 
 /* CONTAINER: WRAPS THE HEADER AND MAIN CONTENT AREA BELOW THE USER TOOLBAR */
 div#container {
-	/* width:90%; */
+	width:925px;
 	padding:0 40px 0 40px;
 	background: url(<?php echo $vars['url']; ?>/_graphics/pagebackground.jpg) no-repeat center top;
 	min-height: 600px;
@@ -178,6 +178,7 @@ div#footer {
 	position: relative;
 	font-size:1em;
 	height:40px;
+	width:925px;
 	margin:20px 40px 20px 40px;
 	font-weight:bold;
 	background: #ffffff;
