f10942fe3da75804f10fd18abec0a9664113afe1
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-05-28 15:43:06 +0000

    Marcus Povey <marcus@dushka.co.uk> * export test import set to use new import action
    git-svn-id: https://code.elgg.org/elgg/trunk@745 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/exporttest/views/default/exporttest/main.php
+++ mod/exporttest/views/default/exporttest/main.php
@@ -15,10 +15,10 @@
 	GUID : <input type="text" value="" name="guid" /> <input type="submit" name="export" value="export" />
 </form>
 
-<form method="post">
+<form method="post" action="<?php echo $vars['url']; ?>action/import/odd">
 	<input type="hidden" name="owner_id" value="<?php echo $vars['owner_id']; ?>" />
 	<input type="hidden" name="action" value="import" />
 	IMPORT : 
-	<textarea name="xml" cols="50" rows="10"></textarea>
+	<textarea name="data" cols="50" rows="10"></textarea>
 	<input type="submit" name="import" value="import" />
 </form>
