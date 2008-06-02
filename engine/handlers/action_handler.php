8223faeddad7da792e577184187ce219548ea5b5
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-02 13:56:08 +0000

    Marcus Povey <marcus@dushka.co.uk> * Moved handlers to engine/handlers/ to avoid them running foul of the callpath_gatekeeper.. UPDATE YOUR .htaccess!
    git-svn-id: https://code.elgg.org/elgg/trunk@770 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ engine/handlers/action_handler.php
@@ -0,0 +1,20 @@
+<?php
+
+    /**
+	 * Elgg action handler
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+    /**
+     *  Load Elgg framework
+     */
+        require_once("../start.php");
+        $action = get_input("action");
+        action($action);
+    
+?>
---
+++ engine/handlers/pagehandler.php
@@ -0,0 +1,26 @@
+<?php
+
+	/**
+	 * Elgg page handler
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+	// Load Elgg engine
+		require_once("../start.php");
+
+	// Get input
+		$handler = get_input('handler');
+		$page = get_input('page');
+		
+	// Call the page handler functions
+		if (!page_handler($handler, $page)) {
+			forward();
+		}
+
+?>
