33cfe143311844cbde22177e07e4f1742bb04e25
icewing <icewing@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-11 13:15:36 +0000

    git-svn-id: https://code.elgg.org/elgg/trunk@866 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/admin.php
+++ engine/lib/admin.php
@@ -14,11 +14,20 @@
 	
 	/**
 	 * Register an admin page with the admin panel.
+	 * This function extends the view "admin/main" with the provided view. This view should provide a description
+	 * and either a control or a link to.
 	 * 
-	 * @param string $view The view associated with the page, this is assumed to be under the view /admin/. 
+	 * Usage:
+	 * 	- To add a control to the main admin panel then extend admin/main
+	 *  - To add a control to a new page create a page which renders a view admin/subpage (where subpage is your new page - 
+	 *    nb. some pages already exist that you can extend), extend the main view to point to it, and add controls to your 
+	 * 	  new view.
+	 * 
+	 * @param string $view The view to extend, by default this is 'admin/main'.
+	 * @param string $new_admin_view The view body associated with the page. 
 	 * @param int $priority Optional priority to govern the appearance in the list.
 	 */
-	function register_elgg_admin_page($view, $priority = 500)
+	function extend_elgg_admin_page($view = 'admin/main', $new_admin_view, $priority = 500)
 	{
 		
 	}
