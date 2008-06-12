c2a5b95aa044431304f65d3b92fd048a9b502c42
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-12 14:30:01 +0000

    Added a simple widget alias so widgets display automatically.
    git-svn-id: https://code.elgg.org/elgg/trunk@894 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/object/widget.php
@@ -0,0 +1,16 @@
+<?php
+
+	/**
+	 * Elgg default widget view
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+		echo elgg_view('widgets/wrapper',$vars);
+
+?>
