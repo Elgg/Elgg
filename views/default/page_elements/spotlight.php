5500d985a267ee0c4fc4d6aa1e6949657eb24848
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-12 13:22:21 +0000

    new view for page spotlight
    git-svn-id: https://code.elgg.org/elgg/trunk@889 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/page_elements/spotlight.php
@@ -0,0 +1,34 @@
+<?php
+
+	/**
+	 * Elgg spotlight
+	 * The spotlight area that displays across the site
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 * 
+	 */
+?>
+
+<div id="layout_spotlight">
+<div id="wrapper_spotlight">
+	
+<div class="collapsable_box no_space_after">
+	<div class="collapsable_box_header">
+		<a href="javascript:void(0);" class="toggle_box_contents">-</a><a href="javascript:void(0);" class="toggle_box_edit_panel">edit</a>
+		<h1>Spotlight</h1>
+	</div>
+	<div class="collapsable_box_editpanel">
+		<p>Here's all the edit controls for this widget</p>
+	</div><!-- /collapsable_box_editpanel -->
+	<div class="collapsable_box_content">
+		<p><img src="<?php echo $vars['url']; ?>_graphics/spacer.gif" border="0" width=100% height=184px /></p>
+	</div><!-- /.collapsable_box_content -->
+</div><!-- /.collapsable_box -->
+	
+</div><!-- /#wrapper_spotlight -->
+</div><!-- /#layout_spotlight -->
