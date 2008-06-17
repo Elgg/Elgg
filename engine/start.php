c6903c11a88ca53823e4e769d7dbe1df2413e614
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-17 17:35:57 +0000

    Fixed #36 - CSS now works on the install page.
    git-svn-id: https://code.elgg.org/elgg/trunk@955 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/start.php
+++ engine/start.php
@@ -150,7 +150,7 @@
 			trigger_elgg_event('boot', 'system');
 			
 		// Forward if we haven't been installed
-			if ((!is_installed() || !is_db_installed()) && !substr_count($_SERVER["PHP_SELF"],"install.php") && !substr_count($_SERVER["PHP_SELF"],"action_handler.php")) {
+			if ((!is_installed() || !is_db_installed()) && !substr_count($_SERVER["PHP_SELF"],"install.php") && !substr_count($_SERVER["PHP_SELF"],"css.php") && !substr_count($_SERVER["PHP_SELF"],"action_handler.php")) {
 					header("Location: install.php");
 					exit;
 			}
