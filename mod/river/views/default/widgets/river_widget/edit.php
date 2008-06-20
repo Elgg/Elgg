3bc3e27d1280383c4e92b0cc4d419521c9ce9e96
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-20 17:10:18 +0000

    Using a different test to handle metadata better
    git-svn-id: https://code.elgg.org/elgg/trunk@1030 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/river/views/default/widgets/river_widget/edit.php
+++ mod/river/views/default/widgets/river_widget/edit.php
@@ -14,7 +14,7 @@
 	
 	<select name="params[limit]">
 		<option value="5" <?php if ($vars['entity']->limit == 5) echo " selected=\"yes\" "; ?>>5</option>
-		<option value="8" <?php if ((!isset($vars['entity']->limit)) || ($vars['entity']->limit == 8)) echo " selected=\"yes\" "; ?>>8</option>
+		<option value="8" <?php if ((!$vars['entity']->limit) || ($vars['entity']->limit == 8)) echo " selected=\"yes\" "; ?>>8</option>
 		<option value="12" <?php if ($vars['entity']->limit == 12) echo " selected=\"yes\" "; ?>>12</option>
 		<option value="15" <?php if ($vars['entity']->limit == 15) echo " selected=\"yes\" "; ?>>15</option>
 	</select>
