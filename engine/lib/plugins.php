801eeb2cf9b1417a3bc8f25f1eec33cb9204be22
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-20 11:52:39 +0000

    Actually closes #60: Enable/disable plugins on a site by site basis http://trac.elgg.org/elgg/ticket/60
    git-svn-id: https://code.elgg.org/elgg/trunk@1018 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/plugins.php
+++ engine/lib/plugins.php
@@ -58,13 +58,16 @@
 				if ($handle = opendir($CONFIG->pluginspath)) {
 					while ($mod = readdir($handle)) {
 						if (!in_array($mod,array('.','..','.svn','CVS')) && is_dir($CONFIG->pluginspath . "/" . $mod)) {
-							if (!@include($CONFIG->pluginspath . $mod . "/start.php"))
-								throw new PluginException(sprintf(elgg_echo('PluginException:MisconfiguredPlugin'), $mod));
-							if (is_dir($CONFIG->pluginspath . $mod . "/views/default")) {
-								autoregister_views("",$CONFIG->pluginspath . $mod . "/views/default",$CONFIG->pluginspath . $mod . "/views/");
-							}
-							if (is_dir($CONFIG->pluginspath . $mod . "/languages")) {
-								register_translations($CONFIG->pluginspath . $mod . "/languages/");
+							if (is_plugin_enabled($mod))
+							{
+								if (!@include($CONFIG->pluginspath . $mod . "/start.php"))
+									throw new PluginException(sprintf(elgg_echo('PluginException:MisconfiguredPlugin'), $mod));
+								if (is_dir($CONFIG->pluginspath . $mod . "/views/default")) {
+									autoregister_views("",$CONFIG->pluginspath . $mod . "/views/default",$CONFIG->pluginspath . $mod . "/views/");
+								}
+								if (is_dir($CONFIG->pluginspath . $mod . "/languages")) {
+									register_translations($CONFIG->pluginspath . $mod . "/languages/");
+								}
 							}
 						}
 					}
