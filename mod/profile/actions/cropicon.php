9e75564ea612baf6cf00d6628ad5bca50b20ce09
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-23 21:35:22 +0000

    Added mission crop icon action
    git-svn-id: https://code.elgg.org/elgg/trunk@1085 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/profile/actions/cropicon.php
@@ -0,0 +1,73 @@
+<?php
+
+	/**
+	 * Elgg profile plugin upload new user icon action
+	 * 
+	 * @package ElggProfile
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Ben Werdmuller <ben@curverider.co.uk>
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
+		$x1 = (int) get_input('x_1',0);
+		$y1 = (int) get_input('y_1',0);
+		$x2 = (int) get_input('x_2',0);
+		$y2 = (int) get_input('y_2',0);
+		
+		$user = page_owner_entity();
+		
+		$filehandler = new ElggFile();
+		$filehandler->owner_guid = $user->getGUID();
+		$filehandler->setFilename("profile/" . $user->username . "master" . ".jpg");
+		$filename = $filehandler->getFilenameOnFilestore();
+		
+		$tiny = get_resized_image_from_existing_file($filename,25,25, true, $x1, $y1, $x2, $y2);
+		$small = get_resized_image_from_existing_file($filename,40,40, true, $x1, $y1, $x2, $y2);
+		$medium = get_resized_image_from_existing_file($filename,100,100, true, $x1, $y1, $x2, $y2);
+		$large = get_resized_image_from_existing_file($filename,200,200, true, $x1, $y1, $x2, $y2);
+		
+		if ($small !== false
+					&& $medium !== false
+					&& $large !== false
+					&& $tiny !== false) {
+				
+					$filehandler = new ElggFile();
+					$filehandler->owner_guid = $_SESSION['user']->getGUID();
+					$filehandler->setFilename("profile/" . $_SESSION['user']->username . "large.jpg");
+					$filehandler->open("write");
+					$filehandler->write($large);
+					$filehandler->close();
+					$filehandler->setFilename("profile/" . $_SESSION['user']->username . "medium.jpg");
+					$filehandler->open("write");
+					$filehandler->write($medium);
+					$filehandler->close();
+					$filehandler->setFilename("profile/" . $_SESSION['user']->username . "small.jpg");
+					$filehandler->open("write");
+					$filehandler->write($small);
+					$filehandler->close();
+					$filehandler->setFilename("profile/" . $_SESSION['user']->username . "tiny.jpg");
+					$filehandler->open("write");
+					$filehandler->write($tiny);
+					$filehandler->close();
+					
+					$user->x1 = $x1;
+					$user->x2 = $x2;
+					$user->y1 = $y1;
+					$user->y2 = $y2;
+					
+					$_SESSION['user']->icontime = time();
+					
+					system_message(elgg_echo("profile:icon:uploaded"));
+				
+				} else {
+					system_message(elgg_echo("profile:icon:notfound"));					
+				}
+			
+	    //forward the user back to the upload page to crop
+	    
+	    $url = $vars['url'] . "mod/profile/editicon.php";
+			
+		if (isloggedin()) forward($url);
+
+?>
