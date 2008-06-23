999f5f77eb07c6b6ab7589c987debf0929a5ebfa
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-23 14:26:55 +0000

    Refs #76: User settings page (to Elgg Classic standard)
    git-svn-id: https://code.elgg.org/elgg/trunk@1062 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ settings/plugins/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+	 * Elgg user settings functions.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey 
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+?>
