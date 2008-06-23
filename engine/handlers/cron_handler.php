cb66a8b4156f26bb4f4dae288fc84e8f289ed86a
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-23 11:23:08 +0000

    Closes #77 - Cron endpoint and event
    git-svn-id: https://code.elgg.org/elgg/trunk@1053 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ engine/handlers/cron_handler.php
@@ -0,0 +1,30 @@
+<?php
+	/**
+	 * Elgg Cron handler.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+	// Load Elgg engine
+	require_once("../start.php");
+	global $CONFIG;
+	
+	// Get a list of parameters
+	$params = array();
+	$params['time'] = time();
+	
+	foreach ($_REQUEST[] as $k => $v)
+		$params[$k] = $v;
+	
+	// Trigger hack
+	$std_out = ""; // Data to return to
+	$std_out = trigger_plugin_hook('system', 'cron', $params, $std_out);
+	
+	// Return event
+	echo $std_out;
+?>
