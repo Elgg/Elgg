fc6b7d5e724215883d3edf01e50b8134e868cc05
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-24 12:01:27 +0000

    Introducing elgg_view_title
    git-svn-id: https://code.elgg.org/elgg/trunk@1097 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/page_elements/title.php
@@ -0,0 +1,18 @@
+<?php
+
+	/**
+	 * Elgg title element
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 * 
+	 * @uses $vars['title'] The page title
+	 */
+
+?>
+
+<h2 class="page_title"><?php echo $vars['title']; ?></h2>
