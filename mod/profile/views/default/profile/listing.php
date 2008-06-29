6bc7f1598dcb73d9140dda024713a3ea5b3e53ec
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-06-29 10:54:55 +0000

    changed the friends list view order
    git-svn-id: https://code.elgg.org/elgg/trunk@1202 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/views/default/profile/listing.php
+++ mod/profile/views/default/profile/listing.php
@@ -11,6 +11,16 @@
 	 * 
 	 * @uses $vars['entity'] The user entity
 	 */
+	 
+	    //grab the users status message with metadata 'state' set to current if it exists
+		if($get_status = get_entities_from_metadata("state", "current", "object", "status", $vars['entity']->guid)){
+    		    
+            foreach($get_status as $s) {
+	            $info = elgg_view("status/friends_view", array('entity' => $s));
+            }
+    		    
+		}
+
 
 		$icon = elgg_view(
 				"profile/icon", array(
@@ -19,20 +29,11 @@
 									  )
 			);
 	
-		$info = "<p><b><a href=\"" . $vars['entity']->getUrl() . "\">" . $vars['entity']->name . "</a></b></p>";
+		$info .= "<p><b><a href=\"" . $vars['entity']->getUrl() . "\">" . $vars['entity']->name . "</a></b></p>";
 	
 		$location = $vars['entity']->location;
 		if (!empty($location)) {
 			$info .= "<p>" . elgg_echo("profile:location") . ": " . elgg_view("output/tags",array('value' => $vars['entity']->location)) . "</p>";
-		}
-		
-		//grab the users status message with metadata 'state' set to current if one exists
-		if($get_status = get_entities_from_metadata("state", "current", "object", "status", $vars['entity']->guid)){
-    		    
-    		    foreach($get_status as $s) {
-        		$info .= elgg_view("status/friends_view", array('entity' => $s));
-    		    }
-    		    
 		}
 		
 		echo elgg_view_listing($icon, $info);
