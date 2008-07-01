c5853b49a6d0096414ec8e0f7696492daabf8f65
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-01 09:20:17 +0000

    Refs #106: Group prototype class
    git-svn-id: https://code.elgg.org/elgg/trunk@1216 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ engine/lib/group.php
@@ -0,0 +1,44 @@
+<?php
+	/**
+	 * Elgg Groups.
+	 * Groups contain other entities, or rather act as a placeholder for other entities to mark any given container
+	 * as their container.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+	/**
+	 * @class ElggGroup Class representing a container for other elgg entities.
+	 * @author Marcus Povey
+	 */
+	class ElggGroup extends ElggEntity
+	{
+		protected function initialise_attributes()
+		{
+			initialise_entity_cache();
+		}
+		
+		
+		
+		// add to container
+		
+		// remove from container
+		
+		
+		// get contained objects (type, subtype, limit, offset)
+		
+		
+		
+		
+		
+		
+		// load
+		
+		//save
+	}
+?>
