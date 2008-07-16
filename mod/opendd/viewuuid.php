b85f962561fb516cb774e6028fde869aedb75f12
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-16 17:44:59 +0000

    Closes #161
    git-svn-id: https://code.elgg.org/elgg/trunk@1446 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/opendd/viewuuid.php
+++ mod/opendd/viewuuid.php
@@ -15,11 +15,23 @@
 	$uuid = get_input('uuid');
 	
 	// Fetch the UUID as an object
-	$entity = opendd_fetch_to_elgg($uuid);
+	$odd = opendd_fetch_uuid($uuid);
 	
-	// If entity then render
-	if ($entity)
+	$body = "";
+	foreach ($odd as $o)
+	{
+		if ($o instanceof ODDMetaData)
+		{
+			if (($o->getAttribute('name') == 'renderedentity') && ($o->getAttribute('type')=='volatile'))
+				$body = $o->getBody();
+		}
+	}
+	
+	if ($body=="")
+	{
+		$entity = opendd_odd_to_elgg($odd);
 		$body = elgg_view_entity($entity, "", true);
+	}
 	
 	$body = elgg_view_layout('one_column',$body);
 		
