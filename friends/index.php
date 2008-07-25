ad4967605b7a1a0bdbda30600a51cf3941de0d25
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-25 16:16:38 +0000

    friends pages altered
    git-svn-id: https://code.elgg.org/elgg/trunk@1546 36083f99-b078-4883-b0ff-0f9b5a30f544

--- friends/index.php
+++ friends/index.php
@@ -18,8 +18,8 @@
 		}
 		
 		$area1 = elgg_view_title(elgg_echo('Friends'));
-		$area1 .= list_entities_from_relationship('friend',$owner->getGUID(),false,'user','',0,10,false);
-		$body = elgg_view_layout('one_column',$area1);
+		$area2 = list_entities_from_relationship('friend',$owner->getGUID(),false,'user','',0,10,false);
+		$body = elgg_view_layout('two_column_left_sidebar',$area1, $area2);
 		
 		echo page_draw(sprintf(elgg_echo("friends:owned"),$owner->name),$body);
 
