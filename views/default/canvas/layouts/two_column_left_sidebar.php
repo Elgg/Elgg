1500a28dc27da1058769c71dc85886eecf61afa9
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-25 14:57:21 +0000

    new canvas layouts
    git-svn-id: https://code.elgg.org/elgg/trunk@1536 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/canvas/layouts/two_column_left_sidebar.php
@@ -0,0 +1,28 @@
+<?php
+
+	/**
+	 * Elgg 2 column left sidebar canvas layout
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+?>
+<!-- left sidebar -->
+<div id="two_column_left_sidebar">
+
+<?php echo $vars['area2']; ?>
+
+</div><!-- /two_column_left_sidebar -->
+
+<!-- main content -->
+<div id="two_column_left_sidebar_maincontent">
+
+<?php echo $vars['area1']; ?>
+
+</div><!-- /two_column_left_sidebar_maincontent -->
+
---
+++ views/default/canvas/layouts/two_column_right_sidebar.php
@@ -0,0 +1,28 @@
+<?php
+
+	/**
+	 * Elgg 2 column right sidebar canvas layout
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+?>
+<!-- main content -->
+<div id="two_column_right_sidebar_maincontent">
+
+<?php echo $vars['area2']; ?>
+
+</div><!-- /two_column_right_sidebar_maincontent -->
+
+<!-- right sidebar -->
+<div id="two_column_right_sidebar">
+
+<?php echo $vars['area1']; ?>
+
+</div><!-- /two_column_right_sidebar -->
+
