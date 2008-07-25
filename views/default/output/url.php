0367f9de2f87e1e63f74a2b50c4ab77c8c122e05
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-25 10:19:44 +0000

    Fixed another oopsy
    git-svn-id: https://code.elgg.org/elgg/trunk@1521 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/output/url.php
+++ views/default/output/url.php
@@ -17,7 +17,7 @@
 
     $val = trim($vars['value']);
     if (!empty($val)) {
-	    if ((substr_count($val, "http://") == 0) || (substr_count($val, "https://") == 0)) {
+	    if ((substr_count($val, "http://") == 0) && (substr_count($val, "https://") == 0)) {
 	        $val = "http://" . $val;
 	    }
 	    echo "<a href=\"{$val}\" target=\"_blank\">{$val}</a>";
