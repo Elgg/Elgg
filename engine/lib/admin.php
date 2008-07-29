ba1ceb336c2ec35816342c38017e8f3700308155
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-29 13:52:16 +0000

    Admin notify method added, view 'admin_message' for this.
    git-svn-id: https://code.elgg.org/elgg/trunk@1568 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/admin.php
+++ engine/lib/admin.php
@@ -70,6 +70,53 @@
 		
 	}
 	
+	/**
+	 * Write a persistent message to the administrator's notification window.
+	 * 
+	 * Currently this writes a message to the admin store, we may want to come up with another way at some point.
+	 *
+	 * @param string $subject Subject of the message
+	 * @param string $message Body of the message
+	 */
+	function send_admin_message($subject, $message)
+	{
+		$subject = sanitise_string($subject);
+		$message = sanitise_string($message);
+		
+		if (($subject) && ($message))
+		{
+			$admin_message = new ElggObject();
+			$admin_message->subtype = 'admin_message';
+			$admin_message->access_id = 2;
+			$admin_message->title = $subject;
+			$admin_message->description = $message;
+			
+			return $admin_message->save();
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * List all admin messages.
+	 *
+	 * @param int $limit Limit
+	 */
+	function list_admin_messages($limit = 10)
+	{
+		return list_entities('object','admin_message',0,$limit);
+	}
+	
+	/**
+	 * Remove an admin message.
+	 *
+	 * @param int $guid The 
+	 */
+	function clear_admin_message($guid)
+	{
+		return delete_entity($guid);
+	}
+	
 	/// Register init function
 	register_elgg_event_handler('init','system','admin_init');
 	
