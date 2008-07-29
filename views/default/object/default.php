7cde2f823aef8c8627c01ee9de85c1eb5ce71d8c
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-29 17:17:54 +0000

    Added some default views.
    git-svn-id: https://code.elgg.org/elgg/trunk@1582 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/object/default.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * ElggEntity default view.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Marcus Povey
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+	echo elgg_view('export/entity', $vars);
+?>
