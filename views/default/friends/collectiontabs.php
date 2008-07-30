68dcf7974b128c3106db203abb70d56dea2eeb7d
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-30 17:19:22 +0000

    pete's collectiontabs code
    git-svn-id: https://code.elgg.org/elgg/trunk@1612 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/friends/collectiontabs.php
+++ views/default/friends/collectiontabs.php
@@ -1,3 +1,4 @@
+/*
 <?php
 
 	global $friendspicker;
@@ -25,3 +26,48 @@
 <li><a href="#" onclick='$("#friends_picker_placeholder<?php echo $friendspicker + 1; ?>").load("<?php echo $vars['url']; ?>friends/pickercallback.php?username=<?php echo $_SESSION['user']->username; ?>&type=picker&members=<?php echo $members; ?>&friends=<?php echo $friends; ?>"); return false;'>Edit collection</a></li>
 </ul>
 </div>
+*/
+
+<?php
+
+?>
+
+<div id="friendsPickerNavigationTabs">
+<ul>
+<li class="selected"><a href="#" class="collectionmembers">Collection members</a></li>
+
+<li><a href="#" class="editmembers">Edit collection</a></li>
+
+</ul>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function () {
+
+	$('a.collectionmembers').click(function () {
+		// load collection members pane
+		$('#friends_picker_placeholder<?php echo $friendspicker; ?>').load('collectionMembers.php');
+		
+		// remove selected state from previous tab
+		$(this).parent().parent().find("#friendsPickerNavigationTabs li").removeClass("selected");
+		// add selected class to current tab
+		$(this).parent().addClass("selected");
+				
+		return false;
+    });
+
+	$('a.editmembers').click(function () {
+		// load friends picker pane
+		$('#friends_picker_placeholder<?php echo $friendspicker; ?>').load('friendsPicker.php');
+
+		// remove selected state from previous tab
+		$(this).parent().parent().find("#friendsPickerNavigationTabs li").removeClass("selected");
+		// add selected class to current tab
+		$(this).parent().addClass("selected");
+	
+		return false;
+    });
+
+
+});
+</script>
