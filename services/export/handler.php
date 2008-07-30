8a8015923368f4adcb7485359f9aed7b785acbe5
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-30 16:09:43 +0000

    git-svn-id: https://code.elgg.org/elgg/trunk@1607 36083f99-b078-4883-b0ff-0f9b5a30f544

--- services/export/handler.php
+++ services/export/handler.php
@@ -5,9 +5,9 @@
 	 * ODD XML file. 
 	 * 
 	 * @package Elgg
-	 * @subpackage ODD
+	 * @subpackage Core
 	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Marcus Povey <marcus@dushka.co.uk>
+	 * @author Marcus Povey 
 	 * @copyright Curverider Ltd 2008
 	 * @link http://elgg.org/
 	 */
