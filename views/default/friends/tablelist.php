33151234eec9e5129a1ce8bd1fca25e40ce8147d
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-30 12:00:32 +0000

    Added the tablelist view
    git-svn-id: https://code.elgg.org/elgg/trunk@1593 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/friends/tablelist.php
@@ -0,0 +1,60 @@
+<?php
+
+	/**
+	 * Elgg friends picker
+	 * Lists the friends picker
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 * 
+	 * @uses $vars['entities'] The array of ElggUser objects
+	 */
+
+		if (is_array($vars['entities'])) {
+			
+?>
+
+	<table>
+		<tr>
+<?php
+			
+			$column = 0;
+			foreach($vars['entities'] as $entity) {
+				
+?>
+
+			<td style="width:25px">
+				<?php echo elgg_view("profile/icon",array('entity' => $entity, 'size' => 'tiny')); ?> 
+			</td>
+			<td style="width: 300px; padding: 5px;">
+<?php
+
+					echo $entity->name;
+				
+?>
+			</td>
+
+<?php
+				
+				$column++;
+				if ($column > 1) {
+					echo "</tr><tr>";
+					$column = 0;
+				}
+				
+			}
+			if ($column > 0) echo "</tr>";
+			
+?>
+
+	</table>
+
+<?php
+			
+		}
+
+?>
