1f0305b7337822f6218aaaa0e3e76080869722a6
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-02 10:02:37 +0000

    Default annotation RSS view.
    git-svn-id: https://code.elgg.org/elgg/trunk@1666 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/rss/annotation/default.php
@@ -0,0 +1,29 @@
+<?php
+
+	/**
+	 * Elgg generic comment
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 * 
+	 */
+
+
+	$vars['entity'] = get_entity($vars['annotation']->entity_guid);
+	$title = substr($vars['annotation']->value,0,32);
+		if (strlen($vars['annotation']->value) > 32)
+			$title .= " ...";
+	
+?>
+
+	<item>
+	  <guid isPermaLink='true'><?php echo $vars['entity']->getURL(); ?>#<?php echo $vars['annotation']->id; ?></guid>
+	  <pubDate><?php echo date("r",$vars['entity']->time_created) ?></pubDate>
+	  <link><?php echo $vars['entity']->getURL(); ?>#<?php echo $vars['annotation']->id; ?></link>
+	  <title><![CDATA[<?php echo $title; ?>]]></title>
+	  <description><![CDATA[<?php echo (autop($vars['annotation']->value)); ?>]]></description>
+	</item>
