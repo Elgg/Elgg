2b0928138f9e03e7d1de2a15eff38ae9fb9aceb8
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-05 16:05:44 +0000

    Widget layout update
    git-svn-id: https://code.elgg.org/elgg/trunk@1714 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/canvas/layouts/widgets.php
+++ views/default/canvas/layouts/widgets.php
@@ -104,13 +104,13 @@ Choose the features you want to add to your page by dragging them from the <b>Wi
 		<h2><?php echo elgg_echo("widgets:rightcolumn"); ?></h2>
 		<div id="rightcolumn_widgets" <?php if(get_context() == "profile")echo "class=\"long\""; ?>>
 		<?php
-			$sidebarwidgetstring = "";
+			$rightcolumn_widgets = "";
 			if (is_array($area3widgets) && sizeof($area3widgets) > 0) {
 				foreach($area3widgets as $widget) {
-					if (!empty($sidebarwidgetstring)) {
-						$sidebarwidgetstring .= "::";
+					if (!empty($rightcolumn_widgets)) {
+						$rightcolumn_widgets .= "::";
 					}
-					$sidebarwidgetstring .= "{$widget->handler}::{$widget->getGUID()}";
+					$rightcolumn_widgets .= "{$widget->handler}::{$widget->getGUID()}";
 		?>
 		
 		<table class="draggable_widget" cellspacing="0"><tr><td width="149px">
@@ -149,13 +149,13 @@ Choose the features you want to add to your page by dragging them from the <b>Wi
 <div id="leftcolumn_widgets">
 
 <?php
-	$mainwidgetstring = "";
+	$leftcolumn_widgets = "";
 	if (is_array($area1widgets) && sizeof($area1widgets) > 0) {
 		foreach($area1widgets as $widget) {
-			if (!empty($mainwidgetstring)) {
-				$mainwidgetstring .= "::";
+			if (!empty($leftcolumn_widgets)) {
+				$leftcolumn_widgets .= "::";
 			}
-			$mainwidgetstring .= "{$widget->handler}::{$widget->getGUID()}";
+			$leftcolumn_widgets .= "{$widget->handler}::{$widget->getGUID()}";
 ?>
 
 <table class="draggable_widget" cellspacing="0"><tr><td width="149px">
@@ -190,13 +190,13 @@ Choose the features you want to add to your page by dragging them from the <b>Wi
 <div id="middlecolumn_widgets">
 
 <?php
-	$sidebarwidgetstring = "";
+	$middlecolumn_widgets = "";
 	if (is_array($area2widgets) && sizeof($area2widgets) > 0) {
 		foreach($area2widgets as $widget) {
-			if (!empty($sidebarwidgetstring)) {
-				$sidebarwidgetstring .= "::";
+			if (!empty($middlecolumn_widgets)) {
+				$middlecolumn_widgets .= "::";
 			}
-			$sidebarwidgetstring .= "{$widget->handler}::{$widget->getGUID()}";
+			$middlecolumn_widgets .= "{$widget->handler}::{$widget->getGUID()}";
 ?>
 
 <table class="draggable_widget" cellspacing="0"><tr><td width="149px">
@@ -237,9 +237,9 @@ Choose the features you want to add to your page by dragging them from the <b>Wi
 </div><!-- /#customise_page_view -->
 
 <form action="<?php echo $vars['url']; ?>action/widgets/reorder" method="post">
-<textarea type="textarea" value="Left widgets"   style="display:none" name="debugField1" id="debugField1" /><?php echo $mainwidgetstring; ?></textarea>
-<textarea type="textarea" value="Middle widgets" style="display:none" name="debugField2" id="debugField2" /><?php echo $sidebarwidgetstring; ?></textarea>
-<textarea type="textarea" value="Right widgets"  style="display:none" name="debugField3" id="debugField3" /><?php echo $leftbarwidgetstring; ?></textarea>
+<textarea type="textarea" value="Left widgets"   style="display:none" name="debugField1" id="debugField1" /><?php echo $leftcolumn_widgets; ?></textarea>
+<textarea type="textarea" value="Middle widgets" style="display:none" name="debugField2" id="debugField2" /><?php echo $middlecolumn_widgets; ?></textarea>
+<textarea type="textarea" value="Right widgets"  style="display:none" name="debugField3" id="debugField3" /><?php echo $rightcolumn_widgets; ?></textarea>
 
 <input type="hidden" name="context" value="<?php echo get_context(); ?>" />
 <input type="hidden" name="owner" value="<?php echo page_owner(); ?>" />
