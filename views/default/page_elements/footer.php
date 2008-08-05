d71ea0185de379c453b655f586177ad78b16a44d
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-05 09:12:42 +0000

    powered by badge in footer
    git-svn-id: https://code.elgg.org/elgg/trunk@1696 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/page_elements/footer.php
+++ views/default/page_elements/footer.php
@@ -24,11 +24,7 @@
 <table width="958" height="79" border="1" cellpadding="0" cellspacing="0">
 	<tr>
 		<td width="310" height="50">
-		<!--
-		<a href="http://www.elgg.org" target="_blank">
-		<img src="<?php echo $vars['url']; ?>_graphics/footer_logo.gif" border="0" />
-		</a>
-		-->
+
 		</td>
 		
 		<td width="648" height="50" align="right">
@@ -47,7 +43,9 @@
 	
 	<tr>
 		<td width="310" height="28">
-		<p><small><a href="http://www.elgg.org" target="_blank">Powered by Elgg</a></small></p>
+		<a href="http://www.elgg.org" target="_blank">
+		<img src="<?php echo $vars['url']; ?>_graphics/powered_by_elgg_badge_drk_bckgnd.gif" border="0" />
+		</a>
 		</td>
 		
 		<td width="648" height="28" align="right">
