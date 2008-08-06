520ec9f99132b7411105164bb4f2c7d06850b9cd
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-06 13:29:59 +0000

    group profile tweak
    git-svn-id: https://code.elgg.org/elgg/trunk@1736 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/groups/profileitems.php
+++ mod/groups/views/default/groups/profileitems.php
@@ -11,13 +11,13 @@
 	 */
 	 
 	 //narrow column
-	 echo "<div class=\"group_narrow_column\" style=\"float:right;width:330px;border:1px solid #efefef;\">";
-	 echo elgg_view("groups/narrow_column",array('entity' => $vars['entity']));
+	 echo "<div class=\"right_column\" style=\"float:right;width:330px;border:1px solid #efefef;\">";
+	 echo elgg_view("groups/right_column",array('entity' => $vars['entity']));
 	 echo "</div>";
 	 
 	 //wider column
-	 echo "<div id=\"group_wide_column\" style=\"width:330px;border:1px solid #efefef;\">";
-	 echo elgg_view("groups/wide_column",array('entity' => $vars['entity']));
+	 echo "<div id=\"left_column\" style=\"width:330px;border:1px solid #efefef;\">";
+	 echo elgg_view("groups/left_column",array('entity' => $vars['entity']));
 	 echo "</div>";	 
 	 
 ?>
