ca0c1bfd57ef7e57d198d2127a381e9688dd2273
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-07 16:44:39 +0000

    changed the latest discussion view
    git-svn-id: https://code.elgg.org/elgg/trunk@1772 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/object/groupforumtopic.php
+++ mod/groups/views/default/object/groupforumtopic.php
@@ -1,11 +1,11 @@
 <?php
 
 	/**
-	 * Elgg Forum listing
+	 * Elgg Groups latest discussion listing
 	 * 
-	 * @package ElggForums
+	 * @package ElggGroups
 	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Dave Tosh <dave@elgg.com>
+	 * @author Curverider <info@elgg.com>
 	 * @copyright Curverider Ltd 2008
 	 * @link http://elgg.com/
 	 */
@@ -14,23 +14,22 @@
     $title = $vars['entity']->title;
     $description = autop($vars['entity']->description);
     $group =  get_entity($vars['entity']->container_guid);
-    $num_topics = $vars['entity']->countEntitiesFromRelationship("forum_topic");
     $forum_created = friendly_time($vars['entity']->time_created);
-    // get the topic entities 
-    $entities = $vars['entity']->getEntitiesFromRelationship("forum_topic");
-    $counter = 0; 
-    foreach($entities as $ent){
-        //count the post annotations for each topic
-        $counter = $ent->countAnnotations("topic_post");
-    }
-	 
-    $info = "<p style=\"float:right;\">" . elgg_echo('created') . " " . $forum_created . ", " . elgg_echo('with') . " " . $num_topics . " " . elgg_echo('topics') . " " . elgg_echo('and') . " " . $counter . " " . elgg_echo('posts') . ".</p>";
+    $counter = $vars['entity']->countAnnotations("group_topic_post");
+    $last_post = $vars['entity']->getAnnotations("group_topic_post", 1, 0, "desc");
+                //get the time and user
+                foreach($last_post as $last){
+                    $last_time = $last->time_created;
+                    $last_user = $last->owner_guid;
+                }
+                
+    $info = "<p style=\"float:right;\">" . elgg_echo('created') . " " . $forum_created . ", " . elgg_echo('with') . " " . $counter . " " . elgg_echo('posts') . "<br />last updated " . friendly_time($last_time) . "<br />by <a href=\"" . get_user($last_user)->getURL() . "\">" . get_user($last_user)->username . "</a></p>";
 	//get the group avatar
 	$icon = elgg_view("profile/icon",array('entity' => $group, 'size' => 'tiny'));
     //get the group and topic title
     $info .= "<p>" . elgg_echo('group') . ": <a href=\"{$group->getURL()}\">{$group->name}</a></p>";
     
-	$info .= "<p>" . elgg_echo('groups:topic') . ": <a href=\"{$vars['url']}mod/groups/topicposts.php?topic={$vars['entity']->guid}&group_guid={$group->guid}\">{$title}</a></p>";
+	$info .= "<p>" . elgg_echo('topic') . ": <a href=\"{$vars['url']}mod/groups/topicposts.php?topic={$vars['entity']->guid}&group_guid={$group->guid}\">{$title}</a></p>";
 	//get the forum description
 	$info .= $description;
 	
