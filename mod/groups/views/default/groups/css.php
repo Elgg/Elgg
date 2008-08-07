2469a36b836950f6ae39bb98e2842adf8fbc57cb
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-07 18:23:52 +0000

    groups css added
    git-svn-id: https://code.elgg.org/elgg/trunk@1782 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/groups/views/default/groups/css.php
@@ -0,0 +1,85 @@
+<?php
+
+	/**
+	 * Elgg Forum CSS extender
+	 * 
+	 * @package ElggForums
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Dave Tosh <dave@elgg.com>
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
+?>
+/* forums overview page */
+.forums_table_head {
+    background: #4690d6;
+    color:#ffffff;
+    padding:4px;
+}
+.forums_table_topics {
+    padding:4px;
+    border-bottom:1px solid #cccccc;
+}
+.forums_table_topics h3 a {
+	font-size: 1.3em;
+}
+.forum_access {
+	font-size: small;	
+}
+.forums_table_topics p {
+	margin:0px 0px 5px 0;
+}
+
+/* topics overview page */
+#topic_titles {
+    background: #4690d6;
+    color:#ffffff;
+    padding:4px;
+    margin:20px 0 0 0;
+}
+
+/* topic posts pages */
+.post_icon {
+    float:left;
+    margin:0 8px 4px 0;
+}
+
+.topic_post {
+    border-bottom:1px solid #cccccc;
+    margin:10px 0 10px 0;
+}
+
+.topic_post h2 {
+    margin-bottom:20px;
+}
+
+.topic_post table, td {
+    border:none;
+}
+
+
+
+
+
+.topic_title {
+	font-size: 1.2em;
+	line-height: 1.1em;
+}
+
+.forum_topics {
+    padding:0;
+    margin:0;
+    border:1px solid #ddd;
+    border-top:0;
+}
+
+/* alternating bckgnd on topics */
+.forum_topics .odd {
+	background-color:#ebebeb;
+	padding: 4px;
+}
+.forum_topics .even {
+	background-color:#f5f5f5;
+	padding: 4px;
+}
