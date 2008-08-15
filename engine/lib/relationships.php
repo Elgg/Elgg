d8a397e02caa7731f4b3c05560404dfa60b9fc18
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-15 13:43:18 +0000

    Added the ability to hide the gallery view on list_entities_from_relationships
    git-svn-id: https://code.elgg.org/elgg/trunk@1937 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/relationships.php
+++ engine/lib/relationships.php
@@ -529,16 +529,17 @@
 	 * @param int $owner_guid The owner (default: all)
 	 * @param int $limit The number of entities to display on a page
 	 * @param true|false $fullview Whether or not to display the full view (default: true)
+	 * @param true|false $viewtypetoggle Whether or not to allow gallery view 
 	 * @return string The viewable list of entities
 	 */
-	function list_entities_from_relationship($relationship, $relationship_guid, $inverse_relationship = false, $type = "", $subtype = "", $owner_guid = 0, $limit = 10, $fullview = true) {
+	function list_entities_from_relationship($relationship, $relationship_guid, $inverse_relationship = false, $type = "", $subtype = "", $owner_guid = 0, $limit = 10, $fullview = true, $viewtypetoggle = false) {
 		
 		$limit = (int) $limit;
 		$offset = (int) get_input('offset');
 		$count = get_entities_from_relationship($relationship, $relationship_guid, $inverse_relationship, $type, $subtype, $owner_guid, "", $limit, $offset, true);
 		$entities = get_entities_from_relationship($relationship, $relationship_guid, $inverse_relationship, $type, $subtype, $owner_guid, "", $limit, $offset);
 
-		return elgg_view_entity_list($entities, $count, $offset, $limit, $fullview);
+		return elgg_view_entity_list($entities, $count, $offset, $limit, $fullview, $viewtypetoggle);
 		
 	}
 	
