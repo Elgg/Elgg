2c3a303bdf91bc229747f3c5f8663c413ee06f8f
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-15 19:07:31 +0000

    Group default RSS view
    git-svn-id: https://code.elgg.org/elgg/trunk@1951 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/rss/group/default.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * Elgg default group view
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+?>
+
+	<item>
+	  <guid isPermaLink='true'><?php echo $vars['entity']->getURL(); ?></guid>
+	  <pubDate><?php echo date("r",$vars['entity']->time_created) ?></pubDate>
+	  <link><?php echo $vars['entity']->getURL(); ?></link>
+	  <title><![CDATA[<?php echo (($vars['entity']->name)); ?>]]></title>
+	  <description><![CDATA[<?php echo (autop($vars['entity']->description)); ?>]]></description>
+	</item>
