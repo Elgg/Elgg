6ad7a51a2967da83fe1d9d067ad42e469ccf6c6b
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-16 13:16:25 +0000

    a little content added to the spotlight
    git-svn-id: https://code.elgg.org/elgg/trunk@1970 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/spotlight/default.php
+++ views/default/spotlight/default.php
@@ -21,9 +21,9 @@
 					<table id="spotlight_table_left_area" width="90%" cellspacing="0">
 						<tr>
 							<td colspan="3" align="left" valign="top">
-							<h2>Help tip / Announcement</h2>
-							
-							<p>Welcome to this Elgg spotlight area. This is a new feature introduced in Elgg v1.0 and we hope you will find it useful. Every plugin within Elgg can set its own content to be displayed in this space. This can include author infomation, help documents or tutorial videos. <a href="">more&hellip;</a><p>
+							<h2>Welcome to the Elgg software</h2>
+							<p>Elgg is a social networking framework. It provides the necessary functionality to allow you to run your own social networking site, whether publicly (like Facebook) or internally on a networked intranet (like Microsoft Sharepoint).</p> 
+							<!-- <p>Welcome to this Elgg spotlight area. This is a new feature introduced in Elgg v1.0 and we hope you will find it useful. Every plugin within Elgg can set its own content to be displayed in this space. This can include author infomation, help documents or tutorial videos.<p> -->
 							</td>
 						</tr>
 						
@@ -31,31 +31,31 @@
 						
 						<tr>
 							<td width="33%" align="left" valign="top">
-							<h2>Help topics</h2>
+							<h2>More information</h2>
 							
 							<ul>
-								<li>Lorem ipsum dolor sit amet</li>
-								<li>Consectetur adipisicing elit</li>
+								<li><a href="http://elgg.org/features.php">Features</a></li>
+								<li><a href="http://elgg.org/about.php">About Elgg</a></li>
 								<li>Sed do eiusmod tempor incididunt</li>
 							</ul>
 							
 							</td>
 							<td width="33%" align="left" valign="top">
 							<ul>
-								<li>Lorem ipsum dolor sit amet</li>
-								<li>Consectetur adipisicing elit</li>
-								<li>Sed do eiusmod tempor incididunt</li>
-								<li>Ut labore et dolore magna aliqua</li>
+								<li><a href="http://news.elgg.org/pg/blog/marcus/read/42/import-and-export-in-elgg-10">Import and Export</a></li>
+								<li><a href="http://news.elgg.org/pg/blog/bwerdmuller/read/38/events-and-auditing-in-elgg">Events and Auditing</li>
+								<li><a href="http://news.elgg.org/pg/blog/bwerdmuller/read/31/translations-and-views-in-elgg-10">Translations and Views</li>
+								<li><a href="http://news.elgg.org/pg/blog/bwerdmuller/read/41/access-control-in-elgg">Access controls</a></li>
 							</ul>
 							
 							
 							</td>
 							<td width="33%" align="left" valign="top">
 							<ul>
-								<li>Ut labore et dolore magna aliqua</li>
-								<li>Lorem ipsum dolor sit amet</li>
-								<li>Consectetur adipisicing elit</li>
-								<li>Sed do eiusmod tempor incididunt</li>
+								<li><a href="http://news.elgg.org">Elgg news</a></li>
+								<li><a href="http://elgg.com">Elgg services</a></li>
+								<li><a href="http://elgg.com">Elgg support</a></li>
+								<li><a href="http://elgg.com">Elgg hosting</a></li>
 							</ul>
 							
 							</td>
@@ -71,14 +71,13 @@
 		<h2>Tutorials</h2>
 					
 		<ul>
-			<li>Consectetur adipisicing elit</li>
-			<li>Sed do eiusmod tempor incididunt</li>
-			<li>Lorem ipsum dolor sit amet</li>
-			<li>Consectetur adipisicing elit</li>
-			<li>Sed do eiusmod tempor incididunt</li>
+			<li><a href="http://docs.elgg.org/wiki/Tutorials/Blog">Simple blog tutorial (plugin)</a></li>
+			<li><a href="http://docs.elgg.org/wiki/Tutorials/Wysiwyg">Simple plugin tutorial</a></li>
+			<li><a href="http://docs.elgg.org/wiki/Widgets#Simple_Example">Simple widget example</a></li>
+			<li><a href="">Populating this spotlight area</a></li>
 		</ul>
 		<!-- /spotlight RHS content -->
+		<a href="http://elgg.com"><img src="http://news.elgg.org/mod/standaloneblog/graphics/elgg_com_badge.gif"></a>
 		</td>
 	</tr>
 </table>
-
