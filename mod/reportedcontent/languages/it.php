a3fb50f366dd63a99968d28503110f5933cdad92
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-09-15 09:51:39 +0000

    Italian language patch
    git-svn-id: https://code.elgg.org/elgg/trunk@2081 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/reportedcontent/languages/it.php
@@ -0,0 +1,52 @@
+<?php
+
+     /**
+	 * Elgg Plugin language pack
+	 * 
+	 * @package Elgg ReportedContent
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 * ****************************************
+     * @Italian Language Pack
+     * @Plugin:  ReportedContent
+     * @version: beta 
+     * english_revision: 2073
+     * @translation by Lord55  
+     * @link http://www.nobilityofequals.com
+     ****************************************/
+
+	$italian = array(
+	
+		/**
+		 * Menu items and titles  ###Argomenti del menu e titoli###
+		 */
+	
+			'item:object:reported_content' => 'Argomenti comunicati',
+			'reportedcontent' => 'contenuto comunicato',
+			'reportedcontent:this' => 'Comunica questo',
+			'reportedcontent:none' => 'Nessun contunuto è stato comunicato',
+			'reportedcontent:report' => 'Comunicalo a un amministratore',
+			'reportedcontent:title' => 'Titolo della Pagina',
+			'reportedcontent:deleted' => 'Il contenuto comunicato è stato cancellato',
+			'reportedcontent:notdeleted' => 'Non siamo stati abilitati a cancellare questa comunicazione',
+			'reportedcontent:delete' => 'Cancellalo',
+			'reportedcontent:areyousure' => 'Sei sicuro di volerlo cancellare?',
+			'reportedcontent:archive' => 'Archivialo',
+			'reportedcontent:archived' => 'La comunicazione è stata cancellata',
+			'reportedcontent:visit' => 'Visit reported item',
+			'reportedcontent:by' => 'Comunicato by',
+			'reportedcontent:objecttitle' => 'Titolo di questo oggetto',
+			'reportedcontent:objecturl' => 'Url di questo oggetto',
+			'reportedcontent:reason' => 'Motivo della comunicazione',
+			'reportedcontent:description' => 'Perchè ci stai comunicando questo?',
+			'reportedcontent:address' => 'Localizzazione di questo argomento',
+			'reportedcontent:success' => 'La tua comunicazione è stata inviata a un amministratore del sito',
+			'reportedcontent:failing' => 'La tua comunicazione non può essere inviata',
+			'reportedcontent:report' => 'Comunicalo', 
+	
+	);
+					
+	add_translation("it",$italian);
+?>
