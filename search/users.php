891090e50ee0ecb5657c61ed2e8865a2adbfc531
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-06 15:31:26 +0000

    User browsing is revamped.
    git-svn-id: https://code.elgg.org/elgg/trunk@2191 36083f99-b078-4883-b0ff-0f9b5a30f544

--- search/users.php
+++ search/users.php
@@ -21,16 +21,20 @@
 	// Get input
 		$tag = get_input('tag');
 		
-		$title = sprintf(elgg_echo('users:searchtitle'),$tag); 
-		
 		if (!empty($tag)) {
+			$title = sprintf(elgg_echo('users:searchtitle'),$tag);
 			$body = "";
 			$body .= elgg_view_title($title); // elgg_view_title(sprintf(elgg_echo('searchtitle'),$tag));
 			$body .= elgg_view('user/search/startblurb',array('tag' => $tag));
 			$body .= list_user_search($tag);
 			$body = elgg_view_layout('two_column_left_sidebar','',$body);
+		} else {
+			$title = elgg_echo('item:user');
+			$body .= elgg_view_title($title);
+			$body .= list_entities('user');
 		}
 		
+		$body = elgg_view_layout('two_column_left_sidebar','',$body);
 		page_draw($title,$body);
 
 ?>
