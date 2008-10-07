cf7047d8eefb3fb691f54afa7152b67d4ce2f763
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-07 09:52:05 +0000

    Thai language pack
    git-svn-id: https://code.elgg.org/elgg/trunk@2203 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/reportedcontent/languages/th.php
@@ -0,0 +1,43 @@
+<?php
+	/**
+	 * Elgg reported content plugin language pack
+	 * 
+	 * @package ElggReportedContent
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
+	$thai = array(
+	
+		/**
+		 * Menu items and titles
+		 */
+	
+			'item:object:reported_content' => 'แจ้งผู้ดูแล',
+			'reportedcontent' => 'รายงานการใช้งาน',
+			'reportedcontent:this' => 'แจ้งผู้ดูแล',
+			'reportedcontent:none' => 'ไม่มีการแจ้ง',
+			'reportedcontent:report' => 'แจ้งไปยังผู้ดูแล',
+			'reportedcontent:title' => 'ชื่อหน้า',
+			'reportedcontent:deleted' => 'รายงานถูกลบแล้ว',
+			'reportedcontent:notdeleted' => 'ไม่สามารถลบรายงานได้',
+			'reportedcontent:delete' => 'ลบเดี่ยวนี้',
+			'reportedcontent:areyousure' => 'แน่ใจไหมที่จะลบ?',
+			'reportedcontent:archive' => 'เก็บไว้ก่อน',
+			'reportedcontent:archived' => 'ระบบได้เก็บรายงานไว้แล้ว',
+			'reportedcontent:visit' => 'ผู้เยี่ยใชมรายงาน',
+			'reportedcontent:by' => 'แจ้งโดย',
+			'reportedcontent:objecttitle' => 'ชื่อ',
+			'reportedcontent:objecturl' => 'ที่อยู่ url',
+			'reportedcontent:reason' => 'เหตุผลที่แจ้ง',
+			'reportedcontent:description' => 'ทำไม่ถึงคิดว่าไม่เหมาะสม?',
+			'reportedcontent:address' => 'ที่อยู่ของหน้าที่แจ้ง',
+			'reportedcontent:success' => 'การรายงานของคุณได้แจ้งไปยังผู้ดูแลแล้ว',
+			'reportedcontent:failing' => 'รายงานของคุณส่งไม่ได้',
+	
+	);
+					
+	add_translation("th",$thai);
+?>
