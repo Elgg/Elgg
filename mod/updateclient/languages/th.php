0895e6b29f640afa40acf495d8bd4e9e3a2f552f
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-07 09:56:41 +0000

    Thai language pack
    git-svn-id: https://code.elgg.org/elgg/trunk@2205 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/updateclient/languages/th.php
@@ -0,0 +1,34 @@
+<?php
+
+	/**
+	 * Update client language pack.
+	 * 
+	 * @package ElggUpdateClient
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */	
+
+	$thai = array(
+	
+		'updateclient:label:core' => 'คอร์',
+		'updateclient:label:plugins' => 'ปลั๊กอิน',
+	
+		'updateclient:settings:days' => 'ตรวจสอบการอัพเดตทุก',
+		'updateclient:days' => 'วัน',
+	
+		'updateclient:settings:server' => 'อัพเดตเซพเวอร์',
+	
+		'updateclient:message:title' => 'Elgg มีรุ่นใหม่!',
+		'updateclient:message:body' => 'Elgg รุ่นใหม่ (%s %s) ชื่อ "%s"ออกมาแล้ว!
+		
+ดาวน์โหลด: %s
+
+หรือเลื่อนไปดูด้านล่างเพื่อดูว่ามีอะไรอัพเดตในรุ่นนี้:
+
+%s',
+	);
+					
+	add_translation("th", $thai);
+?>
