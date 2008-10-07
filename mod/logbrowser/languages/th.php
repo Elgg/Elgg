085a9c30b7cd431af5ff28413d56395e07d2d4c5
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-07 09:33:52 +0000

    Thai language pack
    git-svn-id: https://code.elgg.org/elgg/trunk@2202 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/logbrowser/languages/th.php
@@ -0,0 +1,30 @@
+<?php
+	/**
+	 * Elgg log browser plugin language pack
+	 * 
+	 * @package ElggLogBrowser
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
+	$thai = array(
+	
+		/**
+		 * Menu items and titles
+		 */
+	
+			'logbrowser' => 'ดู ข้อมูลการใช้งาน',
+			'logbrowser:browse' => 'ดูข้อมูลการใช้งานระบบ',
+			'logbrowser:search' => 'ค้นหาข้อมูลการใช้งานระบบ',
+			'logbrowser:user' => 'ค้นหาชื่อผู้ใช้',
+			'logbrowser:starttime' => 'เวลา (เช่น "เดือนที่แล้ว", "1 ชั่วโมงที่แล้ว")',
+			'logbrowser:endtime' => 'เวลาที่จบ',
+	
+			'logbrowser:explore' => 'ข้อมูลการใช้งาน',
+	
+	);
+					
+	add_translation("th",$thai);
+?>
