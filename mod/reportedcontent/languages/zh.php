e198d5bdbb3b3ddb83a590befccf9768ccabd6b4
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-13 13:51:14 +0000

    Refs #424: reportedcontent
    git-svn-id: https://code.elgg.org/elgg/trunk@2243 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/reportedcontent/languages/zh.php
@@ -0,0 +1,51 @@
+<?php
+	/**
+	 * Elgg reported content plugin language pack
+	 * 
+	 * @package ElggReportedContent
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+	/**
+	 *  Chinese Language Package
+	 * 
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @translator Cosmo Mao
+	 * @copyright cOSmoCommerce.com 2008
+	 * @link http://www.elggsns.cn/
+	 * @version 0.1
+	 */
+	$chinese = array(
+	
+		/**
+		 * Menu items and titles
+		 */
+	
+			'item:object:reported_content' => '报告项目',
+			'reportedcontent' => '已经报告',
+			'reportedcontent:this' => '报告这个',
+			'reportedcontent:none' => '没有报告内容',
+			'reportedcontent:report' => '报告给管理员',
+			'reportedcontent:title' => '页面标题',
+			'reportedcontent:deleted' => '报告内容已经删除',
+			'reportedcontent:notdeleted' => '我们无法删除报告内容',
+			'reportedcontent:delete' => '删除',
+			'reportedcontent:areyousure' => '您确认删除吗？',
+			'reportedcontent:archive' => '归档',
+			'reportedcontent:archived' => '报告已经归档',
+			'reportedcontent:visit' => '访问已经报告的项目',
+			'reportedcontent:by' => '报告于',
+			'reportedcontent:objecttitle' => '对象标题',
+			'reportedcontent:objecturl' => '目标URL地址',
+			'reportedcontent:reason' => '报告原因',
+			'reportedcontent:description' => '您问什么报告这个',
+			'reportedcontent:address' => '项目地址',
+			'reportedcontent:success' => '您的报告已经发送给了网站管理员',
+			'reportedcontent:failing' => '您的报告还没有发送',
+	
+	);
+					
+	add_translation("zh",$chinese);
+?>
