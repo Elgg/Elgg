ea2caadad819bac25d8c374a70cc05aec103c08f
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-16 12:51:08 +0000

    Closes #438
    git-svn-id: https://code.elgg.org/elgg/trunk@2269 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/configuration.php
+++ engine/lib/configuration.php
@@ -181,6 +181,7 @@
 				$CONFIG->wwwroot = $CONFIG->site->url;
 				$CONFIG->sitename = $CONFIG->site->name;
 				$CONFIG->sitedescription = $CONFIG->site->description;
+				$CONFIG->siteemail = $CONFIG->site->email;
 			}
 			$CONFIG->url = $CONFIG->wwwroot;
 			
