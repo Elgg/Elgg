65ee9251f2975296a9f9627b36327a59af443b0d
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-17 16:37:54 +0000

    Default output fields for radio and checkboxes
    git-svn-id: https://code.elgg.org/elgg/trunk@2281 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/output/checkboxes.php
@@ -0,0 +1,19 @@
+<?php
+
+	/**
+	 * Elgg text output
+	 * Displays some text that was input using a standard text field
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 * 
+	 * @uses $vars['text'] The text to display
+	 * 
+	 */
+
+    echo elgg_view('output/text',$vars);
+?>
