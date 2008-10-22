fb63a6ab72ac4deb823cecd14c7e95f21801a575
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-22 11:19:34 +0000

    Removed unnecessary column
    git-svn-id: https://code.elgg.org/elgg/trunk@2293 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/schema/mysql.sql
+++ engine/schema/mysql.sql
@@ -272,13 +272,11 @@ CREATE TABLE `prefix_hmac_cache` (
 
 -- PHP Session storage
 CREATE TABLE `prefix_users_sessions` (
-	`id` int(11) NOT NULL auto_increment,
 	`session` varchar(255) NOT NULL,
  	`ts` int(11) unsigned NOT NULL default '0',
 	`data` mediumblob,
 	
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `session` (`session`),
+	PRIMARY KEY `session` (`session`),
 	KEY `ts` (`ts`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- engine/schema/upgrades/2008102101.sql
+++ engine/schema/upgrades/2008102101.sql
@@ -1,11 +1,9 @@
 
 CREATE TABLE  IF NOT EXISTS `prefix_users_sessions` (
-	`id` int(11) NOT NULL auto_increment,
 	`session` varchar(255) NOT NULL,
  	`ts` int(11) unsigned NOT NULL default '0',
 	`data` mediumblob,
 	
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `session` (`session`),
+	PRIMARY KEY `session` (`session`),
 	KEY `ts` (`ts`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
