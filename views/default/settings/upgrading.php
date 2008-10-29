52ba101171a1ded53552b2c6b67d09bd672d8ed9
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-29 13:42:25 +0000

    Added upgrading screen
    git-svn-id: https://code.elgg.org/elgg/trunk@2346 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/settings/upgrading.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<html>
+	<head>
+		<title><?php echo elgg_echo('upgrading'); ?></title>
+		<meta http-equiv="refresh" content="1;url=<?php echo $vars['url']; ?>upgrade.php?upgrade=upgrade"/>
+	</head>
+	<body bgcolor="white">
+		<table width="100%" height="100%" border="0" style="margin: 0px; padding: 0px">
+			<tr>
+				<td width="100%" height="100%" valign="middle" align="center">
+					<img src="<?php echo $vars['url']; ?>_graphics/upgrader.gif" />
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
