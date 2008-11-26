3b430f5e8fbea176fcfe617f0b49206bb7d8c286
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-11-26 21:15:28 +0000

    FOAF header added to profile
    git-svn-id: https://code.elgg.org/elgg/trunk@2493 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/views/default/profile/metatags.php
+++ mod/profile/views/default/profile/metatags.php
@@ -14,3 +14,5 @@
 ?>
 
 	<script type="text/javascript" src="<?php echo $vars['url']; ?>pg/iconjs/profile.js" ></script>
+	<?php if ($owner = page_owner_entity()) { ?><link rel="meta" type="application/rdf+xml" title="FOAF" href="<?php echo full_url(); ?>?view=foaf" /><?php } ?>
+	
