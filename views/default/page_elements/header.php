35d44c0eb3fc509eefbbb42ec72ccb82e9a4acd7
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-01-15 18:12:29 +0000

    Elgg version info now present in headers.
    git-svn-id: https://code.elgg.org/elgg/trunk@2574 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/page_elements/header.php
+++ views/default/page_elements/header.php
@@ -49,11 +49,16 @@ END;
 			$feedref = "";
 		}
 		
+		
+		$version = get_version();
+		$release = get_version(true);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="ElggRelease" content="<?php echo $release; ?>" />
+	<meta name="ElggVersion" content="<?php echo $version; ?>" />
 	<title><?php echo $title; ?></title>
 
 	<script type="text/javascript" src="<?php echo $vars['url']; ?>vendors/jquery/jquery-1.2.6.pack.js"></script>
