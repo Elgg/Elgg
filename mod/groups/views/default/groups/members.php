82abe9787b0397400a7eb606fc6bf19ab07b5a91
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-03 12:19:46 +0000

    changed the number of group members to display on group frontpages from 9999 to 10 (This should become configurable)
    git-svn-id: https://code.elgg.org/elgg/trunk@2632 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/groups/members.php
+++ mod/groups/views/default/groups/members.php
@@ -18,7 +18,7 @@
 
 <?php
 
-    $members = $vars['entity']->getMembers(9999);
+    $members = $vars['entity']->getMembers(10);
     foreach($members as $mem){
            
         echo "<div class=\"member_icon\">" . elgg_view("profile/icon",array('entity' => $mem, 'size' => 'tiny')) . "</div>";   
