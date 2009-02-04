6eedf9ee17e022858491ff2396f6b9b9aa4b2cf7
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-04 14:52:16 +0000

    Added unique title tags to the entity browser. Fixes #722
    git-svn-id: https://code.elgg.org/elgg/trunk@2643 36083f99-b078-4883-b0ff-0f9b5a30f544

--- entities/index.php
+++ entities/index.php
@@ -37,6 +37,11 @@
 			}
 			
 	// Set the body to be the full view of the entity, and the title to be its title
+			if ($entity instanceof ElggObject) {
+				$title = $entity->title;
+			} else if ($entity instanceof ElggEntity) {
+				$title = $entity->name;
+			}
 			$area2 = elgg_view_entity($entity,true);
 			if ($shell) {
 				$body = elgg_view_layout('two_column_left_sidebar', '', $area1 . $area2);
@@ -53,9 +58,10 @@
 		
 	// Display the page
 		if ($shell) {
-			page_draw("", $body);
+			page_draw($title, $body);
 		} else {
 			header("Content-type: text/html; charset=UTF-8");
+			echo $title;
 			echo $body;
 		}
 
