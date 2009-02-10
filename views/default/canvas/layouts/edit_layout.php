1dae4ceb737a031299c910c11e75caf626a4f33c
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-10 11:15:44 +0000

    new edit canvas layout
    git-svn-id: https://code.elgg.org/elgg/trunk@2701 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/canvas/layouts/edit_layout.php
@@ -0,0 +1,20 @@
+<?php
+
+	/**
+	 * Elgg 2 column right sidebar canvas layout
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.org/
+	 */
+
+?>
+
+<div id="blog_edit_page">
+
+<?php if (isset($vars['area1'])) echo $vars['area1']; ?>
+
+</div>
