977c253f7b5b0b4f830ed3b9d3138b60c6d5d515
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-15 17:27:48 +0000

    user settings style brought in line with other settings pages
    git-svn-id: https://code.elgg.org/elgg/trunk@2754 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/usersettings/form.php
+++ views/default/usersettings/form.php
@@ -1,6 +1,6 @@
 <?php
 
-	$form_body = "<div class=\"contentWrapper\">" . elgg_view("usersettings/user") . " ";
+	$form_body = "<div class=\"contentWrapper user_settings\">" . elgg_view("usersettings/user") . " ";
 	$form_body .= "<p>" . elgg_view('input/submit', array('value' => elgg_echo('save'))) . "</p></div>";
 
 	echo elgg_view('input/form', array('action' => "{$vars['url']}action/usersettings/save", 'body' => $form_body));
