a509a16c716315cab6525e8316cada18a340ae71
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-15 17:11:45 +0000

    removed the dropdown menu from forum latest view on group profiles
    git-svn-id: https://code.elgg.org/elgg/trunk@2753 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/groups/forum_latest.php
+++ mod/groups/views/default/groups/forum_latest.php
@@ -19,7 +19,7 @@
                 $count_annotations = $f->countAnnotations("group_topic_post");
                  
         	    echo "<div class=\"forum_latest\">";
-        	    echo "<div class=\"topic_owner_icon\">" . elgg_view('profile/icon',array('entity' => $f->getOwnerEntity(), 'size' => 'tiny')) . "</div>";
+        	    echo "<div class=\"topic_owner_icon\">" . elgg_view('profile/icon',array('entity' => $f->getOwnerEntity(), 'size' => 'tiny', 'override' => true)) . "</div>";
     	        echo "<div class=\"topic_title\"><p><a href=\"{$vars['url']}mod/groups/topicposts.php?topic={$f->guid}&group_guid={$vars['entity']->guid}\">" . $f->title . "</a></p> <p class=\"topic_replies\"><small>".elgg_echo('groups:posts').": " . $count_annotations . "</small></p></div>";
     	        	
     	        echo "</div>";
