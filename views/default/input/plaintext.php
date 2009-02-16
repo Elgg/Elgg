0a17292060a339ef58f6b0cf05b47de424c00db5
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-16 17:32:21 +0000

    Introducing input/plaintext.
    git-svn-id: https://code.elgg.org/elgg/trunk@2770 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/input/plaintext.php
@@ -0,0 +1,25 @@
+<?php
+
+	/**
+	 * Elgg long text input (plaintext)
+	 * Displays a long text input field that should not be overridden by wysiwyg editors.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008-2009
+	 * @link http://elgg.org/
+	 * 
+	 * @uses $vars['value'] The current value, if any
+	 * @uses $vars['js'] Any Javascript to enter into the input tag
+	 * @uses $vars['internalname'] The name of the input field
+	 * 
+	 */
+
+	$class = $vars['class'];
+	if (!$class) $class = "input-textarea";
+	
+?>
+
+<textarea class="<?php echo $class; ?>" name="<?php echo $vars['internalname']; ?>" <?php if ($vars['disabled']) echo ' disabled="yes" '; ?> <?php echo $vars['js']; ?>><?php echo htmlentities($vars['value'], null, 'UTF-8'); ?></textarea> 
