ee4bb6160edbe7c8e12b1983f71f777472874100
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-17 16:14:20 +0000

    river view altered
    git-svn-id: https://code.elgg.org/elgg/trunk@2786 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/river/item/wrapper.php
+++ views/default/river/item/wrapper.php
@@ -18,19 +18,21 @@
 		<div class="river_<?php echo $vars['item']->type; ?>">
 			<div class="river_<?php echo $vars['item']->subtype; ?>">
 				<div class="river_<?php echo $vars['item']->action_type; ?>">				
-					<div class="river_<?php echo $vars['item']->type; ?>_<?php echo $vars['item']->subtype; ?>_<?php echo $vars['item']->action_type; ?>">
-						<span class="river_item_time">
-							<?php
+					<div class="river_<?php echo $vars['item']->type; ?>_<?php if($vars['item']->subtype) echo $vars['item']->subtype . "_"; ?><?php echo $vars['item']->action_type; ?>">
+					<p>
+						<?php
 		
 								echo $vars['body'];
 				
-							?>
+						?>
+						<span class="river_item_time">
 							(<?php
 				
 								echo friendly_time($vars['item']->posted);
 							
 							?>)
 						</span>
+					</p>
 					</div>
 				</div>				
 			</div>
