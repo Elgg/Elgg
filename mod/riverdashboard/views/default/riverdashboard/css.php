cce1163a3a0b25f781a62ec2d1492e53333d7573
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-17 14:59:11 +0000

    css added to sep file
    git-svn-id: https://code.elgg.org/elgg/trunk@2784 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/riverdashboard/views/default/riverdashboard/css.php
+++ mod/riverdashboard/views/default/riverdashboard/css.php
@@ -12,7 +12,44 @@
 
 ?>
 
+/* TEMP riverdashboard css */
 .sidebarBox .recentMember {
-	margin:0 4px 4px 4px;
+	margin:6px 6px 0 0;
 	float:left;
+}
+.sidebarBox #thewire_sidebarInputBox {
+	width:178px;
+}
+.sidebarBox .last_wirepost {
+	margin:20px 0 20px 0;
+}
+.sidebarBox .last_wirepost .thewire-singlepage {
+	margin:0;
+}
+.sidebarBox .last_wirepost .thewire-singlepage .note_date {
+	line-height: 1em;
+	padding:3px 0 0 4px;
+	width:142px;
+}
+.sidebarBox .last_wirepost .thewire-singlepage p.note_body {
+	color:#666666;
+	line-height: 1.2em;
+}
+.sidebarBox .last_wirepost .thewire-singlepage .thewire_icon {
+	margin:6px;
+}
+.sidebarBox .last_wirepost .thewire-singlepage .thewire-post {
+	background-position: 135px bottom;
+}
+.sidebarBox .thewire_characters_remaining {
+	float:right;
+}
+.sidebarBox input.thewire_characters_remaining_field {
+	background: #dedede;
+}
+.sidebarBox input#thewire_submit_button {
+	background: #4690d6;
+	margin:2px 0 0 0;
+	padding:2px 2px 1px 2px;
+	height:auto;
 }
