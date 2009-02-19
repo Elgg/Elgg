21892fdd68f0d5213f2da5ab86ed96fac504c2f5
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-19 11:58:14 +0000

    title tweaked
    git-svn-id: https://code.elgg.org/elgg/trunk@2807 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/edit.php
+++ mod/groups/edit.php
@@ -16,7 +16,7 @@
 	$group = get_entity($group_guid);
 	set_page_owner($group_guid);
 
-	$title = elgg_echo("groups:new");
+	$title = elgg_echo("groups:edit");
 	$body = elgg_view_title($title);
 	
 	if (($group) && ($group->canEdit()))
