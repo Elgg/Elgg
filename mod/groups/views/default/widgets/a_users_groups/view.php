b47af8c49e18f7ec5c59f3930e1ba4c93b0810ce
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-20 16:00:56 +0000

    group widget tweaked
    git-svn-id: https://code.elgg.org/elgg/trunk@2867 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/widgets/a_users_groups/view.php
+++ mod/groups/views/default/widgets/a_users_groups/view.php
@@ -13,8 +13,30 @@
 	$owner = $vars['entity']->owner_guid;
       
     //$groups = get_users_membership($owner);
-    $groups = list_entities_from_relationship('member',$owner,false,'group','',0,$number,false,false,false);
+    //$groups = list_entities_from_relationship('member',$owner,false,'group','',0,$number,false,false,false);
+	$groups = get_entities_from_relationship('member', $owner, false, "group", "", 0, "", $number, 0, false, 0);
 	
-    echo $groups;
+
+    if($groups){
+		
+		echo "<div class=\"sidebarBox featuredgroups\">";
+
+		foreach($groups as $group){
+			$icon = elgg_view(
+				"groups/icon", array(
+									'entity' => $group,
+									'size' => 'small',
+								  )
+				);
+				
+			echo "<div class=\"contentWrapper\">" . $icon . " <p><span>" . $group->name . "</span><br />";
+			echo $group->briefdescription . "</p><div class=\"clearfloat\"></div></div>";
+			
+		}
+		echo "</div>";
+    }
+
+
+   // echo $groups;
       
 ?>
