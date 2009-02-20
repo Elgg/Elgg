fe1b0d56d4515ede1a3128c649c487b15fb6ffa4
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-20 10:40:47 +0000

    new spotlight contents
    git-svn-id: https://code.elgg.org/elgg/trunk@2843 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/spotlight/default.php
+++ views/default/spotlight/default.php
@@ -14,69 +14,20 @@
 	 */
 ?>
 
-<table id="spotlight_table" width="100%"cellspacing="0">
-	<tr>
-		<td align="left" valign="top">
-		<!-- spotlight LHS content -->
-					<table id="spotlight_table_left_area" width="90%" cellspacing="0">
-						<tr>
-							<td colspan="3" align="left" valign="top">
-							<h2>Welcome to the Elgg software</h2>
-							<p>Elgg is a social networking framework. It provides the necessary functionality to allow you to run your own social networking site, whether publicly (like Facebook) or internally on a networked intranet (like Microsoft Sharepoint).</p> 
-							<!-- <p>Welcome to this Elgg spotlight area. This is a new feature introduced in Elgg v1.0 and we hope you will find it useful. Every plugin within Elgg can set its own content to be displayed in this space. This can include author infomation, help documents or tutorial videos.<p> -->
-							</td>
-						</tr>
-						
-						<tr><td width="100%" height="10" colspan="3" align="left" valign="top"></td>	</tr>
-						
-						<tr>
-							<td width="33%" align="left" valign="top">
-							<h2>More information</h2>
-							
-							<ul>
-								<li><a href="http://elgg.org/features.php">Features</a></li>
-								<li><a href="http://elgg.org/about.php">About Elgg</a></li>
-							</ul>
-							
-							</td>
-							<td width="33%" align="left" valign="top">
-							<ul>
-								<li><a href="http://news.elgg.org/pg/blog/marcus/read/42/import-and-export-in-elgg-10">Import and Export</a></li>
-								<li><a href="http://news.elgg.org/pg/blog/bwerdmuller/read/38/events-and-auditing-in-elgg">Events and Auditing</li>
-								<li><a href="http://news.elgg.org/pg/blog/bwerdmuller/read/31/translations-and-views-in-elgg-10">Translations and Views</li>
-								<li><a href="http://news.elgg.org/pg/blog/bwerdmuller/read/41/access-control-in-elgg">Access controls</a></li>
-							</ul>
-							
-							
-							</td>
-							<td width="33%" align="left" valign="top">
-							<ul>
-								<li><a href="http://news.elgg.org">Elgg news</a></li>
-								<li><a href="http://elgg.com">Elgg services</a></li>
-								<li><a href="http://elgg.com">Elgg support</a></li>
-								<li><a href="http://elgg.com">Elgg hosting</a></li>
-							</ul>
-							
-							</td>
-						</tr>
-					</table>
-				<!-- /spotlight LHS content -->
-
-		</td>		
-		
-		
-		<td width="250" align="left" valign="top">
-		<!-- spotlight RHS content -->
-		<h2>Tutorials</h2>
-					
+<div id="spotlight_table">
+	<!-- spotlight RHS content -->
+	<div style="float:right;width:300px;" />
+		<h2>Information</h2>			
 		<ul>
-			<li><a href="http://docs.elgg.org/wiki/Tutorials/Blog">Simple blog tutorial (plugin)</a></li>
-			<li><a href="http://docs.elgg.org/wiki/Tutorials/Wysiwyg">Simple plugin tutorial</a></li>
-			<li><a href="http://docs.elgg.org/wiki/Widgets#Simple_Example">Simple widget example</a></li>
 			<li><a href="http://docs.elgg.org/wiki/Views/SystemViews">Populating this spotlight area</a></li>
+			<li><a href="http://community.elgg.org">Elgg open source community</a></li>
+			<li><a href="http://elgg.com">Elgg commercial services</a></li>
 		</ul>
-		<!-- /spotlight RHS content -->
-		<a href="http://elgg.com"><img src="http://news.elgg.org/mod/standaloneblog/graphics/elgg_com_badge.gif"></a>
-		</td>
-	</tr>
-</table>
+	</div>
+	<!-- spotlight LHS content -->
+	<div id="spotlight_table_left_area">
+		<h2>Welcome to Elgg</h2>
+		<p>Elgg is a social networking engine. It provides the necessary functionality to allow you to run your own social networking site, whether publicly (like Facebook) or internally on an Intranet.</p> 
+	</div><!-- /spotlight LHS content -->
+	<div class="clearfloat"></div>
+</div>
