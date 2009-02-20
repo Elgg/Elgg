0751f7a4460263f083d3d41390afd931f55dfc05
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-20 13:47:10 +0000

    formatted spotlight footer
    git-svn-id: https://code.elgg.org/elgg/trunk@2857 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/spotlight/default.php
+++ views/default/spotlight/default.php
@@ -16,7 +16,7 @@
 
 <div id="spotlight_table">
 	<!-- spotlight RHS content -->
-	<div style="float:right;width:300px;" />
+	<div class="spotlightRHS" />
 		<h2>Information</h2>			
 		<ul>
 			<li><a href="http://docs.elgg.org/wiki/Views/SystemViews">Populating this spotlight area</a></li>
@@ -25,7 +25,7 @@
 		</ul>
 	</div>
 	<!-- spotlight LHS content -->
-	<div id="spotlight_table_left_area">
+	<div class="spotlightLHS">
 		<h2>Welcome to Elgg</h2>
 		<p>
 			Elgg allows you to run your own social networking site, whether publicly (like 
