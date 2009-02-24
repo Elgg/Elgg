516fa00d4079fb504eb4c2b8129f37c276095a7c
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-24 16:47:05 +0000

    all groups search form
    git-svn-id: https://code.elgg.org/elgg/trunk@2925 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/groups/find.php
+++ mod/groups/views/default/groups/find.php
@@ -7,8 +7,12 @@
 ?>
 <div class="sidebarBox">
 <h3><?php echo elgg_echo('groups:searchtag'); ?></h3>
-<form id="groupssearchform" action="" method="get">
-	<input type="text" name="group_find" value="Search" onclick="if (this.value=='Search') { this.value='' }" class="search_input" />
+<form id="groupsearchform" action="<?php echo $vars['url']; ?>pg/groups/world/?" method="get">
+	<input type="text" name="tag" value="tag" onclick="if (this.value=='tag') { this.value='' }" class="search_input" />
+	<input type="hidden" name="subtype" value="" />
+	<input type="hidden" name="object" value="group" />
+	<input type="hidden" name="tagtype" value="" />
+	<input type="hidden" name="owner_guid" value="0" />
 	<input type="submit" value="<?php echo elgg_echo('go'); ?>" />
 </form>
 </div>
