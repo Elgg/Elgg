6d93f199ab182dcb262c2fa01d82c409757fa56c
kevinjardine <kevinjardine@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-03-04 10:59:13 +0000

    Commented out a no longer needed line of code
    git-svn-id: https://code.elgg.org/elgg/trunk@3057 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/tags.php
+++ engine/lib/tags.php
@@ -132,7 +132,7 @@
 			$query .= " and e.container_guid = {$owner_guid} ";
 		}
 		
-		$userid = get_loggedin_userid();
+		//$userid = get_loggedin_userid();
 		//$query .= " and (e.access_id in {$access} or (e.access_id = " . ACCESS_PRIVATE . " and e.owner_guid = {$userid}))";
 		$query .= ' and ' . get_access_sql_suffix("e"); // Add access controls
 		
