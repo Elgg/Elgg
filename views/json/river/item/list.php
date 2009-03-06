5e4d72b3e2f3ca473bdaf86038f133e046cec50c
ben <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-03-06 10:34:04 +0000

    Improved JSON version of the dashboard
    git-svn-id: https://code.elgg.org/elgg/trunk@3107 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/json/river/item/list.php
@@ -0,0 +1,49 @@
+<?php
+
+	global $jsonexport;
+	if (isset($vars['items']) && is_array($vars['items'])) {
+		
+		$i = 0;
+		if (!empty($vars['items']))
+		foreach($vars['items'] as $item) {
+			
+			// echo elgg_view_river_item($item);
+			if (elgg_view_exists($item->view,'default')) {
+				$body = elgg_view($item->view,array(
+									'item' => $item
+								 ),false,false,'default');
+				$time = date("r",$item->posted);
+				if ($entity = get_entity($item->object_guid)) {
+					$url = htmlspecialchars($entity->getURL());
+				} else {
+					$url = $vars['url'];
+				}
+				$title = strip_tags($body);
+
+				$jsonitem = $item;
+				$jsonitem->url = $url;
+				$jsonitem->description = autop($body);
+				$jsonitem->title = $title;
+				unset($jsonitem->view);
+				
+				if ($subject = get_entity($item->subject_guid)) {
+					elgg_view_entity($subject);
+				}
+				if ($object = get_entity($item->object_guid)) {
+					elgg_view_entity($object);
+				}
+				
+				$jsonexport['activity'][] = $jsonitem;
+				
+			}
+			
+			$i++;
+			if ($i >= $vars['limit']) break;
+			
+		}
+		
+	}
+	echo "!";
+	
+
+?>
