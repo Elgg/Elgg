5f029ee36ac96cf13d33c787258af85959b95170
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-03-31 12:39:19 +0000

    Version bump
    git-svn-id: https://code.elgg.org/elgg/trunk@3185 36083f99-b078-4883-b0ff-0f9b5a30f544

--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 	 * @link http://elgg.org/
 	 */
 
-	   $version = 2009033001;  // YYYYMMDD   = Elgg Date
+	   $version = 2009033101;  // YYYYMMDD   = Elgg Date
 	                           //         XX = Interim incrementer
 	
 	   $release = '1.5';    // Human-friendly version name
