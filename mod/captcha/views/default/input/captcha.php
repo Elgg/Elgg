fd24866b8983e652d8b68cd65d7d22dc2731278d
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-05-21 14:41:37 +0000

    captcha layout tweak
    git-svn-id: https://code.elgg.org/elgg/trunk@3298 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/captcha/views/default/input/captcha.php
+++ mod/captcha/views/default/input/captcha.php
@@ -15,20 +15,12 @@
 <div class="captcha">
 	<input type="hidden" name="captcha_token" value="<?php echo $token; ?>" />
 	<label>
-		<?php echo elgg_echo('captcha:entercaptcha'); ?><br />
-		
-		<table>
-			<tr>	
-				<td>
-					<div class="captcha-left">
-						<?php echo elgg_view('input/text', array('internalname' => 'captcha_input', 'class' => 'captcha-input-text')); ?>
-					</div>
-				</td>
-				<td width="125">
-					<div class="captcha-right">
-						<img class="captcha-input-image" src="<?php echo $vars['url'] . "pg/captcha/$token"; ?>" /><br />
-					</div>
-				</td>
-		</table>
+		<?php echo elgg_echo('captcha:entercaptcha'); ?><br /><br />
+		<div class="captcha-right">
+			<img class="captcha-input-image" src="<?php echo $vars['url'] . "pg/captcha/$token"; ?>" /><br />
+		</div><br />
+		<div class="captcha-left">
+			<?php echo elgg_view('input/text', array('internalname' => 'captcha_input', 'class' => 'captcha-input-text')); ?>
+		</div>
 	</label>
 </div>
