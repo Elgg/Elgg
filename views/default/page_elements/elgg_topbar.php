39dfeae3037782f8bd3dc2b5aa6e2da8ff65c63b
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-05-28 14:37:39 +0000

    Refs #965: Topbar search now pointing at correct url.
    git-svn-id: https://code.elgg.org/elgg/trunk@3307 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/page_elements/elgg_topbar.php
+++ views/default/page_elements/elgg_topbar.php
@@ -70,7 +70,7 @@
 </div>
 
 <div id="elgg_topbar_container_search">
-<form id="searchform" action="<?php echo $vars['url']; ?>search/" method="get">
+<form id="searchform" action="<?php echo $vars['url']; ?>pg/search/" method="get">
 	<input type="text" size="21" name="tag" value="<?php echo elgg_echo('search'); ?>" onclick="if (this.value=='<?php echo elgg_echo('search'); ?>') { this.value='' }" class="search_input" />
 	<input type="submit" value="<?php echo elgg_echo('search:go'); ?>" class="search_submit_button" />
 </form>
