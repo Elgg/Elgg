19c8a6be26af62ee54db634ca1e1b1973faec38e
marcus <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-06-14 20:09:18 +0000

    URL shortener view.
    git-svn-id: https://code.elgg.org/elgg/trunk@3334 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ views/default/input/urlshortener.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * This view provides a hook for third parties to provide a URL shortener.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008-2009
+	 * @link http://elgg.org/
+	 */
+?>
