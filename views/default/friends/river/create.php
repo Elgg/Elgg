aaf2f7b1bdf0be7c5c08e21802a636f6df61419b
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-08-21 15:48:28 +0000

    new friending river view applied
    git-svn-id: https://code.elgg.org/elgg/trunk@3437 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/friends/river/create.php
+++ views/default/friends/river/create.php
@@ -7,7 +7,11 @@
 	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 	$string = sprintf(elgg_echo("friends:river:add"),$url) . " ";
 	$string .= "<a href=\"{$performed_on->getURL()}\">{$performed_on->name}</a>";
-
+	$string .= "<div class=\"river_content_display\">";
+	$string .= "<table><tr><td>" . elgg_view("profile/icon",array('entity' => $performed_by, 'size' => 'small')) . "</td>";
+	$string .= "<td><div class=\"following_icon\"></div></td><td>" . elgg_view("profile/icon",array('entity' => $performed_on, 'size' => 'small')) . "</td></tr></table>";
+	$string .= "</div>";
+	
 ?>
 
 <?php 
