7baad302a90c1af727870e482ea3b8d4f3a19156
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-08-27 12:10:06 +0000

    a missing pipe added
    git-svn-id: https://code.elgg.org/elgg/trunk@3445 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/annotation/annotate.php
+++ views/default/annotation/annotate.php
@@ -11,7 +11,7 @@
 	$contents = strip_tags($contents);//this is so we don't get large images etc in the activity river
 	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 	$string = sprintf(elgg_echo("river:posted:generic"),$url) . " ";
-	$string .= elgg_echo("{$subtype}:river:annotate") . " <a href=\"" . $object->getURL() . "\">" . $object->title . "</a>";
+	$string .= elgg_echo("{$subtype}:river:annotate") . " | <a href=\"" . $object->getURL() . "\">" . $object->title . "</a>";
 	$string .= "<div class=\"river_content_display\">";
 	if(strlen($contents) > 200) {
         	$string .= substr($contents, 0, strpos($contents, ' ', 200)) . "...";
