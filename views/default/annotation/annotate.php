039a02659f42e31ae48e145c6fa2308dca5fc9ac
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-08-27 12:06:23 +0000

    a snippet of a new comment on an object will now appear in the activity river
    git-svn-id: https://code.elgg.org/elgg/trunk@3444 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/annotation/annotate.php
+++ views/default/annotation/annotate.php
@@ -4,11 +4,21 @@
 	$object = get_entity($vars['item']->object_guid);
 	$url = $object->getURL();
 	$subtype = get_subtype_from_id($object->subtype);
-
+	$comment = $object->getAnnotations("generic_comment", 1, 0, "desc"); 
+	foreach($comment as $c){
+		$contents = $c->value;
+	}
+	$contents = strip_tags($contents);//this is so we don't get large images etc in the activity river
 	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 	$string = sprintf(elgg_echo("river:posted:generic"),$url) . " ";
 	$string .= elgg_echo("{$subtype}:river:annotate") . " <a href=\"" . $object->getURL() . "\">" . $object->title . "</a>";
-
+	$string .= "<div class=\"river_content_display\">";
+	if(strlen($contents) > 200) {
+        	$string .= substr($contents, 0, strpos($contents, ' ', 200)) . "...";
+    }else{
+	    $string .= $contents;
+    }
+	$string .= "</div>";
 ?>
 
 <?php echo $string; ?>
