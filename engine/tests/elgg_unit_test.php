322bb9cd2be9e51422cb2b82684692e825c2bfb7
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-10-02 18:40:04 +0000

    Added simpletest and start of unit tests.
    git-svn-id: http://code.elgg.org/elgg/trunk@3503 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ engine/tests/elgg_unit_test.php
@@ -0,0 +1,13 @@
+<?php
+
+abstract class ElggCoreUnitTest extends UnitTestCase
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function __destruct()
+	{
+	}
+}
---
+++ engine/tests/suite.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Runs unit tests.
+ * 
+ * @package Elgg
+ * @subpackage Test
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
+
+
+require_once( dirname( __FILE__ ) . '/../start.php' );
+
+$vendor_path = "$CONFIG->path/vendors/simpletest";
+$test_path = "$CONFIG->path/engine/tests";
+
+require_once( "$vendor_path/unit_tester.php" );
+require_once( "$vendor_path/mock_objects.php" );
+require_once( "$vendor_path/reporter.php" );
+require_once( "$test_path/elgg_unit_test.php" );
+
+$suite = new TestSuite( 'Elgg Core Unit Tests' );
+
+if (!$CONFIG->debug) {
+	if ( TextReporter::inCli() )
+	{
+	    exit( $suite->Run( new TextReporter() ) ? 0 : 1 );
+	}
+	$suite->Run( new HtmlReporter() );
+}
