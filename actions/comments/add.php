501c0e13cc4db687abb36a937709f8e9de643339
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-10-14 19:48:10 +0000

    Standardized.
    git-svn-id: http://code.elgg.org/elgg/trunk@3541 36083f99-b078-4883-b0ff-0f9b5a30f544

--- actions/comments/add.php
+++ actions/comments/add.php
@@ -1,57 +1,55 @@
 <?php
+/**
+ * Elgg add comment action
+ *
+ * @package Elgg
+ * @author Curverider <curverider.co.uk>
+ * @link http://elgg.org/
+ */
 
-	/**
-	 * Elgg add comment action
-	 * 
-	 * @package Elgg
+// Make sure we're logged in; forward to the front page if not
+gatekeeper();
+action_gatekeeper();
 
-	 * @author Curverider <curverider.co.uk>
+// Get input
+$entity_guid = (int) get_input('entity_guid');
+$comment_text = get_input('generic_comment');
 
-	 * @link http://elgg.org/
-	 */
+// Let's see if we can get an entity with the specified GUID
+if ($entity = get_entity($entity_guid)) {
 
-	// Make sure we're logged in; forward to the front page if not
-		gatekeeper();
-		action_gatekeeper();
-		
-	// Get input
-		$entity_guid = (int) get_input('entity_guid');
-		$comment_text = get_input('generic_comment');
-		
-	// Let's see if we can get an entity with the specified GUID
-		if ($entity = get_entity($entity_guid)) {
-			
-	        // If posting the comment was successful, say so
-	        	$annotation = create_annotation($entity->guid, 'generic_comment', $comment_text, "", $_SESSION['guid'], $entity->access_id);
-	        	if ($annotation) {
-					if ($entity->owner_guid != $_SESSION['user']->getGUID())
-					notify_user($entity->owner_guid, $_SESSION['user']->getGUID(), elgg_echo('generic_comment:email:subject'), 
-						sprintf(
-									elgg_echo('generic_comment:email:body'),
-									$entity->title,
-									$_SESSION['user']->name,
-									$comment_text,
-									$entity->getURL(),
-									$_SESSION['user']->name,
-									$_SESSION['user']->getURL()
-								)
-					); 
-					system_message(elgg_echo("generic_comment:posted"));
-					//add to river
-					add_to_river('annotation/annotate','comment',$_SESSION['user']->guid,$entity->guid, "", 0, $annotation);
+	// If posting the comment was successful, say so
+	$annotation = create_annotation($entity->guid, 'generic_comment',
+		$comment_text, "", $_SESSION['guid'], $entity->access_id);
 
-					
-				} else {
-					register_error(elgg_echo("generic_comment:failure"));
-				}
-				
-		} else {
-		
-			register_error(elgg_echo("generic_comment:notfound"));
-			
+	if ($annotation) {
+		if ($entity->owner_guid != $_SESSION['user']->getGUID()) {
+			notify_user($entity->owner_guid,
+				$_SESSION['user']->getGUID(),
+				elgg_echo('generic_comment:email:subject'),
+				sprintf(
+					elgg_echo('generic_comment:email:body'),
+					$entity->title,
+					$_SESSION['user']->name,
+					$comment_text,
+					$entity->getURL(),
+					$_SESSION['user']->name,
+					$_SESSION['user']->getURL()
+				)
+			);
 		}
-		
-	// Forward to the 
-		forward($entity->getURL());
 
-?>
+		system_message(elgg_echo("generic_comment:posted"));
+		//add to river
+		add_to_river('annotation/annotate','comment',$_SESSION['user']->guid,$entity->guid, "", 0, $annotation);
+	} else {
+		register_error(elgg_echo("generic_comment:failure"));
+	}
+} else {
+
+	register_error(elgg_echo("generic_comment:notfound"));
+
+}
+
+// Forward to the
+forward($entity->getURL());
--- actions/comments/delete.php
+++ actions/comments/delete.php
@@ -1,35 +1,32 @@
 <?php
+/**
+ * Elgg delete comment action
+ *
+ * @package Elgg
+ * @author Curverider <curverider.co.uk>
+ * @link http://elgg.org/
+ */
 
-	/**
-	 * Elgg delete comment action
-	 * 
-	 * @package Elgg
+// Ensure we're logged in
+if (!isloggedin()) {
+	forward();
+}
 
-	 * @author Curverider <curverider.co.uk>
+// Make sure we can get the comment in question
+$annotation_id = (int) get_input('annotation_id');
+if ($comment = get_annotation($annotation_id)) {
 
-	 * @link http://elgg.org/
-	 */
+	$entity = get_entity($comment->entity_guid);
 
-	// Ensure we're logged in
-		if (!isloggedin()) forward();
-		
-	// Make sure we can get the comment in question
-		$annotation_id = (int) get_input('annotation_id');
-		if ($comment = get_annotation($annotation_id)) {
-    		
-    		$entity = get_entity($comment->entity_guid);
-			
-			if ($comment->canEdit()) {
-				$comment->delete();
-				system_message(elgg_echo("generic_comment:deleted"));
-				forward($entity->getURL());
-			}
-			
-		} else {
-			$url = "";
-		}
-		
-		register_error(elgg_echo("generic_comment:notdeleted"));
+	if ($comment->canEdit()) {
+		$comment->delete();
+		system_message(elgg_echo("generic_comment:deleted"));
 		forward($entity->getURL());
+	}
 
-?>
+} else {
+	$url = "";
+}
+
+register_error(elgg_echo("generic_comment:notdeleted"));
+forward($entity->getURL());
