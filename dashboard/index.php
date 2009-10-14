13796a10ef50d0afe045a3a199a5b19dd5b9bc45
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-10-14 21:16:41 +0000

    Standardized dashboard/*
    git-svn-id: http://code.elgg.org/elgg/trunk@3544 36083f99-b078-4883-b0ff-0f9b5a30f544

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,33 +1,28 @@
 <?php
+/**
+ * Elgg dashboard
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
 
-	/**
-	 * Elgg dashboard
-	 * 
-	 * @package Elgg
-	 * @subpackage Core
+// Get the Elgg engine
+require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
 
-	 * @author Curverider Ltd
+// Ensure that only logged-in users can see this page
+gatekeeper();
 
-	 * @link http://elgg.org/
-	 */
+// Set context and title
+set_context('dashboard');
+set_page_owner(get_loggedin_userid());
+$title = elgg_echo('dashboard');
 
-	// Get the Elgg engine
-		require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
+// wrap intro message in a div
+$intro_message = elgg_view('dashboard/blurb');
 
-	// Ensure that only logged-in users can see this page
-		gatekeeper();
-		
-	// Set context and title
-		set_context('dashboard');
-		set_page_owner(get_loggedin_userid());
-		$title = elgg_echo('dashboard');
-		
-	// wrap intro message in a div
-		$intro_message = elgg_view('dashboard/blurb');
-		
-	// Try and get the user from the username and set the page body accordingly
-		$body = elgg_view_layout('widgets',"","",$intro_message);
-		
-		page_draw($title, $body);
-		
-?>
+// Try and get the user from the username and set the page body accordingly
+$body = elgg_view_layout('widgets',"","",$intro_message);
+
+page_draw($title, $body);
--- dashboard/latest.php
+++ dashboard/latest.php
@@ -1,30 +1,23 @@
 <?php
+/**
+ * Elgg latest content page
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
 
-	/**
-	 * Elgg latest content page
-	 * 
-	 * @package Elgg
-	 * @subpackage Core
+/**
+ * Start the Elgg engine
+ */
+require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
 
-	 * @author Curverider Ltd
-
-	 * @link http://elgg.org/
-	 */
-
-	/**
-	 * Start the Elgg engine
-	 */
-		require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
-		
-		
-	// Load the front page
-        global $CONFIG;
-        $title = elgg_view_title(elgg_echo('content:latest'));
-        set_context('search');
-        $content = list_registered_entities(0,10,true,false,array('object','group'));
-        set_context('latest');
-        $content = elgg_view_layout('two_column_left_sidebar', '', $title . $content);
-        page_draw(elgg_echo('content:latest'), $content);
-		
-
-?>
+// Load the front page
+global $CONFIG;
+$title = elgg_view_title(elgg_echo('content:latest'));
+set_context('search');
+$content = list_registered_entities(0,10,true,false,array('object','group'));
+set_context('latest');
+$content = elgg_view_layout('two_column_left_sidebar', '', $title . $content);
+page_draw(elgg_echo('content:latest'), $content);
