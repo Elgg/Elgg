ee554433ea345fe1037e7ace1f2d82bd49e53cae
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-10-14 19:30:49 +0000

    Standardized account/*
    git-svn-id: http://code.elgg.org/elgg/trunk@3539 36083f99-b078-4883-b0ff-0f9b5a30f544

--- account/forgotten_password.php
+++ account/forgotten_password.php
@@ -1,20 +1,19 @@
 <?php
-	/**
-	 * Forgotten password function.
-	 * 
-	 * @package Elgg
-	 * @subpackage Core
-	 * @author Curverider Ltd
-	 * @link http://elgg.org/
-	 */
+/**
+ * Forgotten password function.
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
 
-	require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
-	
-	if (!isloggedin()) {
-		$body = elgg_view_title(elgg_echo('user:password:lost')) . elgg_view("account/forms/forgotten_password");
-		
-		page_draw(elgg_echo('user:password:lost'), elgg_view_layout("one_column", $body));
-	} else {
-		forward();
-	}
-?>
+require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
+
+if (!isloggedin()) {
+	$body = elgg_view_title(elgg_echo('user:password:lost')) . elgg_view("account/forms/forgotten_password");
+
+	page_draw(elgg_echo('user:password:lost'), elgg_view_layout("one_column", $body));
+} else {
+	forward();
+}
--- account/register.php
+++ account/register.php
@@ -1,30 +1,26 @@
 <?php
 
-	/**
-	 * Elgg registration page
-	 * 
-	 * @package Elgg
-	 * @subpackage Core
+/**
+ * Elgg registration page
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
 
-	 * @author Curverider Ltd
+/**
+ * Start the Elgg engine
+ */
+require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
 
-	 * @link http://elgg.org/
-	 */
+$friend_guid = (int) get_input('friend_guid',0);
+$invitecode = get_input('invitecode');
 
-	/**
-	 * Start the Elgg engine
-	 */
-		require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
-
-		$friend_guid = (int) get_input('friend_guid',0);
-		$invitecode = get_input('invitecode');
-		
-	// If we're not logged in, display the registration page
-		if (!isloggedin()) {
-			page_draw(elgg_echo('register'), elgg_view("account/forms/register", array('friend_guid' => $friend_guid, 'invitecode' => $invitecode)));
-	// Otherwise, forward to the index page
-		} else {
-			forward();
-		}
-		
-?>
+// If we're not logged in, display the registration page
+if (!isloggedin()) {
+	page_draw(elgg_echo('register'), elgg_view("account/forms/register", array('friend_guid' => $friend_guid, 'invitecode' => $invitecode)));
+// Otherwise, forward to the index page
+} else {
+	forward();
+}
