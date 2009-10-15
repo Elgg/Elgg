6c45ead6c42592479c020efa640b8b3317e77b5a
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-10-15 22:39:20 +0000

    Standardized files.
    git-svn-id: http://code.elgg.org/elgg/trunk@3550 36083f99-b078-4883-b0ff-0f9b5a30f544

--- entities/index.php
+++ entities/index.php
@@ -1,68 +1,60 @@
 <?php
+/**
+ * Generic entity viewer
+ * Given a GUID, this page will try and display any entity
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
 
-	/**
-	 * Generic entity viewer
-	 * Given a GUID, this page will try and display any entity
-	 * 
-	 * @package Elgg
-	 * @subpackage Core
+// Load Elgg engine
+require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
 
-	 * @author Curverider Ltd
+// Get the GUID of the entity we want to view
+$guid = (int) get_input('guid');
+$shell = get_input('shell');
+if ($shell == "no") {
+	$shell = false;
+} else {
+	$shell = true;
+}
 
-	 * @link http://elgg.org/
-	 */
+$context = get_input('context');
+if ($context) {
+	set_context($context);
+}
 
-	// Load Elgg engine
-		require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
+// Get the entity, if possible
+if ($entity = get_entity($guid)) {
+	if ($entity->container_guid) {
+		set_page_owner($entity->container_guid);
+	} else {
+		set_page_owner($entity->owner_guid);
+	}
 
-	// Get the GUID of the entity we want to view
-		$guid = (int) get_input('guid');
-		$shell = get_input('shell');
-		if ($shell == "no") {
-			$shell = false;
-		} else {
-			$shell = true;
-		}
-		
-		$context = get_input('context');
-		if ($context) set_context($context);
-		
-	// Get the entity, if possible
-		if ($entity = get_entity($guid)) {
-
-			if ($entity->container_guid) {
-				set_page_owner($entity->container_guid);
-			} else {
-				set_page_owner($entity->owner_guid);
-			}
-			
 	// Set the body to be the full view of the entity, and the title to be its title
-			if ($entity instanceof ElggObject) {
-				$title = $entity->title;
-			} else if ($entity instanceof ElggEntity) {
-				$title = $entity->name;
-			}
-			$area2 = elgg_view_entity($entity,true);
-			if ($shell) {
-				$body = elgg_view_layout('two_column_left_sidebar', '', $area1 . $area2);
-			} else {
-				$body = $area2;
-			}
-			
-	// Otherwise?
-		} else {
-			
-			$body = elgg_echo('notfound');
-			
-		}
-		
-	// Display the page
-		if ($shell) {
-			page_draw($title, $body);
-		} else {
-			header("Content-type: text/html; charset=UTF-8");
-			echo $title;
-			echo $body;
-		}
+	if ($entity instanceof ElggObject) {
+		$title = $entity->title;
+	} else if ($entity instanceof ElggEntity) {
+		$title = $entity->name;
+	}
+	$area2 = elgg_view_entity($entity,true);
+	if ($shell) {
+		$body = elgg_view_layout('two_column_left_sidebar', '', $area1 . $area2);
+	} else {
+		$body = $area2;
+	}
+} else {
+	$body = elgg_echo('notfound');
+}
 
-?>
+// Display the page
+if ($shell) {
+	page_draw($title, $body);
+} else {
+	header("Content-type: text/html; charset=UTF-8");
+	echo $title;
+	echo $body;
+}
--- entities/list.php
+++ entities/list.php
@@ -1,21 +1,16 @@
 <?php
+/**
+ * Activity viewer
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
 
-	/**
-	 * Activity viewer
-	 * 
-	 * @package Elgg
-	 * @subpackage Core
-
-	 * @author Curverider Ltd
-
-	 * @link http://elgg.org/
-	 */
-
-		require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
-		set_context('search');
-		$area2 = list_entities("","",0,10,false);
-		set_context('entities');
-		$body = elgg_view_layout('two_column_left_sidebar',$area1, $area2);
-		page_draw("",$body);
-
-?>
+require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
+set_context('search');
+$area2 = list_entities("","",0,10,false);
+set_context('entities');
+$body = elgg_view_layout('two_column_left_sidebar',$area1, $area2);
+page_draw("",$body);
