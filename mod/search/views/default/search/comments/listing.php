a4640beb99084c79bf889a82c01cf0a13ad51b4b
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-11-08 00:15:03 +0000

    Fixed the wrapper for comment listing.
    git-svn-id: http://code.elgg.org/elgg/trunk@3639 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/search/views/default/search/comments/listing.php
+++ mod/search/views/default/search/comments/listing.php
@@ -7,6 +7,31 @@
  * @author Curverider Ltd
  * @link http://elgg.org/
  */
+?>
+
+<div class="search_listing">
+<?php
+if (!is_array($vars['entities']) || !count($vars['entities'])) {
+	return FALSE;
+}
+
+$title_str = elgg_echo('comments');
+$body = elgg_view_title($title_str);
+
+$query = htmlspecialchars(http_build_query(
+	array(
+		'q' => $vars['params']['query'],
+		'type' => $vars['params']['type'],
+		'limit' => get_input('limit', 10),
+		'offset' => get_input('offset', 0),
+		'subtype' => $vars['params']['subtype']
+	)
+));
+
+$url = "{$vars['url']}pg/search?$query";
+$more = "<a href=\"$url\">+$count more $title_str</a>";
+
+echo elgg_view('page_elements/contentwrapper', array('body' => $body));
 
 foreach ($vars['entities'] as $entity) {
 	if ($owner = $entity->getOwnerEntity()) {
@@ -32,3 +57,5 @@ foreach ($vars['entities'] as $entity) {
 </span>
 ___END;
 }
+?>
+</div>
