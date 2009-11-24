b24d75c8060ff50f07c95831de5e585707b9e89d
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-11-24 01:43:41 +0000

    dashboard latest wasn't specifying fullview as false
    git-svn-id: http://code.elgg.org/elgg/trunk@3704 36083f99-b078-4883-b0ff-0f9b5a30f544

--- dashboard/latest.php
+++ dashboard/latest.php
@@ -17,7 +17,7 @@ require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
 global $CONFIG;
 $title = elgg_view_title(elgg_echo('content:latest'));
 set_context('search');
-$content = list_registered_entities(0,10,true,false,array('object','group'));
+$content = list_registered_entities(0,10,false,false,array('object','group'));
 set_context('latest');
 $content = elgg_view_layout('two_column_left_sidebar', '', $title . $content);
 page_draw(elgg_echo('content:latest'), $content);
