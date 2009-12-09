38590ee868c00a2e5c73b38abafc2152a60a24ee
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-12-09 12:54:11 +0000

    fixes #973 - pulls reported content and bookmark code out of core owner_block view - left images in _graphics since themes probably depend on that and did not want to break themes
    git-svn-id: http://code.elgg.org/elgg/trunk@3745 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/css.php
+++ views/default/css.php
@@ -1546,14 +1546,8 @@ PAGE-OWNER BLOCK
 	margin:0 10px 0 0;
 }
 #owner_block_rss_feed,
-#owner_block_odd_feed,
-#owner_block_bookmark_this,
-#owner_block_report_this {
+#owner_block_odd_feed {
 	padding:5px 0 0 0;
-}
-#owner_block_report_this {
-	padding-bottom:5px;
-	border-bottom:1px solid #cccccc;
 }
 #owner_block_rss_feed a {
 	font-size: 90%;
@@ -1567,22 +1561,8 @@ PAGE-OWNER BLOCK
 	padding:0 0 4px 20px;
 	background: url(<?php echo $vars['url']; ?>_graphics/icon_odd.gif) no-repeat left top;
 }
-#owner_block_bookmark_this a {
-	font-size: 90%;
-	color:#999999;
-	padding:0 0 4px 20px;
-	background: url(<?php echo $vars['url']; ?>_graphics/icon_bookmarkthis.gif) no-repeat left top;
-}
-#owner_block_report_this a {
-	font-size: 90%;
-	color:#999999;
-	padding:0 0 4px 20px;
-	background: url(<?php echo $vars['url']; ?>_graphics/icon_reportthis.gif) no-repeat left top;
-}
 #owner_block_rss_feed a:hover,
-#owner_block_odd_feed a:hover,
-#owner_block_bookmark_this a:hover,
-#owner_block_report_this a:hover {
+#owner_block_odd_feed a:hover {
 	color: #0054a7;
 }
 #owner_block_desc {
@@ -2472,68 +2452,6 @@ div.ajax_loader {
 * html #elgg_horizontal_tabbed_nav .selected a { top: 3px; }
 
 
-/* ***************************************
-	ADMIN AREA - REPORTED CONTENT
-*************************************** */
-.reportedcontent_content {
-	margin:0 0 5px 0;
-	padding:0 7px 4px 10px;
-	-webkit-border-radius: 5px;
-	-moz-border-radius: 5px;
-}
-.reportedcontent_content p.reportedcontent_detail,
-.reportedcontent_content p {
-	margin:0;
-}
-.active_report {
-	border:1px solid #D3322A;
-	background:#F7DAD8;
-}
-.archived_report {
-	border:1px solid #666666;
-	background:#dedede;
-}
-a.archive_report_button {
-	float:right;
-	font: 12px/100% Arial, Helvetica, sans-serif;
-	font-weight: bold;
-	color: #ffffff;
-	background:#4690d6;
-	border: 1px solid #4690d6;
-	-webkit-border-radius: 4px;
-	-moz-border-radius: 4px;
-	width: auto;
-	padding: 4px;
-	margin:15px 0 0 20px;
-	cursor: pointer;
-}
-a.archive_report_button:hover {
-	background: #0054a7;
-	border: 1px solid #0054a7;
-	text-decoration: none;
-}
-a.delete_report_button {
-	float:right;
-	font: 12px/100% Arial, Helvetica, sans-serif;
-	font-weight: bold;
-	color: #ffffff;
-	background:#999999;
-	border: 1px solid #999999;
-	-webkit-border-radius: 4px;
-	-moz-border-radius: 4px;
-	width: auto;
-	padding: 4px;
-	margin:15px 0 0 20px;
-	cursor: pointer;
-}
-a.delete_report_button:hover {
-	background: #333333;
-	border: 1px solid #333333;
-	text-decoration:none;
-}
-.reportedcontent_content .collapsible_box {
-	background: white;
-}
 
 
 /* ***************************************
--- views/default/page_elements/owner_block.php
+++ views/default/page_elements/owner_block.php
@@ -14,11 +14,9 @@ $contents = "";
 
 // Is there a page owner?
 $owner = page_owner_entity();
-// if (!$owner && isloggedin()) $owner = $_SESSION['user'];
 if ($owner instanceof ElggEntity) {
 	$icon = elgg_view("profile/icon",array('entity' => $owner, 'size' => 'tiny'));
 	if ($owner instanceof ElggUser || $owner instanceof ElggGroup) {
-		//$info = $owner->name;
 		$info = '<a href="' . $owner->getURL() . '">' . $owner->name . '</a>';
 	}
 	$display = "<div id=\"owner_block_icon\">" . $icon . "</div>";
@@ -42,13 +40,7 @@ if (isset($autofeed) && $autofeed == true) {
 	} else {
 		$url .= "?view=rss";
 	}
-	//if (substr_count($url2,'?')) {
-	//	$url2 .= "&view=odd";
-	//} else {
-	//	$url2 .= "?view=opendd";
-	//}
 	$label = elgg_echo('feed:rss');
-	//$label2 = elgg_echo('feed:odd');
 	$contents .= <<<END
 
 <div id="owner_block_rss_feed"><a href="{$url}" rel="nofollow">{$label}</a></div>
@@ -56,37 +48,17 @@ if (isset($autofeed) && $autofeed == true) {
 END;
 }
 
-//the follow are for logged in users only
-if(isloggedin()){
-
-	//is the bookmark plugin installed?
-	if(is_plugin_enabled('bookmarks')){
-
-		$label3 = elgg_echo('bookmarks:this');
-		$contents .= "<div id=\"owner_block_bookmark_this\"><a href=\"javascript:location.href='". $CONFIG->wwwroot . "mod/bookmarks/add.php?address='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title)\">{$label3}</a></div>";
-
-	}
-
-	//report this button
-	if (is_plugin_enabled('reportedcontent'))
-	{
-		$label4 = elgg_echo('reportedcontent:report');
-		$contents .= "<div id=\"owner_block_report_this\"><a href=\"javascript:location.href='". $CONFIG->wwwroot . "mod/reportedcontent/add.php?address='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title)\">{$label4}</a></div>";
-	}
-
-}
-
-
 $contents .= elgg_view('owner_block/extend');
+
 
 // Have we been asked to inject any content? If so, display it
 if (isset($vars['content']))
 	$contents .= $vars['content'];
 
-// Initialise the submenu
-$submenu = get_submenu(); // elgg_view('canvas_header/submenu');
+// Initialise the submenu (plugins can add to the submenu)
+$submenu = get_submenu(); 
 if (!empty($submenu))
-	$contents .= "<div id=\"owner_block_submenu\">" . $submenu . "</div>"; // plugins can extend this to add menu options
+	$contents .= "<div id=\"owner_block_submenu\">" . $submenu . "</div>"; 
 
 if (!empty($contents)) {
 	echo "<div id=\"owner_block\">";
