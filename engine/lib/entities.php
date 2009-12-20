df908277c1ae9913eeed2b9365646d20b54f8a44
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-12-20 01:34:58 +0000

    fixed typo in list_entities()
    git-svn-id: http://code.elgg.org/elgg/trunk@3772 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/entities.php
+++ engine/lib/entities.php
@@ -2195,7 +2195,7 @@ function list_entities($type= "", $subtype = "", $owner_guid = 0, $limit = 10, $
 	}
 
 	$options['full_view'] = $fullview;
-	$options['view_toggle_type'] = $viewtoggletype;
+	$options['view_toggle_type'] = $viewtypetoggle;
 	$options['pagination'] = $pagination;
 
 	return elgg_list_entities($options);
