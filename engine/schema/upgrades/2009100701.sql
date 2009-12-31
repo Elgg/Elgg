693ebe1a63b3ccaa249e60fe96633c6430a12f61
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-12-31 01:06:35 +0000

    Removed lock tables from db utf8 migration because it fails on some (all?) servers.
    git-svn-id: http://code.elgg.org/elgg/trunk@3781 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/schema/upgrades/2009100701.sql
+++ engine/schema/upgrades/2009100701.sql
@@ -1,30 +1,23 @@
 SET NAMES utf8;
 
-LOCK TABLES `prefix_metastrings` WRITE;
 ALTER TABLE `prefix_metastrings` DISABLE KEYS;
 REPLACE INTO `prefix_metastrings` (id, string)
 	SELECT id, unhex(hex(convert(string using latin1)))
 	FROM `prefix_metastrings`;
 ALTER TABLE `prefix_metastrings` ENABLE KEYS;
-UNLOCK TABLES;
 
-LOCK TABLES `prefix_groups_entity` WRITE;
 ALTER TABLE `prefix_groups_entity` DISABLE KEYS;
 REPLACE INTO `prefix_groups_entity` (guid, name, description)
 	SELECT guid, unhex(hex(convert(name using latin1))), unhex(hex(convert(description using latin1)))
 	FROM `prefix_groups_entity`;
 ALTER TABLE `prefix_groups_entity` ENABLE KEYS;
-UNLOCK TABLES;
 
-LOCK TABLES `prefix_objects_entity` WRITE;
 ALTER TABLE `prefix_objects_entity` DISABLE KEYS;
 REPLACE INTO `prefix_objects_entity` (guid, title, description)
 	SELECT guid, unhex(hex(convert(title using latin1))), unhex(hex(convert(description using latin1)))
 	FROM `prefix_objects_entity`;
 ALTER TABLE `prefix_objects_entity` ENABLE KEYS;
-UNLOCK TABLES;
 
-LOCK TABLES `prefix_users_entity` WRITE;
 ALTER TABLE `prefix_users_entity` DISABLE KEYS;
 REPLACE INTO `prefix_users_entity` (guid, name, username, password, salt, email, language, code,
 	banned, last_action, prev_last_action, last_login, prev_last_login)
@@ -32,4 +25,3 @@ REPLACE INTO `prefix_users_entity` (guid, name, username, password, salt, email,
 			banned, last_action, prev_last_action, last_login, prev_last_login
 		FROM `prefix_users_entity`;
 ALTER TABLE `prefix_users_entity` ENABLE KEYS;
-UNLOCK TABLES;
