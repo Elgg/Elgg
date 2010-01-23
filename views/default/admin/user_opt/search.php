4f8f83993cd296d02a8380ddbf0f52913f668682
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-01-23 18:38:10 +0000

    Fixes #1455: Updated search URL for admin user search. (The old one worked, but didn't limit to users.)
    git-svn-id: http://code.elgg.org/elgg/trunk@3831 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/admin/user_opt/search.php
+++ views/default/admin/user_opt/search.php
@@ -9,14 +9,15 @@
  */
 ?>
 <div id="search-box">
-	<form action="<?php echo $vars['url']; ?>search/" method="get">
+	<form action="<?php echo $vars['url']; ?>pg/search/" method="get">
 	<b><?php echo elgg_echo('admin:user:label:search'); ?></b>
 	<?php
 
-		echo elgg_view('input/text',array('internalname' => 'tag'));
+		echo elgg_view('input/text',array('internalname' => 'q'));
 
 	?>
-	<input type="hidden" name="object" value="user" />
+	<input type="hidden" name="entity_type" value="user" />
+	<input type="hidden" name="search_type" value="entities" />
 	<input type="submit" name="<?php echo elgg_echo('admin:user:label:searchbutton'); ?>"
 		value="<?php echo elgg_echo('admin:user:label:searchbutton'); ?>" />
 	</form>
