5831aeef5e747f7d4e14d9d255a401adf395e5e6
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-04 04:05:00 +0000

    Merged walledgarden changes.
    git-svn-id: http://code.elgg.org/elgg/trunk@5273 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/walledgarden/index.php
+++ mod/walledgarden/index.php
@@ -1,24 +1,25 @@
 <?php
+/**
+ * Elgg Walled Garden
+ * 
+ * @package WalledGarden
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Curverider Ltd <info@elgg.com>
+ * @copyright Curverider Ltd 2008
+ * @link http://elgg.com/
+ */
 
-	/**
-	 * Elgg custom index
-	 * 
-	 * @package ElggCustomIndex
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Curverider Ltd <info@elgg.com>
-	 * @copyright Curverider Ltd 2008
-	 * @link http://elgg.com/
-	 */
-
-	// Get the Elgg engine
-		require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
-		
-    	//grab the login form
-	$login = elgg_view("account/forms/login");
+// Get the Elgg engine
+require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 	
-    //display the contents in our new canvas layout
-	$body = elgg_view_layout('new_index',$login);
-   
-    page_draw($title, $body);
-		
-?>
+//grab the login form
+$login = elgg_view("account/forms/login");
+
+//temp message
+$area1 = "This site is running in walled garden mode. <br />
+			Therefore you will need to be invited & logged in to see anything.";
+
+//display the contents in our new canvas layout
+$body = elgg_view_layout('one_column_with_sidebar', $login, $area1);
+
+page_draw($title, $body);
