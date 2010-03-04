354d9eebf2c84b66f1103a61a8f4ffa832f350b6
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-04 00:59:34 +0000

    Updated external pages to new interface.
    git-svn-id: http://code.elgg.org/elgg/trunk@5268 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/externalpages/index.php
+++ mod/externalpages/index.php
@@ -1,37 +1,33 @@
 <?php
-	/**
-	 * Elgg External pages
-	 * 
-	 * @package ElggExpages
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Curverider Ltd
-	 * @copyright Curverider Ltd 2008-2010
-	 * @link http://elgg.com/
-	 */
+/**
+ * Elgg External pages
+ * 
+ * @package ElggExpages
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Curverider Ltd
+ * @copyright Curverider Ltd 2008-2010
+ * @link http://elgg.com/
+ */
 
-	require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
+require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 
-	admin_gatekeeper();
-	set_context('admin');
-	$type = get_input('type'); //the type of page e.g about, terms etc
-	if(!$type)
-		$type = "front"; //default to the frontpage
+admin_gatekeeper();
+set_context('admin');
+//the type of page e.g about, terms, privacy, etc
+$type = get_input('type', 'about');
+
+// Set admin user for user block
+set_page_owner($_SESSION['guid']);
+
+//display the title
+$title = elgg_view_title(elgg_echo('expages'));
+
+// Display the correct form
+$edit = elgg_view('expages/forms/edit', array('type' => $type));
 	
-	// Set admin user for user block
-	set_page_owner($_SESSION['guid']);
+// Display the menu
+$body = elgg_view('page_elements/elgg_content',array('body' => elgg_view('expages/menu', array('type' => $type)).$edit));
 	
-	//display the title
-	$title = elgg_view_title(elgg_echo('expages'));
-	
-	// Display the correct form
-	if($type == "front")
-		$edit = elgg_view('expages/forms/editfront');
-	else
-		$edit = elgg_view('expages/forms/edit', array('type' => $type));
-		
-		// Display the menu
-	$body = elgg_view('page_elements/contentwrapper',array('body' => elgg_view('expages/menu', array('type' => $type)).$edit));
-		
-	// Display
-	page_draw(elgg_echo('expages'),elgg_view_layout("two_column_left_sidebar", '', $title . $body));
+// Display
+page_draw(elgg_echo('expages'),elgg_view_layout("one_column_with_sidebar", '', $title . $body));
 ?>
--- mod/externalpages/views/default/expages/css.php
+++ mod/externalpages/views/default/expages/css.php
@@ -1,17 +1,16 @@
 <?php
-
-	/**
-	 * Elgg externalpages CSS
-	 * 
-	 * @package externalpages
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Curverider <info@elgg.com>
-	 * @copyright Curverider Ltd 2008-2010
-	 * @link http://elgg.org/
-	 */
+/**
+ * Elgg externalpages CSS
+ * 
+ * @package externalpages
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Curverider <info@elgg.com>
+ * @copyright Curverider Ltd 2008-2010
+ * @link http://elgg.org/
+ */
 
 ?>
-
-/* IE6 */
-* html #front_left_tbl { width:676px !important; }
-* html #front_right_tbl { width:676px !important; }
+#footer_toolbar_links {
+	text-align:right;
+	margin-bottom:5px;
+}
