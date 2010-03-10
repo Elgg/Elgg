1412cb73b0bbbbf4bdfba4b85560bf622157ffd5
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-10 16:39:56 +0000

    closing div applied. Note: this needs cleaning up.
    git-svn-id: http://code.elgg.org/elgg/trunk@5345 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/riverdashboard/index.php
+++ mod/riverdashboard/index.php
@@ -41,7 +41,7 @@ switch($orient) {
 
 $title = elgg_view_title($title_wording);
 
-$river = elgg_view_river_items($subject_guid, 0, $relationship_type, $type, $subtype, '', 20, 0, 0, true, false);
+$river = elgg_view_river_items($subject_guid, 0, $relationship_type, $type, $subtype, '', 20, 0, 0, true, false)  . "</div>";
 // Replacing callback calls in the nav with something meaningless
 $river = str_replace('callback=true','replaced=88,334',$river);
 		
