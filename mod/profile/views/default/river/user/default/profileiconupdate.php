755367a31d3c1dbc1cc24ce270a217d2390cbe44
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-10 17:05:36 +0000

    river time stamps added to profile edits
    git-svn-id: http://code.elgg.org/elgg/trunk@5347 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/views/default/river/user/default/profileiconupdate.php
+++ mod/profile/views/default/river/user/default/profileiconupdate.php
@@ -3,6 +3,6 @@
 	$performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
 	
 	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
-	$string = "<div class=\"river_content_title\">" . sprintf(elgg_echo("profile:river:update"),$url) ."</div>";
+	$string = "<div class=\"river_content_title\">" . sprintf(elgg_echo("profile:river:update"),$url) ." <span class=\"river_item_time\">" . friendly_time($vars['item']->posted) . "</span></div>";
 	
 	echo $string;
--- mod/profile/views/default/river/user/default/profileupdate.php
+++ mod/profile/views/default/river/user/default/profileupdate.php
@@ -3,7 +3,7 @@
 	$performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
 	
 	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
-	$string = "<div class=\"river_content_title\">" . sprintf(elgg_echo("profile:river:update"),$url) ."</div>";
+	$string = "<div class=\"river_content_title\">" . sprintf(elgg_echo("profile:river:update"),$url) ." <span class=\"river_item_time\">" . friendly_time($vars['item']->posted) . "</span></div>";
 	
 ?>
 
