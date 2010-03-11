2a2b3b4b335bcef183cf78681888bbef26b6b1b4
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-11 14:09:45 +0000

    Refs #1947 - fixes pagination for thewire plugin
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5363 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/thewire/everyone.php
+++ mod/thewire/everyone.php
@@ -18,7 +18,8 @@
 		//add form
 		$area2 .= elgg_view("thewire/forms/add");
 
-		$area2 .= elgg_list_entities(array('types' => 'object', 'subtypes' => 'thewire')); // elgg_view("thewire/view",array('entity' => $thewireposts));
+		$offset = (int)get_input('offset', 0);
+		$area2 .= elgg_list_entities(array('types' => 'object', 'subtypes' => 'thewire', 'offset' => $offset));
 	    $body = elgg_view_layout("two_column_left_sidebar", '', $area2);
 		
 	// Display page
