c9b4489fdb8fa905587461a7878aa650440422bb
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-11 02:14:28 +0000

    Fixes #1944 - full_url() now correctly handles standard https port
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5356 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/elgglib.php
+++ engine/lib/elgglib.php
@@ -2374,7 +2374,7 @@ function elgg_normalise_plural_options_array($options, $singulars) {
 function full_url() {
 	$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
 	$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
-	$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
+	$port = ($_SERVER["SERVER_PORT"] == "80" || $_SERVER["SERVER_PORT"] == "443") ? "" : (":".$_SERVER["SERVER_PORT"]);
 	return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
 }
 
