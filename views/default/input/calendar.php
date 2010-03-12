0111b73264cc614e6050b143a42dbdccc8cb974e
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-12 02:17:13 +0000

    Added datepicker view and deprecated calendar view.
    git-svn-id: http://code.elgg.org/elgg/trunk@5371 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/input/calendar.php
+++ views/default/input/calendar.php
@@ -1,44 +1,6 @@
 <?php
-/**
- * Elgg calendar input
- * Displays a calendar input field
- *
- * @package Elgg
- * @subpackage Core
- * @author Curverider Ltd
- * @link http://elgg.org/
- *
- * @uses $vars['value'] The current value, if any
- * @uses $vars['js'] Any Javascript to enter into the input tag
- * @uses $vars['internalname'] The name of the input field
- *
- */
+// @deprecated Use datepicker instead.
 
-static $calendarjs;
-if (empty($calendarjs)) {
-	echo <<< END
+elgg_deprecated_notice('view: input/calendar is deprecated by input/datepicker', 1.8);
 
-<script language="JavaScript" src="{$vars['url']}vendors/calendarpopup/CalendarPopup.js"></script>
-
-END;
-	$calendarjs = 1;
-}
-$strippedname = sanitise_string($vars['internalname']);
-$js = "cal" . $strippedname;
-
-if (!isset($vars['value']) || $vars['value'] === FALSE) {
-	$vars['value'] = elgg_get_sticky_value($vars['internalname']);
-}
-
-if ($vars['value'] > 86400) {
-	$val = date("F j, Y",$vars['value']);
-} else {
-	$val = $vars['value'];
-}
-
-?>
-<script language="JavaScript">
-var cal<?php echo $strippedname; ?> = new CalendarPopup();
-</script>
-<input type="text" <?php echo $vars['js']; ?> name="<?php echo $vars['internalname']; ?>" id="<?php echo $strippedname; ?>" value="<?php echo $val; ?>" />
-<a href="#" onclick="<?php echo $js; ?>.select(document.getElementById('<?php echo $strippedname; ?>'),'anchor<?php echo $strippedname; ?>','MMM dd, yyyy'); return false;" TITLE="<?php echo $js; ?>.select(document.forms[0].<?php echo $strippedname; ?>,'anchor<?php echo $strippedname; ?>','MMM dd, yyyy'); return false;" NAME="anchor<?php echo $strippedname; ?>" ID="anchor<?php echo $strippedname; ?>">select</a>
+echo elgg_view('input/datepicker', $vars);
---
+++ views/default/input/datepicker.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Elgg datepicker input
+ * Displays a text field with a popup date picker.
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ *
+ * @uses $vars['value'] The current value, if any
+ * @uses $vars['js'] Any Javascript to enter into the input tag
+ * @uses $vars['internalname'] The name of the input field
+ *
+ */
+
+$cal_name = sanitise_string($vars['internalname']);
+
+if (isset($vars['class'])) {
+	$class = "{$vars['class']} popup_calendar";
+} else {
+	$class = 'popup_calendar';
+}
+
+if (!isset($vars['value']) || $vars['value'] === FALSE) {
+	$vars['value'] = elgg_get_sticky_value($vars['internalname']);
+}
+
+if ($vars['value'] > 86400) {
+	$val = date("F j, Y", $vars['value']);
+} else {
+	$val = $vars['value'];
+}
+
+?>
+<input type="text" name="<?php echo $vars['internalname']; ?>" value="<?php echo $val; ?>" />
+<script language="JavaScript">
+	$(document).ready(function() {
+		$('input[type=text][name=<?php echo $cal_name; ?>]').datepicker();
+	});
+</script>
