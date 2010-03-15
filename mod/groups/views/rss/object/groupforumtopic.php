598c988d876ebcab41ad2470563f8f50dd30884e
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-15 11:54:36 +0000

    Group discussion RSS feed was not displaying text of post because it was saved in annotation
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5397 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/groups/views/rss/object/groupforumtopic.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Elgg groupforumtopic view
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
+
+$title = $vars['entity']->title;
+
+$body = '';
+$annotation = $vars['entity']->getAnnotations('group_topic_post', 1, 0, "asc");
+if (count($annotation == 1)) {
+	$body = $annotation[0]->value;
+}
+?>
+
+<item>
+<guid isPermaLink='true'><?php echo htmlspecialchars($vars['entity']->getURL()); ?></guid>
+<pubDate><?php echo date("r",$vars['entity']->time_created) ?></pubDate>
+<link><?php echo htmlspecialchars($vars['entity']->getURL()); ?></link>
+<title><![CDATA[<?php echo $title; ?>]]></title>
+<description><![CDATA[<?php echo (autop($body)); ?>]]></description>
+<?php
+		$owner = $vars['entity']->getOwnerEntity();
+		if ($owner) {
+?>
+<dc:creator><?php echo $owner->name; ?></dc:creator>
+<?php
+		}
+?>
+</item>
