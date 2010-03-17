cf0bd9e0381e77b1879f508c1a8fe62beb2899a8
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-17 21:27:11 +0000

    Added blog languages.
    git-svn-id: http://code.elgg.org/elgg/trunk@5432 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/blog/languages/en.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Blog English langauge file.
+ *
+ */
+
+$english = array(
+	'item:object:blog' => 'Blog',
+
+	'blog:blog' => 'Blog',
+	'blog:yours' => 'Your blog',
+	'blog:all' => 'All blogs',
+	'blog:friends' => 'Friends\' blogs',
+
+	// Editing
+	'blog:new' => 'New blog post',
+	'blog:edit' => 'Edit blog post',
+	'blog:excerpt' => 'Excerpt',
+	'blog:body' => 'Body',
+	'blog:save_status' => 'Last saved: ',
+	'blog:never' => 'Never',
+	'blog:publish_date' => 'Publish Date',
+
+	// Statuses
+	'blog:status' => 'Status',
+	'blog:status:draft' => 'Draft',
+	'blog:status:published' => 'Published',
+
+	'blog:revision' => 'Revision',
+
+	// messages
+	'blog:message:saved' => 'Blog post saved.',
+	'blog:error:cannot_save' => 'Cannot save blog post.',
+	'blog:error:cannot_write_to_container' => 'Insufficient access to save blog to group.',
+);
+
+add_translation('en', $english);
