8cba2e15f386297a0f62c94de7fa3eaafe8a852c
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-21 18:03:02 +0000

    hide the river comment form - still need to add a handle to each one so the correct comment form opens
    git-svn-id: http://code.elgg.org/elgg/trunk@5464 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/riverdashboard/views/default/riverdashboard/rivercomment.php
+++ mod/riverdashboard/views/default/riverdashboard/rivercomment.php
@@ -5,8 +5,11 @@
  */
 	
 if (isset($vars['entity']) && isloggedin()) {
-	 $form_body = elgg_view('input/text',array('internalname' => 'generic_comment', 'value' => 'Add a comment...'));
+	 $form_body = "<a onclick=\" $('.river_comment_form').show('slow');\">Add comment</a>";
+	 $form_body .= "<div class=\"river_comment_form\" style=\"display:none;\">";
+	 $form_body .= elgg_view('input/text',array('internalname' => 'generic_comment', 'value' => 'Add a comment...'));
 	 $form_body .= elgg_view('input/hidden', array('internalname' => 'entity_guid', 'value' => $vars['entity']->getGUID()));
 	 $form_body .= elgg_view('input/submit', array('value' => elgg_echo("post")));
+	 $form_body .= "</div>";
 	 echo elgg_view('input/form', array('body' => $form_body, 'action' => "{$vars['url']}action/comments/add"));
 }
