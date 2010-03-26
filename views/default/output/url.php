8a99c05d4663548ef55ce73243cc91a266b49a5c
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-26 13:44:46 +0000

    Refs #2015: It's too early. Using the correct variable.
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5521 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/output/url.php
+++ views/default/output/url.php
@@ -24,8 +24,8 @@ if (isset($vars['value'])) {
 $url = trim($vars['href']);
 
 if (!empty($url)) {
-	if ((substr_count($val, "http://") == 0) && (substr_count($val, "https://") == 0)) { 
-		$val = "http://" . $val; 
+	if ((substr_count($url, "http://") == 0) && (substr_count($url, "https://") == 0)) { 
+		$url = "http://" . $url; 
 	}
 
 	if (array_key_exists('is_action', $vars) && $vars['is_action']) {
