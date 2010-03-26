c7acf3635df332f9b4de7bfd849f2231b3ca183b
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-26 11:32:21 +0000

    removed undeclared variable $area1 from blog index
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5519 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/blog/index.php
+++ mod/blog/index.php
@@ -45,7 +45,7 @@
 		$area3 = elgg_view('blog/categorylist',array('baseurl' => $CONFIG->wwwroot . 'search/?subtype=blog&owner_guid='.$page_owner->guid.'&tagtype=universal_categories&tag=','subtype' => 'blog', 'owner_guid' => $page_owner->guid));
 		
 	// Display them in the page
-        $body = elgg_view_layout("two_column_left_sidebar", '', $area1 . $area2, $area3);
+        $body = elgg_view_layout("two_column_left_sidebar", '', $area2, $area3);
 		
 	// Display page
 		page_draw(sprintf(elgg_echo('blog:user'),$page_owner->name),$body);
