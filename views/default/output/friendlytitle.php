e961e1fa9d7fde00c0ca6972499c7234f0717d43
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-03 21:52:17 +0000

    those who have iconv can use it with friendly titles by uncommenting
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5613 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/output/friendlytitle.php
+++ views/default/output/friendlytitle.php
@@ -9,10 +9,11 @@
 
 $title = $vars['title'];
 
-$title = elgg_strtolower($title);
+//$title = iconv('UTF-8', 'ASCII//TRANSLIT', $title);
 $title = preg_replace("/[^\w ]/","",$title);
-$title = trim($title);
 $title = str_replace(" ","-",$title);
 $title = str_replace("--","-",$title);
+$title = trim($title);
+$title = strtolower($title);
 
 echo $title;
