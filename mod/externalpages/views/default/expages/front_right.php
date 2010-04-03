f28bf27ba9dffa2133672ad7b6572eb43c741cc6
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-03 12:38:47 +0000

    Fixes #1308 - if subtype front is not defined, externalpages was throwing a warning
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5591 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/externalpages/views/default/expages/front_right.php
+++ mod/externalpages/views/default/expages/front_right.php
@@ -14,6 +14,11 @@
 	 //get frontpage right code
 		$contents = elgg_get_entities(array('type' => 'object', 'subtype' => 'front', 'limit' => 1));
 
+		// nothing to show so we return TRUE to indicate the view was valid
+		if ($contents == FALSE) {
+			return TRUE;
+		}
+
 		$show = '';
 		foreach($contents as $cont){
 			$show = $cont->description;
