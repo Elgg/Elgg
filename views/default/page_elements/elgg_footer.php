a2538127a036d7be0c5ce80ea196878d36be2686
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-03 16:56:56 +0000

    removed two elements not allowed with xhtml strict and removed blank line at top of html sent to browser
    git-svn-id: http://code.elgg.org/elgg/trunk@5599 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/page_elements/elgg_footer.php
+++ views/default/page_elements/elgg_footer.php
@@ -23,8 +23,8 @@
 			}
 		?>
 		<?php echo elgg_view('footer/links'); ?>
-		<a href="http://www.elgg.org" target="_blank" class="powered_by_elgg_badge">
-			<img src="<?php echo $vars['url']; ?>_graphics/powered_by_elgg_badge_drk_bckgnd.gif" border="0" alt="Powered by Elgg" />
+		<a href="http://www.elgg.org" class="powered_by_elgg_badge">
+			<img src="<?php echo $vars['url']; ?>_graphics/powered_by_elgg_badge_drk_bckgnd.gif" alt="Powered by Elgg" />
 		</a>
 	</div>
 </div>
