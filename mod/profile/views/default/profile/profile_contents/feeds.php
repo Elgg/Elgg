394f52f0ef0d743cb50352fce14020837662d891
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-05 13:46:49 +0000

    count added
    git-svn-id: http://code.elgg.org/elgg/trunk@5620 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/views/default/profile/profile_contents/feeds.php
+++ mod/profile/views/default/profile/profile_contents/feeds.php
@@ -5,7 +5,7 @@
 
 //$twitter_username = $vars['entity']->twitter;
 $page_owner = page_owner();
-$feeds = elgg_get_entities(array('types' => 'object', 'subtypes' => 'aggregator_feed_url', 'owner_guids' => $page_owner));
+$feeds = elgg_get_entities(array('types' => 'object', 'subtypes' => 'aggregator_feed_url', 'owner_guids' => $page_owner, 'count' => true));
 
 // if the twitter username is empty, then do not show
 ?>
