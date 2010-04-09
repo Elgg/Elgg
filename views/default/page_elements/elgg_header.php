e9e6279885ae5fa299a107160c5f9cd36c2851a6
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-09 11:19:40 +0000

    allow for a site logo
    git-svn-id: http://code.elgg.org/elgg/trunk@5671 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/page_elements/elgg_header.php
+++ views/default/page_elements/elgg_header.php
@@ -7,8 +7,16 @@
 ?>
 <div id="elgg_header">
 	<div id="elgg_header_contents">
-		<!-- display site name -->
-		<h1><a href="<?php echo $vars['url']; ?>"><?php echo $vars['config']->sitename; ?></a></h1>
+		<!-- site name -->
+		<h1><a href="<?php echo $vars['url']; ?>"><?php
+		$logo = $vars['config']->site->customlogo;
+		$logo_time = $vars['config']->site->customlogo_time;
+		if (empty($logo)) {
+			echo "<span class='default_site_logo'>".$vars['config']->sitename ."</span>";
+		} else {
+			echo "<img src=\"{$vars['url']}pg/csseditor/{$logo_time}.jpg\" alt=\"". htmlentities($vars['config']->sitename) ."\" style=\"max-width: 270px; max-height: 70px\" />";	
+		}
+		?></a></h1>
 		<?php
 			// insert site-wide navigation
 			echo elgg_view('navigation/site_nav');
