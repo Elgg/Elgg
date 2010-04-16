4b7edf24875b66b576e595f0eab9e8d87688f2a3
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-16 15:46:54 +0000

    Fixed activity stream ecml view.
    git-svn-id: http://code.elgg.org/elgg/trunk@5777 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/riverdashboard/views/default/riverdashboard/ecml/activity.php
+++ mod/riverdashboard/views/default/riverdashboard/ecml/activity.php
@@ -9,19 +9,20 @@
  * @link http://elgg.org/
  */
 
-$user = (isset($vars['user'])) ? $vars['user'] : NULL;
+// $vars['user'] is apparently the loggedin user.
+$owner = (isset($vars['owner'])) ? $vars['owner'] : NULL;
 $type = (isset($vars['type'])) ? $vars['type'] : NULL;
 $subtype = (isset($vars['subtype'])) ? $vars['subtype'] : NULL;
 $limit = (isset($vars['limit'])) ? $vars['limit'] : 10;
 
 // resolve usernames to guids
-if (!is_numeric($user)) {
-	if ($user_obj = get_user_by_username($user)) {
-		$user = $user_obj->getGUID();
+if ($owner && !is_numeric($owner)) {
+	if ($user_obj = get_user_by_username($owner)) {
+		$owner = $user_obj->getGUID();
 	}
 }
 
-$river = elgg_view_river_items($user, NULL, NULL, $type, $subtype, '', $limit, 0, 0, true, false)  . "</div>";
+$river = elgg_view_river_items($owner, NULL, NULL, $type, $subtype, '', $limit, 0, 0, true, false)  . "</div>";
 
 // Replacing callback calls in the nav with something meaningless
 $river = str_replace('callback=true','replaced=88,334',$river);
