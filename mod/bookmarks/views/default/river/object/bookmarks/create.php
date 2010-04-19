2c27964706285c91845dbc3f116e58dac32c3913
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-19 16:31:17 +0000

    #2087 fixed
    git-svn-id: http://code.elgg.org/elgg/trunk@5800 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/bookmarks/views/default/river/object/bookmarks/create.php
+++ mod/bookmarks/views/default/river/object/bookmarks/create.php
@@ -1,12 +1,6 @@
 <?php
 /**
  * Elgg bookmark river entry view
- * 
- * @package ElggBookmarks
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
- * @author Curverider <info@elgg.com>
- * @copyright Curverider Ltd 2008-2010
- * @link http://elgg.org/
  */
 
 $performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
@@ -14,5 +8,6 @@ $object = get_entity($vars['item']->object_guid);
 $url = $object->getURL();
 $url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 $string = sprintf(elgg_echo("bookmarks:river:created"),$url) . " ";
-$string .= "<a href=\"" . $object->address . "\">" . $object->title . "</a> <span class='entity_subtext'>" . friendly_time($object->time_updated) . "</span>";
+$string .= "<a href=\"" . $object->address . "\">" . $object->title . "</a> <span class='entity_subtext'>" . friendly_time($object->time_updated) . "</span> <a class='river_comment_form_button link'>Comment</a>";
+$string .= elgg_view('likes/forms/link', array('entity' => $object));
 echo $string;
