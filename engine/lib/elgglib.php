0e2f46f04c79338f4555ecaab50285230b37758b
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-20 19:42:21 +0000

    URL encoding quotes in query param to resolve security issue in IE. Viewtypes with non-alphanumeric characters will return empty content.

--- engine/lib/elgglib.php
+++ engine/lib/elgglib.php
@@ -292,6 +292,12 @@
 		// Get the current viewtype
 			if (empty($viewtype))
 				$viewtype = elgg_get_viewtype(); 
+
+
+		// Viewtypes can only be alphanumeric
+			if (preg_match('[\W]', $viewtype)) 
+				return '';
+			}
 		
 		// Set up any extensions to the requested view
 		    if (isset($CONFIG->views->extensions[$view])) {
@@ -2013,7 +2019,11 @@
 		$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
 		$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
 		$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
-		return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
+		
+		$quotes = array('\'', '"');
+		$encoded = array('%27', '%22');
+
+		return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . str_replace($quotes, $encoded, $_SERVER['REQUEST_URI']);
 	}
 	
 	/**
