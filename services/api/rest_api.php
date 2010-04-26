4524886d39a5cdb012e81ab192240add7ba7458b
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-26 11:23:57 +0000

    removed session PAM from default web services handler
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5872 36083f99-b078-4883-b0ff-0f9b5a30f544

--- services/api/rest_api.php
+++ services/api/rest_api.php
@@ -29,8 +29,10 @@ if ((isset($CONFIG->disable_api)) && ($CONFIG->disable_api == true)) {
 
 // plugins should return true to control what API and user authentication handlers are registered
 if (trigger_plugin_hook('rest', 'init', null, false) == false) {	
-	// check session - this usually means a REST call from a web browser
-	register_pam_handler('pam_auth_session');
+	// for testing from a web browser, you can use the session PAM
+	// do not use for production sites!!
+	//register_pam_handler('pam_auth_session');
+
 	// user token can also be used for user authentication
 	register_pam_handler('pam_auth_usertoken');
 
