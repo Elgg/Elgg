52e034fe1c742f3fd7311ca0a10aad84083b63f0
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-26 20:37:31 +0000

    Fixes #2098: River view for blog pulls correct time.
    git-svn-id: http://code.elgg.org/elgg/trunk@5884 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/blog/views/default/river/object/blog/create.php
+++ mod/blog/views/default/river/object/blog/create.php
@@ -6,21 +6,23 @@
 $performed_by = get_entity($vars['item']->subject_guid);
 $object = get_entity($vars['item']->object_guid);
 $url = $object->getURL();
-$contents = strip_tags($object->description); //strip tags from the contents to stop large images etc blowing out the river view
+$contents = strip_tags($object->excerpt);
 
 $url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 $title = sprintf(elgg_echo('blog:river:create'), $url);
-$string .= $title . " <a href=\"" . $object->getURL() . "\">" . $object->title . "</a> <span class='entity_subtext'>" . friendly_time($object->publish_time) . "</span>";
-if (get_plugin_setting('activitytype', 'riverdashboard') != 'classic'){
-	$string .= "<a class='river_comment_form_button link'>Comment</a>";
+$string .= $title . " <a href=\"{$object->getURL()}\">{$object->title}</a> <span class=\"entity_subtext\">" . friendly_time($object->time_created) . '</span>';
+
+if (get_plugin_setting('activitytype', 'riverdashboard') != 'classic') {
+	$string .= '<a class="river_comment_form_button link">' . elgg_echo('generic_comments:text') . '</a>';
 	$string .= elgg_view('likes/forms/link', array('entity' => $object));
 }
-$string .= "<div class=\"river_content_display\">";
-	$string .= "<div class='river_object_blog_create'></div>";
-	if(strlen($contents) > 200) {
-        	$string .= substr($contents, 0, strpos($contents, ' ', 200)) . "&hellip;";
-    }else{
-	    $string .= $contents;
-    }
-	$string .= "</div>";
+
+$string .= '<div class="river_content_display">';
+$string .= '<div class="river_object_blog_create"></div>';
+if (strlen($contents) > 200) {
+		$string .= substr($contents, 0, strpos($contents, ' ', 200)) . '&hellip;';
+} else {
+	$string .= $contents;
+}
+$string .= '</div>';
 echo $string;
