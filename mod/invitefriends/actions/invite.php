ccc7c5933117f67733e508802f37ec117d074965
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-27 01:23:12 +0000

    brought the invite friends plugin up to standards
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5900 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/invitefriends/actions/invite.php
+++ mod/invitefriends/actions/invite.php
@@ -1,73 +1,71 @@
 <?php
 
-	/**
-	 * Elgg invite action
-	 * 
-	 * @package ElggFile
-	 * @author Curverider Ltd
-	 * @copyright Curverider Ltd 2008-2010
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @link http://elgg.org/
-	 */
+/**
+ * Elgg invite friends action
+ *
+ * @package ElggInviteFriends
+ * @author Curverider Ltd
+ * @copyright Curverider Ltd 2008-2010
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @link http://elgg.org/
+ */
 
-	global $CONFIG;
-	
-	gatekeeper();
-	
-	$emails = get_input('emails');
-	$emailmessage = get_input('emailmessage');
+$emails = get_input('emails');
+$emailmessage = get_input('emailmessage');
 
-	$emails = trim($emails);
-	if (strlen($emails) > 0) {
-		$emails = preg_split('/\\s+/', $emails, -1, PREG_SPLIT_NO_EMPTY);
-	}
-	
-	if (!is_array($emails) || count($emails) == 0) {
-		register_error(elgg_echo('invitefriends:failure'));
-		forward($_SERVER['HTTP_REFERER']);
-	}
-	
-	$error = FALSE;
-	$bad_emails = array();
-	foreach($emails as $email) {
-				
-		$email = trim($email);
-		if (empty($email)) {
-			continue;
-		}
-	
-		// send out other email addresses	
-		if (!is_email_address($email)) {
-			$error = TRUE;
-			$bad_emails[] = $email;
-			continue;
-		}
-		
-		$link = $CONFIG->wwwroot . 'pg/register?friend_guid=' . $_SESSION['guid'] . '&invitecode=' . generate_invite_code($_SESSION['user']->username);
-		$message = sprintf(elgg_echo('invitefriends:email'),
-							$CONFIG->site->name,
-							$_SESSION['user']->name,
-							$emailmessage,
-							$link
-						);
+$emails = trim($emails);
+if (strlen($emails) > 0) {
+	$emails = preg_split('/\\s+/', $emails, -1, PREG_SPLIT_NO_EMPTY);
+}
 
-		$subject = sprintf(elgg_echo('invitefriends:subject'), $CONFIG->site->name);
-
-		// create the from address
-		$site = get_entity($CONFIG->site_guid);
-		if (($site) && (isset($site->email))) {
-			$from = $site->email;
-		} else {
-			$from = 'noreply@' . get_site_domain($CONFIG->site_guid);
-		}
-
-		elgg_send_email($from, $email, $subject, $message);
-	}
-
-	if ($error) {
-		register_error(sprintf(elgg_echo('invitefriends:email_error'), implode(', ', $bad_emails)));
-	} else {	
-		system_message(elgg_echo('invitefriends:success'));
-	}
-
+if (!is_array($emails) || count($emails) == 0) {
+	register_error(elgg_echo('invitefriends:failure'));
 	forward($_SERVER['HTTP_REFERER']);
+}
+
+$current_user = get_loggedin_user();
+
+$error = FALSE;
+$bad_emails = array();
+foreach($emails as $email) {
+
+	$email = trim($email);
+	if (empty($email)) {
+		continue;
+	}
+
+	// send out other email addresses
+	if (!is_email_address($email)) {
+		$error = TRUE;
+		$bad_emails[] = $email;
+		continue;
+	}
+
+	$link = $CONFIG->wwwroot . 'pg/register?friend_guid=' . $current_user->guid . '&invitecode=' . generate_invite_code($current_user->username);
+	$message = sprintf(elgg_echo('invitefriends:email'),
+					$CONFIG->site->name,
+					$current_user->name,
+					$emailmessage,
+					$link
+	);
+
+	$subject = sprintf(elgg_echo('invitefriends:subject'), $CONFIG->site->name);
+
+	// create the from address
+	$site = get_entity($CONFIG->site_guid);
+	if (($site) && (isset($site->email))) {
+		$from = $site->email;
+	} else {
+		$from = 'noreply@' . get_site_domain($CONFIG->site_guid);
+	}
+
+	elgg_send_email($from, $email, $subject, $message);
+}
+
+if ($error) {
+	register_error(sprintf(elgg_echo('invitefriends:email_error'), implode(', ', $bad_emails)));
+} else {
+	system_message(elgg_echo('invitefriends:success'));
+}
+
+forward($_SERVER['HTTP_REFERER']);
--- mod/invitefriends/languages/en.php
+++ mod/invitefriends/languages/en.php
@@ -1,31 +1,32 @@
 <?php
 
-	/**
-	 * Elgg invite page
-	 * 
-	 * @package ElggFile
-	 * @author Curverider Ltd
-	 * @copyright Curverider Ltd 2008-2010
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @link http://elgg.org/
-	 */
+/**
+ * Elgg invite language file
+ * 
+ * @package ElggInviteFriends
+ * @author Curverider Ltd
+ * @copyright Curverider Ltd 2008-2010
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @link http://elgg.org/
+ */
 
-	$english = array(
+$english = array(
 
-		'friends:invite' => 'Invite friends',
-		'invitefriends:introduction' => 'To invite friends to join you on this network, enter their email addresses below (one per line):',
-		'invitefriends:message' => 'Enter a message they will receive with your invitation:',
-		'invitefriends:subject' => 'Invitation to join %s',
+	'friends:invite' => 'Invite friends',
+	'invitefriends:introduction' => 'To invite friends to join you on this network, enter their email addresses below (one per line):',
+	'invitefriends:message' => 'Enter a message they will receive with your invitation:',
+	'invitefriends:subject' => 'Invitation to join %s',
+
+	'invitefriends:success' => 'Your friends were invited.',
+	'invitefriends:email_error' => 'Invitations were sent, but the following addresses are not valid: %s',
+	'invitefriends:failure' => 'Your friends could not be invited.',
 	
-		'invitefriends:success' => 'Your friends were invited.',
-		'invitefriends:email_error' => 'Invitations were sent, but the following addresses are not valid: %s',
-		'invitefriends:failure' => 'Your friends could not be invited.',
-	
-		'invitefriends:message:default' => '
+	'invitefriends:message:default' => '
 Hi,
 
 I want to invite you to join my network here on %s.',
-		'invitefriends:email' => '
+
+	'invitefriends:email' => '
 You have been invited to join %s by %s. They included the following message:
 
 %s
@@ -38,5 +39,4 @@ You will automatically add them as a friend when you create your account.',
 	
 	);
 					
-	add_translation("en",$english);
-?>
+add_translation("en", $english);
