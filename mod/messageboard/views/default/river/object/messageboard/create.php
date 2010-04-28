40b397c533ff1ba0064b09f5d3ea2362045483f3
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-28 11:43:35 +0000

    standardized the messageboard plugin
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5908 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/messageboard/views/default/river/object/messageboard/create.php
+++ mod/messageboard/views/default/river/object/messageboard/create.php
@@ -1,12 +1,10 @@
 <?php
 
-	$performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
-	$performed_on = get_entity($vars['item']->object_guid);
+$performed_by = get_entity($vars['item']->subject_guid);
+$performed_on = get_entity($vars['item']->object_guid);
 
-    $url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
-    $string = sprintf(elgg_echo("messageboard:river:added"),$url)  . " <a href=\"{$performed_on->getURL()}\">" . $performed_on->name . "'s</a> " . elgg_echo("messageboard:river:messageboard");
-	    
-	
-    echo $string; 
+$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
+$string = sprintf(elgg_echo("messageboard:river:added"),$url)  . " <a href=\"{$performed_on->getURL()}\">" . $performed_on->name . "'s</a> " . elgg_echo("messageboard:river:messageboard");
 
-?>
+
+echo $string; 
