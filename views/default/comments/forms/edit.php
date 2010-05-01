6240e789b39db26fa2d908a181b43ac3953137bc
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-01 18:13:09 +0000

    you post comments - not save them - this confuses content owners who think they are saving their content
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5928 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/comments/forms/edit.php
+++ views/default/comments/forms/edit.php
@@ -12,7 +12,7 @@
 if (isset($vars['entity']) && isloggedin()) {
 	$form_body = "<div class=\"contentWrapper\"><p class='longtext_editarea'><label>".elgg_echo("generic_comments:text")."<br />" . elgg_view('input/longtext',array('internalname' => 'generic_comment')) . "</label></p>";
 	$form_body .= "<p>" . elgg_view('input/hidden', array('internalname' => 'entity_guid', 'value' => $vars['entity']->getGUID()));
-	$form_body .= elgg_view('input/submit', array('value' => elgg_echo("save"))) . "</p></div>";
+	$form_body .= elgg_view('input/submit', array('value' => elgg_echo("post"))) . "</p></div>";
 
 	echo elgg_view('input/form', array('body' => $form_body, 'action' => "{$vars['url']}action/comments/add"));
 
