f52be8823d40d23e5a5ac7f6e2bf9c609ffcbe38
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-04 13:04:14 +0000

    Added id to bookmark edit form (for use theming).
    git-svn-id: http://code.elgg.org/elgg/trunk@5964 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/bookmarks/views/default/bookmarks/form.php
+++ mod/bookmarks/views/default/bookmarks/form.php
@@ -38,7 +38,7 @@ if(isset($vars['entity'])){
 	$url = "action/bookmarks/add";
 }
 ?>
-<form class="margin_top" action="<?php echo $vars['url'] . $url; ?>" method="post">
+<form id="bookmark_edit_form" class="margin_top" action="<?php echo $vars['url'] . $url; ?>" method="post">
 	<?php echo elgg_view('input/securitytoken'); ?>
 	<p>
 		<label>
