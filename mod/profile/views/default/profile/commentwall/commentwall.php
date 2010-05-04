bf870e9a8adcef0459cf94c544e31066dd188b1a
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-04 12:53:33 +0000

    Minor update to the comment wall wrapper div.
    git-svn-id: http://code.elgg.org/elgg/trunk@5963 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/views/default/profile/commentwall/commentwall.php
+++ mod/profile/views/default/profile/commentwall/commentwall.php
@@ -2,21 +2,17 @@
 /**
  * Elgg Commentwall display page
  */
-	 
+//start the div which will wrap all the message board contents
+echo "<div id='comment_wall_display'>";	 
+
 // If there is any content to view, view it
-if (is_array($vars['annotation']) && sizeof($vars['annotation']) > 0) {
-    		
-	//start the div which will wrap all the message board contents
-	echo "<div id='comment_wall_display'>";
-			
+if (is_array($vars['annotation']) && sizeof($vars['annotation']) > 0) {		
 	//loop through all annotations and display
 	foreach($vars['annotation'] as $content) {
 		echo elgg_view("profile/commentwall/commentwall_content", array('annotation' => $content));
-	}
-			
-	//close the wrapper div
-	echo "</div>";
-			
+	}			
 } else {
 	echo "<p class='margin_top'>" . elgg_echo("profile:commentwall:none") . "</p>";
 }
+//close the wrapper div
+echo "</div>";
