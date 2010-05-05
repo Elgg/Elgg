81ffac29fabc175eebdbf95578da046f4f00611b
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-05 16:59:27 +0000

    Added ID to submit button on user settings form.
    git-svn-id: http://code.elgg.org/elgg/trunk@5976 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/usersettings/form.php
+++ views/default/usersettings/form.php
@@ -7,6 +7,6 @@
  */
 
 $form_body = elgg_view("usersettings/user");
-$form_body .= "<div class='divider'></div>".elgg_view('input/submit', array('value' => elgg_echo('save')));
+$form_body .= "<div class='divider'></div>".elgg_view('input/submit', array('value' => elgg_echo('save'), 'class' => 'submit_button usersettings_save'));
 
 echo elgg_view('input/form', array('action' => "{$vars['url']}action/usersettings/save", 'body' => $form_body));
