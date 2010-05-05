f79bf1046415f4cdb47ee7831d29af0330fb6c77
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-05 01:41:30 +0000

    Fixes #2156: Blog draft and preview work without body content.
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@5971 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/blog/views/default/blog/forms/edit.php
+++ mod/blog/views/default/blog/forms/edit.php
@@ -2,13 +2,13 @@
 
 	/**
 	 * Elgg blog edit/add page
-	 * 
+	 *
 	 * @package ElggBlog
 	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
 	 * @author Curverider Ltd <info@elgg.com>
 	 * @copyright Curverider Ltd 2008-2010
 	 * @link http://elgg.com/
-	 * 
+	 *
 	 * @uses $vars['object'] Optionally, the blog post to edit
 	 */
 
@@ -30,57 +30,56 @@
 			$action = "blog/add";
 			$tags = "";
 			$title = "";
+			$body = "";
 			$comments_on = true;
 			$description = "";
 			if (defined('ACCESS_DEFAULT'))
 				$access_id = ACCESS_DEFAULT;
 			else
 				$access_id = 0;
-				
+
 			$container = $vars['container_guid'] ? elgg_view('input/hidden', array('internalname' => 'container_guid', 'value' => $vars['container_guid'])) : "";
 		}
 
 	// Just in case we have some cached details
 		if (empty($body)) {
 			$body = $vars['user']->blogbody;
-			if (!empty($body)) {
-				$title = $vars['user']->blogtitle;
-				$tags = $vars['user']->blogtags;
-			}
+			$title = $vars['user']->blogtitle;
+			$tags = $vars['user']->blogtags;
 		}
 
 	// set the required variables
 
-                $title_label = elgg_echo('title');
-                $title_textbox = elgg_view('input/text', array('internalname' => 'blogtitle', 'value' => $title));
-                $text_label = elgg_echo('blog:text');
-                $text_textarea = elgg_view('input/longtext', array('internalname' => 'blogbody', 'value' => $body));
-                $tag_label = elgg_echo('tags');
-                $tag_input = elgg_view('input/tags', array('internalname' => 'blogtags', 'value' => $tags));
-                $access_label = elgg_echo('access');
+				$title_label = elgg_echo('title');
+				$title_textbox = elgg_view('input/text', array('internalname' => 'blogtitle', 'value' => $title));
+				$text_label = elgg_echo('blog:text');
+				$text_textarea = elgg_view('input/longtext', array('internalname' => 'blogbody', 'value' => $body));
+				$tag_label = elgg_echo('tags');
+				$tag_input = elgg_view('input/tags', array('internalname' => 'blogtags', 'value' => $tags));
+				$access_label = elgg_echo('access');
 
-		  //$comments_select = elgg_view('input/checkboxes', array('internalname' => 'comments_on', 'value' => ''));
-		  if($comments_on)
-		  	$comments_on_switch = "checked=\"checked\"";
-		  else
+		//$comments_select = elgg_view('input/checkboxes', array('internalname' => 'comments_on', 'value' => ''));
+		if($comments_on)
+			$comments_on_switch = "checked=\"checked\"";
+		else
 			$comment_on_switch = "";
 
-          $access_input = elgg_view('input/access', array('internalname' => 'access_id', 'value' => $access_id));
-          $submit_input = elgg_view('input/submit', array('internalname' => 'submit', 'value' => elgg_echo('publish')));
-		  $conversation = elgg_echo('Conversation');
-		  $publish = elgg_echo('publish');
-		  $cat = elgg_echo('categories');
-		  $preview = elgg_echo('blog:preview');
-		  $privacy = elgg_echo('access');
-		  $savedraft = elgg_echo('blog:draft:save');
-		  $draftsaved = elgg_echo('blog:draft:saved');
-		  $never = elgg_echo('blog:never');
-		  $allowcomments = elgg_echo('blog:comments:allow');
-		  
+		$access_input = elgg_view('input/access', array('internalname' => 'access_id', 'value' => $access_id));
+		$submit_input = elgg_view('input/submit', array('internalname' => 'submit', 'value' => elgg_echo('publish')));
+		$conversation = elgg_echo('Conversation');
+		$publish = elgg_echo('publish');
+		$cat = elgg_echo('categories');
+		$preview = elgg_echo('blog:preview');
+		$privacy = elgg_echo('access');
+		$savedraft = elgg_echo('blog:draft:save');
+		$draftsaved = elgg_echo('blog:draft:saved');
+		$never = elgg_echo('blog:never');
+		$allowcomments = elgg_echo('blog:comments:allow');
+
 	// INSERT EXTRAS HERE
-		  $extras = elgg_view('categories',$vars);
-		  if (!empty($extras)) $extras = '<div id="blog_edit_sidebar">' . $extras . '</div>';
-		  
+		$extras = elgg_view('categories',$vars);
+		if (!empty($extras)) $extras = '<div id="blog_edit_sidebar">' . $extras . '</div>';
+
 ?>
 
 <?php
@@ -89,7 +88,7 @@
 
 	<div id="two_column_left_sidebar_210">
 
-    		<div id="blog_edit_sidebar">
+			<div id="blog_edit_sidebar">
 			<div id="content_area_user_title">
 				<div class="preview_button"><a  onclick="javascript:saveDraft(true);return true;">{$preview}</a></div>
 			<h2>{$publish}</h2></div>
@@ -120,7 +119,7 @@
 		</div>
 
 		{$extras}
-		
+
 		$container
 
 	</div><!-- /two_column_left_sidebar_210 -->
@@ -132,29 +131,29 @@ EOT;
 ?>
 
 <?php
-               
-                if (isset($vars['entity'])) {
-                  $entity_hidden = elgg_view('input/hidden', array('internalname' => 'blogpost', 'value' => $vars['entity']->getGUID()));
-                } else {
-                  $entity_hidden = '';
-                }
 
-                $form_body .= <<<EOT
+				if (isset($vars['entity'])) {
+				$entity_hidden = elgg_view('input/hidden', array('internalname' => 'blogpost', 'value' => $vars['entity']->getGUID()));
+				} else {
+				$entity_hidden = '';
+				}
+
+				$form_body .= <<<EOT
 		<p>
 			<label>$title_label</label><br />
-                        $title_textbox
+						$title_textbox
 		</p>
 		<p class='longtext_editarea'>
 			<label>$text_label</label><br />
-                        $text_textarea
+						$text_textarea
 		</p>
 		<p>
 			<label>$tag_label</label><br />
-                        $tag_input
+						$tag_input
 		</p>
 		<!-- <p>
 			<label>$access_label</label><br />
-                        $access_input
+						$access_input
 		</p> -->
 		<p>
 			$entity_hidden
@@ -163,7 +162,7 @@ EOT;
 	</div><div class="clearfloat"></div><!-- /two_column_left_sidebar_maincontent -->
 EOT;
 
-      echo elgg_view('input/form', array('action' => "{$vars['url']}action/$action", 'body' => $form_body, 'internalid' => 'blogPostForm'));
+	echo elgg_view('input/form', array('action' => "{$vars['url']}action/$action", 'body' => $form_body, 'internalid' => 'blogPostForm'));
 ?>
 
 <script type="text/javascript">
@@ -174,14 +173,14 @@ EOT;
 		if (typeof(tinyMCE) != 'undefined') {
 			tinyMCE.triggerSave();
 		}
-		
+
 		var drafturl = "<?php echo $vars['url']; ?>mod/blog/savedraft.php";
 		var temptitle = $("input[name='blogtitle']").val();
 		var tempbody = $("textarea[name='blogbody']").val();
 		var temptags = $("input[name='blogtags']").val();
-		
+
 		var postdata = { blogtitle: temptitle, blogbody: tempbody, blogtags: temptags };
-		
+
 		$.post(drafturl, postdata, function() {
 			var d = new Date();
 			var mins = d.getMinutes() + '';
@@ -194,7 +193,7 @@ EOT;
 				//document.blogPostForm.submit();
 			}
 		});
-				
+
 	}
-	
+
 </script>
