402134632f0d60e9644d0121c2a84db4a4c1a169
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-13 01:11:33 +0000

    merged in notifications plugin cleanup from 1.7 branch: [5962], [5978] - [5980], [6000]
    git-svn-id: http://code.elgg.org/elgg/trunk@6020 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/notifications/start.php
+++ mod/notifications/start.php
@@ -1,47 +1,75 @@
 <?php
 
-	/**
-	 * Elgg notifications plugin
-	 * 
-	 * @package ElggNotifications
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Curverider Ltd
-	 * @copyright Curverider Ltd 2008-2010
-	 * @link http://elgg.com/
-	 */
+/**
+ * Elgg notifications plugin
+ *
+ * @package ElggNotifications
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Curverider Ltd
+ * @copyright Curverider Ltd 2008-2010
+ * @link http://elgg.com/
+ */
 
 
-	/**
-	 * Notification settings page setup function
-	 *
-	 */
-		function notifications_plugin_pagesetup() {
-			global $CONFIG;
-			if (get_context() == 'settings') {
-				add_submenu_item(elgg_echo('notifications:subscriptions:changesettings'), $CONFIG->wwwroot . "mod/notifications/");
-				if (is_plugin_enabled('groups'))
-					add_submenu_item(elgg_echo('notifications:subscriptions:changesettings:groups'), $CONFIG->wwwroot . "mod/notifications/groups.php");
-			}
+function notifications_plugin_init() {
+	global $CONFIG;
+
+	elgg_extend_view('css','notifications/css');
+
+	register_page_handler('notifications', 'notifications_page_handler');
+
+	register_elgg_event_handler('pagesetup', 'system', 'notifications_plugin_pagesetup');
+
+	// Unset the default notification settings
+	unregister_plugin_hook('usersettings:save', 'user', 'notification_user_settings_save');
+	
+	// must wait until elgg_unextend_view() is merged in from 1.7 branch - if still needed
+	//elgg_unextend_view('usersettings/user', 'notifications/settings/usersettings');
+}
+
+/**
+ * Route page requests
+ *
+ * @param array $page Array of url parameters
+ */
+function notifications_page_handler($page) {
+	global $CONFIG;
+
+	// default to personal notifications
+	if (!isset($page[0])) {
+		$page[0] = 'personal';
+	}
+
+	switch ($page[0]) {
+		case 'group':
+			require $CONFIG->pluginspath . "notifications/groups.php";
+			break;
+		case 'personal':
+		default:
+			require $CONFIG->pluginspath . "notifications/index.php";
+			break;
+	}
+
+	return TRUE;
+}
+
+/**
+ * Notification settings page setup function
+ *
+ */
+function notifications_plugin_pagesetup() {
+	global $CONFIG;
+	if (get_context() == 'settings') {
+		add_submenu_item(elgg_echo('notifications:subscriptions:changesettings'), $CONFIG->wwwroot . "pg/notifications/personal");
+		if (is_plugin_enabled('groups')) {
+			add_submenu_item(elgg_echo('notifications:subscriptions:changesettings:groups'), $CONFIG->wwwroot . "pg/notifications/group");
 		}
-		
-		function notifications_plugin_init() {
-			elgg_extend_view('css','notifications/css');
-			global $CONFIG;
-			
-			// Unset the default user settings hook
-			if (isset($CONFIG->hooks['usersettings:save']['user']))
-				foreach($CONFIG->hooks['usersettings:save']['user'] as $key => $function) {
-					if ($function == 'notification_user_settings_save')
-						unset($CONFIG->hooks['usersettings:save']['user'][$key]); 
-				}
-		}
+	}
+}
 
-		register_elgg_event_handler('pagesetup','system','notifications_plugin_pagesetup',1000);
-		register_elgg_event_handler('init','system','notifications_plugin_init',1000);
 
-	// Register action
-		global $CONFIG;
-		register_action("notificationsettings/save",false,$CONFIG->pluginspath . "notifications/actions/save.php");
-		register_action("notificationsettings/groupsave",false,$CONFIG->pluginspath . "notifications/actions/groupsave.php");
-		
-?>
+register_elgg_event_handler('init', 'system', 'notifications_plugin_init', 1000);
+
+// Register action
+register_action("notificationsettings/save", FALSE, $CONFIG->pluginspath . "notifications/actions/save.php");
+register_action("notificationsettings/groupsave", FALSE, $CONFIG->pluginspath . "notifications/actions/groupsave.php");
--- mod/notifications/views/default/notifications/subscriptions/collections.php
+++ mod/notifications/views/default/notifications/subscriptions/collections.php
@@ -5,10 +5,10 @@
 		for ( var i in members ) {
 			var checked = $('#' + method + 'collections' + id).children("INPUT[type='checkbox']").attr('checked');
 			if ($("#"+method+members[i]).children("INPUT[type='checkbox']").attr('checked') != checked) {  
-    			$("#"+method+members[i]).children("INPUT[type='checkbox']").attr('checked', checked);
-    			functioncall = 'adjust' + method + '_alt("'+method+members[i]+'");';
-    			eval(functioncall);
-    		}
+				$("#"+method+members[i]).children("INPUT[type='checkbox']").attr('checked', checked);
+				functioncall = 'adjust' + method + '_alt("'+method+members[i]+'");';
+				eval(functioncall);
+			}
 		} 
 	}
 	
@@ -21,121 +21,130 @@
 	<?php echo elgg_echo('notifications:subscriptions:collections:description'); ?>
 </p>
 <table id="notificationstable" cellspacing="0" cellpadding="4" border="0" width="100%">
-  <tr>
-    <td>&nbsp;</td>
+	<tr>
+		<td>&nbsp;</td>
 <?php
 	$i = 0; 
 	global $NOTIFICATION_HANDLERS;
 	foreach($NOTIFICATION_HANDLERS as $method => $foo) {
-		if ($i > 0)
+		if ($i > 0) {
 			echo "<td class='spacercolumn'>&nbsp;</td>";
+		}
 ?>
 	<td class="<?php echo $method; ?>togglefield"><?php echo elgg_echo('notification:method:'.$method); ?></td>
 <?php
 		$i++;
 	}
 ?>
-    <td>&nbsp;</td>
-  </tr>
+		<td>&nbsp;</td>
+	</tr>
 <?php
-		$members = array();
-		if ($friends = get_user_friends($vars['user']->guid,'',9999,0)) {
-			foreach($friends as $friend)
-				$members[] = $friend->guid;
+	$members = array();
+	if ($friends = get_user_friends($vars['user']->guid, '', 9999, 0)) {
+		foreach($friends as $friend) {
+			$members[] = $friend->guid;
 		}
-		$memberno = sizeof($members);
-		$members = implode(',',$members);
+	}
+	$memberno = sizeof($members);
+	$members = implode(',',$members);
 
 ?>
-  <tr>
-    <td class="namefield">
-    	<p>
-    		<?php echo elgg_echo('friends:all'); ?> (<?php echo $memberno; ?>)
-    	</p>
-    </td>
+	<tr>
+		<td class="namefield">
+			<p>
+				<?php echo elgg_echo('friends:all'); ?> (<?php echo $memberno; ?>)
+			</p>
+		</td>
 <?php
 
-		$fields = '';
-		$i = 0;
-		foreach($NOTIFICATION_HANDLERS as $method => $foo) {
-			$metaname = 'collections_notifications_preferences_' . $method;
-			if ($collections_preferences = $vars['user']->$metaname) {
-				if (!empty($collections_preferences) && !is_array($collections_preferences))
-					$collections_preferences = array($collections_preferences);
-				if (is_array($collections_preferences))
+	$fields = '';
+	$i = 0;
+	foreach($NOTIFICATION_HANDLERS as $method => $foo) {
+		$metaname = 'collections_notifications_preferences_' . $method;
+		if ($collections_preferences = $vars['user']->$metaname) {
+			if (!empty($collections_preferences) && !is_array($collections_preferences)) {
+				$collections_preferences = array($collections_preferences);
+			}
+			if (is_array($collections_preferences)) {
 				if (in_array(-1,$collections_preferences)) {
 					$collectionschecked[$method] = 'checked="checked"';
 				} else {
 					$collectionschecked[$method] = '';
 				}
 			}
-			if ($i > 0) $fields .= "<td class='spacercolumn'>&nbsp;</td>";
-			$fields .= <<< END
-			    <td class="{$method}togglefield">
-			    <a border="0" id="{$method}collections-1" class="{$method}toggleOff" onclick="adjust{$method}_alt('{$method}collections-1'); setCollection([{$members}],'{$method}',-1);">
-			    <input type="checkbox" name="{$method}collections[]" id="{$method}checkbox" onclick="adjust{$method}('{$method}collections-1');" value="-1" {$collectionschecked[$method]} /></a></td>
-END;
-			$i++;
 		}
-		echo $fields;
+		if ($i > 0) {
+			$fields .= "<td class='spacercolumn'>&nbsp;</td>";
+		}
+		$fields .= <<< END
+			<td class="{$method}togglefield">
+			<a border="0" id="{$method}collections-1" class="{$method}toggleOff" onclick="adjust{$method}_alt('{$method}collections-1'); setCollection([{$members}],'{$method}',-1);">
+			<input type="checkbox" name="{$method}collections[]" id="{$method}checkbox" onclick="adjust{$method}('{$method}collections-1');" value="-1" {$collectionschecked[$method]} /></a></td>
+END;
+		$i++;
+	}
+	echo $fields;
 
 ?>
-	<td>&nbsp;</td>
-  </tr>
+		<td>&nbsp;</td>
+	</tr>
 <?php
 /*
 	@todo
 	collections removed from notifications - they are no longer used and will be replaced with shared access collections
 	
 	if ($collections = get_user_access_collections($vars['user']->guid)) {
-	foreach($collections as $collection) {
-		$members = get_members_of_access_collection($collection->id, true);
-		$memberno = sizeof($members);
-		$members = implode(',',$members);
+		foreach($collections as $collection) {
+			$members = get_members_of_access_collection($collection->id, true);
+			$memberno = sizeof($members);
+			$members = implode(',', $members);
 
 ?>
-  <tr>
-    <td class="namefield">
-    	<p>
-    		<?php echo $collection->name; ?> (<?php echo $memberno; ?>)
-    	</p>
-    	
-    </td>
-    
+	<tr>
+		<td class="namefield">
+			<p>
+				<?php echo $collection->name; ?> (<?php echo $memberno; ?>)
+			</p>
+		</td>
+
 <?php
 
-		$fields = '';
-		$i = 0;
-		foreach($NOTIFICATION_HANDLERS as $method => $foo) {
-			$metaname = 'collections_notifications_preferences_' . $method;
-			if ($collections_preferences = $vars['user']->$metaname) {
-				if (!empty($collections_preferences) && !is_array($collections_preferences))
-					$collections_preferences = array($collections_preferences);
-				if (is_array($collections_preferences))
-				if (in_array($collection->id,$collections_preferences)) {
-					$collectionschecked[$method] = 'checked="checked"';
-				} else {
-					$collectionschecked[$method] = '';
+			$fields = '';
+			$i = 0;
+			foreach($NOTIFICATION_HANDLERS as $method => $foo) {
+				$metaname = 'collections_notifications_preferences_' . $method;
+				if ($collections_preferences = $vars['user']->$metaname) {
+					if (!empty($collections_preferences) && !is_array($collections_preferences)) {
+						$collections_preferences = array($collections_preferences);
+					}
+					if (is_array($collections_preferences)) {
+						if (in_array($collection->id,$collections_preferences)) {
+							$collectionschecked[$method] = 'checked="checked"';
+						} else {
+							$collectionschecked[$method] = '';
+						}
+					}
 				}
-			}
-			if ($i > 0) $fields .= "<td class='spacercolumn'>&nbsp;</td>";
-			$fields .= <<< END
-			    <td class="{$method}togglefield">
-			    <a border="0" id="{$method}collections{$collection->id}" class="{$method}toggleOff" onclick="adjust{$method}_alt('{$method}collections{$collection->id}'); setCollection([{$members}],'{$method}',{$collection->id});">
-			    <input type="checkbox" name="{$method}collections[]" id="{$method}checkbox" onclick="adjust{$method}('{$method}collections{$collection->id}');" value="{$collection->id}" {$collectionschecked[$method]} /></a></td>
+				if ($i > 0) {
+					$fields .= "<td class='spacercolumn'>&nbsp;</td>";
+				}
+				$fields .= <<< END
+					<td class="{$method}togglefield">
+					<a border="0" id="{$method}collections{$collection->id}" class="{$method}toggleOff" onclick="adjust{$method}_alt('{$method}collections{$collection->id}'); setCollection([{$members}],'{$method}',{$collection->id});">
+					<input type="checkbox" name="{$method}collections[]" id="{$method}checkbox" onclick="adjust{$method}('{$method}collections{$collection->id}');" value="{$collection->id}" {$collectionschecked[$method]} /></a></td>
 END;
-			$i++;
-		}
-		echo $fields;
+				$i++;
+			}
+			echo $fields;
 
 ?>
-  
-    <td>&nbsp;</td>
-  </tr>
+
+		<td>&nbsp;</td>
+	</tr>
 <?php
 
+		}
 	}
-}
 
 */
 ?>
