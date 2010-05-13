a95432e1e0938dab78734c22314197da9fb2a95c
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-13 02:01:03 +0000

    removed extra div from riverdashboard index page
    git-svn-id: http://code.elgg.org/elgg/trunk@6023 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/riverdashboard/index.php
+++ mod/riverdashboard/index.php
@@ -46,9 +46,9 @@ $title = elgg_view_title($title_wording);
 
 //select the correct river
 if (get_plugin_setting('activitytype', 'riverdashboard') == 'classic') {
-	$river = elgg_view_river_items($subject_guid, 0, $relationship_type, $type, $subtype, '', 20, 0, 0, true, true)  . "</div>";
+	$river = elgg_view_river_items($subject_guid, 0, $relationship_type, $type, $subtype, '', 20, 0, 0, true, true);
 } else {
-	$river = elgg_view_river_items($subject_guid, 0, $relationship_type, $type, $subtype, '', 20, 0, 0, true, false)  . "</div>";
+	$river = elgg_view_river_items($subject_guid, 0, $relationship_type, $type, $subtype, '', 20, 0, 0, true, false);
 }
 
 // Replacing callback calls in the nav with something meaningless
