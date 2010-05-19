eececd48863ba5afbe0ddd43b8f1320b646e6619
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-19 11:47:02 +0000

    Fixes #1879 - admins can now edit and delete group forum topics and posts in groups they do not belong to
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@6091 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/actions/forums/editpost.php
+++ mod/groups/actions/forums/editpost.php
@@ -10,13 +10,9 @@
 	 * @link http://elgg.com/
 	 */
 
-    // Make sure we're logged in (send us to the front page if not)
-		if (!isloggedin()) forward();
 		
-	// Check the user is a group member
 		$group_guid = get_input('group');
 	    $group_entity =  get_entity($group_guid);
-	    if (!$group_entity->isMember($vars['user'])) forward();
 	    
 	//get the required variables
 		$post = get_input("post");
@@ -27,25 +23,24 @@
 		$access_id = $annotation->access_id;
 		$topic = get_input("topic");
 		
-		if($annotation){
+		if ($annotation) {
 			
 			//can edit? Either the comment owner or admin can
-			if(groups_can_edit_discussion($annotation, page_owner_entity()->owner_guid)){
+			if (groups_can_edit_discussion($annotation, page_owner_entity()->owner_guid)) {
 				
 				update_annotation($post, "group_topic_post", $post_comment, "",$commentOwner, $access_id);
 			    system_message(elgg_echo("groups:forumpost:edited"));
 				   
-			}else{
+			} else {
 				system_message(elgg_echo("groups:forumpost:error"));
 			}
 			
-		}else{
+		} else {
 			
 				system_message(elgg_echo("groups:forumpost:error"));
 		}
 		
 		// Forward to the group forum page
-	    global $CONFIG;
 	    $url = $CONFIG->wwwroot . "mod/groups/topicposts.php?topic={$topic}&group_guid={$group_guid}/";
 		forward($url);
   
