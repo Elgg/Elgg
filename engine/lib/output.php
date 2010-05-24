f0c725f29aeb7e5e9b2d1fc9e846f90cdbb152a3
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-24 19:43:14 +0000

    Replacing '...' with html elipse character (&#8230) in elgg_make_excerpt().
    git-svn-id: http://code.elgg.org/elgg/trunk@6188 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/output.php
+++ engine/lib/output.php
@@ -112,7 +112,7 @@ function elgg_make_excerpt($text, $num_chars = 250) {
 	$excerpt = trim(elgg_substr($excerpt, 0, $space));
 
 	if ($string_length != elgg_strlen($excerpt)) {
-		$excerpt .= '...';
+		$excerpt .= '&#8230';
 	}
 
 	return $excerpt;
