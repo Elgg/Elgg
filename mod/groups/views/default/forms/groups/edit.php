a19462d8b0c1bc07597c78c92a79415694eee3eb
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-29 02:16:50 +0000

    Merged [6223] - [6228] into trunk.
    git-svn-id: http://code.elgg.org/elgg/trunk@6283 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/forms/groups/edit.php
+++ mod/groups/views/default/forms/groups/edit.php
@@ -132,11 +132,13 @@ if (is_array($vars['config']->group) && sizeof($vars['config']->group) > 0)
 	?>
 	<p>
 		<?php
-			if ($vars['entity']) { 
-				?><input type="hidden" name="group_guid" value="<?php echo $vars['entity']->getGUID(); ?>" /><?php 
+			if ($vars['entity'])
+			{ 
+		?>
+		<input type="hidden" name="group_guid" value="<?php echo $vars['entity']->getGUID(); ?>" />
+		<?php
 			}
 		?>
-		<input type="hidden" name="user_guid" value="<?php echo page_owner_entity()->guid; ?>" />
 		<input type="submit" class="submit_button" value="<?php echo elgg_echo("save"); ?>" />
 		
 	</p>
--- version.php
+++ version.php
@@ -12,7 +12,7 @@
 
 // YYYYMMDD = Elgg Date
 // XX = Interim incrementer
-$version = 2010050701;
+$version = 2010052601;
 
 // Human-friendly version name
 $release = '1.8-svn';
