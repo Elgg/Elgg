e79810c63fca3d572aeb800bdc4b3c5de964934f
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-02 16:50:03 +0000

    Removing unused views and files from embed.
    git-svn-id: http://code.elgg.org/elgg/trunk@6327 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/embed/views/default/embed/link.php
+++ mod/embed/views/default/embed/link.php
@@ -3,4 +3,4 @@
  * Displays an "Embed media" link in longtext inputs.
  */
 ?>
-<a class="longtext_control small link" href="<?php echo $vars['url'] . 'pg/embed/media'; ?>?internalname=<?php echo $vars['internalname']; ?>" rel="facebox"><?php echo elgg_echo('media:insert'); ?></a>
+<a class="longtext_control small link" href="<?php echo $vars['url'] . 'pg/embed'; ?>?internalname=<?php echo $vars['internalname']; ?>" rel="facebox"><?php echo elgg_echo('media:insert'); ?></a>
