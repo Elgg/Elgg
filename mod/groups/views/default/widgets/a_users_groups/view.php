2d4b8c829073f5378d8889a9e2e92862556edcfc
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-04 12:08:55 +0000

    Closes #2260 - group names are now links in widget
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@6351 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/widgets/a_users_groups/view.php
+++ mod/groups/views/default/widgets/a_users_groups/view.php
@@ -1,42 +1,51 @@
 <?php
 
-    /** 
-      *  Group profile widget - this displays a users groups on their profile
-      **/
-      
-    //the number of groups to display
-	$number = (int) $vars['entity']->num_display;
-	if (!$number)
-		$number = 4;
-		
-    //the page owner
-	$owner = $vars['entity']->owner_guid;
-      
-    //$groups = get_users_membership($owner);
-    //$groups = list_entities_from_relationship('member',$owner,false,'group','',0,$number,false,false,false);
-	$groups = get_entities_from_relationship('member', $owner, false, "group", "", 0, "", $number, 0, false, 0);
-	
+/** 
+ *  Group profile widget - this displays a users groups on their profile
+ **/
 
-    if($groups){
-		
-		echo "<div class=\"groupmembershipwidget\">";
+//the number of groups to display
+$number = (int) $vars['entity']->num_display;
+if (!$number) {
+	$number = 4;
+}
 
-		foreach($groups as $group){
-			$icon = elgg_view(
+//the page owner
+$owner = $vars['entity']->owner_guid;
+
+//$groups = get_users_membership($owner);
+//$groups = list_entities_from_relationship('member',$owner,false,'group','',0,$number,false,false,false);
+$groups = get_entities_from_relationship('member', $owner, false, "group", "", 0, "", $number, 0, false, 0);
+
+
+if ($groups) {
+
+	echo "<div class=\"groupmembershipwidget\">";
+
+	foreach ($groups as $group) {
+		$icon = elgg_view(
 				"groups/icon", array(
-									'entity' => $group,
-									'size' => 'small',
-								  )
-				);
-				
-			echo "<div class=\"contentWrapper\">" . $icon . " <div class='search_listing_info'><p><span>" . $group->name . "</span><br />";
-			echo $group->briefdescription . "</p></div><div class=\"clearfloat\"></div></div>";
-			
-		}
-		echo "</div>";
-    }
+				'entity' => $group,
+				'size' => 'small',
+				)
+		);
 
+		$group_link = $group->getURL();
 
-   // echo $groups;
-      
-?>
+		echo <<<___END
+
+<div class="contentWrapper">
+	$icon
+	<div class="search_listing_info">
+		<p>
+			<span><a href="$group_link">$group->name</a></span><br />
+			$group->briefdescription
+		</p>
+	</div>
+	<div class="clearfloat"></div>
+</div>
+___END;
+
+	}
+	echo "</div>";
+}
