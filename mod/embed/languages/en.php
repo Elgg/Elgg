8aa30424492f2e1fe55712a9fde69c60889e639c
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-04 22:47:58 +0000

    Added missing language string for embed.
    git-svn-id: http://code.elgg.org/elgg/trunk@6362 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/embed/languages/en.php
+++ mod/embed/languages/en.php
@@ -8,6 +8,10 @@ $english = array(
 	'media:insert' => 'Embed media',
 	'embed:instructions' => 'Click on any file to embed it into your content.',
 	'embed:media' => 'Embed media',
+	'embed:upload' => 'Upload media',
+
+	// messages
+	'embed:no_upload_content' => 'No upload content!',
 );
 
 add_translation("en", $english);
