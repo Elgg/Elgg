5fa28d61cdb031ea48659f1a08372225f2395d32
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-04 13:21:59 +0000

    Removed profile content wrapper from comment wall content.
    git-svn-id: http://code.elgg.org/elgg/trunk@6352 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/views/default/profile/profile_contents/commentwall.php
+++ mod/profile/views/default/profile/profile_contents/commentwall.php
@@ -2,12 +2,9 @@
 /**
  * Elgg profile comment wall
  */
-?>
-<div id="profile_content">
-<?php
+
 if(isloggedin()){
 	echo elgg_view("profile/commentwall/commentwalladd");
 }
 echo elgg_view("profile/commentwall/commentwall", array('annotation' => $vars['comments']));
 ?>
-</div>
