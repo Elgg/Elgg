4165cd270dbefd3d9b8cb143af325b9c7450eb1c
pete <pete@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-10 21:42:40 +0000

    Added wrapper to profile page commentwall
    git-svn-id: http://code.elgg.org/elgg/trunk@6451 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/views/default/profile/profile_contents/commentwall.php
+++ mod/profile/views/default/profile/profile_contents/commentwall.php
@@ -2,9 +2,12 @@
 /**
  * Elgg profile comment wall
  */
-
+?>
+<div id="profile_content">
+<?php
 if(isloggedin()){
 	echo elgg_view("profile/commentwall/commentwalladd");
 }
 echo elgg_view("profile/commentwall/commentwall", array('annotation' => $vars['comments']));
 ?>
+</div>
