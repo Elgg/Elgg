86ef028d14d1e587768a8635d2ea78a26806aa51
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-15 00:48:28 +0000

    Backported 1.8's engine/start.php file to 1.7 because it's much nicer.
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@6489 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/start.php
+++ engine/start.php
@@ -23,94 +23,32 @@ if (!isset($CONFIG)) {
 	$CONFIG = new stdClass;
 }
 
-/**
- * Load important prerequisites
- */
+$lib_dir = dirname(__FILE__) . '/lib/';
 
-if (!include_once(dirname(__FILE__) . "/lib/exceptions.php")) {		// Exceptions
-	echo "Error in installation: could not load the Exceptions library.";
-	exit;
+// bootstrapping with required files in a required order
+$required_files = array(
+	'exceptions.php', 'elgglib.php', 'views.php', 'access.php', 'system_log.php', 'export.php',
+	'sessions.php', 'languages.php', 'input.php', 'install.php', 'cache.php', 'output.php'
+);
+
+foreach ($required_files as $file) {
+	$path = $lib_dir . $file;
+	if (!include($path)) {
+		echo "Could not load file '$path'. "
+		. 'Please check your Elgg installation for all required files.';
+		exit;
+	}
 }
-
-if (!include_once(dirname(__FILE__) . "/lib/elgglib.php")) {		// Main Elgg library
-	echo "Elgg could not load its main library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/views.php")) {		// Views
-	echo "Error in installation: could not load the Views library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/access.php")) {		// Access library
-	echo "Error in installation: could not load the Access library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/system_log.php")) {		// Logging library
-	echo "Error in installation: could not load the System Log library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/export.php")) {		// Export library
-	echo "Error in installation: could not load the Export library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/sessions.php")) {
-	echo ("Error in installation: Elgg could not load the Sessions library");
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/languages.php")) {		// Languages library
-	echo "Error in installation: could not load the languages library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/input.php")) {		// Input library
-	echo "Error in installation: could not load the input library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/install.php")) {		// Installation library
-	echo "Error in installation: could not load the installation library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/cache.php")) {		// Installation library
-	echo "Error in installation: could not load the cache library.";
-	exit;
-}
-
-if (!include_once(dirname(__FILE__) . "/lib/output.php")) {		// Installation library
-	echo "Error in installation: could not load the output library.";
-	exit;
-}
-
-
 
 // Use fallback view until sanitised
 $oldview = get_input('view');
 set_input('view', 'failsafe');
 
-/**
- * Set light mode default
- */
-$lightmode = false;
-
-/**
- * Establish handlers
- */
-
 // Register the error handler
 set_error_handler('__elgg_php_error_handler');
 set_exception_handler('__elgg_php_exception_handler');
 
-/**
- * If there are basic issues with the way the installation is formed, don't bother trying
- * to load any more files
- */
-// Begin portion for sanitised installs only
+// attempt to save settings.php and .htaccess if in installation.
 if ($sanitised = sanitised()) {
 	/**
 	 * Load the system settings
@@ -119,37 +57,26 @@ if ($sanitised = sanitised()) {
 		throw new InstallationException("Elgg could not load the settings file.");
 	}
 
-	/**
-	 * Load and initialise the database
-	 */
-	if (!include_once(dirname(__FILE__) . "/lib/database.php")) {
-		throw new InstallationException("Elgg could not load the main Elgg database library.");
-	}
-
-	if (!include_once(dirname(__FILE__) . "/lib/actions.php")) {
-		throw new InstallationException("Elgg could not load the Actions library");
-	}
-
 	// Get config
 	global $CONFIG;
 
 	// load the rest of the library files from engine/lib/
 	$lib_files = array(
+		// these want to be loaded first apparently?
+		'database.php', 'actions.php',
+
 		'admin.php', 'annotations.php', 'api.php', 'cache.php',
 		'calendar.php', 'configuration.php', 'cron.php', 'entities.php',
 		'export.php', 'extender.php', 'filestore.php', 'group.php',
 		'input.php', 'install.php', 'location.php', 'mb_wrapper.php',
 		'memcache.php', 'metadata.php', 'metastrings.php', 'notification.php',
-		'objects.php', 'opendd.php', 'pagehandler.php', 
+		'objects.php', 'opendd.php', 'pagehandler.php',
 		'pageowner.php', 'pam.php', 'plugins.php', 'query.php',
 		'relationships.php', 'river.php', 'sites.php', 'social.php',
 		'statistics.php', 'system_log.php', 'tags.php', 'usersettings.php',
 		'users.php', 'version.php', 'widgets.php', 'xml.php', 'xml-rpc.php'
 	);
 
-	$lib_dir = dirname(__FILE__) . '/lib/';
-
-	// Include them
 	foreach($lib_files as $file) {
 		$file = $lib_dir . $file;
 		elgg_log("Loading $file...");
@@ -157,7 +84,7 @@ if ($sanitised = sanitised()) {
 			throw new InstallationException("Could not load {$file}");
 		}
 	}
-} else {	// End portion for sanitised installs only
+} else {
 	throw new InstallationException(elgg_echo('installation:error:configuration'));
 }
 
@@ -171,14 +98,8 @@ trigger_elgg_event('boot', 'system');
 $installed = is_installed();
 $db_installed = is_db_installed();
 
-// Determine light mode
-$lm = strtolower(get_input('lightmode'));
-if ($lm == 'true') {
-	$lightmode = true;
-}
-
 // Load plugins, if we're not in light mode
-if (($installed) && ($db_installed) && ($sanitised) && (!$lightmode)) {
+if (($installed) && ($db_installed) && ($sanitised)) {
 	load_plugins();
 
 	trigger_elgg_event('plugins_boot', 'system');
@@ -187,19 +108,17 @@ if (($installed) && ($db_installed) && ($sanitised) && (!$lightmode)) {
 // Forward if we haven't been installed
 if ((!$installed || !$db_installed)
 	&& !substr_count($_SERVER["PHP_SELF"], "install.php")
-	&& !substr_count($_SERVER["PHP_SELF"],"css.php")
-	&& !substr_count($_SERVER["PHP_SELF"],"action_handler.php")) {
+	&& !substr_count($_SERVER["PHP_SELF"], "css.php")
+	&& !substr_count($_SERVER["PHP_SELF"], "action_handler.php")) {
 
 		header("Location: install.php");
 		exit;
 }
 
 // Trigger events
-if (!substr_count($_SERVER["PHP_SELF"],"install.php") &&
-	!substr_count($_SERVER["PHP_SELF"],"setup.php") &&
-	!$lightmode
+if (!substr_count($_SERVER["PHP_SELF"],"install.php")
+	&& !substr_count($_SERVER["PHP_SELF"],"setup.php")
 	&& !(defined('upgrading') && upgrading == 'upgrading')) {
-
 
 	trigger_elgg_event('init', 'system');
 }
