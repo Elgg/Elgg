4a108fa6f8181d565b6e8c525b299e90ae80ffe5
dave <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-26 16:40:25 +0000

    The wire can now have comments and likes via the activity stream.
    git-svn-id: http://code.elgg.org/elgg/trunk@6571 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/thewire/views/default/river/object/thewire/create.php
+++ mod/thewire/views/default/river/object/thewire/create.php
@@ -1,17 +1,16 @@
 <?php
 
-	$performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
-	$object = get_entity($vars['item']->object_guid);
-	$url = $object->getURL();
+$performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
+$object = get_entity($vars['item']->object_guid);
+$url = $object->getURL();
 
-	$string = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}:</a> ";
-	$desc = $object->description;
-	//$desc = preg_replace('/\@([A-Za-z0-9\_\.\-]*)/i','@<a href="' . $vars['url'] . 'pg/thewire/$1">$1</a>',$desc);
-	$string .= parse_urls($desc);
-
-	$string .= " <span class='entity_subtext'>" . friendly_time($object->time_created) . "</span> <a href=\"{$vars['url']}mod/thewire/add.php?wire_username={$object->getOwnerEntity()->username}\" class='reply_link'>" . elgg_echo('thewire:reply') . "</a>";
-?>
-
-<?php 
-	echo $string; 
-?>
+$string = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}:</a> ";
+$desc = $object->description;
+//$desc = preg_replace('/\@([A-Za-z0-9\_\.\-]*)/i','@<a href="' . $vars['url'] . 'pg/thewire/$1">$1</a>',$desc);
+$string .= parse_urls($desc);
+$string .= " <span class='entity_subtext'>" . friendly_time($object->time_created) . "</span>";
+	if (isloggedin()){
+		$string .= "<a class='river_comment_form_button link'>Comment</a>";
+		$string .= elgg_view('likes/forms/link', array('entity' => $object));
+	}
+echo $string;
