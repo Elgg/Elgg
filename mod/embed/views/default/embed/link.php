9bc3a4f6794b88944f983c1b44b2a1cbd89f2c98
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-07-09 19:32:29 +0000

    Web services tab now default embed tab if ecml is enabled.
    git-svn-id: http://code.elgg.org/elgg/trunk@6681 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/embed/views/default/embed/link.php
+++ mod/embed/views/default/embed/link.php
@@ -2,5 +2,13 @@
 /**
  * Displays an "Embed media" link in longtext inputs.
  */
+
+// yeah this is naughty.  embed and ecml might want to merge.
+if (is_plugin_enabled('ecml')) {
+	$active_section = 'active_section=web_services&';
+} else {
+	$active_section = '';
+}
+
 ?>
-<a class="longtext_control small link" href="<?php echo $vars['url'] . 'pg/embed'; ?>?internal_name=<?php echo $vars['internalname']; ?>" rel="facebox"><?php echo elgg_echo('media:insert'); ?></a>
+<a class="longtext_control small link" href="<?php echo $vars['url'] . 'pg/embed'; ?>?<?php echo $active_section; ?>internal_name=<?php echo $vars['internalname']; ?>" rel="facebox"><?php echo elgg_echo('media:insert'); ?></a>
