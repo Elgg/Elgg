4ece630febf2c1275c7781e8cb9d4f4321aacfc1
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-07-15 11:17:01 +0000

    Fixes #2284.  Pulled out home link in elgg_header to page_elements/elgg_header_logo. Removed csseditor-specific code that caused custom logos to still appear even when csseditor was disabled.
    git-svn-id: http://code.elgg.org/elgg/trunk@6709 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/page_elements/elgg_header.php
+++ views/default/page_elements/elgg_header.php
@@ -7,22 +7,16 @@
 ?>
 <div id="elgg_header">
 	<div id="elgg_header_contents">
-		<!-- site name -->
-		<h1><a href="<?php echo $vars['url']; ?>"><?php
-		$logo = $vars['config']->site->customlogo;
-		$logo_time = $vars['config']->site->customlogo_time;
-		if (empty($logo)) {
-			echo "<span class='network_title'>".$vars['config']->sitename ."</span>";
-		} else {
-			echo "<img class='custom_logo' src=\"{$vars['url']}pg/csseditor/{$logo_time}.jpg\" alt=\"". htmlentities($vars['config']->sitename) ."\" />";	
-		}
-		?></a></h1>
 		<?php
+			// link back to main site.
+			echo elgg_view('page_elements/elgg_header_logo', $vars);
+			
 			// drop-down login
 			echo elgg_view('account/forms/login_dropdown');
 		
 			// insert site-wide navigation
 			echo elgg_view('navigation/site_nav');
+
 			// insert a view which can be extended
 			echo elgg_view('header/extend');
 		?>
---
+++ views/default/page_elements/elgg_header_logo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Elgg header logo
+ * The logo to display in elgg_header.
+ **/
+?>
+
+<h1><a href="<?php echo $vars['url']; ?>">
+<span class='network_title'><?php echo $vars['config']->sitename; ?></span>
+</a></h1>
