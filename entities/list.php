5c98b6bc059688e9cdeb0766dca30315edb59102
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-07-16 09:25:07 +0000

    Refs #2327. Applied Evan's patch to remove a call to list_entities in /entities/list.php
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@6723 36083f99-b078-4883-b0ff-0f9b5a30f544

--- entities/list.php
+++ entities/list.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Activity viewer
+ * Entity viewer
  *
  * @package Elgg
  * @subpackage Core
@@ -9,8 +9,11 @@
  */
 
 require_once(dirname(dirname(__FILE__)) . "/engine/start.php");
+
 set_context('search');
-$area2 = list_entities("","",0,10,false);
+$area2 = elgg_list_entities();
+
 set_context('entities');
-$body = elgg_view_layout('two_column_left_sidebar',$area1, $area2);
-page_draw("",$body);
+$body = elgg_view_layout('two_column_left_sidebar', $area1, $area2);
+
+page_draw("", $body);
