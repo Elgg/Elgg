757c0938cd7549169e01304f0406b9e567e86d1f
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-08-23 22:16:48 +0000

    Fixes #2412 - catch DB exception and create the group acl
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@6857 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/upgrades/2010062301.php
+++ engine/lib/upgrades/2010062301.php
@@ -13,8 +13,19 @@ if ($groups) {
 	foreach ($groups as $group) {
 		$acl = $group->group_acl;
 
-		$query = "UPDATE {$CONFIG->dbprefix}access_collections SET owner_guid = $group->guid WHERE id = $acl";
-		update_data($query);
+		try {
+			$query = "UPDATE {$CONFIG->dbprefix}access_collections SET owner_guid = $group->guid WHERE id = $acl";
+			update_data($query);
+		} catch (Exception $e) {
+			// no acl so create one
+			$ac_name = elgg_echo('groups:group') . ": " . $group->name;
+			$group_acl = create_access_collection($ac_name, $group->guid);
+			if ($group_acl) {
+				create_metadata($group->guid, 'group_acl', $group_acl, 'integer', $group->owner_guid);
+				$object->group_acl = $group_id;
+			}
+		}
+
 	}
 }
 elgg_set_ignore_access(FALSE);
