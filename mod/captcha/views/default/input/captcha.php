4bf0d12ffd08f5af82b037a42ba00cce4d5abfdb
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-08-25 12:02:07 +0000

    standardized captcha plugin
    git-svn-id: http://code.elgg.org/elgg/trunk@6864 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/captcha/views/default/input/captcha.php
+++ mod/captcha/views/default/input/captcha.php
@@ -1,23 +1,23 @@
 <?php
-	/**
-	 * Elgg captcha plugin captcha hook view override.
-	 * 
-	 * @package ElggCaptcha
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Curverider Ltd
-	 * @copyright Curverider Ltd 2008-2010
-	 * @link http://elgg.com/
-	 */
+/**
+ * Elgg captcha plugin captcha hook view override.
+ *
+ * @package ElggCaptcha
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Curverider Ltd
+ * @copyright Curverider Ltd 2008-2010
+ * @link http://elgg.com/
+ */
 
-	// Generate a token which is then passed into the captcha algorithm for verification
-	$token = captcha_generate_token();
+// Generate a token which is then passed into the captcha algorithm for verification
+$token = captcha_generate_token();
 ?>
 <div class="captcha">
 	<input type="hidden" name="captcha_token" value="<?php echo $token; ?>" />
 	<label>
 		<?php echo elgg_echo('captcha:entercaptcha'); ?>
 		<div class="captcha_image">
-			<img class="captcha-input-image" src="<?php echo $vars['url'] . "pg/captcha/$token"; ?>" />
+			<img class="captcha-input-image" alt="captcha" src="<?php echo $vars['url'] . "pg/captcha/$token"; ?>" />
 		</div>
 		<div class="captcha_input">
 			<?php echo elgg_view('input/text', array('internalname' => 'captcha_input')); ?>
