faa012606820529cc30e06abf3ece01815ca3066
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-08-31 11:03:52 +0000

    Fixes #2429 - profile only adds sidebar links when viewer has proper permissions
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@6889 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/start.php
+++ mod/profile/start.php
@@ -204,8 +204,10 @@
 			if (get_context() == "profile") {
 				$page_owner = page_owner_entity();
 
-				add_submenu_item(elgg_echo('profile:editdetails'), $CONFIG->wwwroot . "pg/profile/{$page_owner->username}/edit/");
-				add_submenu_item(elgg_echo('profile:editicon'), $CONFIG->wwwroot . "pg/profile/{$page_owner->username}/editicon/");
+				if ($page_owner->canEdit()) {
+					add_submenu_item(elgg_echo('profile:editdetails'), $CONFIG->wwwroot . "pg/profile/{$page_owner->username}/edit/");
+					add_submenu_item(elgg_echo('profile:editicon'), $CONFIG->wwwroot . "pg/profile/{$page_owner->username}/editicon/");
+				}
 			}
 		}
 
