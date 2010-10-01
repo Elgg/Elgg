f69dde39ee95d342de202fbbc81dcbb1f8fd0314
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-10-01 01:48:54 +0000

    Fixes #2499 - fixed resize of non square tall images and resizing of images not allowed to be upscaled
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@6990 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/profile/actions/iconupload.php
+++ mod/profile/actions/iconupload.php
@@ -24,10 +24,10 @@
 			) {
 				
 				
-				$topbar = get_resized_image_from_uploaded_file('profileicon',16,16, true);
-				$tiny = get_resized_image_from_uploaded_file('profileicon',25,25, true);
-				$small = get_resized_image_from_uploaded_file('profileicon',40,40, true);
-				$medium = get_resized_image_from_uploaded_file('profileicon',100,100, true);
+				$topbar = get_resized_image_from_uploaded_file('profileicon',16,16, true, true);
+				$tiny = get_resized_image_from_uploaded_file('profileicon',25,25, true, true);
+				$small = get_resized_image_from_uploaded_file('profileicon',40,40, true, true);
+				$medium = get_resized_image_from_uploaded_file('profileicon',100,100, true, true);
 				$large = get_resized_image_from_uploaded_file('profileicon',200,200);
 				$master = get_resized_image_from_uploaded_file('profileicon',550,550);
 				
