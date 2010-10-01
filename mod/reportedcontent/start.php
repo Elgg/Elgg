b700cf5f3d8b9f86a9b399946617657e727e06f0
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-10-01 18:51:46 +0000

    Fixed incorrect echoing of an array.
    git-svn-id: http://code.elgg.org/elgg/trunk@6995 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/reportedcontent/start.php
+++ mod/reportedcontent/start.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Elgg Reported content.
- * 
+ *
  * @package ElggReportedContent
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
  * @author Curverider Ltd
@@ -15,18 +15,18 @@
  */
 function reportedcontent_init() {
 	global $CONFIG;
-	
+
 	// Extend CSS
 	elgg_extend_view('css', 'reportedcontent/css');
-			
+
 	// Extend context menu and footer with report content link
 	if (isloggedin()) {
-	    elgg_extend_view('profile/menu/links', 'reportedcontent/user_report');
+		elgg_extend_view('profile/menu/links', 'reportedcontent/user_report');
 		elgg_extend_view('footer/links', 'reportedcontent/footer_link');
 	}
-	
+
 	elgg_add_admin_submenu_item('reportedcontent', elgg_echo('reportedcontent'), 'overview');
-	
+
 	//register action
 	register_action('reportedcontent/add', FALSE, "{$CONFIG->pluginspath}reportedcontent/actions/add.php");
 	register_action('reportedcontent/delete', FALSE, "{$CONFIG->pluginspath}reportedcontent/actions/delete.php");
--- mod/reportedcontent/views/default/admin/overview/reportedcontent.php
+++ mod/reportedcontent/views/default/admin/overview/reportedcontent.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Elgg Reported content
- * 
+ *
  * @package ElggReportedContent
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
  * @author Curverider Ltd
@@ -13,11 +13,10 @@ $title = elgg_view_title(elgg_echo('reportedcontent'));
 
 $reported = elgg_get_entities(array('types' => 'object', 'subtypes' => 'reported_content', 'limit' => 9999));
 $list = elgg_view("reportedcontent/listing", array('entity' => $reported));
-	
+
 // Display main admin menu
 $body = <<<__HTML
 $title
-$reported
 $list
 __HTML;
 
