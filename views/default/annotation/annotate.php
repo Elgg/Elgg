7bbb0ca2fb7e205074a9009f7604cb2c289cca73
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-10-20 23:33:54 +0000

    Merges [7038] into trunk from 1.7 branch
    git-svn-id: http://code.elgg.org/elgg/trunk@7111 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/annotation/annotate.php
+++ views/default/annotation/annotate.php
@@ -1,29 +1,31 @@
 <?php
 
-	$performed_by = get_entity($vars['item']->subject_guid);
-	$object = get_entity($vars['item']->object_guid);
-	$url = $object->getURL();
-	$title = $object->title;
-	if(!$title)
-		$title = elgg_echo('file:untitled');
-	$subtype = get_subtype_from_id($object->subtype);
-	//grab the annotation, if one exists
-	if($vars['item']->annotation_id != 0)
-		$comment = get_annotation($vars['item']->annotation_id)->value; 
-	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
-	$string = sprintf(elgg_echo("river:posted:generic"),$url) . " ";
-	$string .= elgg_echo("{$subtype}:river:annotate") . "  <a href=\"{$object->getURL()}\">" . $title . "</a> <span class='entity_subtext'>". elgg_view_friendly_time($object->time_created) ."<a class='river_comment_form_button link'>Comment</a>";
-	$string .= elgg_view('likes/forms/link', array('entity' => $object));
-	$string .= "</span>";
-	if(get_context() != 'riverdashboard'){
+$performed_by = get_entity($vars['item']->subject_guid);
+$object = get_entity($vars['item']->object_guid);
+$url = $object->getURL();
+$title = $object->title;
+if (!$title) {
+	$title = elgg_echo('file:untitled');
+}
+
+$subtype = get_subtype_from_id($object->subtype);
+//grab the annotation, if one exists
+$comment = '';
+if ($vars['item']->annotation_id != 0) {
+	$comment = get_annotation($vars['item']->annotation_id)->value;
+}
+
+$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
+$string = sprintf(elgg_echo("river:posted:generic"), $url) . " ";
+$string .= elgg_echo("{$subtype}:river:annotate") . "  <a href=\"{$object->getURL()}\">" . $title . "</a> <span class='entity_subtext'>" . elgg_view_friendly_time($object->time_created) . "<a class='river_comment_form_button link'>Comment</a>";
+$string .= elgg_view('likes/forms/link', array('entity' => $object));
+$string .= "</span>";
+if (get_context() != 'riverdashboard') {
+	$comment = elgg_get_excerpt($comment, 200);
+	if ($comment) {
 		$string .= "<div class='river_content_display'>";
-		if($comment){
-			$contents = strip_tags($comment);//this is so we don't get large images etc in the activity river
-			if(strlen($contents) > 200)
-	        		$string .= substr($contents, 0, strpos($contents, ' ', 200)) . "&hellip;";
-	    		else
-		    		$string .= $contents;
-       	}
+		$string .= $comment;
 		$string .= "</div>";
 	}
+}
 echo $string;
