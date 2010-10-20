a30a5dd649422c00204f0c1da33ac2b87cdffc0d
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-10-20 11:41:23 +0000

    Refs #2498 - merged [6982] into trunk from 1.7 branch
    git-svn-id: http://code.elgg.org/elgg/trunk@7105 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/river/forum/create.php
+++ mod/groups/views/default/river/forum/create.php
@@ -3,24 +3,27 @@
  * Any comment on original post
  */
 
-	$performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
+	$performed_by = get_entity($vars['item']->subject_guid);
 	$object = get_entity($vars['item']->object_guid);
 	$object_url = $object->getURL();
+
 	$forumtopic = $object->guid;
 	$group_guid = $object->container_guid;
 	//grab the annotation, if one exists
 	if($vars['item']->annotation_id != 0) {
 		$comment = get_annotation($vars['item']->annotation_id)->value; 
 	}
-	$contents = strip_tags($contents);//this is so we don't get large images etc in the activity river
+	$comment = strip_tags($comment);//this is so we don't get large images etc in the activity river
 	$url = $vars['url'] . "mod/groups/topicposts.php?topic=" . $forumtopic . "&group_guid=" . $group_guid;
 	$url_user = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 	$string = sprintf(elgg_echo("groupforum:river:posted"),$url_user) . " ";
 	$string .= elgg_echo("groupforum:river:annotate:create") . " | <a href=\"" . $url . "\">" . $object->title . "</a> <span class='entity_subtext'>". elgg_view_friendly_time($object->time_created) ."<a class='river_comment_form_button link' href=\"{$object_url}\">Visit discussion</a>";
 	$string .= elgg_view('likes/forms/link', array('entity' => $object));
 	$string .= "</span>";
-	$string .= "<div class=\"river_content_display\">";
-	$string .= elgg_get_excerpt($contents, 200);
-	$string .= "</div>";
+	if ($comment) {
+		$string .= "<div class=\"river_content_display\">";
+		$string .= elgg_get_excerpt($comment, 200);
+		$string .= "</div>";
+	}
 
 	echo $string;
