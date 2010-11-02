c554c1b3f81011723b56b93f9fe19122a6fdba70
ewinslow <ewinslow@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-11-02 21:40:07 +0000

    Corrected a couple typos in the documentation that are being registered as php errors
    git-svn-id: http://code.elgg.org/elgg/trunk@7206 36083f99-b078-4883-b0ff-0f9b5a30f544

--- documentation/examples/hooks/register/basic.php
+++ documentation/examples/hooks/register/basic.php
@@ -6,7 +6,7 @@ function example_plugin_hook_handler($event, $type, $value, $params) {
 	var_dump($event);
 	var_dump($type);
 	var_dump($value);
-	var_dump($params):
+	var_dump($params);
 	
 	return true;
 }
