08a56c121cca9b6df1d027d203046c9051f9ae35
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-11-03 22:46:28 +0000

    Fixed missing ).
    git-svn-id: http://code.elgg.org/elgg/trunk@7231 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/friends/river/create.php
+++ views/default/friends/river/create.php
@@ -10,7 +10,7 @@ $performed_by = get_entity($vars['item']->subject_guid);
 $performed_on = get_entity($vars['item']->object_guid);
 
 $url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
-$string = elgg_echo("friends:river:add", array($url) . " ";
+$string = elgg_echo("friends:river:add", array($url)) . " ";
 $string .= "<a href=\"{$performed_on->getURL()}\">{$performed_on->name}</a>";
 $string .= "<div class=\"river_content_display\">";
 $string .= "<table><tr><td>" . elgg_view("profile/icon",array('entity' => $performed_by, 'size' => 'tiny')) . "</td>";
