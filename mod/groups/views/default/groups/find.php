d7df2a2b0e506b33d3c9b4c8c4013c5a5b2a73ce
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-11-03 17:10:27 +0000

    Refs #2582. Merged [7224] to trunk.
    git-svn-id: http://code.elgg.org/elgg/trunk@7225 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/groups/find.php
+++ mod/groups/views/default/groups/find.php
@@ -10,5 +10,5 @@ $tag_string = elgg_echo('groups:search:tags');
 <h3><?php echo elgg_echo('groups:searchtag'); ?></h3>
 <form id="groupsearchform" action="<?php echo elgg_get_site_url(); ?>pg/groups/world/" method="get">
 	<input type="text" name="tag" value="<?php echo $tag_string; ?>" onclick="if (this.value=='<?php echo $tag_string; ?>') { this.value='' }" class="search_input" />
-	<input type="submit" value="<?php echo elgg_echo('go'); ?>" />
+	<input type="submit" value="<?php echo elgg_echo('search:go'); ?>" />
 </form>
