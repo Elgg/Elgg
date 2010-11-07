64b098604bcbaa5b57d06f2b254f809a3af82e1d
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-11-07 13:37:04 +0000

    Refs #2630 invite friends plugin in 1.7 branch now uses page handler
    git-svn-id: http://code.elgg.org/elgg/branches/1.7@7255 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/invitefriends/start.php
+++ mod/invitefriends/start.php
@@ -10,19 +10,38 @@
  * @link http://elgg.org/
  */
 
+register_elgg_event_handler('init', 'system', 'invitefriends_init');
+
+/**
+ * Invite friends initialization
+ */
+function invitefriends_init() {
+	global $CONFIG;
+
+	register_page_handler('invitefriends', 'invitefriends_page_handler');
+
+	register_elgg_event_handler('pagesetup', 'system', 'invitefriends_menu_setup');
+
+	$action_base = $CONFIG->pluginspath . 'invitefriends/actions';
+	register_action('invitefriends/invite', false, "$action_base/invite.php");
+}
+
+/**
+ * Load the invite friends page
+ */
+function invitefriends_page_handler() {
+	global $CONFIG;
+	require "{$CONFIG->pluginspath}invitefriends/index.php";
+}
+
 /**
  * Add menu item for invite friends
  */
-function invitefriends_pagesetup() {
-
+function invitefriends_menu_setup() {
 	global $CONFIG;
-	if (get_context() == "friends" ||
-		get_context() == "friendsof" ||
-		get_context() == "collections") {
-
-			add_submenu_item(elgg_echo('friends:invite'),$CONFIG->wwwroot."mod/invitefriends/",'invite');
+	$context = get_context();
+	if ($context == "friends" || $context == "friendsof" || $context == "collections") {
+		$url = "{$CONFIG->wwwroot}pg/invitefriends/";
+		add_submenu_item(elgg_echo('friends:invite'), $url, 'invite');
 	}
 }
-
-register_action('invitefriends/invite', false, $CONFIG->pluginspath . 'invitefriends/actions/invite.php');
-register_elgg_event_handler('pagesetup', 'system', 'invitefriends_pagesetup');
