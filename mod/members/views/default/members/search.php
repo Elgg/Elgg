18ed4a4d43498fdf4d6e87674e3adcfff0ea60a4
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-11-21 20:44:56 +0000

    Refs #2612: Merged r7402 to trunk.
    git-svn-id: http://code.elgg.org/elgg/trunk@7403 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/members/views/default/members/search.php
+++ mod/members/views/default/members/search.php
@@ -11,7 +11,7 @@
 <form id="memberssearchform" class="member_tags" action="<?php echo elgg_get_site_url(); ?>mod/members/index.php?" method="get">
 	<input type="text" name="tag" value="Member tags" onclick="if (this.value=='Member tags') { this.value='' }" class="search_input" />
 	<input type="hidden" name="filter" value="search_tags" />	
-	<input type="submit" value="<?php echo elgg_echo('go'); ?>" />
+	<input type="submit" value="<?php echo elgg_echo('search:go'); ?>" />
 </form>
 
 <h3><?php echo elgg_echo('members:searchname'); ?></h3>
@@ -20,7 +20,7 @@
 	<input type="hidden" name="subtype" value="" />
 	<input type="hidden" name="object" value="user" />
 	<input type="hidden" name="filter" value="search" />	
-	<input type="submit" value="<?php echo elgg_echo('go'); ?>" />
+	<input type="submit" value="<?php echo elgg_echo('search:go'); ?>" />
 </form>
 
 </div>
