cda80c696522c713d4e802f5aaa5c91b82f05353
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-11-28 13:41:56 +0000

    moved rest/rpc web services handler into web_services library
    git-svn-id: http://code.elgg.org/elgg/trunk@7460 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/web_services.php
+++ engine/lib/web_services.php
@@ -1342,8 +1342,6 @@ function unregister_service_handler($handler) {
 	}
 }
 
-// REST handler
-
 /**
  * REST API handler
  *
@@ -1352,10 +1350,52 @@ function unregister_service_handler($handler) {
 function rest_handler() {
 	global $CONFIG;
 
-	require $CONFIG->path . "services/api/rest_api.php";
+	// Register the error handler
+	error_reporting(E_ALL);
+	set_error_handler('_php_api_error_handler');
+
+	// Register a default exception handler
+	set_exception_handler('_php_api_exception_handler');
+
+	// Check to see if the api is available
+	if ((isset($CONFIG->disable_api)) && ($CONFIG->disable_api == true)) {
+		throw new SecurityException(elgg_echo('SecurityException:APIAccessDenied'));
+	}
+
+	// plugins should return true to control what API and user authentication handlers are registered
+	if (elgg_trigger_plugin_hook('rest', 'init', null, false) == false) {
+		// for testing from a web browser, you can use the session PAM
+		// do not use for production sites!!
+		//register_pam_handler('pam_auth_session');
+
+		// user token can also be used for user authentication
+		register_pam_handler('pam_auth_usertoken');
+
+		// simple API key check
+		register_pam_handler('api_auth_key', "sufficient", "api");
+		// hmac
+		register_pam_handler('api_auth_hmac', "sufficient", "api");
+	}
+
+	// Get parameter variables
+	$method = get_input('method');
+	$result = null;
+
+	// this will throw an exception if authentication fails
+	authenticate_method($method);
+
+	$result = execute_method($method);
+
+
+	if (!($result instanceof GenericResult)) {
+		throw new APIException(elgg_echo('APIException:ApiResultUnknown'));
+	}
+
+	// Output the result
+	echo elgg_view_page($method, elgg_view("api/output", array("result" => $result)));
 }
 
-// Initialisation
+// Initialization
 
 /**
  * Unit tests for API
