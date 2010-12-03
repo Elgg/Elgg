c0641e3dccefd19676f91560b96054eae3e9ac8f
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-12-03 17:04:14 +0000

    Refs #2706. Added configuration.php to installation engine bootstrapping.  Required because of use of elgg_get_site_url().
    git-svn-id: http://code.elgg.org/elgg/trunk@7518 36083f99-b078-4883-b0ff-0f9b5a30f544

--- install/ElggInstaller.php
+++ install/ElggInstaller.php
@@ -686,7 +686,7 @@ class ElggInstaller {
 
 		// bootstrapping with required files in a required order
 		$required_files = array(
-			'elgglib.php', 'views.php', 'access.php', 'system_log.php', 'export.php',
+			'elgglib.php', 'views.php', 'access.php', 'system_log.php', 'export.php', 'configuration.php',
 			'sessions.php', 'languages.php', 'input.php', 'install.php', 'cache.php', 'output.php'
 		);
 
