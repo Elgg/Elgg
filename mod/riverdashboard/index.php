1197b7ea0a4f4a3432ebdabd7a2a32e499858400
Hellekin O. Wolf =8) <hellekin@cepheide.org>
2010-12-04 07:51:52 +0100

    Fix typo in riverdashboard

--- mod/riverdashboard/index.php
+++ mod/riverdashboard/index.php
@@ -41,7 +41,7 @@ switch($orient) {
 
 $title .= elgg_view_title($title_wording);
 $extend = elgg_view("activity/extend");
-$river = elgg_view_river-items($subject_guid, 0, $relationship_type, $type, $subtype, '', 20, 0, 0, TRUE, FALSE);
+$river = elgg_view_river_items($subject_guid, 0, $relationship_type, $type, $subtype, '', 20, 0, 0, TRUE, FALSE);
 
 // Replacing callback calls in the nav with something meaningless
 $river = str_replace('callback=true', 'replaced=88,334', $river);
