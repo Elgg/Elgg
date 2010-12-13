4cc94c22cf097a0234e7cdf808224cab7f3caf44
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-12-13 16:17:57 +0000

    front page code does not depend on riverdashboard plugin anymore
    git-svn-id: http://code.elgg.org/elgg/trunk@7613 36083f99-b078-4883-b0ff-0f9b5a30f544

--- index.php
+++ index.php
@@ -11,52 +11,39 @@
  */
 require_once(dirname(__FILE__) . "/engine/start.php");
 
-if (!elgg_trigger_plugin_hook('index', 'system', null, FALSE)) {
-	if (isloggedin()) {
-		forward('pg/dashboard/');
-	}
+// allow plugins to override the front page (return true to stop this front page code)
+if (elgg_trigger_plugin_hook('index', 'system', null, FALSE) != FALSE) {
+	exit;
+}
 
-	/*
-	River dashboard should respond to the index:system plugin hook instead of
-	being hard-coded here.
-	if(is_plugin_enabled('riverdashboard')){
-		$title = elgg_view_title(elgg_echo('content:latest'));
-		elgg_set_context('search');
-		$content = elgg_list_registered_entities(array('limit' => 10, '
-			full_view' => FALSE, 'allowed_types' => array('object','group')));
-		elgg_set_context('main');
-	}
-	*/
+if (isloggedin()) {
+	forward('pg/activity/');
+}
 
-	//Load the front page
-	$title = elgg_view_title(elgg_echo('content:latest'));
-	elgg_set_context('search');
-	$offset = (int)get_input('offset', 0);
-	if (is_plugin_enabled('riverdashboard')) {
-		$activity = elgg_view_river_items(0, 0, '', '', '', '', 10, 0, 0, true, false);
-	} else {
-		$options = array(
-			'limit' => 10,
-			'offset' => $offset,
-			'full_view' => FALSE,
-			'allowed_types' => array('object','group')
-		);
+//Load the front page
+$title = elgg_view_title(elgg_echo('content:latest'));
+elgg_set_context('search');
+$offset = (int)get_input('offset', 0);
+$options = array(
+		'limit' => 10,
+		'offset' => $offset,
+		'full_view' => FALSE,
+		'allowed_types' => array('object','group')
+);
 
-		$activity = elgg_list_registered_entities($options);
-	}
-	elgg_set_context('main');
+$activity = elgg_list_registered_entities($options);
+elgg_set_context('main');
 
-	global $autofeed;
-	$autofeed = FALSE;
+global $autofeed;
+$autofeed = FALSE;
 
-	// if drop-down login in header option not selected
-	$login_box = elgg_view('account/login_box');
+// if drop-down login in header option not selected
+$login_box = elgg_view('account/login_box');
 
-	$content = $title . $activity;
-	$params = array(
+$content = $title . $activity;
+$params = array(
 		'content' => $content,
 		'sidebar' => $login_box
-	);
-	$body = elgg_view_layout('one_column_with_sidebar', $params);
-	echo elgg_view_page(null, $body);
-}
+);
+$body = elgg_view_layout('one_column_with_sidebar', $params);
+echo elgg_view_page(null, $body);
