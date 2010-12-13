e52e501e8ae21be42652b529915e22fadd8ca9e2
Hellekin O. Wolf =8) <hellekin@cepheide.org>
2010-12-13 05:24:44 +0100

    Removes all remaining deprecation warnings

--- mod/diagnostics/start.php
+++ mod/diagnostics/start.php
@@ -28,7 +28,7 @@ function diagnostics_pagesetup()
 {
 	if (elgg_get_context() == 'admin' && isadminloggedin()) {
 		global $CONFIG;
-		add_submenu_item(elgg_echo('diagnostics'), 'pg/diagnostics/');
+		elgg_add_submenu_item(array('text' => elgg_echo('diagnostics'), 'href' => 'pg/diagnostics/'));
 	}
 }
 
