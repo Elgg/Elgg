fa2c88ca281a9d661b2f8121a56a64d3ce78784a
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-12-15 20:22:27 +0000

    Ignore SSL certificate errors when testing URL rewriting during installation (from hellekin)
    git-svn-id: http://code.elgg.org/elgg/trunk@7634 36083f99-b078-4883-b0ff-0f9b5a30f544

--- install/ElggRewriteTester.php
+++ install/ElggRewriteTester.php
@@ -78,6 +78,7 @@ class ElggRewriteTester {
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			$response = curl_exec($ch);
 			curl_close($ch);
 			return $response === 'success';
