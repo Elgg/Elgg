59cb58582f64b4f239da53ede602ad2c82f02077
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-12-29 13:37:26 +0000

    forgot to add this file for the fie plugin
    git-svn-id: http://code.elgg.org/elgg/trunk@7739 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/file/view.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * View a file
+ *
+ * @package ElggFile
+ */
+
+$file = get_entity(get_input('guid'));
+
+elgg_set_page_owner_guid($file->getContainerGUID());
+$owner = elgg_get_page_owner();
+
+elgg_push_breadcrumb(elgg_echo('file'), 'pg/file/all');
+
+$crumbs_title = elgg_echo('blog:owned_blogs', array($owner->name));
+if (elgg_instanceof($owner, 'group')) {
+	elgg_push_breadcrumb($crumbs_title, "pg/file/group/$owner->guid/owner");
+} else {
+	elgg_push_breadcrumb($crumbs_title, "pg/file/owner/$owner->username");
+}
+
+$title = $file->title;
+
+elgg_push_breadcrumb($title);
+
+$content = elgg_view_entity($file, true);
+//$content .= elgg_view_comments($file);
+
+$body = elgg_view_layout('content', array(
+	'content' => $content,
+	'title' => $title,
+	'filter' => '',
+	'header' => '',
+));
+
+echo elgg_view_page($title, $body);
