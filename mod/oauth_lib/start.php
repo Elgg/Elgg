9e2b31bc71a0272e6a1a91975995e8bca95356ea
Hellekin O. Wolf =8) <hellekin@cepheide.org>
2011-01-03 15:05:52 +0100

    Replace deprecated call to $CONFIG, Closes #2764

--- mod/oauth_lib/start.php
+++ mod/oauth_lib/start.php
@@ -3,18 +3,20 @@
  * 
  */
 
+$oauth_lib = elgg_get_plugin_path() . 'oauth_lib/vendors/oauth/library/';
+
 // require all vendor libraries
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/OAuthDiscovery.php";
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/OAuthRequest.php";
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/OAuthRequester.php";
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/OAuthRequestVerifier.php";
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/OAuthServer.php";
+require_once "{$oauth_lib}OAuthDiscovery.php";
+require_once "{$oauth_lib}OAuthRequest.php";
+require_once "{$oauth_lib}OAuthRequester.php";
+require_once "{$oauth_lib}OAuthRequestVerifier.php";
+require_once "{$oauth_lib}OAuthServer.php";
 
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/body/OAuthBodyMultipartFormdata.php";
+require_once "{$oauth_lib}body/OAuthBodyMultipartFormdata.php";
 
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/store/OAuthStoreAbstract.class.php";
+require_once "{$oauth_lib}store/OAuthStoreAbstract.class.php";
 
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/signature_method/OAuthSignatureMethod_HMAC_SHA1.php";
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/signature_method/OAuthSignatureMethod_MD5.php";
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/signature_method/OAuthSignatureMethod_PLAINTEXT.php";
-require_once "{$CONFIG->pluginspath}oauth_lib/vendors/oauth/library/signature_method/OAuthSignatureMethod_RSA_SHA1.php";
+require_once "{$oauth_lib}signature_method/OAuthSignatureMethod_HMAC_SHA1.php";
+require_once "{$oauth_lib}signature_method/OAuthSignatureMethod_MD5.php";
+require_once "{$oauth_lib}signature_method/OAuthSignatureMethod_PLAINTEXT.php";
+require_once "{$oauth_lib}signature_method/OAuthSignatureMethod_RSA_SHA1.php";
