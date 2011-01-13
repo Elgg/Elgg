279ee839d267589bfef846bcc49377dd0b593505
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-01-13 02:17:19 +0000

    Refs #2791 merged fix into trunk
    git-svn-id: http://code.elgg.org/elgg/trunk@7871 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/twitter/languages/en.php
+++ mod/twitter/languages/en.php
@@ -1,30 +1,16 @@
 <?php
+/**
+ * Twitter widget language file
+ */
 
-	$english = array(
-	
-		/**
-		 * twitter widget details
-		 */
-		
-	
-		'twitter:username' => 'Enter your twitter username.',
-		'twitter:num' => 'The number of tweets to show.',
-		'twitter:visit' => 'visit my twitter',
-		'twitter:notset' => 'This Twitter widget is not yet set to go. To display your latest tweets, click on - edit - and fill in your details',
-		
-		
-		 /**
-	     * twitter widget river
-	     **/
-	        
-	        //generic terms to use
-	        'twitter:river:created' => "%s added the twitter widget.",
-	        'twitter:river:updated' => "%s updated their twitter widget.",
-	        'twitter:river:delete' => "%s removed their twitter widget.",
-	        
-		
-	);
-					
-	add_translation("en",$english);
+$english = array(
 
-?>
+	'twitter:title' => 'Twitter',
+	'twitter:info' => 'Display your latest tweets',
+	'twitter:username' => 'Enter your twitter username.',
+	'twitter:num' => 'The number of tweets to show.',
+	'twitter:visit' => 'visit my twitter',
+	'twitter:notset' => 'This Twitter widget is not yet set to go. To display your latest tweets, click on - edit - and fill in your details',
+);
+
+add_translation("en", $english);
--- mod/twitter/start.php
+++ mod/twitter/start.php
@@ -1,22 +1,14 @@
 <?php
+/**
+ * Elgg twitter widget
+ * This plugin allows users to pull in their twitter feed to display on their profile
+ *
+ * @package ElggTwitter
+ */
 
-	/**
-	 * Elgg twitter widget
-	 * This plugin allows users to pull in their twitter feed to display on their profile
-	 * 
-	 * @package ElggTwitter
-	 */
-	
-		function twitter_init() {
-    		
-    		//extend css if style is required
-    		    elgg_extend_view('css/screen', 'twitter/css');
-    		
-    		//add a widget
-			    elgg_register_widget_type('twitter',"Twitter","This is your twitter feed");
-			
-		}
-		
-		elgg_register_event_handler('init','system','twitter_init');
+register_elgg_event_handler('init', 'system', 'twitter_init');
 
-?>
+function twitter_init() {
+	elgg_extend_view('css/screen', 'twitter/css');
+	add_widget_type('twitter', elgg_echo('twitter:title'), elgg_echo('twitter:info'));
+}
