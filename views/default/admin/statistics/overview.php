1f3149f54dd9ec94ff074dc9a0d2acaa732be7f6
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-01-26 21:53:18 +0000

    updated the overview statistics page
    git-svn-id: http://code.elgg.org/elgg/trunk@7944 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/admin/statistics/overview.php
+++ views/default/admin/statistics/overview.php
@@ -14,33 +14,39 @@ $total_users = get_number_users(true);
 $version = get_version();
 $release = get_version(true);
 
-?>
-<div class="admin_settings site_stats">
-	<?php echo elgg_view('overview/extend'); ?>
-	<h3><?php echo elgg_echo('admin:statistics:label:basic'); ?></h3>
-	<table class="styled">
-		<tr class="odd">
-			<td class="column-one"><b><?php echo elgg_echo('admin:statistics:label:version'); ?> :</b></td>
-			<td><?php echo elgg_echo('admin:statistics:label:version:release'); ?> - <?php echo $release; ?>, <?php echo elgg_echo('admin:statistics:label:version:version'); ?> - <?php echo $version; ?></td>
-		</tr>
-		<tr class="even">
-			<td class="column-one"><b><?php echo elgg_echo('admin:statistics:label:numusers'); ?> :</b></td>
-			<td><?php echo $users_stats; ?> <?php echo elgg_echo('active'); ?> / <?php echo $total_users; ?> <?php echo elgg_echo('total') ?></td>
-		</tr>
+echo elgg_view('admin/statistics/extend');
 
-	</table>
+?>
+<div class="elgg-module elgg-inline-module">
+	<div class="elgg-head">
+		<h3><?php echo elgg_echo('admin:statistics:label:basic'); ?></h3>
+	</div>
+	<div class="elgg-body">
+		<table class="elgg-table-alt">
+			<tr class="odd">
+				<td><b><?php echo elgg_echo('admin:statistics:label:version'); ?> :</b></td>
+				<td><?php echo elgg_echo('admin:statistics:label:version:release'); ?> - <?php echo $release; ?>, <?php echo elgg_echo('admin:statistics:label:version:version'); ?> - <?php echo $version; ?></td>
+			</tr>
+			<tr class="even">
+				<td><b><?php echo elgg_echo('admin:statistics:label:numusers'); ?> :</b></td>
+				<td><?php echo $users_stats; ?> <?php echo elgg_echo('active'); ?> / <?php echo $total_users; ?> <?php echo elgg_echo('total') ?></td>
+			</tr>
+		</table>
+	</div>
 </div>
 
 <?php
-
 
 // Get entity statistics
 $entity_stats = get_entity_statistics();
 $even_odd = "";
 ?>
-<div class="admin_settings site_entities">
-	<h3><?php echo elgg_echo('admin:statistics:label:numentities'); ?></h3>
-	<table class="styled">
+<div class="elgg-module elgg-inline-module">
+	<div class="elgg-head">
+		<h3><?php echo elgg_echo('admin:statistics:label:numentities'); ?></h3>
+	</div>
+	<div class="elgg-body">
+		<table class="elgg-table-alt">
 		<?php
 			foreach ($entity_stats as $k => $entry) {
 				arsort($entry);
@@ -66,12 +72,13 @@ $even_odd = "";
 						}
 					echo <<< END
 						<tr class="{$even_odd}">
-							<td class="column-one">{$a}:</td>
+							<td>{$a}:</td>
 							<td>{$b}</td>
 						</tr>
 END;
 				}
 			}
 		?>
-	</table>
+		</table>
+	</div>
 </div>
