84dcdeb236487a52486e0bd9234b214f2b64dd3c
ewinslow <ewinslow@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-11 00:04:03 +0000

    Removed unused view: output/rss_view
    git-svn-id: http://code.elgg.org/elgg/trunk@8103 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/output/rss_view.php
+++
@@ -1,7 +0,0 @@
-<?php
-	/**
-	 * View to display the RSS link
-	 * @todo check this - it doesn't appear this view is ever called
-	 **/
-?>
-<div class="rss-link clearfix"><a href="" title="RSS feed for the wire">RSS feed for the wire</a></div>
