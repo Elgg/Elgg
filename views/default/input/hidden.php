4959fa0e2c17dd4060db0736b2acf96ac2073710
ewinslow <ewinslow@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-12 06:26:15 +0000

    Refs #2143: DRY up input/hidden
    git-svn-id: http://code.elgg.org/elgg/trunk@8143 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/input/hidden.php
+++ views/default/input/hidden.php
@@ -1,17 +1,12 @@
 <?php
 /**
  * Create a hidden data field
- * Use this view for forms rather than creating a hidden tag in the wild as it provides
- * extra security which help prevent CSRF attacks.
  *
  * @package Elgg
  * @subpackage Core
  *
  * @uses $vars['value'] The current value, if any
- * @uses $vars['js'] Any Javascript to enter into the input tag
- * @uses $vars['internalname'] The name of the input field
- *
  */
 
 ?>
-<input type="hidden" <?php echo $vars['js']; ?> name="<?php echo $vars['internalname']; ?>" <?php if (isset($vars['internalid'])) echo "id=\"{$vars['internalid']}\""; ?> value="<?php echo htmlentities($vars['value'], ENT_QUOTES, 'UTF-8'); ?>" />
+<input type="hidden" <?php echo elgg_format_attributes($vars); ?> />
