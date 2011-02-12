3893e5f4b3a30f39c43ab17179611004b6184689
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-12 15:50:11 +0000

    fixed another bug from last night
    git-svn-id: http://code.elgg.org/elgg/trunk@8159 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/input/access.php
+++ views/default/input/access.php
@@ -5,24 +5,25 @@
  *
  * @package Elgg
  * @subpackage Core
-
-
  *
  * @uses $vars['value'] The current value, if any
- * @uses $vars['js'] Any Javascript to enter into the input tag
+ * @uses $vars['options_values']
  * @uses $vars['internalname'] The name of the input field
- *
  */
 
 $defaults = array(
 	'class' => 'elgg-input-access',
 	'disabled' => FALSE,
 	'value' => get_default_access(),
-	'options' => get_write_access_array(),
+	'options_values' => get_write_access_array(),
 );
 
 $vars = array_merge($defaults, $vars);
 
-if (is_array($vars['options']) && sizeof($vars['options']) > 0) {
+if ($vars['value'] == ACCESS_DEFAULT) {
+	$vars['value'] = get_default_access();
+}
+
+if (is_array($vars['options_values']) && sizeof($vars['options_values']) > 0) {
 	echo elgg_view('input/dropdown', $vars);
 }
