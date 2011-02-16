8e28d88c621a600b144f028ea9cbf28023b69745
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-16 22:15:01 +0000

    Fixes #2915. Can edit widget settings again.
    git-svn-id: http://code.elgg.org/elgg/trunk@8265 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/layout/objects/widget/settings.php
+++ views/default/layout/objects/widget/settings.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 /**
- * 
+ * Elgg widget edit settings
+ *
+ * @uses $vars['widget']
  */
 
+$widget = elgg_extract('widget', $vars);
 ?>
+
 <div class="elgg-widget-edit" id="elgg-togglee-widget-<?php echo $widget->guid; ?>">
-
 	<?php echo elgg_view_form('widgets/save', array(), $vars); ?>
-
 </div>
