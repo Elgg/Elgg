49771e949c286e435380b55a2e2cac18b9021507
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-17 03:55:36 +0000

    Fixed typo in filter nav view.
    git-svn-id: http://code.elgg.org/elgg/trunk@8270 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/layout/shells/content/filter.php
+++ views/default/layout/shells/content/filter.php
@@ -18,7 +18,7 @@ $context = elgg_extract('context', $vars, elgg_get_context());
 
 if (elgg_is_logged_in() && $context) {
 	$username = elgg_get_logged_in_user_entity()->username;
-	$filter_context = elgg_extract('filter_context', $vars, 'everyone');
+	$filter_context = elgg_extract('filter_context', $vars, 'all');
 
 	// generate a list of default tabs
 	$tabs = array(
