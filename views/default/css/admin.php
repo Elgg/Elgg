bf45a375767dff6c02abdce370426e922ef4054e
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-19 21:05:35 +0000

    Copied all the icons into the admin theme.
    git-svn-id: http://code.elgg.org/elgg/trunk@8348 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/css/admin.php
+++ views/default/css/admin.php
@@ -258,7 +258,7 @@ table.mceLayout {
 	text-decoration: none;
 	margin-bottom: 2px;
 	border: 1px solid #CCC;
-	
+
 	-webkit-border-radius: 5px;
 	-moz-border-radius: 5px;
 	border-radius: 5px;
@@ -1129,8 +1129,9 @@ ul.admin_plugins {
 }
 
 /* ***************************************
-	Icons
+	ICONS
 *************************************** */
+
 .elgg-icon {
 	background: transparent url(<?php echo elgg_get_site_url(); ?>_graphics/elgg_sprites.png) no-repeat left;
 	width: 16px;
@@ -1196,6 +1197,68 @@ ul.admin_plugins {
 	width: 21px;
 	height: 21px;
 }
+.elgg-avatar > .elgg-icon-hover-menu {
+	display: none;
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	margin: 0;
+	cursor: pointer;
+}
+
+.elgg-ajax-loader {
+	background-color: white;
+	background-image: url(<?php echo elgg_get_site_url(); ?>_graphics/ajax_loader_bw.gif);
+	background-repeat: no-repeat;
+	background-position: center center;
+	min-height: 33px;
+	min-width: 33px;
+}
+
+/* ***************************************
+	AVATAR ICONS
+*************************************** */
+.elgg-avatar {
+	position: relative;
+}
+.elgg-avatar > a > img {
+	display: block;
+}
+.elgg-avatar-tiny > a > img {
+	width: 25px;
+	height: 25px;
+	/* remove the border-radius if you don't want rounded avatars in supported browsers */
+	-webkit-border-radius: 3px;
+	-moz-border-radius: 3px;
+	-moz-background-clip:  border;
+
+	-o-background-size: 25px;
+	-webkit-background-size: 25px;
+	-khtml-background-size: 25px;
+	-moz-background-size: 25px;
+}
+.elgg-avatar-small > a > img {
+	width: 40px;
+	height: 40px;
+	/* remove the border-radius if you don't want rounded avatars in supported browsers */
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	-moz-background-clip:  border;
+
+	-o-background-size: 40px;
+	-webkit-background-size: 40px;
+	-khtml-background-size: 40px;
+	-moz-background-size: 40px;
+}
+.elgg-avatar-medium > a > img {
+	width: 100px;
+	height: 100px;
+}
+.elgg-avatar-large > a > img {
+	width: 200px;
+	height: 200px;
+}
+
 
 .elgg-list-metadata {
 	list-style-type: none;
