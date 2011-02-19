3741b9a4eb8a91bd5e1a3bf9979d35c5826628cf
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-19 03:08:32 +0000

    missed this is a commit last week on external pages
    git-svn-id: http://code.elgg.org/elgg/trunk@8306 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ mod/externalpages/views/default/admin/site/expages.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Admin section for editing external pages
+ */
+
+$type = get_input('type', 'about');
+
+echo elgg_view('expages/menu', array('type' => $type));
+
+echo elgg_view_form('expages/edit', array(), array('type' => $type));
