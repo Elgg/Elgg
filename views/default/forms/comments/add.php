489ef6e9ba4faeb1bf4e343a867c4208f0ffc9a6
ewinslow <ewinslow@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-22 00:43:10 +0000

    merged comments/inline and comments/add forms
    git-svn-id: http://code.elgg.org/elgg/trunk@8398 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/forms/comments/add.php
+++ views/default/forms/comments/add.php
@@ -7,17 +7,29 @@
  * @uses $vars['entity']
  */
 
-if (isset($vars['entity']) && elgg_is_logged_in()) {
-?>
-<div class="mbn">
-	<label><?php echo elgg_echo("generic_comments:add"); ?></label>
-	<?php echo elgg_view('input/longtext', array('name' => 'generic_comment')); ?>
-</div>
-<?php
 
-	echo elgg_view('input/hidden', array(
-		'name' => 'entity_guid',
-		'value' => $vars['entity']->getGUID()
-	));
-	echo elgg_view('input/submit', array('value' => elgg_echo("generic_comments:post")));
+if (isset($vars['entity']) && elgg_is_logged_in()) {
+	
+	$inline = elgg_extract('inline', $vars, false);
+	
+	if ($inline) {
+		echo elgg_view('input/text', array('name' => 'generic_comment'));
+		echo elgg_view('input/hidden', array(
+			'name' => 'entity_guid',
+			'value' => $vars['entity']->getGUID()
+		));
+		echo elgg_view('input/submit', array('value' => elgg_echo('comment')));
+	} else {
+?>
+		<div class="mbn">
+			<label><?php echo elgg_echo("generic_comments:add"); ?></label>
+			<?php echo elgg_view('input/longtext', array('name' => 'generic_comment')); ?>
+		</div>
+<?php
+		echo elgg_view('input/hidden', array(
+			'name' => 'entity_guid',
+			'value' => $vars['entity']->getGUID()
+		));
+		echo elgg_view('input/submit', array('value' => elgg_echo("generic_comments:post")));
+	}
 }
