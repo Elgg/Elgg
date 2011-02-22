50f05d7dfd51fb1b130fff48b12cf3b3bdf36c17
ewinslow <ewinslow@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-22 08:16:12 +0000

    Utilizing elgg_extract in output/url
    git-svn-id: http://code.elgg.org/elgg/trunk@8424 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/output/url.php
+++ views/default/output/url.php
@@ -8,8 +8,8 @@
  *
  * @uses string $vars['text']        The string between the <a></a> tags.
  * @uses string $vars['href']        The unencoded url string
- * @uses bool   $vars['encode_text'] Run $vars['text'] through htmlspecialchars()?
- * @uses bool   $vars['is_action']   Is this a link to an action?
+ * @uses bool   $vars['encode_text'] Run $vars['text'] through htmlspecialchars() (false)
+ * @uses bool   $vars['is_action']   Is this a link to an action (false)
  *
  */
 
@@ -20,7 +20,7 @@ if (!$url and isset($vars['value'])) {
 }
 
 if (isset($vars['text'])) {
-	if (isset($vars['encode_text']) && $vars['encode_text']) {
+	if (elgg_extract('encode_text', $vars, false)) {
 		$text = htmlspecialchars($vars['text'], ENT_QUOTES, 'UTF-8', false);
 	} else {
 		$text = $vars['text'];
@@ -35,7 +35,7 @@ unset($vars['encode_text']);
 if ($url) {
 	$url = elgg_normalize_url($url);
 
-	if (isset($vars['is_action'])) {
+	if (elgg_extract('is_action', $vars, false)) {
 		$url = elgg_add_action_tokens_to_url($url, false);
 		unset($vars['is_action']);
 	}
