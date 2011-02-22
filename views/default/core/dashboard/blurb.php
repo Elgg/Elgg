ea2e005d0fe64515ea099f31fe1724de0ebff542
ewinslow <ewinslow@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-22 00:05:59 +0000

    Cleaned up core/dashboard/blurb to take advantage of the new <div> wrapper around output/longtext
    git-svn-id: http://code.elgg.org/elgg/trunk@8392 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/core/dashboard/blurb.php
+++ views/default/core/dashboard/blurb.php
@@ -5,10 +5,13 @@
  */
 ?>
 
-<div class="elgg-col elgg-col-2of3" id="dashboard-info">
-	<div class="elgg-inner pas mhs mbl">
-		<p>
-			<?php echo elgg_echo("dashboard:nowidgets"); ?>
-		</p>
-	</div>
+<div class="elgg-col elgg-col-2of3">
+<?php 
+	echo elgg_view('output/longtext', array(
+		'id' => 'dashboard-info',
+		'class' => 'elgg-inner pas mhs mbl',
+		'value' => elgg_echo("dashboard:nowidgets"),
+	));
+
+?>
 </div>
