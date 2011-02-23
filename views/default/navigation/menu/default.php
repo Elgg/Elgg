d43992cca884c2530f529dc2a085ec5ab20ee1a1
ewinslow <ewinslow@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-02-23 00:20:24 +0000

    DRY'ed up default menu a bit
    git-svn-id: http://code.elgg.org/elgg/trunk@8434 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/navigation/menu/default.php
+++ views/default/navigation/menu/default.php
@@ -12,9 +12,9 @@ if (isset($vars['class'])) {
 }
 
 foreach ($vars['menu'] as $section => $menu_items) {
-	echo "<ul class=\"$class\">";
-	foreach ($menu_items as $menu_item) {
-		echo elgg_view('navigation/menu/elements/item', array('item' => $menu_item));
-	}
-	echo '</ul>';
+	echo elgg_view('navigation/menu/elements/group', array(
+		'section' => $section, 
+		'items' => $items,
+		'class' => $class,
+	));
 }
