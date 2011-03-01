3237b492070d6deb54b59353f5a566946361d776
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-03-01 01:24:37 +0000

    improved the default annotation view
    git-svn-id: http://code.elgg.org/elgg/trunk@8513 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/annotation/default.php
+++ views/default/annotation/default.php
@@ -5,9 +5,43 @@
  * @uses $vars['annotation']
  */
 
-$owner = get_user($vars['annotation']->owner_guid);
+$annotation = $vars['annotation'];
+
+$owner = get_entity($annotation->owner_guid);
+if (!$owner) {
+	return true;
+}
 $icon = elgg_view_entity_icon($owner, 'tiny');
+$owner_link = "<a href=\"{$owner->getURL()}\">$owner->name</a>";
 
-$info = elgg_view("output/longtext", array("value" => $vars['annotation']->value));
+$delete_action = elgg_extract('delete_action', $vars, '');
 
-echo elgg_view_image_block($icon, $info);
+$text = elgg_view("output/longtext", array("value" => $annotation->value));
+
+$friendlytime = elgg_view_friendly_time($annotation->time_created);
+
+$delete_button = '';
+if ($delete_action && $annotation->canEdit()) {
+	$url = elgg_http_add_url_query_elements($delete_action, array(
+		'annotation_id' => $annotation->id,
+	));
+	$delete_button = elgg_view("output/confirmlink", array(
+						'href' => $url,
+						'text' => elgg_echo('delete'),
+						'confirm' => elgg_echo('deleteconfirm')
+					));
+	$delete_button = "<span class=\"elgg-button elgg-button-delete\">$delete_button</span>";
+}
+
+$body = <<<HTML
+<div class="mbn">
+	$delete_button
+	$owner_link
+	<span class="elgg-subtext">
+		$friendlytime
+	</span>
+	$text
+</div>
+HTML;
+
+echo elgg_view_image_block($icon, $body);
