35be18083c4262c94d58f85609aabfff5a0f9412
ewinslow <ewinslow@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-03-01 08:00:53 +0000

    Refs #2538: Cleaned up custom tinymce code into elgg.tinymce.*. Refs #2895: TinyMCE makes use of longtext menu
    git-svn-id: http://code.elgg.org/elgg/trunk@8533 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/tinymce/start.php
+++ mod/tinymce/start.php
@@ -8,7 +8,24 @@
 function tinymce_init() {
 	elgg_extend_view('css/elgg', 'tinymce/css');
 	elgg_extend_view('css/admin', 'tinymce/css');
+	
+	elgg_extend_view('input/longtext', 'tinymce/init');
+	
 	elgg_extend_view('embed/custom_insert_js', 'tinymce/embed_custom_insert_js');
+	
+	elgg_register_plugin_hook_handler('register', 'menu:longtext', 'tinymce_longtext_menu');
+}
+
+function tinymce_longtext_menu($hook, $type, $items, $vars) {
+	
+	$items[] = array(
+		'name' => 'tinymce_toggler',
+		'class' => 'tinymce-toggle-editor',
+		'href' => "javascript:elgg.tinymce.toggleEditor('{$vars['id']}');",
+		'text' => elgg_echo('tinymce:remove'),
+	);
+	
+	return $items;
 }
 
 elgg_register_event_handler('init', 'system', 'tinymce_init', 9999);
--- views/default/input/longtext.php
+++ views/default/input/longtext.php
@@ -12,6 +12,7 @@
 
 $defaults = array(
 	'class' => 'elgg-input-longtext',
+	'id' => 'elgg-input-' . rand(), //@todo make this more robust
 );
 
 // work around for deprecation code in elgg_views()
@@ -20,4 +21,5 @@ unset($vars['internalid']);
 
 $vars = array_merge($defaults, $vars);
 
+echo elgg_view_menu('longtext', array('sort_by' => 'weight'));
 echo elgg_view('input/plaintext', $vars);
