19286968e6c08321e037e2271047ca74aeef1a49
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-03-13 16:58:12 +0000

    Fixes #3072 using elgg_list_entities*() in group members sidebar view
    git-svn-id: http://code.elgg.org/elgg/trunk@8684 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/groups/views/default/groups/sidebar/members.php
+++ mod/groups/views/default/groups/sidebar/members.php
@@ -2,22 +2,28 @@
 /**
  * Group members sidebar
  *
- * @todo clean up html
- *
  * @package ElggGroups
+ *
+ * @uses $vars['entity'] Group entity
+ * @uses $vars['limit']  The number of members to display
  */
+
+$limit = elgg_extract('limit', $vars, 10);
 
 $all_link = elgg_view('output/url', array(
 	'href' => 'groups/members/' . $vars['entity']->guid,
 	'text' => elgg_echo('groups:members:more'),
 ));
 
-$body = '<ul class="elgg-menu-hz">';
-$members = $vars['entity']->getMembers(10);
-foreach ($members as $mem) {
-	$body .= '<li class="pas">' . elgg_view_entity_icon($mem, 'tiny', array('override' => true)) . '</li>';
-}
-$body .= '</ul>';
+$body = elgg_list_entities_from_relationship(array(
+	'relationship' => 'member',
+	'relationship_guid' => $vars['entity']->guid,
+	'inverse_relationship' => true,
+	'types' => 'user',
+	'limit' => $limit,
+	'gallery' => true,
+));
+
 $body .= "<div class='center mts'>$all_link</div>";
 
 echo elgg_view_module('aside', elgg_echo('groups:members'), $body);
