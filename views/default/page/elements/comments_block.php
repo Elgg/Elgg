a56299d152912d2617ffe4f7f59fce7f3d925e88
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-03-22 00:54:42 +0000

    Fixes #2090 context sensitive sidebar box is added - groups off
    git-svn-id: http://code.elgg.org/elgg/trunk@8813 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/page/elements/comments_block.php
+++ views/default/page/elements/comments_block.php
@@ -2,7 +2,7 @@
 /**
  * Display the latest related comments
  *
- * Generally used in a sidebar
+ * Generally used in a sidebar. Does not work with groups currently.
  *
  * @uses $vars['subtypes']   Object subtype string or array of subtypes
  * @uses $vars['owner_guid'] The owner of the content being commented on
@@ -12,6 +12,12 @@
 $owner_guid = elgg_extract('owner_guid', $vars, ELGG_ENTITIES_ANY_VALUE);
 if (!$owner_guid) {
 	$owner_guid = ELGG_ENTITIES_ANY_VALUE;
+}
+
+$owner_entity = get_entity($owner_guid);
+if ($owner_entity && elgg_instanceof($owner_entity, 'group')) {
+	// not supporting groups so return
+	return true;
 }
 
 $options = array(
