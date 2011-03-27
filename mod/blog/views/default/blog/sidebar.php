5d6d475f8dc84456c35975073e82e8af261c4bf3
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-03-27 00:36:59 +0000

    Fixes #2969 turned off group blog archives
    git-svn-id: http://code.elgg.org/elgg/trunk@8864 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/blog/views/default/blog/sidebar.php
+++ mod/blog/views/default/blog/sidebar.php
@@ -17,7 +17,10 @@ if ($vars['page'] == 'all') {
 	));
 }
 
-echo elgg_view('blog/sidebar/archives', $vars);
+// only users can have archives at present
+if (elgg_instanceof(elgg_get_page_owner_entity(), 'user')) {
+	echo elgg_view('blog/sidebar/archives', $vars);
+}
 
 if ($vars['page'] != 'friends') {
 	echo elgg_view('page/elements/tagcloud_block', array(
