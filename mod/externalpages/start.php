23586cdca3fbdaa64c51707cfdb4bb95704cce7c
Kishanthan <kshanth2101@gmail.com>
2011-03-28 02:48:45 -0700

    removed some repeated code from previous commit changes. Also added backwards compatibly. If we try to visit '/expages/read/about' or '/expages/about', it will get forwarded to '/about'.

--- mod/externalpages/start.php
+++ mod/externalpages/start.php
@@ -8,18 +8,20 @@ register_elgg_event_handler('init', 'system', 'expages_init');
 function expages_init() {
 
 	// Register a page handler, so we can have nice URLs
-	elgg_register_page_handler('about', 'about_page_handler');
-        elgg_register_page_handler('terms', 'terms_page_handler');
-        elgg_register_page_handler('privacy', 'privacy_page_handler');
-
+	elgg_register_page_handler('about', 'expages_page_handler');
+        elgg_register_page_handler('terms', 'expages_page_handler');
+        elgg_register_page_handler('privacy', 'expages_page_handler');
+	elgg_register_page_handler('expages', 'expages_page_handler');
+	
 	// add a menu item for the admin edit page
-	elgg_register_admin_menu_item('configure', 'expages', 'site');
+	elgg_register_admin_menu_item('configure', 'expages', 'appearance');
 
+	
 	// add footer links
 	expages_setup_footer_menu();
 
 	// register action
-	$actions_base = elgg_get_plugins_path() . 'externalpages/actions';
+	$actions_base = elgg_get_plugins_path() . 'externalpages/about/actions';
 	elgg_register_action("expages/edit", "$actions_base/edit.php", 'admin');
 }
 
@@ -29,10 +31,12 @@ function expages_init() {
 function expages_setup_footer_menu() {
     $pages = array('about', 'terms', 'privacy');
     foreach ($pages as $page) {
+	
         $url = "$page";
         $item = new ElggMenuItem($page, elgg_echo("expages:$page"), $url);
 		$item->setSection('alt');
         elgg_register_menu_item('footer', $item);
+	
     }
 }
 
@@ -41,8 +45,16 @@ function expages_setup_footer_menu() {
  *
  * 
  */
-function about_page_handler() {
-	$type = 'about';
+
+function expages_page_handler() {
+	$page = preg_split('/\//', $_SERVER["REQUEST_URI"]);
+
+	if ($page[2] == 'expages') {
+		expages_url_forwarder($page);
+		return 0;
+	}
+	
+	$type = strtolower($page[2]);
 	
 	$title = elgg_echo("expages:$type");
 	$content = elgg_view_title($title);
@@ -58,49 +70,24 @@ function about_page_handler() {
 		$content .= elgg_echo("expages:notset");
 	}
 
+	
 	$body = elgg_view_layout("one_sidebar", array('content' => $content));
 	echo elgg_view_page($title, $body);
+
 }
 
-function terms_page_handler() {
-	$type = 'terms';
-	
-	$title = elgg_echo("expages:$type");
-	$content = elgg_view_title($title);
-
-	$object = elgg_get_entities(array(
-		'type' => 'object',
-		'subtype' => $type,
-		'limit' => 1,
-	));
-	if ($object) {
-		$content .= elgg_view('output/longtext', array('value' => $object[0]->description));
-	} else {
-		$content .= elgg_echo("expages:notset");
+/**
+ * Forward to the new style of URLs
+ *
+ * @param string $page
+ */
+function expages_url_forwarder($page) {
+	global $CONFIG;
+	if ($page[3] == 'read') {
+		$url = "{$CONFIG->wwwroot}{$page[4]}";
 	}
-
-	$body = elgg_view_layout("one_sidebar", array('content' => $content));
-	echo elgg_view_page($title, $body);
-}
-
-function privacy_page_handler() {
-	$type = 'privacy';
-	
-	$title = elgg_echo("expages:$type");
-	$content = elgg_view_title($title);
-
-	$object = elgg_get_entities(array(
-		'type' => 'object',
-		'subtype' => $type,
-		'limit' => 1,
-	));
-	if ($object) {
-		$content .= elgg_view('output/longtext', array('value' => $object[0]->description));
-	} else {
-
-		$content .= elgg_echo("expages:notset");
-	}
-
-	$body = elgg_view_layout("one_sidebar", array('content' => $content));
-	echo elgg_view_page($title, $body);
+	else {
+		$url = "{$CONFIG->wwwroot}{$page[3]}";
+	}	
+	forward($url);
 }
