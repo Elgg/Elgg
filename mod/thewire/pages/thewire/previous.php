6644a332427c94352b47fa3901040ca415c1abe4
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-03-30 01:57:29 +0000

    Refs #3200 pulling in e1d75071737b2ed35cf9 from anirupdutta's pull request
    git-svn-id: http://code.elgg.org/elgg/trunk@8880 36083f99-b078-4883-b0ff-0f9b5a30f544

--- mod/thewire/pages/thewire/previous.php
+++ mod/thewire/pages/thewire/previous.php
@@ -4,8 +4,19 @@
  */
 
 $guid = (int) get_input('guid');
+$title = elgg_echo('thewire:previous');
 
 $parent = thewire_get_parent($guid);
 if ($parent) {
-	echo elgg_view_entity($parent);
+
+	$body=elgg_view_entity($parent);
 }
+
+$body = elgg_view_layout('content', array(
+	'filter' => false,
+	'content' => $body,
+	'title' => $title,
+	'buttons' => false,
+));
+
+echo elgg_view_page($title, $body);
