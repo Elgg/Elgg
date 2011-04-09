94d316ea1f20fbca680641cc846c9ead979e7017
cash <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2011-04-09 12:34:38 +0000

    Refs #3241 cleaned up comments on upgrades and removed the turning off of time limit in individual upgrades since this is now handled by the upgrade function
    git-svn-id: http://code.elgg.org/elgg/trunk@8961 36083f99-b078-4883-b0ff-0f9b5a30f544

--- engine/lib/upgrades/2008100701.php
+++ engine/lib/upgrades/2008100701.php
@@ -1,7 +1,7 @@
 <?php
-	/// Activate mail plugin
-	/**
-	 * Because Elgg now has a plugable account activation process we need to activate
-	 * the email account activation plugin for existing installs.
-	 */
-	enable_plugin('uservalidationbyemail', $CONFIG->site->guid);
+
+/**
+ * Because Elgg now has a plugable account activation process we need to activate
+ * the email account activation plugin for existing installs.
+ */
+enable_plugin('uservalidationbyemail', $CONFIG->site->guid);
--- engine/lib/upgrades/2009070101.php
+++ engine/lib/upgrades/2009070101.php
@@ -1,10 +1,9 @@
 <?php
 
-	global $CONFIG;
+global $CONFIG;
 
-	/// Deprecate kses and activate htmlawed
-	/**
-	 * Kses appears to be a dead project so we are deprecating it in favour of htmlawed.
-	 */
-	disable_plugin('kses', $CONFIG->site->guid);
-	enable_plugin('htmlawed', $CONFIG->site->guid);
+/**
+ * Kses appears to be a dead project so we are deprecating it in favour of htmlawed.
+ */
+disable_plugin('kses', $CONFIG->site->guid);
+enable_plugin('htmlawed', $CONFIG->site->guid);
