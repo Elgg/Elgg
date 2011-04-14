71b12bed081671303b4bc9e9d1b4d5eb7ef513e4
Shashank Sabesan <shashank.sabesan@gmail.com>
2011-04-14 12:48:41 +0530

    Added setting to logrotate plugin to delete archived logs older than a specified period

--- mod/logrotate/start.php
+++ mod/logrotate/start.php
@@ -9,6 +9,7 @@ elgg_register_event_handler('init', 'system', 'logrotate_init');
 
 function logrotate_init() {
 	$period = elgg_get_plugin_setting('period', 'logrotate');
+	$time = elgg_get_plugin_setting('time', 'logrotate');
 	switch ($period) {
 		case 'weekly':
 		case 'monthly' :
@@ -18,14 +19,16 @@ function logrotate_init() {
 			$period = 'monthly';
 	}
 
-	// Register cron hook
-	elgg_register_plugin_hook_handler('cron', $period, 'logrotate_cron');
+	// Register cron hook for archival of logs
+	elgg_register_plugin_hook_handler('cron', $period, 'logrotate_archive_cron');
+	// Register cron hook for deletion of selected archived logs
+	elgg_register_plugin_hook_handler('cron', $time, 'logrotate_delete_cron');
 }
 
 /**
  * Trigger the log rotation.
  */
-function logrotate_cron($hook, $entity_type, $returnvalue, $params) {
+function logrotate_archive_cron($hook, $entity_type, $returnvalue, $params) {
 	$resulttext = elgg_echo("logrotate:logrotated");
 
 	$day = 86400;
@@ -50,4 +53,67 @@ function logrotate_cron($hook, $entity_type, $returnvalue, $params) {
 	}
 
 	return $returnvalue . $resulttext;
+}
+
+/**
+ * Trigger the log deletion.
+ */
+function logrotate_delete_cron($hook, $entity_type, $returnvalue, $params) {
+	$resulttext = elgg_echo("logrotate:logdeleted");
+
+	$day = 86400;
+
+	$offset = 0;
+	$period = elgg_get_plugin_setting('time', 'logrotate');
+	switch ($period) {
+		case 'weekly':
+			$offset = $day * 7;
+			break;
+		case 'yearly':
+			$offset = $day * 365;
+			break;
+		case 'monthly':
+		default:
+			// assume 28 days even if a month is longer. Won't cause data loss.
+			$offset = $day * 28;
+	}
+
+	if (!log_browser_delete_log($offset)) {
+		$resulttext = elgg_echo("logrotate:lognotdeleted");
+	}
+
+	return $returnvalue . $resulttext;
+}
+
+/**
+ * This function deletes archived copies of the system logs that are older than specified.
+ *
+ * @param int $time_of_delete An offset in seconds from now to delete (useful for log deletion)
+ */
+
+function log_browser_delete_log($time_of_delete) {
+	global $CONFIG;
+
+	$offset = (int)$time_of_delete;
+	$now = time(); // Take a snapshot of now
+
+	$ts = $now - $offset;
+
+	$FLAG = 1;      
+	$result = mysql_query("SHOW TABLES like '{$CONFIG->dbprefix}system_log_%'");
+	while ($showtablerow = mysql_fetch_array($result)) {
+		$log_time = explode("{$CONFIG->dbprefix}system_log_", $showtablerow[0]);  //To obtain time of archival
+		if ($log_time < $ts) {
+			if (!mysql_query("DROP TABLE $showtablerow[0]")) {         //If the time of archival is before the required offset then delete
+				$FLAG = 0;
+			}	
+		}
+	}
+
+	if ($FLAG) {
+		return true;                                                           //Check if the appropriate tables have been deleted and return true if yes
+	} else {
+		return false;
+	}
+
 }
