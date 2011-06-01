5ce0b61f361809949f88fcd6566eebc3abb2c8df
Russell McLoughlin <mcloughlin3@llnl.gov>
2011-06-01 13:16:39 -0700

    Modified upload script to guarentee that the directory under elgg data is created or exists. Removed strange chunking code with builtin php function

--- mod/file/actions/file/upload.php
+++ mod/file/actions/file/upload.php
@@ -106,6 +106,9 @@ if (isset($_FILES['upload']['name']) && !empty($_FILES['upload']['name'])) {
 	$file->originalfilename = $_FILES['upload']['name'];
 	$file->simpletype = file_get_simple_type($_FILES['upload']['type']);
 
+	// Open the file to guarentee the directory exists
+	$file->open("write");
+	$file->close();
 	move_uploaded_file($_FILES['upload']['tmp_name'], $file->getFilenameOnFilestore());
 
 	$guid = $file->save();
