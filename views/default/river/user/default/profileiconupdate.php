5df3bcf4151d6731c32d71f758d7922d0c1c4932
cash <cash.costello@gmail.com>
2011-06-25 12:10:33 -0400

    actually read Evan's comment on pull request #48 and fixed previous

--- views/default/river/user/default/profileiconupdate.php
+++ views/default/river/user/default/profileiconupdate.php
@@ -6,8 +6,16 @@
 
 $subject = $vars['item']->getSubjectEntity();
 
-echo elgg_view('river/item', array(
-	'item' => $vars['item'],
-	'attachments' => elgg_view_entity_icon($subject, 'tiny'),
+$subject_link = elgg_view('output/url', array(
+	'href' => $subject->getURL(),
+	'text' => $subject->name,
+	'class' => 'elgg-river-subject',
 ));
 
+$string = elgg_echo('river:update:user:avatar', array($subject_link));
+
+echo elgg_view('river/item', array(
+	'item' => $vars['item'],
+	'summary' => $string,
+	'attachments' => elgg_view_entity_icon($subject, 'tiny'),
+));
