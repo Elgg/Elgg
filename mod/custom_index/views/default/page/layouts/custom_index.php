a879cd60b98ccf5497467b87037792499a56103c
cash <cash.costello@gmail.com>
2011-07-03 14:59:36 -0400

    removed deprecated function from custom_index plugin

--- mod/custom_index/views/default/page/layouts/custom_index.php
+++ mod/custom_index/views/default/page/layouts/custom_index.php
@@ -17,7 +17,7 @@
 // Top box for login or welcome message
 if (elgg_is_logged_in()) {
 	$top_box = "<h2>" . elgg_echo("welcome") . " ";
-	$top_box .= get_loggedin_user()->name;
+	$top_box .= elgg_get_logged_in_user_entity()->name;
 	$top_box .= "</h2>";
 } else {
 	$top_box = $vars['login'];
