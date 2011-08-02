48f42ec219a8b541b1a619e968c01d0d48e3630c
Steve Clay <steve@mrclay.org>
2011-08-02 10:46:01 -0300

    Default group icons are GIFs, not JPGs.

--- mod/groups/icon.php
+++ mod/groups/icon.php
@@ -28,7 +28,7 @@ if ($filehandler->open("read")) {
 }
 
 if (!$success) {
-	$location = elgg_get_plugins_path() . "groups/graphics/default{$size}.jpg";
+	$location = elgg_get_plugins_path() . "groups/graphics/default{$size}.gif";
 	$contents = @file_get_contents($location);
 }
 
