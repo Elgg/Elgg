e54163d643d5e86bf990e4a790accfaa29225262
Brett Profitt <brett.profitt@gmail.com>
2011-08-13 11:30:32 -0700

    Refs #3318 - Forcing IE9 to IE8 document mode for jQuery UI draggable and sortable. For 1.7.12 we'll want to upgrade jQuery UI and jQuery.

--- views/default/page_elements/header.php
+++ views/default/page_elements/header.php
@@ -52,6 +52,8 @@ $release = get_version(true);
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<!-- Force IE 9 to IE 8 Document Mode for jQuery-UI 1.7.2 draggable and sortable -->
+	<meta http-equiv="X-UA-Compatible" content="IE=8" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="ElggRelease" content="<?php echo $release; ?>" />
 	<meta name="ElggVersion" content="<?php echo $version; ?>" />
