acc849f41fd05cfa38512a4ad5167ab8d52c905a
Brett Profitt <brett.profitt@gmail.com>
2011-08-27 19:13:47 -0700

    Fixes #3561. Only adding the static profile block height to the widget canvas if on the profile page.

--- mod/profile/views/default/profile/js.php
+++ mod/profile/views/default/profile/js.php
@@ -1,3 +1,6 @@
 elgg.register_hook_handler('init', 'system', function() {
-	$('#elgg-widget-col-1').css('min-height', $('.profile').outerHeight(true) + 1);
+	// only do this on the profile page's widget canvas.
+	if ($('.profile').length) {
+		$('#elgg-widget-col-1').css('min-height', $('.profile').outerHeight(true) + 1);
+	}
 });
