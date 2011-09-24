61ad84e217b0a1bae8d607a1504c4b477d976ac5
cash <cash.costello@gmail.com>
2011-09-24 16:08:04 -0400

    Refs #3864 updated the two form bodies for edit profile fields

--- views/default/admin/appearance/profile_fields.php
+++ views/default/admin/appearance/profile_fields.php
@@ -10,9 +10,7 @@ $reset = elgg_view_form('profile/fields/reset', array(), array());
 $body = <<<__HTML
 $add
 $list
-<div class="default_profile_reset">
-	$reset
-</div>
+$reset
 __HTML;
 
 echo $body;
--- views/default/forms/profile/fields/reset.php
+++ views/default/forms/profile/fields/reset.php
@@ -3,8 +3,10 @@
  * Reset profile fields form
  */
 
+echo '<div class="elgg-foot">';
 $params = array(
 	'value' => elgg_echo('profile:resetdefault'),
 	'class' => 'elgg-button-cancel',
 );
 echo elgg_view('input/submit', $params);
+echo '</div>';
