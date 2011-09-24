fc5fd10695883a1385645dbb9a16db9e9e0df36c
cash <cash.costello@gmail.com>
2011-09-24 15:53:51 -0400

    Fixes #3856 adds elgg-foot to forms missing it in /views/default/forms and updated documentation

--- views/default/forms/plugins/usersettings/save.php
+++ views/default/forms/plugins/usersettings/save.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Plugin user settings
+ *
+ * Calls the plugin admin settings form body with type set to 'user'
+ * 
+ * @package Elgg.Core
+ * @subpackage Plugins
+ */
+
 $vars['type'] = 'user';
 
 // Can't use elgg_view_form() because it overrides the $vars['action'] parameter
