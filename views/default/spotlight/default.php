4988b577e6b85ce3ac496f0a6cfc95a0b57dea46
cash <cash.costello@gmail.com>
2011-09-25 14:24:12 -0400

    Fixes #3874 removes references to elgg.com

--- views/default/spotlight/default.php
+++ views/default/spotlight/default.php
@@ -14,9 +14,9 @@
 	<div class="spotlightRHS">
 		<h2>Information</h2>
 		<ul>
-			<li><a href="http://docs.elgg.org/wiki/Views/SystemViews">Populating this spotlight area</a></li>
-			<li><a href="http://community.elgg.org">Elgg open source community</a></li>
-			<li><a href="http://elgg.com">Elgg commercial services</a></li>
+			<li><a href="http://docs.elgg.org/">Elgg documentation</a></li>
+			<li><a href="http://community.elgg.org/">Elgg open source community</a></li>
+			<li><a href="http://trac.elgg.org/">Elgg bug tracker</a></li>
 		</ul>
 	</div>
 	<!-- spotlight LHS content -->
@@ -25,9 +25,8 @@
 		<p>
 			Elgg allows you to run your own social networking site, whether publicly (like
 			Facebook) or privately within your company or organization.
-			<a href="http://elgg.com/">Support and commercial services</a> are available,
-			or you can find new tools for your Elgg site at the
-			<a href="http://community.elgg.org/">Elgg community</a>.
+			You can find plugins and support for your Elgg site at the
+			<a href="http://community.elgg.org/">Elgg community site</a>.
 		</p>
 	</div><!-- /spotlight LHS content -->
 	<div class="clearfloat"></div>
