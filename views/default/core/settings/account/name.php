0ca443a22676573801d1a08814fca202f5314dda
Tom <tom@lorinthe.com>
2011-10-10 18:33:06 +0300

    See http://trac.elgg.org/ticket/3953

--- views/default/core/settings/account/name.php
+++ views/default/core/settings/account/name.php
@@ -9,7 +9,22 @@
  */
 
 $user = elgg_get_page_owner_entity();
+?>
+<div class="elgg-module elgg-module-info">
+  <div class="elgg-head">
+		<h3><?php echo elgg_echo('user:name:label'); ?></h3>
+	</div>
+	<div class="elgg-body">
+		<p>
+			<?php echo elgg_echo('name'); ?>:
+			<?php
+			echo elgg_view('input/text', array('name' => 'name', 'value' => $user->name));
+			?>
+		</p>
+	</div>
+</div>
+
+<?php
 
 // all hidden, but necessary for properly updating user details
-echo elgg_view('input/hidden', array('name' => 'name', 'value' => $user->name));
 echo elgg_view('input/hidden', array('name' => 'guid', 'value' => $user->guid));
