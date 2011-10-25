65bd37c1f0e3e3adb04bd48dad6183bcd3c7e26c
Cash Costello <cash.costello@gmail.com>
2011-10-25 07:56:31 -0400

    Refs #3201 a working ical view type

--- views/ical/export/entity.php
+++ views/ical/export/entity.php
@@ -9,23 +9,25 @@
 
 $entity = $vars['entity'];
 
-if (
-	($entity instanceof Notable) &&
-	($entity->getCalendarStartTime()) &&
-	($entity->getCalendarEndTime())
-)
-{
-?>
-BEGIN:VEVENT
-DTSTAMP:<?php echo date("Ymd\THis\Z", $entity->getTimeCreated());  ?>
-DTSTART:<?php echo date("Ymd\THis\Z", $entity->getCalendarStartTime());  ?>
-DTEND:<?php echo date("Ymd\THis\Z", $entity->getCalendarEndTime());  ?>
-SUMMARY:<?php echo $event->title; ?>
-LAST-MODIFIED:<?php echo date("Ymd\THis\Z", $entity->getTimeUpdated());  ?>
-END:VEVENT
-<?php
-}
-?>
-	if (
+if ($entity instanceof Notable &&
+	$entity->getCalendarStartTime() &&
+	$entity->getCalendarEndTime()) {
 
-	)
+	$timestamp = date("Ymd\THis\Z", $entity->getTimeCreated());
+	$start = date("Ymd\THis\Z", $entity->getCalendarStartTime());
+	$end = date("Ymd\THis\Z", $entity->getCalendarEndTime());
+	$summary = $entity->title;
+	$modified = date("Ymd\THis\Z", $entity->getTimeUpdated());
+
+	echo <<< ICAL
+BEGIN:VEVENT
+DTSTAMP:$timestamp
+DTSTART:$start
+DTEND:$end
+SUMMARY:$summary
+LAST-MODIFIED:$modified
+END:VEVENT
+
+ICAL;
+
+}
