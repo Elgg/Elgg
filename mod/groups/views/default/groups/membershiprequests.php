2f59afd29170066cb0d8ae19aa76ef2b7cb0b7db
cash <cash.costello@gmail.com>
2011-10-27 17:51:26 -0400

    Refs #3296 displaying all group membership requests on one page

--- mod/groups/views/default/groups/membershiprequests.php
+++ mod/groups/views/default/groups/membershiprequests.php
@@ -10,39 +10,37 @@ if (!empty($vars['requests']) && is_array($vars['requests'])) {
 	$user = elgg_get_logged_in_user_entity();
 	echo '<ul class="elgg-list">';
 	foreach ($vars['requests'] as $user) {
-		if ($user instanceof ElggUser) {
-			$icon = elgg_view_entity_icon($user, 'tiny', array('hover' => 'true'));
+		$icon = elgg_view_entity_icon($user, 'tiny', array('hover' => 'true'));
 
-			$user_title = elgg_view('output/url', array(
-				'href' => $user->getURL(),
-				'text' => $user->name,
-				'is_trusted' => true,
-			));
+		$user_title = elgg_view('output/url', array(
+			'href' => $user->getURL(),
+			'text' => $user->name,
+			'is_trusted' => true,
+		));
 
-			$url = "action/groups/addtogroup?user_guid={$user->guid}&group_guid={$vars['entity']->guid}";
-			$url = elgg_add_action_tokens_to_url($url);
-			$accept_button = elgg_view('output/url', array(
+		$url = "action/groups/addtogroup?user_guid={$user->guid}&group_guid={$vars['entity']->guid}";
+		$url = elgg_add_action_tokens_to_url($url);
+		$accept_button = elgg_view('output/url', array(
+			'href' => $url,
+			'text' => elgg_echo('accept'),
+			'class' => 'elgg-button elgg-button-submit',
+			'is_trusted' => true,
+		));
+
+		$url = 'action/groups/killrequest?user_guid=' . $user->guid . '&group_guid=' . $vars['entity']->guid;
+		$delete_button = elgg_view('output/confirmlink', array(
 				'href' => $url,
-				'text' => elgg_echo('accept'),
-				'class' => 'elgg-button elgg-button-submit',
-				'is_trusted' => true,
-			));
+				'confirm' => elgg_echo('groups:joinrequest:remove:check'),
+				'text' => elgg_echo('delete'),
+				'class' => 'elgg-button elgg-button-delete mlm',
+		));
 
-			$url = 'action/groups/killrequest?user_guid=' . $user->guid . '&group_guid=' . $vars['entity']->guid;
-			$delete_button = elgg_view('output/confirmlink', array(
-					'href' => $url,
-					'confirm' => elgg_echo('groups:joinrequest:remove:check'),
-					'text' => elgg_echo('delete'),
-					'class' => 'elgg-button elgg-button-delete mlm',
-			));
+		$body = "<h4>$user_title</h4>";
+		$alt = $accept_button . $delete_button;
 
-			$body = "<h4>$user_title</h4>";
-			$alt = $accept_button . $delete_button;
-
-			echo '<li class="pvs">';
-			echo elgg_view_image_block($icon, $body, array('image_alt' => $alt));
-			echo '</li>';
-		}
+		echo '<li class="pvs">';
+		echo elgg_view_image_block($icon, $body, array('image_alt' => $alt));
+		echo '</li>';
 	}
 	echo '</ul>';
 } else {
