5d480f2d8a767c682878071c3c8f7f6b45770c14
Cash Costello <cash.costello@gmail.com>
2011-11-04 06:36:12 -0400

    Refs #2937 featured is primary module class and highlight is extension

--- mod/custom_index/views/default/custom_index/css.php
+++ mod/custom_index/views/default/custom_index/css.php
@@ -11,12 +11,12 @@
 .custom-index {
 	padding: 10px 0;
 }
-.elgg-module-front {
+.elgg-module-highlight {
 	-webkit-box-shadow: 1px 1px 5px #CCC;
 	-moz-box-shadow: 1px 1px 5px #CCC;
 	box-shadow: 1px 1px 5px #CCC;
 }
-.elgg-module-front:hover {
+.elgg-module-highlight:hover {
 	-webkit-box-shadow: 1px 1px 6px #AAA;
 	-moz-box-shadow: 1px 1px 6px #AAA;
 	box-shadow: 1px 1px 6px #AAA;
