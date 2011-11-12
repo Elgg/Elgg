76bea966f7f93e719ccbf6e2350f647636d4da2a
cash <cash.costello@gmail.com>
2011-11-12 14:28:52 -0500

    added better plugin skeleton and added documentation to some of the examples

--- documentation/examples/events/all.php
+++ documentation/examples/events/all.php
@@ -1,17 +1,24 @@
 <?php
+/**
+ * If you register an 'all' string for the event name, the handler function will
+ * be called for all events with that name, regardless of event type. The same
+ * can be done for the event type argument. Registering 'all' for both
+ * argyuments results in a handler being called for every event.
+ */
 
 elgg_register_event_handler('all', 'object', 'example_event_handler');
 
 // This function will be called for any event of type 'object'
-function example_event_handler($event, $type, $params) {
+function example_event_handler($event, $type, $object) {
 	// check what sort of object is passed
-	if ($params instanceof ElggObject) {
-		$subtype = $params->getSubtype();
+	if ($object instanceof ElggObject) {
+		$subtype = $object->getSubtype();
 
-		switch($subtype) {
+		switch ($subtype) {
 			case 'blog':
 			case 'thewire':
 			case 'pages':
+				// prevent these object subtypes from being saved or changed
 				return false;
 			default:
 				return true;
@@ -21,4 +28,3 @@ function example_event_handler($event, $type, $params) {
 
 	return true;
 }
-
