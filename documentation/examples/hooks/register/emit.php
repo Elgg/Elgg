0ec65a0130347b3b846628e9e59a1f1cbf0f0e06
cash <cash.costello@gmail.com>
2011-11-12 14:49:36 -0500

    cleaned up the plugin hooks documentation

--- documentation/examples/hooks/register/emit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// @todo this is an event, not a hook
-elgg_register_event_handler('test', 'example', 'example_init_system_callback');
-
-$params = new ElggObject();
-elgg_trigger_event('test', 'example', $params);
--- documentation/examples/hooks/trigger/basic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$result = elgg_trigger_plugin_hook('get_status', 'example', null, true);
-
-if ($result) {
-	var_dump('Plugin hook says ok!');
-} else {
-	var_dump('Plugin hook says no.');
-}
