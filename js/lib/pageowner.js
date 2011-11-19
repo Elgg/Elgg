be4488de483e87d91a231ca06f5639861d4d14b3
cash <cash.costello@gmail.com>
2011-11-18 22:28:18 -0500

    Fixes #4093 making sure elgg.page_owner is defined

--- js/lib/pageowner.js
+++ js/lib/pageowner.js
@@ -6,9 +6,13 @@
  */
 
 /**
- * @return {number} The GUID of the logged in user
+ * @return {number} The GUID of the page owner entity or 0 for no owner
  */
 elgg.get_page_owner_guid = function() {
-	return elgg.page_owner.guid || 0;
+	if (elgg.page_owner !== undefined) {
+		return elgg.page_owner.guid;
+	} else {
+		return 0;
+	}
 };
 
