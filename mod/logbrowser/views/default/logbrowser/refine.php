a9811d7d0641371e8067700603b27abb8f8e65e6
cash <cash.costello@gmail.com>
2011-11-22 21:59:12 -0500

    Fixes #3775 updates the log browser link and keeps the refine area open if the page is loaded with a user selected

--- mod/logbrowser/views/default/logbrowser/refine.php
+++ mod/logbrowser/views/default/logbrowser/refine.php
@@ -18,13 +18,18 @@ $toggle_link = elgg_view('output/url', array(
 	'rel' => 'toggle',
 ));
 
+$form_class = 'elgg-module elgg-module-inline';
+if (!isset($vars['user_guid'])) {
+	$form_class .= ' hidden';
+}
+
 ?>
 
 <div id="logbrowser-search-area" class="mbm">
 	<div>
 		<?php echo $toggle_link; ?>
 	</div>
-	<div id="log-browser-search-form" class="elgg-module elgg-module-inline hidden">
+	<div id="log-browser-search-form" class="<?php echo $form_class; ?>">
 		<div class="elgg-head">
 			<h3><?php echo elgg_echo('logbrowser:search'); ?></h3>
 		</div>
