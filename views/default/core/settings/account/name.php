4a151030205c2e3bbdf609472627843040270b50
Cash Costello <cash.costello@gmail.com>
2011-11-27 21:34:41 -0500

    Fixes #3953 adds back the ability to change display name from settings page

--- views/default/core/settings/account/name.php
+++ views/default/core/settings/account/name.php
@@ -4,12 +4,23 @@
  *
  * @package Elgg
  * @subpackage Core
-
-
  */
 
 $user = elgg_get_page_owner_entity();
-
-// all hidden, but necessary for properly updating user details
-echo elgg_view('input/hidden', array('name' => 'name', 'value' => $user->name));
+?>
+<div class="elgg-module elgg-module-info">
+  <div class="elgg-head">
+		<h3><?php echo elgg_echo('user:name:label'); ?></h3>
+	</div>
+	<div class="elgg-body">
+		<p>
+			<?php echo elgg_echo('name'); ?>:
+			<?php
+			echo elgg_view('input/text', array('name' => 'name', 'value' => $user->name));
+			?>
+		</p>
+	</div>
+</div>
+<?php
+// need the user's guid to make sure the correct user gets updated
 echo elgg_view('input/hidden', array('name' => 'guid', 'value' => $user->guid));
