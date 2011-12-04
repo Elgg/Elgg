998b5cdc825de34d02d3204adf3bf6eccea74b66
cash <cash.costello@gmail.com>
2011-12-03 20:04:01 -0500

    Refs #4176 added elgg_list_registered_entities() fix to 1.8 branch

--- engine/lib/entities.php
+++ engine/lib/entities.php
@@ -2147,8 +2147,13 @@ function elgg_list_registered_entities(array $options = array()) {
 		}
 	}
 
-	$count = elgg_get_entities(array_merge(array('count' => TRUE), $options));
-	$entities = elgg_get_entities($options);
+	if (!empty($options['type_subtype_pairs'])) {
+		$count = elgg_get_entities(array_merge(array('count' => TRUE), $options));
+		$entities = elgg_get_entities($options);
+	} else {
+		$count = 0;
+		$entities = array();
+	}
 
 	return elgg_view_entity_list($entities, $count, $options['offset'],
 		$options['limit'], $options['full_view'], $options['list_type_toggle'], $options['pagination']);
