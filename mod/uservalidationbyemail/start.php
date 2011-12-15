38d01011697f30d33d623881362a46dc809fc098
Cash Costello <cash.costello@gmail.com>
2011-12-15 07:18:46 -0500

    Fixes #4196 defining the metastring for 1 if not already defined to avoid bad sql

--- mod/uservalidationbyemail/start.php
+++ mod/uservalidationbyemail/start.php
@@ -188,7 +188,10 @@ function uservalidationbyemail_get_unvalidated_users_sql_where() {
 	global $CONFIG;
 
 	$validated_id = get_metastring_id('validated');
-	$one_id = get_metastring_id(1);
+	$one_id = get_metastring_id('1');
+	if ($one_id === false) {
+		$one_id = add_metastring('1');
+	}
 
 	// thanks to daveb@freenode for the SQL tips!
 	$wheres = array();
