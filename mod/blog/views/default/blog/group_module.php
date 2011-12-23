1b657e860b966260ca88a0b0e91ee3d058a78a6a
cash <cash.costello@gmail.com>
2011-12-23 14:24:04 -0500

    Fixes #4240 not showing draft blog posts in group module

--- mod/blog/views/default/blog/group_module.php
+++ mod/blog/views/default/blog/group_module.php
@@ -20,11 +20,12 @@ $options = array(
 	'type' => 'object',
 	'subtype' => 'blog',
 	'container_guid' => elgg_get_page_owner_guid(),
+	'metadata_name_value_pairs' => array('name' => 'status', 'value' => 'published'),
 	'limit' => 6,
 	'full_view' => false,
 	'pagination' => false,
 );
-$content = elgg_list_entities($options);
+$content = elgg_list_entities_from_metadata($options);
 elgg_pop_context();
 
 if (!$content) {
