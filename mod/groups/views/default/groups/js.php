cfcf985b803e2ce4187d1dad30905a30e4c937b8
Cash Costello <cash.costello@gmail.com>
2012-01-08 14:24:09 -0500

    updated groups js to use new JS hook system

--- mod/groups/views/default/groups/js.php
+++ mod/groups/views/default/groups/js.php
@@ -1,5 +1,6 @@
 
-$(function() {
+// this adds a class to support IE8 and older
+elgg.register_hook_handler('init', 'system', function() {
 	// jQuery uses 0-based indexing
 	$('#groups-tools').children('li:even').addClass('odd');
 });
