b9b13949c9dce7ef979d3edd06dcd2197e01cb81
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2012-03-28 19:39:46 +0200

    fixes 4422

--- views/default/core/settings/statistics/numentities.php
+++ views/default/core/settings/statistics/numentities.php
@@ -1,51 +1,44 @@
 <?php
+
 /**
  * Elgg statistics screen
  *
  * @package Elgg
  * @subpackage Core
  */
-
 // Get entity statistics
 $entity_stats = get_entity_statistics(elgg_get_logged_in_user_guid());
 
 if ($entity_stats) {
-?>
-<div class="elgg-module elgg-module-info">
-	<div class="elgg-head">
-		<h3><?php echo elgg_echo('usersettings:statistics:label:numentities'); ?></h3>
-	</div>
-	<div class="elgg-body">
-		<table class="elgg-table-alt">
-		<?php
-			foreach ($entity_stats as $k => $entry) {
-				foreach ($entry as $a => $b) {
+	$rows = '';
+	foreach ($entity_stats as $k => $entry) {
+		foreach ($entry as $a => $b) {
 
-					//This function controls the alternating class
-					$even_odd = ( 'odd' != $even_odd ) ? 'odd' : 'even';
+			//This function controls the alternating class
+			$even_odd = ( 'odd' != $even_odd ) ? 'odd' : 'even';
 
-					if ($a == "__base__") {
-						$a = elgg_echo("item:{$k}");
-						if (empty($a)) {
-							$a = $k;
-						}
-					} else {
-						$a = elgg_echo("item:{$k}:{$a}");
-						if (empty($a)) {
-							$a = "$k $a";
-						}
-					}
-					echo <<< END
+			if ($a == "__base__") {
+				$a = elgg_echo("item:{$k}");
+				if (empty($a)) {
+					$a = $k;
+				}
+			} else {
+				$a = elgg_echo("item:{$k}:{$a}");
+				if (empty($a)) {
+					$a = "$k $a";
+				}
+			}
+			$rows .= <<< END
 						<tr class="{$even_odd}">
 							<td class="column-one"><b>{$a}:</b></td>
 							<td>{$b}</td>
 						</tr>
 END;
-				}
-			}
-		?>
-		</table>
-	</div>
-</div>
-<?php
+		}
+	}
+
+	$title = elgg_echo('usersettings:statistics:label:numentities');
+	$content = "<table class=\elgg-table-alt\">$rows</table>";
+
+	echo elgg_view_module('info', $title, $content);
 }
