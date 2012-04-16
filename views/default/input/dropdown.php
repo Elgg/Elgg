cd90d8abad8d42fa9e313ca89fee67c82a2a70c1
Aday Talavera <aday.talavera@gmail.com>
2012-04-16 11:08:54 +0100

    Deprecated input/dropdown view with the new input/select view that allows multiselection

--- views/default/input/dropdown.php
+++ views/default/input/dropdown.php
@@ -1,70 +1,9 @@
 <?php
 /**
- * Elgg dropdown input
- * Displays a dropdown (select) input field
+ * Deprecated dropdown input view - use 'input/select' instead.
  *
- * @warning Default values of FALSE or NULL will match '' (empty string) but not 0.
- *
- * @package Elgg
- * @subpackage Core
- *
- * @uses $vars['value']          The current value or an array of current values
- * @uses $vars['options']        An array of strings representing the options for the dropdown field
- * @uses $vars['options_values'] An associative array of "value" => "option"
- *                               where "value" is the name and "option" is
- * 								 the value displayed on the button. Replaces
- *                               $vars['options'] when defined.
- * @uses $vars['class']          Additional CSS class
+ * @deprecated 1.8
  */
 
-if (isset($vars['class'])) {
-	$vars['class'] = "elgg-input-dropdown {$vars['class']}";
-} else {
-	$vars['class'] = "elgg-input-dropdown";
-}
-
-$defaults = array(
-	'disabled' => false,
-	'value' => '',
-	'options_values' => array(),
-	'options' => array(),
-);
-
-$vars = array_merge($defaults, $vars);
-
-$options_values = $vars['options_values'];
-unset($vars['options_values']);
-
-$options = $vars['options'];
-unset($vars['options']);
-
-$value = is_array($vars['value'])? $vars['value'] : array($vars['value']);
-unset($vars['value']);
-
-?>
-<select <?php echo elgg_format_attributes($vars); ?>>
-<?php
-
-if ($options_values) {
-	foreach ($options_values as $opt_value => $option) {
-		$option_attrs = elgg_format_attributes(array(
-			'value' => $opt_value,
-			'selected' => in_array((string)$opt_value, $value),
-		));
-
-		echo "<option $option_attrs>$option</option>";
-	}
-} else {
-	if (is_array($options)) {
-		foreach ($options as $option) {
-
-			$option_attrs = elgg_format_attributes(array(
-				'selected' => in_array((string)$opt_value, $value)
-			));
-
-			echo "<option $option_attrs>$option</option>";
-		}
-	}
-}
-?>
-</select>
+elgg_deprecated_notice("input/dropdown was deprecated by input/select", 1.8);
+echo elgg_view('input/select', $vars);
