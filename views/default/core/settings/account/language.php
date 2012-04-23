0561b57c624cff5655d76bf3a95bac5a7d5db226
Aday Talavera <aday.talavera@gmail.com>
2012-04-23 10:28:40 +0100

    Converted old uses of input/dropdown view to input/select view

--- views/default/core/settings/account/language.php
+++ views/default/core/settings/account/language.php
@@ -11,7 +11,7 @@ $user = elgg_get_page_owner_entity();
 if ($user) {
 	$title = elgg_echo('user:set:language');
 	$content = elgg_echo('user:language:label') . ': ';
-	$content .= elgg_view("input/dropdown", array(
+	$content .= elgg_view("input/select", array(
 		'name' => 'language',
 		'value' => $user->language,
 		'options_values' => get_installed_translations()
--- views/default/forms/admin/site/update_advanced.php
+++ views/default/forms/admin/site/update_advanced.php
@@ -49,7 +49,7 @@ $form_body .= elgg_view("input/checkboxes", array(
 
 $debug_options = array('0' => elgg_echo('installation:debug:none'), 'ERROR' => elgg_echo('installation:debug:error'), 'WARNING' => elgg_echo('installation:debug:warning'), 'NOTICE' => elgg_echo('installation:debug:notice'));
 $form_body .= "<div>" . elgg_echo('installation:debug');
-$form_body .= elgg_view('input/dropdown', array(
+$form_body .= elgg_view('input/select', array(
 	'options_values' => $debug_options,
 	'name' => 'debug',
 	'value' => elgg_get_config('debug'),
