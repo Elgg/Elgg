3c06fb17da7d42af1155d5bcd686665b1293ffc8
Sem <sembrestels@riseup.net>
2012-04-28 04:44:22 +0200

    Fixes #4396. Revert avatar also deletes avatar's files and crop coords now.

--- actions/avatar/revert.php
+++ actions/avatar/revert.php
@@ -6,6 +6,25 @@
 $guid = get_input('guid');
 $user = get_entity($guid);
 if ($user) {
+	// Delete all icons from diskspace
+	$icon_sizes = elgg_get_config('icon_sizes');
+	foreach ($icon_sizes as $name => $size_info) {
+		$file = new ElggFile();
+		$file->owner_guid = $guid;
+		$file->setFilename("profile/{$guid}{$name}.jpg");
+		$filepath = $file->getFilenameOnFilestore();
+		if (!$file->delete()) {
+			elgg_log("Avatar file revert failed. Remove $filepath manually, please.", 'WARNING');
+		}
+	}
+	
+	// Revert crop coords
+	unset($user->x1);
+	unset($user->x2);
+	unset($user->y1);
+	unset($user->y2);
+	
+	// Revert icon
 	unset($user->icontime);
 	system_message(elgg_echo('avatar:revert:success'));
 } else {
