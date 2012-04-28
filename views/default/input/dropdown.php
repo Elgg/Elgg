eff95dd8e810f07e41b761e802b576e93312e58e
Aday Talavera <aday.talavera@gmail.com>
2012-04-28 13:14:50 +0100

    Updated input/dropdown deprecated message to target 1.9 version

--- views/default/input/dropdown.php
+++ views/default/input/dropdown.php
@@ -2,8 +2,8 @@
 /**
  * Deprecated dropdown input view - use 'input/select' instead.
  *
- * @deprecated 1.8
+ * @deprecated 1.9
  */
 
-elgg_deprecated_notice("input/dropdown was deprecated by input/select", 1.8);
+elgg_deprecated_notice("input/dropdown was deprecated by input/select", 1.9);
 echo elgg_view('input/select', $vars);
