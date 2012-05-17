201d455d7ab0f6a2e85e03ad5a691dd5865c0de7
Matt Beckett <beck24@gmail.com>
2012-05-17 16:16:03 -0600

    Fixes #4500

--- mod/pages/pages/pages/view.php
+++ mod/pages/pages/pages/view.php
@@ -7,39 +7,39 @@
 
 $page_guid = get_input('guid');
 $page = get_entity($page_guid);
-if (!$page) {
-	forward();
-}
 
-elgg_set_page_owner_guid($page->getContainerGUID());
+if ($page) {
+	elgg_set_page_owner_guid($page->getContainerGUID());
 
-group_gatekeeper();
+	group_gatekeeper();
 
-$container = elgg_get_page_owner_entity();
-if (!$container) {
-}
+	$container = elgg_get_page_owner_entity();
 
-$title = $page->title;
 
-if (elgg_instanceof($container, 'group')) {
-	elgg_push_breadcrumb($container->name, "pages/group/$container->guid/all");
-} else {
-	elgg_push_breadcrumb($container->name, "pages/owner/$container->username");
-}
-pages_prepare_parent_breadcrumbs($page);
-elgg_push_breadcrumb($title);
+	$title = $page->title;
 
-$content = elgg_view_entity($page, array('full_view' => true));
-$content .= elgg_view_comments($page);
+	if (elgg_instanceof($container, 'group')) {
+		elgg_push_breadcrumb($container->name, "pages/group/$container->guid/all");
+	} else {
+		elgg_push_breadcrumb($container->name, "pages/owner/$container->username");
+	}
+	pages_prepare_parent_breadcrumbs($page);
+	elgg_push_breadcrumb($title);
 
-if (elgg_get_logged_in_user_guid() == $page->getOwnerGuid()) {
-	$url = "pages/add/$page->guid";
-	elgg_register_menu_item('title', array(
+	$content = elgg_view_entity($page, array('full_view' => true));
+	$content .= elgg_view_comments($page);
+
+	if (elgg_get_logged_in_user_guid() == $page->getOwnerGuid()) {
+		$url = "pages/add/$page->guid";
+		elgg_register_menu_item('title', array(
 			'name' => 'subpage',
 			'href' => $url,
 			'text' => elgg_echo('pages:newchild'),
 			'link_class' => 'elgg-button elgg-button-action',
-	));
+		));
+	}
+} else {
+	$content = elgg_echo('pages:notfound');
 }
 
 $body = elgg_view_layout('content', array(
