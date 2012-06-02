15af429ad8a760179bab0b7ffed546735949f8f6
Cash Costello <cash.costello@gmail.com>
2012-06-02 17:44:39 -0400

    Fixes #4536 removes use of deprecated list_annotations() from pages plugin

--- mod/pages/pages/pages/history.php
+++ mod/pages/pages/pages/history.php
@@ -30,7 +30,12 @@ elgg_push_breadcrumb(elgg_echo('pages:history'));
 
 $title = $page->title . ": " . elgg_echo('pages:history');
 
-$content = list_annotations($page_guid, 'page', 20, false);
+$content = elgg_list_annotations(array(
+		'guid' => $page_guid,
+		'annotation_name' => 'page',
+		'limit' => 20,
+		'order_by' => "n_table.time_created desc"
+));
 
 $body = elgg_view_layout('content', array(
 	'filter' => '',
