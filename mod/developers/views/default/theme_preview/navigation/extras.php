b734009e41a0a88168b18d965a1434e6e421ca5d
Cash Costello <cash.costello@gmail.com>
2012-06-24 19:24:09 -0400

    Refs #3041 adds footer and entity menu. Fixes several issues with other menus.

--- mod/developers/views/default/theme_preview/navigation/extras.php
+++ mod/developers/views/default/theme_preview/navigation/extras.php
@@ -3,11 +3,11 @@
 $params = array();
 $params['menu'] = array();
 $params['menu']['default'] = array();
-for ($i=1; $i<=5; $i++) {
-	$params['menu']['default'][] = new ElggMenuItem($i, "Page $i", "$url#");
-}
-$params['class'] = 'elgg-menu-extras';
-
+$params['menu']['default'][] = new ElggMenuItem(1, elgg_view_icon('push-pin-alt'), "#");
+$params['menu']['default'][] = new ElggMenuItem(2, elgg_view_icon('rss'), "#");
+$params['menu']['default'][] = new ElggMenuItem(3, elgg_view_icon('star-alt'), "#");
+$params['name'] = 'extras';
+$params['class'] = 'elgg-menu-hz';
 
 ?>
 
--- mod/developers/views/default/theme_preview/navigation/page.php
+++ mod/developers/views/default/theme_preview/navigation/page.php
@@ -5,11 +5,10 @@ $params['menu'] = array();
 $params['menu']['default'] = array();
 for ($i=1; $i<=5; $i++) {
 	$params['menu']['default'][] = new ElggMenuItem($i, "Page $i", "#");
-	$params['menu']['alt'][] = new ElggMenuItem($i, "Page $i", "#");
 }
 $params['menu']['default'][2]->setSelected(true);
 
-$m = new ElggMenuItem(10, "Child", "$url#");
+$m = new ElggMenuItem(10, "Child", "#");
 $m->setParent($params['menu']['default'][1]);
 $params['menu']['default'][1]->addChild($m);
 ?>
