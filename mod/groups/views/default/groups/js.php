25b469d50ea1ddf0f1582f31b5abb3e66cdf0951
Sem <sembrestels@riseup.net>
2012-07-08 19:31:39 +0200

    Fixes #1854. Group owners can delegate the ownership to another group member.

--- mod/groups/views/default/groups/js.php
+++ mod/groups/views/default/groups/js.php
@@ -1,6 +1,31 @@
+<?php
+/**
+ * Javascript for Groups forms
+ *
+ * @package ElggGroups
+ */
+?>
 
 // this adds a class to support IE8 and older
 elgg.register_hook_handler('init', 'system', function() {
 	// jQuery uses 0-based indexing
 	$('#groups-tools').children('li:even').addClass('odd');
+});
+
+// Hide group owner dropdown
+elgg.register_hook_handler('init', 'system', function() {
+	$('.groups-owner-input').hide().each(function(){
+		$(this).after($(
+			'<?php
+				echo elgg_view('output/url', array(
+					'text' => elgg_echo('groups:handover'),
+					'rel' => 'toggle',
+					'class' => 'elgg-button elgg-button-delete',
+				));
+			?>'
+		).click(function(){
+			$(this).hide()
+			.prev().show();
+		}));
+	});
 });
