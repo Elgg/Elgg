e8f546a9548d3ff0f1ff6ef278ca0ddf2392df6f
Sem <sembrestels@riseup.net>
2012-07-08 05:53:15 +0200

    Refs 4014. Remade tinymce javascript for ckeditor.

---
+++ mod/ckeditor/languages/en.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * CKEditor language pack.
+ *
+ * @package ElggCKEditor
+ */
+
+$english = array(
+	'ckeditor:remove' => "Remove editor",
+	'ckeditor:add' => "Add editor",
+	'ckeditor:word_count' => 'Word count: ',
+);
+
+add_translation("en", $english);
