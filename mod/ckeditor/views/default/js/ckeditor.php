8f2edf2286e54d3ee597b40fcb4def75b7736ffd
Sem <sembrestels@riseup.net>
2012-07-12 02:41:32 +0200

    Refs #4014. Removed TinyMCE plugin.

--- mod/ckeditor/views/default/js/ckeditor.php
+++ mod/ckeditor/views/default/js/ckeditor.php
@@ -57,42 +57,6 @@ elgg.ckeditor.init = function() {
 			$('#cke_wordcount_' + CKEDITOR.currentInstance.name).html(text);
 		});
 	}, config);
-/*
-	tinyMCE.init({
-		mode : "specific_textareas",
-		editor_selector : "elgg-input-longtext",
-		theme : "advanced",
-		plugins : "lists,spellchecker,autosave,fullscreen,paste",
-		relative_urls : false,
-		remove_script_host : false,
-		document_base_url : elgg.config.wwwroot,
-		theme_advanced_buttons1 : "bold,italic,underline,separator,strikethrough,bullist,numlist,undo,redo,link,unlink,image,blockquote,code,pastetext,pasteword,more,fullscreen",
-		theme_advanced_buttons2 : "",
-		theme_advanced_buttons3 : "",
-		theme_advanced_toolbar_location : "top",
-		theme_advanced_toolbar_align : "left",
-		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true,
-		theme_advanced_path : true,
-		width : "100%",
-		extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|style],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
-		setup : function(ed) {
-			//show the number of words
-			ed.onLoadContent.add(function(ed, o) {
-				var strip = (tinyMCE.activeEditor.getContent()).replace(/(&lt;([^&gt;]+)&gt;)/ig,"");
-				var text = elgg.echo('tinymce:word_count') + strip.split(' ').length + ' ';
-				tinymce.DOM.setHTML(tinymce.DOM.get(tinyMCE.activeEditor.id + '_path_row'), text);
-			});
-
-			ed.onKeyUp.add(function(ed, e) {
-				var strip = (tinyMCE.activeEditor.getContent()).replace(/(&lt;([^&gt;]+)&gt;)/ig,"");
-				var text = elgg.echo('tinymce:word_count') + strip.split(' ').length + ' ';
-				tinymce.DOM.setHTML(tinymce.DOM.get(tinyMCE.activeEditor.id + '_path_row'), text);
-			});
-		},
-		content_css: elgg.config.wwwroot + 'mod/tinymce/css/elgg_tinymce.css'
-	});
-*/
 }
 
 elgg.register_hook_handler('init', 'system', elgg.ckeditor.init);
