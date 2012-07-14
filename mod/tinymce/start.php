3030ece211778704d8b96f57b2e98dce1eb09f9c
Matt Beckett <beck24@gmail.com>
2012-07-14 00:52:11 -0600

    taking tinymce js global

--- mod/tinymce/start.php
+++ mod/tinymce/start.php
@@ -15,7 +15,9 @@ function tinymce_init() {
 	elgg_register_js('elgg.tinymce', elgg_get_simplecache_url('js', 'tinymce'));
 	elgg_register_simplecache_view('js/tinymce');
 	
-	elgg_extend_view('input/longtext', 'tinymce/init');
+	elgg_extend_view('input/longtext', 'tinymce/inline');
+	elgg_load_js('tinymce');
+	elgg_load_js('elgg.tinymce');
 	
 	elgg_extend_view('embed/custom_insert_js', 'tinymce/embed_custom_insert_js');
 	
