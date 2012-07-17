47e12030d46c3efa3e5ecdfe81d35eeb30d75ccc
Sem <sembrestels@riseup.net>
2012-07-18 01:52:12 +0200

    Refs #4680. Added 'groupname' functions in group lib and class.

--- engine/lib/group.php
+++ engine/lib/group.php
@@ -25,6 +25,40 @@ function get_group_entity_as_row($guid) {
 }
 
 /**
+ * Get group by groupname
+ *
+ * @param string $groupname The group's groupname
+ *
+ * @return ElggGroup|false Depending on success
+ * @since 1.9
+ */
+function get_group_by_groupname($groupname) {
+	global $CONFIG, $GROUPNAME_TO_GUID_MAP_CACHE;
+
+	$groupname = sanitise_string($groupname);
+	$access = get_access_sql_suffix('e');
+
+	// Caching
+	if ((isset($GROUPNAME_TO_GUID_MAP_CACHE[$groupname]))
+	&& (retrieve_cached_entity($GROUPNAME_TO_GUID_MAP_CACHE[$groupname]))) {
+		return retrieve_cached_entity($GROUPNAME_TO_GUID_MAP_CACHE[$groupname]);
+	}
+
+	$query = "SELECT e.* from {$CONFIG->dbprefix}groups_entity g
+		join {$CONFIG->dbprefix}entities e on e.guid=g.guid
+		where g.groupname='$groupname' and $access ";
+
+	$entity = get_data_row($query, 'entity_row_to_elggstar');
+	if ($entity) {
+		$GROUPNAME_TO_GUID_MAP_CACHE[$groupname] = $entity->guid;
+	} else {
+		$entity = false;
+	}
+
+	return $entity;
+}
+
+/**
  * Create or update the entities table for a given group.
  * Call create_entity first.
  *
@@ -34,11 +68,12 @@ function get_group_entity_as_row($guid) {
  *
  * @return bool
  */
-function create_group_entity($guid, $name, $description) {
+function create_group_entity($guid, $name, $groupname, $description) {
 	global $CONFIG;
 
 	$guid = (int)$guid;
 	$name = sanitise_string($name);
+	$groupname = sanitise_string($groupname);
 	$description = sanitise_string($description);
 
 	$row = get_entity_as_row($guid);
@@ -48,7 +83,7 @@ function create_group_entity($guid, $name, $description) {
 		$exists = get_data_row("SELECT guid from {$CONFIG->dbprefix}groups_entity WHERE guid = {$guid}");
 		if ($exists) {
 			$query = "UPDATE {$CONFIG->dbprefix}groups_entity set"
-				. " name='$name', description='$description' where guid=$guid";
+				. " name='$name', groupname='$groupname', description='$description' where guid=$guid";
 			$result = update_data($query);
 			if ($result != false) {
 				// Update succeeded, continue
@@ -62,7 +97,7 @@ function create_group_entity($guid, $name, $description) {
 		} else {
 			// Update failed, attempt an insert.
 			$query = "INSERT into {$CONFIG->dbprefix}groups_entity"
-				. " (guid, name, description) values ($guid, '$name', '$description')";
+				. " (guid, name, groupname, description) values ($guid, '$name', '$groupname', '$description')";
 
 			$result = insert_data($query);
 			if ($result !== false) {
