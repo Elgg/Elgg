6621e2d73395a9b15abd2cf6fe539317f9a5f03f
Sem <sembrestels@riseup.net>
2012-07-18 17:14:18 +0200

    Refs #4014. Moved CKEditor anonymous wordcount funciton and config outside of init function.

--- mod/ckeditor/views/default/js/ckeditor.php
+++ mod/ckeditor/views/default/js/ckeditor.php
@@ -12,13 +12,47 @@ elgg.ckeditor.toggleEditor = function(event) {
 	var target = $(this).attr('href');
 	
 	if (!$(target).data('ckeditorInstance')) {
-		$(target).ckeditor();
+		$(target).ckeditor(elgg.ckeditor.wordCount, elgg.ckeditor.config);
 		$(this).html(elgg.echo('ckeditor:remove'));
 	} else {
 		$(target).ckeditorGet().destroy();
 		$(this).html(elgg.echo('ckeditor:add'));
 	}
 }
+
+/**
+ * Toggles the CKEditor
+ *
+ * @param {Object} event
+ * @return void
+ */
+elgg.ckeditor.wordCount = function() {
+	$('#cke_bottom_' + this.name).prepend(
+		'<div id="cke_wordcount_' + this.name + '" class="cke_wordcount">' + 
+			elgg.echo('ckeditor:word_count') + '0' +
+		'</div>'   
+	);
+	this.document.on('keyup', function(event) {
+		//show the number of words
+		var words = this.getBody().getText().trim().split(' ').length;
+		var text = elgg.echo('ckeditor:word_count') + words + ' ';
+		$('#cke_wordcount_' + CKEDITOR.currentInstance.name).html(text);
+	});
+}
+
+elgg.ckeditor.config = {
+	toolbar : [['Bold', 'Italic', 'Underline', '-', 'Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
+	toolbarCanCollapse : false,
+	baseHref : elgg.config.wwwroot,
+	removePlugins : 'contextmenu',
+	defaultLanguage : 'en',
+	language : elgg.config.language,
+	resize_maxWidth : '100%',
+	skin : 'BootstrapCK-Skin',
+	uiColor : '#EEEEEE',
+	contentsCss : elgg.config.wwwroot + 'mod/ckeditor/css/elgg_ckeditor.css',
+	disableNativeSpellChecker : false,
+};
 
 /**
  * CKEditor initialization script
@@ -29,34 +63,7 @@ elgg.ckeditor.toggleEditor = function(event) {
 elgg.ckeditor.init = function() {
 
 	$('.ckeditor-toggle-editor').live('click', elgg.ckeditor.toggleEditor);
-	
-	config = {
-		toolbar : [['Bold', 'Italic', 'Underline', '-', 'Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
-		toolbarCanCollapse : false,
-		baseHref : elgg.config.wwwroot,
-		removePlugins : 'contextmenu',
-		defaultLanguage : 'en',
-		language : elgg.config.language,
-		resize_maxWidth : '100%',
-		skin : 'BootstrapCK-Skin',
-		uiColor : '#EEEEEE',
-		contentsCss : elgg.config.wwwroot + 'mod/ckeditor/css/elgg_ckeditor.css',
-		disableNativeSpellChecker : false,
-	};
-	
-	$('.elgg-input-longtext').ckeditor(function() {
-		$('#cke_bottom_' + this.name).prepend(
-			'<div id="cke_wordcount_' + this.name + '" class="cke_wordcount">' + 
-				elgg.echo('ckeditor:word_count') + '0' +
-			'</div>'   
-		);
-		this.document.on('keyup', function(event) {
-			//show the number of words
-			var words = this.getBody().getText().trim().split(' ').length;
-			var text = elgg.echo('ckeditor:word_count') + words + ' ';
-			$('#cke_wordcount_' + CKEDITOR.currentInstance.name).html(text);
-		});
-	}, config);
+	$('.elgg-input-longtext').ckeditor(elgg.ckeditor.wordCount, elgg.ckeditor.config);
 }
 
 elgg.register_hook_handler('init', 'system', elgg.ckeditor.init);
