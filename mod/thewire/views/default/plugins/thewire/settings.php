09bec543a5658573bb4c9065d309b1a4d1fa46d6
Evan Winslow <evan@elgg.org>
2012-08-25 20:07:35 -0700

    Using number input instead of select on thewire settings page

--- mod/thewire/views/default/plugins/thewire/settings.php
+++ mod/thewire/views/default/plugins/thewire/settings.php
@@ -1,9 +1,15 @@
-<p>
-  <?php echo elgg_echo('thewire:settings:limit'); ?>
- 
-  <select name="params[limit]">
-  	<option value="0" <?php if ($vars['entity']->limit == 0) echo 'selected = "selected"'; ?>><?php echo elgg_echo('thewire:settings:limit:none'); ?></option>
-  	<option value="140" <?php if ($vars['entity']->limit == 140) echo 'selected = "selected"'; ?>>140</option>
-  	<option value="256" <?php if ($vars['entity']->limit == 256) echo 'selected = "selected"'; ?>>256</option>
-  </select>
-</p>
+<?php
+
+$plugin = $vars['entity'];
+
+?>
+
+<div>
+	<label for="thewire-limit"><?php echo elgg_echo('thewire:settings:limit'); ?></label><br/>
+	<input type="number" name="params[limit]" value="<?php echo $plugin->limit; ?>" id="thewire-limit" list="thewire-limit-list" />
+	<datalist id="thewire-limit-list">
+		<option>0</option>
+		<option>140</option>
+		<option>256</option>
+	</datalist>
+</div>
