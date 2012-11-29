c773f7fe0f7820b7e058c17fd1d1f7b5abec32ab
ura soul <ura@infiniteeureka.com>
2012-11-29 20:54:47 +0000

    Update views/default/page/components/list.php
    correcting logic

--- views/default/page/components/list.php
+++ views/default/page/components/list.php
@@ -65,26 +65,27 @@ if (is_array($items) && count($items) > 0) {
 }
 
 if ($position == 'before' || $position == 'both') {
-	$html = $nav . $html;
-}
-if ($position == 'before' || $position == 'both') {
     $html = $nav . $html;
 }
 if (($count > 0) && ($context != 'widgets')){
 	$from_count = $offset + 1;
+	$to_count = ' to ';
 	if ($from_count < $count){
-		$to_count = ' to ' . ($offset + $limit);
+		if (($count - $from_count) < $limit ){
+			$to_count .= $count;
+		}
+		else {
+			$to_count .= ($offset + $limit);	
+		}
 	}
 	else {
 		$to_count = '';
 	}
-	$html .= '<div class="elgg-list-count">displaying: ' . $from_count . $to_count . ' of ' . $count . '</div>';
+	$html .= '<div class="list-count">displaying: ' . $from_count . $to_count . ' of ' . $count . '</div>';
 }
+
 if ($position == 'after' || $position == 'both') {
     $html .= $nav;
-}
-if ($position == 'after' || $position == 'both') {
-	$html .= $nav;
 }
 
 echo $html;
