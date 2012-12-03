8c581fa6f587e8f4b1ae15034c7e20cbb44e4daa
Evan Winslow <evan.b.winslow@gmail.com>
2012-12-03 02:43:28 -0500

    Fixes travis php errors about auto-creating objects from empty values

--- engine/tests/phpunit/bootstrap.php
+++ engine/tests/phpunit/bootstrap.php
@@ -1,6 +1,13 @@
 <?php
-
 $engine = dirname(dirname(__DIR__));
+
+/**
+ * This is here as a temporary solution only. Instead of adding more global
+ * state to this file as we migrate tests, try to refactor the code to be
+ * testable without global state.
+ */
+global $CONFIG;
+$CONFIG = new stdClass;
 
 // Set up class auto-loading
 require_once "$engine/lib/elgglib.php";
