5a8d52cc6468073d6dd366dcf92f53a7a3b051fd
Paweł Sroka <srokap@gmail.com>
2012-12-25 21:59:11 +0100

    Refs #4918 - Moves developers functions to class methods.

--- mod/developers/classes/ElggDevelopersService.php
+++ mod/developers/classes/ElggDevelopersService.php
@@ -56,7 +56,7 @@ class ElggDevelopersService {
 	 * @return string
 	 */
 	function displayQueryData($hook, $type, $returnvalue, $params) {
-		global $dbcalls, $START_MICROTIME;
+		global $START_MICROTIME;
 		$this->collectQueryData('foot');
 
 		$output = '<pre>';
@@ -70,4 +70,125 @@ class ElggDevelopersService {
 		return $returnvalue;
 	}
 
+	/**
+	 * Clear all the strings so the raw descriptor strings are displayed
+	 */
+	function clearStrings() {
+		global $CONFIG;
+
+		$language = get_language();
+		$CONFIG->translations[$language] = array();
+		$CONFIG->translations['en'] = array();
+	}
+
+	/**
+	 * Post-process a view to add wrapper comments to it
+	 */
+	function wrapViews($hook, $type, $result, $params) {
+		if (elgg_get_viewtype() != "default") {
+			return;
+		}
+
+		$excluded_bases = array('css', 'js', 'input', 'output', 'embed', 'icon',);
+
+		$excluded_views = array(
+			'page/default',
+			'page/admin',
+			'page/elements/head',
+		);
+
+		$view = $params['view'];
+
+		$view_hierarchy = explode('/',$view);
+		if (in_array($view_hierarchy[0], $excluded_bases)) {
+			return;
+		}
+
+		if (in_array($view, $excluded_views)) {
+			return;
+		}
+
+		if ($result) {
+			$result = "<!-- developers:begin $view -->$result<!-- developers:end $view -->";
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Log the events and plugin hooks
+	 */
+	function logEvents($name, $type) {
+
+		// filter out some very common events
+		if ($name == 'view' || $name == 'display' || $name == 'log' || $name == 'debug') {
+			return;
+		}
+		if ($name == 'session:get' || $name == 'validate') {
+			return;
+		}
+
+		$stack = debug_backtrace();
+		if ($stack[2]['function'] == 'elgg_trigger_event') {
+			$event_type = 'Event';
+		} else {
+			$event_type = 'Plugin hook';
+		}
+		$function = $stack[3]['function'] . '()';
+		if ($function == 'require_once' || $function == 'include_once') {
+			$function = $stack[3]['file'];
+		}
+
+		$msg = elgg_echo('developers:event_log_msg', array(
+			$event_type,
+			$name,
+			$type,
+			$function,
+		));
+		elgg_dump($msg, false, 'WARNING');
+
+		unset($stack);
+	}
+
+	/**
+	 * Serve the theme preview pages
+	 *
+	 * @param array $page
+	 * @return bool
+	 */
+	function themePreviewController($page) {
+		if (!isset($page[0])) {
+			forward('theme_preview/general');
+		}
+
+		$pages = array(
+			'buttons',
+			'components',
+			'forms',
+			'grid',
+			'icons',
+			'modules',
+			'navigation',
+			'typography',
+		);
+
+		foreach ($pages as $page_name) {
+			elgg_register_menu_item('page', array(
+				'name' => $page_name,
+				'text' => elgg_echo("theme_preview:$page_name"),
+				'href' => "theme_preview/$page_name",
+			));
+		}
+
+		$title = elgg_echo("theme_preview:{$page[0]}");
+		$body =  elgg_view("theme_preview/{$page[0]}");
+
+		$layout = elgg_view_layout('one_sidebar', array(
+			'title' => $title,
+			'content' => $body,
+		));
+
+		echo elgg_view_page($title, $layout, 'theme_preview');
+		return true;
+	}
 }
--- mod/developers/start.php
+++ mod/developers/start.php
@@ -51,18 +51,21 @@ function developers_process_settings() {
 	}
 
 	if (elgg_get_plugin_setting('show_strings', 'developers') == 1) {
+		$developers = ElggDevelopersService::getInstance();
 		// first and last in case a plugin registers a translation in an init method
-		elgg_register_event_handler('init', 'system', 'developers_clear_strings', 1000);
-		elgg_register_event_handler('init', 'system', 'developers_clear_strings', 1);
+		elgg_register_event_handler('init', 'system', array($developers, 'clearStrings'), 1000);
+		elgg_register_event_handler('init', 'system', array($developers, 'clearStrings'), 1);
 	}
 
 	if (elgg_get_plugin_setting('wrap_views', 'developers') == 1) {
-		elgg_register_plugin_hook_handler('view', 'all', 'developers_wrap_views');
+		$developers = ElggDevelopersService::getInstance();
+		elgg_register_plugin_hook_handler('view', 'all', array($developers, 'wrapViews'));
 	}
 
 	if (elgg_get_plugin_setting('log_events', 'developers') == 1) {
-		elgg_register_event_handler('all', 'all', 'developers_log_events', 1);
-		elgg_register_plugin_hook_handler('all', 'all', 'developers_log_events', 1);
+		$developers = ElggDevelopersService::getInstance();
+		elgg_register_event_handler('all', 'all', array($developers, 'logEvents'), 1);
+		elgg_register_plugin_hook_handler('all', 'all', array($developers, 'logEvents'), 1);
 	}
 }
 
@@ -84,123 +87,12 @@ function developers_setup_menu() {
 }
 
 /**
- * Clear all the strings so the raw descriptor strings are displayed
- */
-function developers_clear_strings() {
-	global $CONFIG;
-
-	$language = get_language();
-	$CONFIG->translations[$language] = array();
-	$CONFIG->translations['en'] = array();
-}
-
-/**
- * Post-process a view to add wrapper comments to it
- */
-function developers_wrap_views($hook, $type, $result, $params) {
-	if (elgg_get_viewtype() != "default") {
-		return;
-	}
-
-	$excluded_bases = array('css', 'js', 'input', 'output', 'embed', 'icon',);
-
-	$excluded_views = array(
-		'page/default',
-		'page/admin',
-		'page/elements/head',
-	);
-
-	$view = $params['view'];
-
-	$view_hierarchy = explode('/',$view);
-	if (in_array($view_hierarchy[0], $excluded_bases)) {
-		return;
-	}
-
-	if (in_array($view, $excluded_views)) {
-		return;
-	}
-
-	if ($result) {
-		$result = "<!-- developers:begin $view -->$result<!-- developers:end $view -->";
-	}
-
-	return $result;
-}
-
-/**
- * Log the events and plugin hooks
- */
-function developers_log_events($name, $type) {
-
-	// filter out some very common events
-	if ($name == 'view' || $name == 'display' || $name == 'log' || $name == 'debug') {
-		return;
-	}
-	if ($name == 'session:get' || $name == 'validate') {
-		return;
-	}
-
-	$stack = debug_backtrace();
-	if ($stack[2]['function'] == 'elgg_trigger_event') {
-		$event_type = 'Event';
-	} else {
-		$event_type = 'Plugin hook';
-	}
-	$function = $stack[3]['function'] . '()';
-	if ($function == 'require_once' || $function == 'include_once') {
-		$function = $stack[3]['file'];
-	}
-
-	$msg = elgg_echo('developers:event_log_msg', array(
-		$event_type,
-		$name,
-		$type,
-		$function,
-	));
-	elgg_dump($msg, false, 'WARNING');
-
-	unset($stack);
-}
-
-/**
  * Serve the theme preview pages
  *
  * @param array $page
  * @return bool
  */
 function developers_theme_preview_controller($page) {
-	if (!isset($page[0])) {
-		forward('theme_preview/general');
-	}
-
-	$pages = array(
-		'buttons',
-		'components',
-		'forms',
-		'grid',
-		'icons',
-		'modules',
-		'navigation',
-		'typography',
-	);
-
-	foreach ($pages as $page_name) {
-		elgg_register_menu_item('page', array(
-			'name' => $page_name,
-			'text' => elgg_echo("theme_preview:$page_name"),
-			'href' => "theme_preview/$page_name",
-		));
-	}
-
-	$title = elgg_echo("theme_preview:{$page[0]}");
-	$body =  elgg_view("theme_preview/{$page[0]}");
-
-	$layout = elgg_view_layout('one_sidebar', array(
-		'title' => $title,
-		'content' => $body,
-	));
-
-	echo elgg_view_page($title, $layout, 'theme_preview');
-	return true;
+	$developers = ElggDevelopersService::getInstance();
+	return $developers->themePreviewController($page);
 }
