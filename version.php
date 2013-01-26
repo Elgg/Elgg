8d3a7ab1755829c6e070a038d6b33e326de8fc8f
cash <cash.costello@gmail.com>
2013-01-26 15:11:28 -0500

    updated version for 1.8.13 release

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 
 // YYYYMMDD = Elgg Date
 // XX = Interim incrementer
-$version = 2012120500;
+$version = 2013012900;
 
 // Human-friendly version name
-$release = '1.8.12';
+$release = '1.8.13';
