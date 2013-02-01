feed34eaf567924394bdcc0463c4d21231ad38d3
Cash Costello <cash.costello@gmail.com>
2013-02-01 07:22:03 -0500

    Fixes #5022 not setting forward on ajax calls for walled garden

--- engine/classes/ElggSite.php
+++ engine/classes/ElggSite.php
@@ -381,7 +381,9 @@ class ElggSite extends ElggEntity {
 				elgg_register_plugin_hook_handler('index', 'system', 'elgg_walled_garden_index', 1);
 
 				if (!$this->isPublicPage()) {
-					$_SESSION['last_forward_from'] = current_page_url();
+					if (!elgg_is_xhr()) {
+						$_SESSION['last_forward_from'] = current_page_url();
+					}
 					register_error(elgg_echo('loggedinrequired'));
 					forward();
 				}
@@ -442,8 +444,6 @@ class ElggSite extends ElggEntity {
 
 		// include a hook for plugin authors to include public pages
 		$plugins = elgg_trigger_plugin_hook('public_pages', 'walled_garden', NULL, array());
-
-		// lookup admin-specific public pages
 
 		// allow public pages
 		foreach (array_merge($defaults, $plugins) as $public) {
