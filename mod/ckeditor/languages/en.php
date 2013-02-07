4db54523b523ccedeb158827e2aeb686cc1a6b3d
cash <cash.costello@gmail.com>
2013-02-07 18:18:29 -0500

    storing uploaded photo in user's assets directory

--- mod/ckeditor/languages/en.php
+++ mod/ckeditor/languages/en.php
@@ -11,6 +11,8 @@ $english = array(
 	'ckeditor:word_count' => 'Word count: ',
 	
 	'ckeditor:failure:upload' => 'Image upload failed',
+	'ckeditor:failure:resize' => 'Failed to resize the uploaded image',
+	'ckeditor:failure:too_big' => 'The image is too large. Please resize before uploading',
 );
 
 add_translation("en", $english);
