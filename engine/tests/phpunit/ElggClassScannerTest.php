b041c654c0d35236809dc3fbc80a7d3617c408fb
Steve Clay <steve@mrclay.org>
2013-02-07 16:06:01 -0500

    Make tests align with token_get_all's real abilities

--- engine/tests/phpunit/ElggClassScannerTest.php
+++ engine/tests/phpunit/ElggClassScannerTest.php
@@ -23,6 +23,9 @@ class ElggClassScannerTest extends PHPUnit_Framework_TestCase {
 				'NS4_20130207\\T4_20130207' => "$dir{$ds}4.php",
 			);
 		} elseif (version_compare(PHP_VERSION, '5.3', '>=')) {
+
+			// PHP 5.3 just ignores the traits, so as long as these are in separate files, there should be
+			// no problem at runtime
 			$expected = array(
 				'I1_20130207' => "$dir{$ds}1.php",
 				'C1_20130207' => "$dir{$ds}1.php",
@@ -39,6 +42,10 @@ class ElggClassScannerTest extends PHPUnit_Framework_TestCase {
 				//'NS4_20130207\\T4_20130207' => "$dir{$ds}4.php",
 			);
 		} else {
+
+			// PHP 5.2 also ignores the namespace declaration, making the class map think every class/interface
+			// is global. If namespaced classnames collide with global classnames, at runtime the engine
+			// may load the namespaced code, resulting in a fatal parse error.
 			$expected = array(
 				'I1_20130207' => "$dir{$ds}1.php",
 				'C1_20130207' => "$dir{$ds}1.php",
@@ -49,10 +56,14 @@ class ElggClassScannerTest extends PHPUnit_Framework_TestCase {
 
 				//'NS3_20130207\\I3_20130207' => "$dir{$ds}3.php",
 				//'NS3_20130207\\C3_20130207' => "$dir{$ds}3.php",
+				'I3_20130207' => "$dir{$ds}3.php",
+				'C3_20130207' => "$dir{$ds}3.php",
 
 				//'NS4_20130207\\I4_20130207' => "$dir{$ds}4.php",
 				//'NS4_20130207\\C4_20130207' => "$dir{$ds}4.php",
 				//'NS4_20130207\\T4_20130207' => "$dir{$ds}4.php",
+				'I4_20130207' => "$dir{$ds}4.php",
+				'C4_20130207' => "$dir{$ds}4.php",
 			);
 		}
 
