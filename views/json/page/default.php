803e6549458bb17e24b995a49713b2fcc988d6f9
cash <cash.costello@gmail.com>
2013-02-16 14:32:42 -0500

    added deprecation notice and backward compatibility for json output

--- views/json/page/default.php
+++ views/json/page/default.php
@@ -11,3 +11,10 @@
 header("Content-Type: application/json");
 
 echo $vars['body'];
+
+// backward compatibility
+global $jsonexport;
+if (isset($jsonexport)) {
+	elgg_deprecated_notice("Using \$jsonexport to produce json output has been deprecated", 1.9);
+	echo json_encode($jsonexport);
+}
