04dad1a5cd6379cba05cdb5ab33572b7ebf11952
Sem <sembrestels@riseup.net>
2013-02-21 04:24:40 +0100

    Refs #4670. Better if begining from first created users: no problems with new registrations.

--- mod/adminshout/actions/adminshout/send.php
+++ mod/adminshout/actions/adminshout/send.php
@@ -18,13 +18,14 @@ $options = array(
 	'type' => 'user',
 	'offset' => $offset,
 	'limit' => $limit,
+	'order_by'=> 'e.time_created ASC',
 );
 $total = elgg_get_entities(array_merge($options, array('count' => true)));
-$batch = new ElggBatch('elgg_get_entities', $options);
+$users = elgg_get_entities($options);
 
-foreach ($batch as $user) {
+foreach ($users as $user) {
 	notify_user($user->guid, elgg_get_logged_in_user_guid(), $subject, $message, array(), 'email');
 }
 
-$sent = $offset + count($batch);
+$sent = $offset + count($users);
 echo "{\"sent\": $sent, \"total\": $total}";
