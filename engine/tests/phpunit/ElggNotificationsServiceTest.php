1d0eeb519637f1e86810620d8b67656b985ff737
cash <cash.costello@gmail.com>
2013-02-23 21:01:59 -0500

    ported notifications code to use DIC with service class

---
+++ engine/tests/phpunit/ElggNotificationsServiceTest.php
@@ -0,0 +1,77 @@
+<?php
+
+$engine = dirname(dirname(dirname(__FILE__)));
+require_once "$engine/lib/entities.php";
+
+class ElggNotificationsServiceTest extends PHPUnit_Framework_TestCase {
+
+	public function testRegisterEvent() {
+		$service = new Elgg_Notifications_Service(new Elgg_Util_MemoryQueue());
+
+		$service->registerEvent('foo', 'bar');
+		$events = array(
+			'foo' => array(
+				'bar' => array('create')
+			)
+		);
+		$this->assertEquals($events, $service->getEvents());
+
+		$service->registerEvent('foo', 'bar', array('test'));
+		$events['foo']['bar'] = array('create', 'test');
+		$this->assertEquals($events, $service->getEvents());
+
+		$service->registerEvent('foo', 'bar');
+		$this->assertEquals($events, $service->getEvents());
+	}
+
+	public function testUnregisterEvent() {
+		$service = new Elgg_Notifications_Service(new Elgg_Util_MemoryQueue());
+
+		$service->registerEvent('foo', 'bar');
+		$this->assertTrue($service->unregisterEvent('foo', 'bar'));
+
+		$events = array(
+			'foo' => array()
+		);
+		$this->assertEquals($events, $service->getEvents());
+		$this->assertFalse($service->unregisterEvent('foo', 'bar'));
+	}
+
+	public function testRegisterMethod() {
+		$service = new Elgg_Notifications_Service(new Elgg_Util_MemoryQueue());
+
+		$service->registerMethod('foo');
+		$methods = array('foo' => 'foo');
+		$this->assertEquals($methods, $service->getMethods());
+	}
+
+	public function testUnregisterMethod() {
+		$service = new Elgg_Notifications_Service(new Elgg_Util_MemoryQueue());
+
+		$service->registerMethod('foo');
+		$this->assertTrue($service->unregisterMethod('foo'));
+		$this->assertEquals(array(), $service->getMethods());
+		$this->assertFalse($service->unregisterMethod('foo'));
+	}
+
+	public function testEnqueueEvent() {
+		$queue = new Elgg_Util_MemoryQueue();
+		$service = new Elgg_Notifications_Service($queue);
+
+		$service->registerEvent('object', 'bar');
+		$object = new ElggObject();
+		$object->subtype = 'bar';
+		$service->enqueueEvent('create', '', $object);
+		$event = new Elgg_Notifications_Event($object, 'create');
+		$this->assertEquals($event, $queue->dequeue());
+		$this->assertNull($queue->dequeue());
+
+		// unregistered action type
+		$service->enqueueEvent('null', '', $object);
+		$this->assertNull($queue->dequeue());
+
+		// unregistered object type
+		$service->enqueueEvent('create', '', new ElggObject());
+		$this->assertNull($queue->dequeue());
+	}
+}
