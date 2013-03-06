4b889e9cc7c629bb408fc8e470a80a7ab283fffc
cash <cash.costello@gmail.com>
2013-03-06 15:26:14 -0500

    Fixes #4566 upgrades location metadata on users

--- version.php
+++ version.php
@@ -5,13 +5,13 @@
  * This is compared against the values stored in the database to determine
  * whether upgrades should be performed.
  *
- * @package Elgg
+ * @package    Elgg
  * @subpackage Core
  */
 
 // YYYYMMDD = Elgg Date
 // XX = Interim incrementer
-$version = 2013012900;
+$version = 2013030600;
 
 // Human-friendly version name
-$release = '1.8.13';
+$release = '1.8.14';
