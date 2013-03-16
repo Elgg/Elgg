b9d4494f187f05e92d56705105ce568db1543807
Sem <sembrestels@riseup.net>
2013-03-16 03:04:31 +0100

    Removed categories plugin.

--- mod/categories/deactivate.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Remove admin notice to populate categories.
- */
-
-elgg_delete_admin_notice('categories_admin_notice_no_categories');
--- mod/categories/views/default/categories/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @deprecated 1.8
- */
-
-elgg_deprecated_notice("Use output/categories instead of categories/view", 1.8);
-
-echo elgg_view('output/categories', $vars);
