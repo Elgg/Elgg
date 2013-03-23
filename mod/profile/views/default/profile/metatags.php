8dcd914bf6e2042e0eff1dcfa5094a0282fc8703
Cash Costello <cash.costello@gmail.com>
2013-03-23 09:01:01 -0400

    Refs #4801 change link type for FOAF for HTML5 validation

--- mod/profile/views/default/profile/metatags.php
+++ mod/profile/views/default/profile/metatags.php
@@ -10,7 +10,7 @@ $owner = elgg_get_page_owner_entity();
 
 if (elgg_instanceof($owner, 'user')) {
 ?>
-	<link rel="meta" type="application/rdf+xml" title="FOAF" href="<?php echo full_url(); ?>?view=foaf" />
+	<link rel="alternate" type="application/rdf+xml" title="FOAF" href="<?php echo full_url(); ?>?view=foaf" />
 <?php
 
 }
