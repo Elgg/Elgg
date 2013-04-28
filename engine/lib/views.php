5a8dd28bd02e670bda7d143e8778067567610986
Cash Costello <cash.costello@gmail.com>
2013-04-28 14:32:06 -0400

    Fixes #5407 colorbox theme

--- engine/lib/views.php
+++ engine/lib/views.php
@@ -1334,9 +1334,7 @@ function elgg_views_boot() {
 	$lightbox_js_url = elgg_get_simplecache_url('js', 'lightbox');
 	elgg_register_js('lightbox', $lightbox_js_url);
 
-	elgg_register_simplecache_view('css/lightbox');
-	$lightbox_css_url = elgg_get_simplecache_url('css', 'lightbox');
-	elgg_register_css('lightbox', $lightbox_css_url);
+	elgg_register_css('lightbox', 'vendors/jquery/colorbox/theme/colorbox.css');
 
 	elgg_register_simplecache_view('css/elgg');
 	$elgg_css_url = elgg_get_simplecache_url('css', 'elgg');
