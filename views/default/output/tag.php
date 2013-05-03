0726860ca04b4f2ed9b010cc5c5692c23c32e2d2
cash <cash.costello@gmail.com>
2013-05-02 20:40:50 -0400

    Fixes #5421 fixes some encoding issues where wrong context was used

--- views/default/output/tag.php
+++ views/default/output/tag.php
@@ -8,25 +8,25 @@
  *
  */
 
+if (!empty($vars['type'])) {
+	$type = "&type=" . rawurlencode($vars['type']);
+} else {
+	$type = "";
+}
 if (!empty($vars['subtype'])) {
-	$subtype = "&subtype=" . urlencode($vars['subtype']);
+	$subtype = "&subtype=" . rawurlencode($vars['subtype']);
 } else {
 	$subtype = "";
 }
 if (!empty($vars['object'])) {
-	$object = "&object=" . urlencode($vars['object']);
+	$object = "&object=" . rawurlencode($vars['object']);
 } else {
 	$object = "";
 }
 
 if (isset($vars['value'])) {
+	$url = elgg_get_site_url() . 'search?q=' . rawurlencode($vars['value']) . "&search_type=tags{$type}{$subtype}{$object}";
 	$vars['value'] = htmlspecialchars($vars['value'], ENT_QUOTES, 'UTF-8', false);
-	if (!empty($vars['type'])) {
-		$type = "&type={$vars['type']}";
-	} else {
-		$type = "";
-	}
-	$url = elgg_get_site_url() . 'search?q=' . urlencode($vars['value']) . "&search_type=tags{$type}{$subtype}{$object}";
 	echo elgg_view('output/url', array(
 		'href' => $url,
 		'text' => $vars['value'],
