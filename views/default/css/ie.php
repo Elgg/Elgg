b3ffbc1cbeb0720e59140720ba3f8b5b0f171a06
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-05-03 14:48:50 +0300

    fixes jumping avatar icons IE8
    fixes issue in ie8 hovering over an avatar in a widget or group sidebar listing

--- views/default/css/ie.php
+++ views/default/css/ie.php
@@ -6,3 +6,13 @@
 .elgg-avatar {
 	display: block;
 }
+
+/* fixes issue in ie8 hovering over an avatar in a widget or group sidebar listing */
+.elgg-gallery-users > .elgg-item {
+	float: left;
+	margin-bottom: 5px;
+}
+
+.elgg-gallery-users {
+	display: inline-block;
+}
