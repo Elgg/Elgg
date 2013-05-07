d6d2fbaa2125c0c4f101b5d8739de4b35135d4b5
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-05-07 14:10:44 +0300

    Updated comments to make use of the new river target.

--- views/default/river/object/comment/create.php
+++ views/default/river/object/comment/create.php
@@ -2,10 +2,13 @@
 /**
  * Post comment river view
  */
- 
+
 $comment = $vars['item']->getObjectEntity();
+
+$summary = elgg_view('river/object/comment/summary', $vars);
 
 echo elgg_view('river/elements/layout', array(
 	'item' => $vars['item'],
 	'message' => elgg_get_excerpt($comment->description),
+	'summary' => $summary,
 ));
