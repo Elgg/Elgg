eb3e9b9e2490b86044da2a12f1207a67f404a1c7
Paweł Sroka <srokap@gmail.com>
2013-05-08 22:50:46 +0200

    Fixed web_services test

--- mod/web_services/tests/ElggCoreWebServicesApiTest.php
+++ mod/web_services/tests/ElggCoreWebServicesApiTest.php
@@ -300,7 +300,7 @@ class ElggCoreWebServicesApiTest extends ElggCoreUnitTest {
 	public function testApiAuthKeyBadKey() {
 		global $CONFIG;
 		
-		$CONFIG->input['api_key'] = 'BAD';
+		set_input('api_key', 'BAD');
 		try {
 			api_auth_key();
 			$this->assertTrue(FALSE);
