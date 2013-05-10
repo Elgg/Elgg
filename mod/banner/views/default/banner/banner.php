3d4474f14b19a6ca01a8f3d9b1b77db398cda288
aleph <aleph@riseup.net>
2013-05-11 00:56:45 +0200

    Make the banner appear only in front page. Css styles to close button. Reupload js.php

--- mod/banner/views/default/banner/banner.php
+++ mod/banner/views/default/banner/banner.php
@@ -4,9 +4,8 @@ $cookie_timestamp = $_COOKIE['banner'];
 
 $timestamp = elgg_get_plugin_setting("timestamp", "banner");
 
-if ($timestamp != $cookie_timestamp) {
+if (full_url() == elgg_get_site_url() && $timestamp != $cookie_timestamp) {
 ?>
-
 	<div class="elgg-banner">
 		<a href="javascript:void(0);" class="elgg-banner-close" data-timestamp="<?php echo elgg_echo($timestamp); ?>"><?php echo elgg_echo('close'); ?></a>
 		<? echo elgg_get_plugin_setting("text", "banner"); ?>
