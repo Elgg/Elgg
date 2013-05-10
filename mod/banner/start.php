e50242f448c54612f3d10555c3803764e2d725e6
aleph <aleph@riseup.net>
2013-05-10 08:34:05 +0200

    Implementation of storing the user closing action on cookie to avoid showing the banner from this time on.

--- mod/banner/start.php
+++ mod/banner/start.php
@@ -3,11 +3,22 @@ elgg_register_event_handler('init', 'system', 'banner_init');
 
 function banner_init() {
 	
-	
-	elgg_extend_view("page/elements/body", "banner/banner", 0);
-	elgg_register_action("banner/banner", elgg_get_plugins_path() . "banner/actions/banner.php");
-	elgg_extend_view('css','banner/css');
+	$opened = banner_get_cookie();
+
+	if ($opened) {
+		elgg_extend_view("page/elements/body", "banner/banner", 0);
+		elgg_register_action("banner/banner", elgg_get_plugins_path() . "banner/actions/banner.php");
+		elgg_extend_view('css','banner/css');
+		elgg_extend_view('js/elgg', 'banner/js');
+	}	
 	
 }
 
 
+function banner_get_cookie() {
+
+	echo var_dump($_COOKIE['banner']);
+	
+	return $_COOKIE['banner']!='closed';	
+	
+}
--- mod/banner/views/default/banner/banner.php
+++ mod/banner/views/default/banner/banner.php
@@ -1,36 +1,19 @@
-<?php?>
-	<div id='layout_spotlight' class="banner">
-	  <div id='wrapper_spotlight'>
-	   <div class="collapsable_box">
-	     <div class="collapsable_box_header">
-
 <?php
-		$closed = false;
-		if (get_loggedin_user() instanceof ElggUser) {
-			if (get_loggedin_user()->migrationbanner_closed) {
-				$closed = true;
-			}
-		}
-		if ($closed) {
 ?>
-				<a href="javascript:void(0);" class="toggle_box_contents" onclick="$.post('<?php echo elgg_add_action_tokens_to_url("{$vars['url']}action/banner/banner?closed=false"); ?>')">+</a>
-		<?php
-			} else {
-				?>
-				<a href="javascript:void(0);" class="toggle_box_contents" onclick="$.post('<?php echo elgg_add_action_tokens_to_url("{$vars['url']}action/banner/banner?closed=true"); ?>')">-</a>
-		<?php
+<div id='layout_spotlight' class="migration_banner">
+<div id='wrapper_spotlight'>
+<div class="collapsable_box">
+<div class="collapsable_box_header">
 
-			}
+<a href="javascript:void(0);" class="elgg-banner-close"><?php echo elgg_echo('close'); ?></a>
+<div class="collapsable_box_content" <?php if ($closed) echo "style=\"display:none\"" ?>>
+<div><? echo elgg_get_plugin_setting("text", "banner"); ?></div>
+</div>
 
-		?>
-		
-		</div>
 
-		<div class="collapsable_box_content" <?php if ($closed) echo "style=\"display:none\"" ?>>
-		<div><? echo elgg_get_plugin_setting("text", "banner"); ?></div>
-              </div>
-            </div>
-          </div>
-        </div>
-       <div>
-     </div>
+</div>
+</div>
+</div>
+</div>
+<div>
+</div>
