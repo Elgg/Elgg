6a1c3bdc9932b47fb0e07844f50eff1ecc1e3a00
aleph <aleph@riseup.net>
2013-05-10 08:57:27 +0200

    Moving check control from start.php to view/banner.php. Deleting garbage in code.

--- mod/banner/start.php
+++ mod/banner/start.php
@@ -2,22 +2,12 @@
 elgg_register_event_handler('init', 'system', 'banner_init');
 
 function banner_init() {
-	
-	$opened = banner_get_cookie();
-
-	if ($opened) {
-		elgg_extend_view("page/elements/body", "banner/banner", 0);
-		elgg_register_action("banner/banner", elgg_get_plugins_path() . "banner/actions/banner.php");
-		elgg_extend_view('css','banner/css');
-		elgg_extend_view('js/elgg', 'banner/js');
-	}	
+		
+	elgg_extend_view("page/elements/body", "banner/banner", 0);		
+	elgg_extend_view('css','banner/css');
+	elgg_extend_view('js/elgg', 'banner/js');
 	
 }
 
 
-function banner_get_cookie() {
 
-	
-	return $_COOKIE['banner']!='closed';	
-	
-}
--- mod/banner/views/default/banner/banner.php
+++ mod/banner/views/default/banner/banner.php
@@ -1,19 +1,20 @@
 <?php
+
+$opened = banner_get_cookie();
+
+if ($opened) {
 ?>
-<div id='layout_spotlight' class="migration_banner">
-<div id='wrapper_spotlight'>
-<div class="collapsable_box">
-<div class="collapsable_box_header">
+	<div>
+		<a href="javascript:void(0);" class="elgg-banner-close"><?php echo elgg_echo('close'); ?></a>
+		<? echo elgg_get_plugin_setting("text", "banner"); ?>
+	</div>
+<?php
+	}	
 
-<a href="javascript:void(0);" class="elgg-banner-close"><?php echo elgg_echo('close'); ?></a>
-<div class="collapsable_box_content" <?php if ($closed) echo "style=\"display:none\"" ?>>
-<div><? echo elgg_get_plugin_setting("text", "banner"); ?></div>
-</div>
+function banner_get_cookie() {	
+	return $_COOKIE['banner']!='closed';		
+}
+?>
 
 
-</div>
-</div>
-</div>
-</div>
-<div>
-</div>
+
