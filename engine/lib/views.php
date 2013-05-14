34fa07ab4621c835c03c3f1105943f48d75f0058
Sem <sembrestels@riseup.net>
2013-05-14 06:59:14 +0200

    Update to jQuery Migrate 1.2.1 and jQuery UI 1.10.3.

--- engine/lib/views.php
+++ engine/lib/views.php
@@ -1340,8 +1340,8 @@ function elgg_views_boot() {
 
 	elgg_register_js('require', '/vendors/requirejs/require-2.1.4.min.js', 'head'); 
 	elgg_register_js('jquery', '/vendors/jquery/jquery-1.9.1.min.js', 'head');
-	elgg_register_js('jquery-migrate', '/vendors/jquery/jquery-migrate-1.1.1.min.js', 'head');
-	elgg_register_js('jquery-ui', '/vendors/jquery/jquery-ui-1.10.2.min.js', 'head');
+	elgg_register_js('jquery-migrate', '/vendors/jquery/jquery-migrate-1.2.1.min.js', 'head');
+	elgg_register_js('jquery-ui', '/vendors/jquery/jquery-ui-1.10.3.min.js', 'head');
 	elgg_register_js('jquery.form', array(
 		'src' => '/vendors/jquery/jquery.form.js',
 		'location' => 'head',
