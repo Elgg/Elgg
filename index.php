ade2c81c1d0696a131c3ad8dd59003c3d7957f13
cash <cash.costello@gmail.com>
2013-05-17 21:01:13 -0400

    front page uses routing system

--- index.php
+++ index.php
@@ -6,31 +6,11 @@
  * @subpackage Core
  */
 
-/**
- * Start the Elgg engine
- */
 require_once(dirname(__FILE__) . "/engine/start.php");
 
-elgg_set_context('main');
+$router = _elgg_services()->router;
+$request = _elgg_services()->request;
 
-// allow plugins to override the front page (return true to stop this front page code)
-if (elgg_trigger_plugin_hook('index', 'system', null, FALSE) != FALSE) {
-	exit;
+if (!$router->route($request)) {
+	forward('', '404');
 }
-
-if (elgg_is_logged_in()) {
-	forward('activity');
-}
-
-
-$content = elgg_view_title(elgg_echo('content:latest'));
-$content .= elgg_list_river();
-
-$login_box = elgg_view('core/account/login_box');
-
-$params = array(
-		'content' => $content,
-		'sidebar' => $login_box
-);
-$body = elgg_view_layout('one_sidebar', $params);
-echo elgg_view_page(null, $body);
