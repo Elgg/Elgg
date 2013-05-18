7fecf3b458ee1b5877df3cd7b65068c09ac57dfc
cash <cash.costello@gmail.com>
2013-05-17 21:04:03 -0400

    updated the custom index plugin to use page handler

--- mod/custom_index/start.php
+++ mod/custom_index/start.php
@@ -12,19 +12,18 @@ function custom_index_init() {
 	elgg_extend_view('css/elgg', 'custom_index/css');
 
 	// Replace the default index page
-	elgg_register_plugin_hook_handler('index', 'system', 'custom_index');
+	elgg_register_page_handler('', 'custom_index');
 }
 
-function custom_index($hook, $type, $return, $params) {
-	if ($return == true) {
-		// another hook has already replaced the front page
-		return $return;
-	}
-
+/**
+ * Serve the front page
+ * 
+ * @return bool Whether the page was sent.
+ */
+function custom_index() {
 	if (!include_once(dirname(__FILE__) . "/index.php")) {
 		return false;
 	}
 
-	// return true to signify that we have handled the front page
 	return true;
 }
