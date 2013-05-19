cf801894cc392ba9e1353d9556a723ae206e8b12
Steve Clay <steve@mrclay.org>
2013-05-19 14:32:44 -0400

    Refs #5508: Allows colorbox configuration down in 3 ways: js/lightbox/settings view, JSON in data-colorbox-opts attribute, options in elgg.ui.lightbox.open() call

--- mod/developers/views/default/theme_preview/miscellaneous/lightbox.php
+++ mod/developers/views/default/theme_preview/miscellaneous/lightbox.php
@@ -8,12 +8,12 @@ $ipsum = elgg_view('developers/ipsum');
 $link = elgg_view('output/url', array(
 	'text' => 'Open lighbox',
 	'href' => "#elgg-lightbox-test",
-	'class' => 'elgg-lightbox'
+	'class' => 'elgg-lightbox',
 ));
 
 echo $link;
 echo '<div class="hidden">';
 echo elgg_view_module('aside', 'Lightbox Test', $ipsum, array(
-	'id' => 'elgg-lightbox-test'
+	'id' => 'elgg-lightbox-test',
 ));
 echo '</div>';
