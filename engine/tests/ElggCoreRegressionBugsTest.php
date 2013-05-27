d9ffec520e328d4e14791c672e772edb6c573a7a
Paweł Sroka <srokap@gmail.com>
2013-05-27 05:17:08 +0200

    Refs #5538 - Added regression test

--- engine/tests/ElggCoreRegressionBugsTest.php
+++ engine/tests/ElggCoreRegressionBugsTest.php
@@ -242,4 +242,41 @@ class ElggCoreRegressionBugsTest extends ElggCoreUnitTest {
 			$this->assertEqual($output, parse_urls($input));
 		}
 	}
+	
+	/**
+	 * Checks if additional select columns does not leak to entity attributes.
+	 * 
+	 * https://github.com/Elgg/Elgg/issues/5538
+	 */
+	public function testSqlAdditionalSelectsLeakToAttributes() {
+		global $ENTITY_CACHE;
+		
+		$access = elgg_set_ignore_access(false); //remove ignore access as it disables entity cache
+		
+		//may not have groups in DB - let's create one	
+		$group = new ElggGroup();
+		$group->name = 'test_group';
+		$group->access_id = ACCESS_PUBLIC;
+		$this->assertTrue($group->save() !== false);
+		
+		//entity cache interferes with our test
+		$ENTITY_CACHE = array();
+		elgg_get_metadata_cache()->flush();
+		
+		foreach (array('site', 'user', 'group', 'object') as $type) {
+			$entities = elgg_get_entities(array(
+				'type' => $type,
+				'selects' => array('42 as added_col1'),
+				'limit' => 1,
+			));
+			$entity = array_shift($entities);
+			$this->assertTrue($entity instanceof ElggEntity);
+			$this->assertEqual($entity->added_col1, null, "Additional select columns are leaking to attributes for " . get_class($entity));
+		}
+		
+		elgg_set_ignore_access($access);
+		
+		$group->delete();
+	}
+
 }
