eff0d1b7a6b9d9cd6ca66a9b2c9e92ba526cc9df
Sem <sembrestels@riseup.net>
2013-05-28 09:30:31 +0200

    Added markup to form.

--- mod/projects-contact/views/default/forms/projects-contact/save.php
+++ mod/projects-contact/views/default/forms/projects-contact/save.php
@@ -10,11 +10,13 @@ $message = elgg_extract('message', $vars, '');
 	<?php echo elgg_echo('projects_contact:from'); ?>
 	<label><?php echo $from_entity->name; ?></label> / 
 	<?php echo elgg_echo('projects_contact:to'); ?>
-	<label id="toName" value="<?php echo $to_entity->name; ?>"><?php echo $to_entity->name; ?></label><br />
-
+	<label><?php echo $to_entity->name; ?></label><br />
+</div>
+<div>
 	<label><?php echo elgg_echo('projects_contact:subject'); ?></label><br />
 	<?php echo elgg_view('input/text', array('name' => 'subject', 'value' => $subject)); ?>
-
+</div>
+<div>
 	<label><?php echo elgg_echo('projects_contact:message'); ?></label>
 	<?php echo elgg_view('input/longtext', array('name' => 'message', 'value' => $message)); ?>
 </div>
