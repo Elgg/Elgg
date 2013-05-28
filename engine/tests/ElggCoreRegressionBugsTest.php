ee3b8b612fd24d10119bb4d2578cea4cf5d305d6
Paweł Sroka <srokap@gmail.com>
2013-05-29 01:47:16 +0200

    Refs #5544 - Regression test

--- engine/tests/ElggCoreRegressionBugsTest.php
+++ engine/tests/ElggCoreRegressionBugsTest.php
@@ -315,5 +315,57 @@ class ElggCoreRegressionBugsTest extends ElggCoreUnitTest {
 	
 		$group->delete();
 	}
-
+	
+	/**
+	 * Checks if additional select columns are readable as volatile data even if we hit the cache while fetching entity.
+	 *
+	 * https://github.com/Elgg/Elgg/issues/5544
+	 */
+	public function testSqlAdditionalSelectsAsVolatileDataWithCache() {
+		global $ENTITY_CACHE;
+	
+		$access = elgg_set_ignore_access(false); //remove ignore access as it disables entity cache
+	
+		//may not have groups in DB - let's create one
+		$group = new ElggGroup();
+		$group->name = 'test_group';
+		$group->access_id = ACCESS_PUBLIC;
+		$this->assertTrue($group->save() !== false);
+	
+		//entity cache interferes with our test
+		$ENTITY_CACHE = array();
+		elgg_get_metadata_cache()->flush();
+	
+		foreach (array('site', 'user', 'group', 'object') as $type) {
+			$entities = elgg_get_entities(array(
+				'type' => $type,
+				'selects' => array('42 as added_col3'),
+				'limit' => 1,
+			));
+			$entity = array_shift($entities);
+			$this->assertTrue($entity instanceof ElggEntity);
+			$this->assertEqual($entity->added_col3, null, "Additional select columns are leaking to attributes for " . get_class($entity));
+			$this->assertEqual($entity->getVolatileData('row:added_col3'), 42);
+		}
+	
+		//make sure we have our entities cached
+		$this->assertTrue(count($ENTITY_CACHE) >= 4);
+	
+		//run these again but with different value to make sure cache does not interfere
+		foreach (array('site', 'user', 'group', 'object') as $type) {
+			$entities = elgg_get_entities(array(
+				'type' => $type,
+				'selects' => array('64 as added_col3'),
+				'limit' => 1,
+			));
+			$entity = array_shift($entities);
+			$this->assertTrue($entity instanceof ElggEntity);
+			$this->assertEqual($entity->added_col3, null, "Additional select columns are leaking to attributes for " . get_class($entity));
+			$this->assertEqual($entity->getVolatileData('row:added_col3'), 64, "Failed to overwrite volatile data in cached entity");
+		}
+	
+		elgg_set_ignore_access($access);
+	
+		$group->delete();
+	}
 }
