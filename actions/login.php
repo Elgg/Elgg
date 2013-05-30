ff3442503189b4ac365afa48482d794bd90ada37
Jerome Bakker <jeabakker@coldtrick.com>
2013-05-30 13:17:05 +0200

    fixed: lst_forward_from is unset even if login fails, thus losing the forward url on the next login attempt

--- actions/login.php
+++ actions/login.php
@@ -6,17 +6,6 @@
  * @subpackage User.Authentication
  */
 
-// set forward url
-if (!empty($_SESSION['last_forward_from'])) {
-	$forward_url = $_SESSION['last_forward_from'];
-	unset($_SESSION['last_forward_from']);
-} elseif (get_input('returntoreferer')) {
-	$forward_url = REFERER;
-} else {
-	// forward to main index page
-	$forward_url = '';
-}
-
 $username = get_input('username');
 $password = get_input('password', null, false);
 $persistent = (bool) get_input("persistent");
@@ -60,6 +49,17 @@ if ($user->language) {
 	$message = elgg_echo('loginok', array(), $user->language);
 } else {
 	$message = elgg_echo('loginok');
+}
+
+// set forward url
+if (!empty($_SESSION['last_forward_from'])) {
+	$forward_url = $_SESSION['last_forward_from'];
+	unset($_SESSION['last_forward_from']);
+} elseif (get_input('returntoreferer')) {
+	$forward_url = REFERER;
+} else {
+	// forward to main index page
+	$forward_url = '';
 }
 
 system_message($message);
