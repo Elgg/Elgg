50dee6fe09a1f600089b1684f6f8f91599c365a1
Steve Clay <steve@mrclay.org>
2013-06-01 22:13:08 -0400

    Refs #5538: Adds missing attributes to ElggUser and ElggCoreUserTest (by Paweł Sroka)

--- engine/tests/objects/users.php
+++ engine/tests/objects/users.php
@@ -65,6 +65,9 @@ class ElggCoreUserTest extends ElggCoreUnitTest {
 		$attributes['code'] = NULL;
 		$attributes['banned'] = 'no';
 		$attributes['admin'] = 'no';
+		$attributes['prev_last_action'] = NULL;
+		$attributes['last_login'] = NULL;
+		$attributes['prev_last_login'] = NULL;
 		ksort($attributes);
 
 		$entity_attributes = $this->user->expose_attributes();
