6a56bcf4ecc34f1de258749b1319d84cc94f16fe
cash <cash.costello@gmail.com>
2013-06-26 23:49:22 -0400

    fixes unit test

--- engine/classes/Elgg/Request.php
+++ engine/classes/Elgg/Request.php
@@ -133,6 +133,94 @@ class Elgg_Request {
 	}
 
 	/**
+	 * Creates a Request based on a given URI and configuration.
+	 *
+	 * The information contained in the URI always take precedence
+	 * over the other information (server and parameters).
+	 *
+	 * @param string $uri        The URI
+	 * @param string $method     The HTTP method
+	 * @param array  $parameters The query (GET) or request (POST) parameters
+	 * @param array  $cookies    The request cookies ($_COOKIE)
+	 * @param array  $files      The request files ($_FILES)
+	 * @param array  $server     The server parameters ($_SERVER)
+	 *
+	 * @return Elgg_Request
+	 */
+	public static function create($uri, $method = 'GET', $parameters = array(), $cookies = array(), $files = array(), $server = array()) {
+		$server = array_replace(array(
+			'SERVER_NAME' => 'localhost',
+			'SERVER_PORT' => 80,
+			'HTTP_HOST' => 'localhost',
+			'HTTP_USER_AGENT' => 'Symfony/2.X',
+			'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'HTTP_ACCEPT_LANGUAGE' => 'en-us,en;q=0.5',
+			'HTTP_ACCEPT_CHARSET' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+			'REMOTE_ADDR' => '127.0.0.1',
+			'SCRIPT_NAME' => '',
+			'SCRIPT_FILENAME' => '',
+			'SERVER_PROTOCOL' => 'HTTP/1.1',
+			'REQUEST_TIME' => time(),
+		), $server);
+
+		$server['PATH_INFO'] = '';
+		$server['REQUEST_METHOD'] = strtoupper($method);
+
+		$components = parse_url($uri);
+		if (isset($components['host'])) {
+			$server['SERVER_NAME'] = $components['host'];
+			$server['HTTP_HOST'] = $components['host'];
+		}
+
+		if (isset($components['scheme'])) {
+			if ('https' === $components['scheme']) {
+				$server['HTTPS'] = 'on';
+				$server['SERVER_PORT'] = 443;
+			} else {
+				unset($server['HTTPS']);
+				$server['SERVER_PORT'] = 80;
+			}
+		}
+
+		if (isset($components['port'])) {
+			$server['SERVER_PORT'] = $components['port'];
+			$server['HTTP_HOST'] = $server['HTTP_HOST'] . ':' . $components['port'];
+		}
+
+		if (!isset($components['path'])) {
+			$components['path'] = '/';
+		}
+
+		switch (strtoupper($method)) {
+			case 'POST':
+			case 'PUT':
+			case 'DELETE':
+				if (!isset($server['CONTENT_TYPE'])) {
+					$server['CONTENT_TYPE'] = 'application/x-www-form-urlencoded';
+				}
+			case 'PATCH':
+				$request = $parameters;
+				$query = array();
+				break;
+			default:
+				$request = array();
+				$query = $parameters;
+				break;
+		}
+
+		if (isset($components['query'])) {
+			parse_str(html_entity_decode($components['query']), $qs);
+			$query = array_replace($qs, $query);
+		}
+		$queryString = http_build_query($query, '', '&');
+
+		$server['REQUEST_URI'] = $components['path'] . ('' !== $queryString ? '?' . $queryString : '');
+		$server['QUERY_STRING'] = $queryString;
+
+		return new Elgg_Request($query, $request, $cookies, $files, $server);
+	}
+
+	/**
 	 * Normalizes a query string.
 	 *
 	 * It builds a normalized query string, where keys/value pairs are alphabetized,
