2205d3e4f8632f9afd02320aacd162e29971595a
Cash Costello <cash.costello@gmail.com>
2013-06-27 07:13:57 -0400

    adds upgrade script for db queue

---
+++ engine/lib/upgrades/2013062700-1.9.0_dev-add_db_queue-e6af82afc6d3eee3.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Elgg 1.9.0-dev upgrade 2013062700
+ * add_db_queue
+ *
+ * Creates the table for queue support
+ */
+
+$db_prefix = elgg_get_config('dbprefix');
+
+// create queue table
+$query = <<<SQL
+CREATE TABLE IF NOT EXISTS `{$db_prefix}queue` (
+  `name` varchar(255) NOT NULL,
+  `data` mediumblob NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  `worker` varchar(32) NULL,
+  KEY `name` (`name`),
+  KEY `retrieve` (`timestamp`,`worker`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SQL;
+update_data($query);
