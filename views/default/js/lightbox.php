169e55580696971e8bba8c10e431a86d8ef977d9
cash <cash.costello@gmail.com>
2013-07-01 20:24:58 -0400

    Adds 3 ways to configure colorbox (code from @mrclay)

--- views/default/js/lightbox.php
+++ views/default/js/lightbox.php
@@ -3,58 +3,80 @@
  * Elgg lightbox
  *
  * Usage
- * Call elgg_load_js('lightbox') and elgg_load_css('lightbox') then
- * apply the class elgg-lightbox to links.
+ * ---------------
+ * Call elgg_load_js('lightbox') and elgg_load_css('lightbox'). 
+ * Then apply the class elgg-lightbox to links.
  *
+ * 
  * Advanced Usage
+ * -----------------
  * Elgg is distributed with the Colorbox jQuery library. Please go to
  * http://www.jacklmoore.com/colorbox for more information on the options of this lightbox.
+ * 
+ * You can change global options by overriding the js/lightbox/settings view.
  *
- * Overriding
+ * You may apply colorbox options to an individual .elgg-lightbox element
+ * by setting the attribute data-colorbox-opts to a JSON settings object. You
+ * can also set options in the elgg.ui.lightbox.bind() method, but data
+ * attributes will take precedence.
+ * 
+ * To support a hidden div as the source, add "inline: true" as a 
+ * data-colorbox-opts option. For example, using the output/url view, add:
+ *    'data-colorbox-opts' => '{"inline": true}',
+ * 
+ * 
+ * Overriding with a different lightbox
+ * -------------------------------------
  * In a plugin, override this view and override the registration for the
  * lightbox JavaScript and CSS (@see elgg_views_boot()).
- *
- * @todo add support for passing options: $('#myplugin-lightbox').elgg.ui.lightbox(options);
  */
 
-if (0) { ?><script><?php }
 ?>
+//<script>
 
 elgg.provide('elgg.ui.lightbox');
+
+<?php echo elgg_view('js/lightbox/settings'); ?>
 
 /**
  * Lightbox initialization
  */
 elgg.ui.lightbox.init = function() {
-	$.extend($.colorbox.settings, {
-		current: elgg.echo('js:lightbox:current', ['{current}', '{total}']),
-		previous: elgg.echo('previous'),
-		next: elgg.echo('next'),
-		close: elgg.echo('close'),
-		xhrError: elgg.echo('error:default'),
-		imgError: elgg.echo('error:default'),
-		opacity: 0.5,
-		maxWidth: '100%',
-
-		// workaround for https://github.com/jackmoore/colorbox/issues/291
-		reposition: $(window).height() > 600
-	});
+	$.extend($.colorbox.settings, elgg.ui.lightbox.getSettings());
+	elgg.ui.lightbox.bind($(".elgg-lightbox"));
 
 	if (typeof $.fancybox === 'undefined') {
 		$.fancybox = {
+			// error message for firefox users
 			__noSuchMethod__ : function() {
 				elgg.register_error("fancybox lightbox has been replaced by colorbox", 9999999999999);
 			}
 		};
 	}
-
-	$('.elgg-lightbox').colorbox();
-
-	// automatically detect hidden div targets for lightbox (fancybox handled this automatically)
-	$('.elgg-lightbox[href^="#"]').colorbox({inline: true});
-
 };
 
+/**
+ * Bind colorbox lightbox to HTML
+ *
+ * @param {Object} $element jQuery object containing colorbox openers
+ * @param {Object} opts     Colorbox options. These are overridden by data-colorbox-opts options
+ */
+elgg.ui.lightbox.bind = function ($element, opts) {
+	if (!$.isPlainObject(opts)) {
+		opts = {};
+	}
+	// Q: why not use "colorbox"? A: https://github.com/jackmoore/colorbox/issues/435
+	var dataOpts = $element.data('colorboxOpts');
+	if ($.isPlainObject(dataOpts)) {
+		opts = $.extend(opts, dataOpts);
+	}
+	$element.colorbox(opts);
+};
+
+/**
+ * Close the colorbox
+ *
+ */
 elgg.ui.lightbox.close = function() {
 	$.colorbox.close();
 };
