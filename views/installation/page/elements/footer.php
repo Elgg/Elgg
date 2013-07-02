a0fca1e10ee4a6bea044acd5725fe30a8ed22443
Paweł Sroka <srokap@gmail.com>
2013-07-02 05:40:34 +0200

    Updated install page footer link

--- views/installation/page/elements/footer.php
+++ views/installation/page/elements/footer.php
@@ -6,5 +6,5 @@
 <ul>
 	<li><a href="http://docs.elgg.org/wiki/Installation" target="_blank">Install instructions</a></li>
 	<li><a href="http://docs.elgg.org/wiki/Install_Troubleshooting" target="_blank">Install troubleshooting</a></li>
-	<li><a href="http://community.elgg.org/pg/groups/world" target="_blank">Elgg community forums</a></li>
+	<li><a href="http://community.elgg.org/groups/all" target="_blank">Elgg community forums</a></li>
 </ul>
