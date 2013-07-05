8cae10bb12f3f4d221a5cef3897d5fa8437c3026
cash <cash.costello@gmail.com>
2013-07-05 19:44:19 -0400

    Fixes #5725 using SSL for setting password when https login enabled

--- views/default/core/settings/account.php
+++ views/default/core/settings/account.php
@@ -6,4 +6,13 @@
  * @subpackage Core
  */
 
-echo elgg_view_form('usersettings/save', array('class' => 'elgg-form-alt'));
+$action_url = elgg_get_site_url();
+if (elgg_get_config('https_login')) {
+	$action_url = str_replace("http:", "https:", $action_url);
+}
+$action_url .= 'action/usersettings/save';
+
+echo elgg_view_form('usersettings/save', array(
+	'class' => 'elgg-form-alt',
+	'action' => $action_url,
+));
