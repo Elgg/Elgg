0986acb608f5e02e23532bde55bc6865dc821c8d
Cash Costello <cash.costello@gmail.com>
2013-07-18 06:34:46 -0400

    fixed display of site menu

--- mod/developers/views/default/theme_sandbox/navigation/site.php
+++ mod/developers/views/default/theme_sandbox/navigation/site.php
@@ -7,12 +7,5 @@ for ($i=1; $i<=5; $i++) {
 	$params['menu']['default'][] = new ElggMenuItem($i, "Page $i", "#");
 }
 $params['menu']['default'][2]->setSelected(true);
-?>
 
-<div class="elgg-page-header">
-	<div class="elgg-inner">
-	<?php 
-		echo elgg_view('navigation/menu/site', $params); 
-	?>
-	</div>
-</div>
+echo elgg_view('navigation/menu/site', $params); 
