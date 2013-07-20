cfdaea89172b0cefa09a5f6ec35fba7dfe3fb00b
cash <cash.costello@gmail.com>
2013-07-20 17:25:54 -0400

    filtering comments by subtype of container

--- views/default/page/elements/comments_block.php
+++ views/default/page/elements/comments_block.php
@@ -27,6 +27,34 @@ $options = array(
 	'limit' => elgg_extract('limit', $vars, 4),
 );
 
+// @todo where should this go
+// join on the entities table for container subtype
+$subtypes = elgg_extract('subtypes', $vars, ELGG_ENTITIES_ANY_VALUE);
+if ($subtypes != ELGG_ENTITIES_ANY_VALUE) {
+	$db_prefix = elgg_get_config('dbprefix');
+	$options['joins'] = array("JOIN {$db_prefix}entities ce ON e.container_guid = ce.guid");
+	if (is_array($subtypes)) {
+		$subtype_ids = array();
+		foreach ($subtypes as $subtype) {
+			$id = (int)get_subtype_id('object', $subtype);
+			if ($id) {
+				$subtype_ids[] = $id;
+			}
+		}
+		if ($subtype_ids) {
+			$subtype_string = implode(',', $subtype_ids);
+			$options['wheres'] = array("ce.subtype IN ($subtype_string)");
+		} else {
+			// subtype ids do not exist so not comments
+			$options['wheres'] = array("1 = -1");
+		}
+	} else {
+		$subtype_id = (int)get_subtype_id('object', $subtypes);
+		$options['wheres'] = array("ce.subtype = $subtype_id");
+	}
+}
+
+
 $title = elgg_echo('generic_comments:latest');
 $comments = elgg_get_entities($options);
 if ($comments) {
