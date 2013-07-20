30fc1f9b8bad483b6a201cfdaf01072fb1a11c89
cash <cash.costello@gmail.com>
2013-07-20 17:39:36 -0400

    cleaned up river view for comments

--- views/default/river/object/comment/create.php
+++ views/default/river/object/comment/create.php
@@ -3,9 +3,34 @@
  * Post comment river view
  */
 
-$comment = $vars['item']->getObjectEntity();
+$item = $vars['item'];
 
-$summary = elgg_view('river/object/comment/summary', $vars);
+$comment = $item->getObjectEntity();
+$subject = $item->getSubjectEntity();
+$target = $item->getTargetEntity();
+
+$subject_link = elgg_view('output/url', array(
+	'href' => $subject->getURL(),
+	'text' => $subject->name,
+	'class' => 'elgg-river-subject',
+	'is_trusted' => true,
+));
+
+$target_link = elgg_view('output/url', array(
+	'href' => $target->getURL(),
+	'text' => $target->getDisplayName(),
+	'class' => 'elgg-river-target',
+	'is_trusted' => true,
+));
+
+$type = $target->getType();
+$subtype = $target->getSubtype() ? $target->getSubtype() : 'default';
+$key = "river:comment:$type:$subtype";
+$summary = elgg_echo($key, array($subject_link, $target_link));
+if ($summary == $key) {
+	$key = "river:comment:$type:default";
+	$summary = elgg_echo($key, array($subject_link, $target_link));
+}
 
 echo elgg_view('river/elements/layout', array(
 	'item' => $vars['item'],
