a671b8ad0339ca4a8c60f1a0fd6a90b17dfd2d8b
cash <cash.costello@gmail.com>
2013-07-23 20:44:04 -0400

    moved head registration from view to plugin hook

--- mod/aalborg_theme/start.php
+++ mod/aalborg_theme/start.php
@@ -4,9 +4,9 @@
  *
  * @package AalborgTheme
  */
- 
+
 elgg_register_event_handler('init','system','aalborg_theme_init');
- 
+
 function aalborg_theme_init() {
 
 	elgg_register_event_handler('pagesetup', 'system', 'aalborg_theme_pagesetup', 1000);
@@ -14,14 +14,14 @@ function aalborg_theme_init() {
 	// theme specific CSS
 	elgg_extend_view('css/elgg', 'aalborg_theme/css');
 
-	elgg_extend_view('page/elements/head', 'aalborg_theme/meta', 1);
+	elgg_register_plugin_hook_handler('head', 'page', 'aalborg_theme_setup_head');
 
 	elgg_register_js('respond', 'mod/aalborg_theme/vendors/js/respond.min.js');
-	elgg_load_js('respond');	
+	elgg_load_js('respond');
 	elgg_register_js('elgg.theme', 'mod/aalborg_theme/vendors/js/aalborg_theme.js', 'footer');
 	elgg_load_js('elgg.theme');
-	
-	if (!elgg_is_logged_in()) {	
+
+	if (!elgg_is_logged_in()) {
 		elgg_unregister_plugin_hook_handler('output:before', 'layout', 'elgg_views_add_rss_link');
 	}
 
@@ -29,11 +29,11 @@ function aalborg_theme_init() {
 
 function aalborg_theme_pagesetup() {
 
-	elgg_unextend_view('page/elements/header', 'search/header');	
+	elgg_unextend_view('page/elements/header', 'search/header');
 	if (elgg_is_logged_in()) {
 		elgg_extend_view('page/elements/sidebar', 'search/header', 0);
 	}
-	
+
 	elgg_unregister_menu_item('topbar', 'dashboard');
 	if (elgg_is_active_plugin('dashboard')) {
 		elgg_register_menu_item('site', array(
@@ -42,11 +42,11 @@ function aalborg_theme_pagesetup() {
 			'text' => elgg_echo('dashboard'),
 		));
 	}
-	
+
 	if (elgg_is_logged_in()) {
 
 		$user = elgg_get_logged_in_user_entity();
-		
+
 		elgg_register_menu_item('topbar', array(
 			'name' => 'account',
 			'text' => elgg_echo('account'),
@@ -55,7 +55,7 @@ function aalborg_theme_pagesetup() {
 			'section' => 'alt',
 			'link_class' => 'elgg-topbar-dropdown',
 		));
-		
+
 		elgg_unregister_menu_item('topbar', 'usersettings');
 		elgg_register_menu_item('topbar', array(
 			'name' => 'usersettings',
@@ -65,7 +65,7 @@ function aalborg_theme_pagesetup() {
 			'priority' => 103,
 			'section' => 'alt',
 		));
-		
+
 		elgg_unregister_menu_item('topbar', 'logout');
 		elgg_register_menu_item('topbar', array(
 			'name' => 'logout',
@@ -76,9 +76,9 @@ function aalborg_theme_pagesetup() {
 			'priority' => 104,
 			'section' => 'alt',
 		));
-		
+
 		elgg_unregister_menu_item('topbar', 'administration');
-		if (elgg_is_admin_logged_in()) {		
+		if (elgg_is_admin_logged_in()) {
 			elgg_register_menu_item('topbar', array(
 				'name' => 'administration',
 				'parent_name' => 'account',
@@ -87,8 +87,8 @@ function aalborg_theme_pagesetup() {
 				'priority' => 101,
 				'section' => 'alt',
 			));
-		}		
-		
+		}
+
 		elgg_unregister_menu_item('footer', 'report_this');
 		if (elgg_is_active_plugin('reportedcontent')) {
 			$href = "javascript:elgg.forward('reportedcontent/add'";
@@ -104,4 +104,26 @@ function aalborg_theme_pagesetup() {
 			));
 		}
 	}
+}
+
+/**
+ * Register items for the html head
+ *
+ * @param string $hook Hook name ('head')
+ * @param string $type Hook type ('page')
+ * @param array  $data Array of items for head
+ * @return array
+ */
+function aalborg_theme_setup_head($hook, $type, $data) {
+	$data['metas'][] = array(
+		'name' => 'viewport',
+		'content' => 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
+	);
+
+	$data['links'][] = array(
+		'rel' => 'apple-touch-icon',
+		'href' => elgg_normalize_url('mod/aalborg_theme/graphics/homescreen.png'),
+	);
+
+	return $data;
 }
