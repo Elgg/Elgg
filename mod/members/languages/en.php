fe199e648778ccbe62702bc9871a52ca5d09a93c
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-08-14 09:55:24 +0200

    members now uses new core language strings

--- mod/members/languages/en.php
+++ mod/members/languages/en.php
@@ -4,8 +4,6 @@
  */
 
 return array(
-	'members:label:newest' => 'Newest',
-	'members:label:popular' => 'Popular',
 	'members:label:online' => 'Online',
 	'members:search' => 'Search members',
 	'members:title:search' => 'Member search for %s',
