8fa18dfe12b2e4ed79f24967d08166d0176fa57b
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-08-30 09:08:16 +0200

    added lazy_hover page handler to public pages

--- start.php
+++ start.php
@@ -1,16 +1,34 @@
 <?php
 
 	function lazy_hover_init() {
-		// elgg_register_plugin_hook_handler('register', 'menu:user_hover', "lazy_hover_register_hover_menu", 1);
 		elgg_extend_view("js/elgg", "js/lazy_hover/site");
 		
 		elgg_register_page_handler("lazy_hover", "lazy_hover_page_handler");
+		
+		// extend public pages
+		elgg_register_plugin_hook_handler('public_pages', 'walled_garden', 'lazy_hover_public_pages');
 		
 	}
 	
 	function lazy_hover_page_handler($page) {
 		include(dirname(__FILE__) . "/pages/lazy_hover.php");
 		return true;
+	}
+	
+	/**
+	 *
+	 * Extend public pages
+	 * @param unknown_type $hook_name
+	 * @param unknown_type $entity_type
+	 * @param unknown_type $return_value
+	 * @param unknown_type $parameters
+	 */
+	function lazy_hover_public_pages($hook_name, $entity_type, $return_value, $params){
+		$return = $return_value;
+		if(is_array($return)){
+			$return[] = "lazy_hover";
+		}
+		return $return;
 	}
 
 	// register default elgg events
