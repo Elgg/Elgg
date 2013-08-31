0ec1378561c1f2659a58657e9448efd277ef7062
Steve Clay <steve@mrclay.org>
2013-08-30 21:37:19 -0400

    Avoid deprecation notice in mock object

--- engine/lib/elgglib.php
+++ engine/lib/elgglib.php
@@ -1014,7 +1014,7 @@ function elgg_get_version($human_readable = false) {
 				return false;
 			}
 		}
-		return (!$human_readable) ? $version : $release;
+		return $human_readable ? $release : $version;
 	}
 
 	return false;
--- engine/tests/phpunit/ElggRelationshipTest.php
+++ engine/tests/phpunit/ElggRelationshipTest.php
@@ -3,13 +3,13 @@
 class ElggRelationshipTest extends PHPUnit_Framework_TestCase {
 
 	public function testSettingAndGettingAttribute() {
-		$obj = $this->getMockForAbstractClass('ElggRelationship');
+		$obj = $this->getMockForAbstractClass('ElggRelationship', array(), '', false);
 		$obj->relationship = 'hasSister';
 		$this->assertEquals('hasSister', $obj->relationship);
 	}
 
 	public function testGettingNonexistentAttribute() {
-		$obj = $this->getMockForAbstractClass('ElggRelationship');
+		$obj = $this->getMockForAbstractClass('ElggRelationship', array(), '', false);
 		$this->assertNull($obj->foo);
 	}
 }
