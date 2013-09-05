f5a3ca12d3f95d3dca4be4499be5f74d4d8e49b8
Steve Clay <steve@mrclay.org>
2013-09-04 21:54:34 -0400

    Fixes #6024: Adds support for ":before" and ":after" events After events cannot be cancelled, all handlers always run.

--- engine/classes/Elgg/EventsService.php
+++ engine/classes/Elgg/EventsService.php
@@ -15,15 +15,19 @@ class Elgg_EventsService extends Elgg_HooksRegistrationService {
 	 * Triggers an Elgg event.
 	 * 
 	 * @see elgg_trigger_event
+	 * @see elgg_trigger_after_event
 	 * @access private
 	 */
-	public function trigger($event, $type, $object = null) {
+	public function trigger($event, $type, $object = null, $stoppable = true) {
 		$events = $this->getOrderedHandlers($event, $type);
 		$args = array($event, $type, $object);
 
 		foreach ($events as $callback) {
-			if (is_callable($callback) && (call_user_func_array($callback, $args) === false)) {
-				return false;
+			if (is_callable($callback)) {
+				$return = call_user_func_array($callback, $args);
+				if ($stoppable && ($return === false)) {
+					return false;
+				}
 			}
 		}
 
