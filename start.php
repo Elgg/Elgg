84e75c1ae3c78b20eebcff8152e84c3725978c5e
Steve Clay <steve@mrclay.org>
2013-09-09 23:49:33 -0400

    Adds MAC for securely passing contexts through client. Replaces form with more compact span markup. No more JS/PHP globals.

--- start.php
+++ start.php
@@ -1,36 +1,41 @@
 <?php
 
-	function lazy_hover_init() {
-		elgg_extend_view("js/elgg", "js/lazy_hover/site");
-		
-		elgg_register_page_handler("lazy_hover", "lazy_hover_page_handler");
-		
-		// extend public pages
-		elgg_register_plugin_hook_handler('public_pages', 'walled_garden', 'lazy_hover_public_pages');
-		
-	}
-	
-	function lazy_hover_page_handler($page) {
-		include(dirname(__FILE__) . "/pages/lazy_hover.php");
-		return true;
-	}
-	
-	/**
-	 *
-	 * Extend public pages
-	 * @param unknown_type $hook_name
-	 * @param unknown_type $entity_type
-	 * @param unknown_type $return_value
-	 * @param unknown_type $parameters
-	 */
-	function lazy_hover_public_pages($hook_name, $entity_type, $return_value, $params){
-		$return = $return_value;
-		if(is_array($return)){
-			$return[] = "lazy_hover";
-		}
-		return $return;
-	}
+function lazy_hover_init() {
+	elgg_extend_view("js/elgg", "js/lazy_hover/site");
 
-	// register default elgg events
-	elgg_register_event_handler("init", "system", "lazy_hover_init");
-	
+	elgg_register_page_handler("lazy_hover", "lazy_hover_page_handler");
+
+	// extend public pages
+	elgg_register_plugin_hook_handler('public_pages', 'walled_garden', 'lazy_hover_public_pages');
+
+}
+
+/**
+ * @param string[] $page URL segments
+ * @return bool
+ */
+function lazy_hover_page_handler($page) {
+	require dirname(__FILE__) . "/pages/lazy_hover.php";
+	return true;
+}
+
+/**
+ * Extend public pages
+ *
+ * @param string   $hook_name    "public_pages"
+ * @param string   $entity_type  "walled_garden"
+ * @param string[] $return_value array of public pages
+ * @param mixed    $params       unused
+ *
+ * @return string[]
+ */
+function lazy_hover_public_pages($hook_name, $entity_type, $return_value, $params) {
+	$return = $return_value;
+	if (is_array($return)) {
+		$return[] = "lazy_hover";
+	}
+	return $return;
+}
+
+// register default elgg events
+elgg_register_event_handler("init", "system", "lazy_hover_init");
--- views/default/lazy_hover/placeholder.php
+++ views/default/lazy_hover/placeholder.php
@@ -1,37 +1,35 @@
 <?php
 
-global $LAZY_HOVER;
-
-if (!isset($LAZY_HOVER)) {
-	$LAZY_HOVER = array();
-}
+$spans_present = (array)elgg_get_config('lazy_hover:spans');
 
 $user = elgg_extract("entity", $vars);
-if (elgg_instanceof($user)) {
-		
-	$guid = $user->getGUID();
-	$page_owner_guid = (int) elgg_get_page_owner_guid();
-	$contexts = elgg_get_config("context");
-	
-	$md5 = md5($guid . "-" . $page_owner_guid . "-" . implode("-", $contexts));
-	
-	echo "<div rel='" . $md5 . "' class='hidden lazy-hover-placeholder'>";
-	
-	if (!in_array($md5, $LAZY_HOVER)) {
-		echo "<form action='" . elgg_get_site_url() . "lazy_hover'>";
-		echo "<input type='hidden' name='guid' value='$guid' />";
-		
-		if ($page_owner_guid) {
-			echo "<input type='hidden' name='page_owner_guid' value='$page_owner_guid' />";
-		}
-		
-		foreach ($contexts as $context) {
-			echo "<input type='hidden' name='context[]' value='$context' />";
-		}
-		echo "</form>";
-		
-		$LAZY_HOVER[] = $md5;
-	}
-	
-	echo "</div>";
+if (!elgg_instanceof($user, 'user')) {
+	return;
 }
+
+$guid = (int)$user->guid;
+$page_owner_guid = (int)elgg_get_page_owner_guid();
+$contexts = (array)elgg_get_config("context");
+
+// generate MAC so we don't have to trust the client's choice of contexts
+$data = serialize(array($guid, $page_owner_guid, $contexts));
+$mac = hash_hmac('sha256', $data, get_site_secret());
+
+echo "<div rel='$mac' class='hidden lazy-hover-placeholder'>";
+
+if (empty($spans_present[$mac])) {
+	$attrs = array(
+		'data-json' => json_encode(array(
+			'g' => $guid,
+			'pog' => $page_owner_guid,
+			'c' => $contexts,
+			'm' => $mac,
+		)),
+	);
+	echo "<span " . elgg_format_attributes($attrs) . '></span>';
+
+	$spans_present[$mac] = true;
+	elgg_set_config('lazy_hover:spans', $spans_present);
+}
+	
+echo "</div>";
