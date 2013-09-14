aef57bfa961791a4d7706ab45f49e9064c612ce4
Sem <sembrestels@riseup.net>
2013-09-14 01:38:39 +0000

    Showing a throbber while loading the hover menu.

--- views/default/js/lazy_hover/site.php
+++ views/default/js/lazy_hover/site.php
@@ -6,34 +6,37 @@ elgg.provide("elgg.lazy_hover");
 elgg.lazy_hover.init = function() {
 	$(".elgg-avatar > .elgg-icon-hover-menu").live('click', function(event) {
 		var $button = $(this),
-			$placeholder = $(this).parent().find(".lazy-hover-placeholder"),
+			$placeholder = $(this).parent().find(".elgg-menu-hover.elgg-ajax-loader"),
 			$all_placeholders,
-			$span,
+			$ul,
 			action;
 
 		if ($placeholder.length) {
 			// select all similar placeholders
-			$all_placeholders = $(".lazy-hover-placeholder[rel='" + $placeholder.attr("rel") + "']");
+			$all_placeholders = $(".elgg-menu-hover[rel='" + $placeholder.attr("rel") + "']");
 
-			// find first form for this menu
-			$span = $all_placeholders.find("span:first");
+			// find the <ul> that contains data for this menu
+			$ul = $all_placeholders.filter('[data-json]');
 			action = elgg.get_site_url() + 'ajax/view/lazy_hover/user_hover';
-			if ($span.length) {
+			if ($ul.length) {
 				elgg.get(action, {
-					data: $span.data('json'),
+					data: $ul.data('json'),
 					success: function(data) {
 						if (data) {
 							// replace all existing placeholders with new menu
-							$all_placeholders.replaceWith(data);
+							$all_placeholders.removeClass('elgg-ajax-loader pvl')
+								.html($(data).children());
 
-							// restart click events
-							$button.click();
+							// show the new menu in the popup
+							var $popup = $('.elgg-menu-hover:visible');
+							if ($popup.attr("rel") === $placeholder.attr('rel')) {
+								$popup.removeClass('elgg-ajax-loader pvl')
+									.html($(data).children());
+							}
 						}
 					}
 				});
 			}
-			// prevent other events
-			event.stopImmediatePropagation();
 		}
 	});
 };
