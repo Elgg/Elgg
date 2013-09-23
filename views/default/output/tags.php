f8f069aa6fc69257553b246b894e240462edd7ca
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-23 15:04:28 +0200

    refactor output tags and added support for changing base url

--- views/default/output/tags.php
+++ views/default/output/tags.php
@@ -17,28 +17,8 @@ if (isset($vars['entity'])) {
 	unset($vars['entity']);
 }
 
-if (!empty($vars['type'])) {
-	$type = "&type=" . rawurlencode($vars['type']);
-} else {
-	$type = "";
-}
-if (!empty($vars['subtype'])) {
-	$subtype = "&subtype=" . rawurlencode($vars['subtype']);
-} else {
-	$subtype = "";
-}
-if (!empty($vars['object'])) {
-	$object = "&object=" . rawurlencode($vars['object']);
-} else {
-	$object = "";
-}
-
 if (empty($vars['tags']) && !empty($vars['value'])) {
 	$vars['tags'] = $vars['value'];
-}
-
-if (empty($vars['tags']) && isset($vars['entity'])) {
-	$vars['tags'] = $vars['entity']->tags;
 }
 
 if (!empty($vars['tags'])) {
@@ -59,12 +39,13 @@ if (!empty($vars['tags'])) {
 	$icon_class = elgg_extract('icon_class', $vars);
 	$list_items = '<li>' . elgg_view_icon('tag', $icon_class) . '</li>';
 		
+	$params = $vars;
 	foreach($vars['tags'] as $tag) {
-		$url = elgg_get_site_url() . 'search?q=' . rawurlencode($tag) . "&search_type=tags{$type}{$subtype}{$object}";
 		if (is_string($tag)) {
-			$tag = htmlspecialchars($tag, ENT_QUOTES, 'UTF-8', false);
+			$params['value'] = $tag;
+			
 			$list_items .= "<li class=\"$item_class\">";
-			$list_items .= elgg_view('output/url', array('href' => $url, 'text' => $tag, 'rel' => 'tag'));
+			$list_items .= elgg_view('output/tag', $params);
 			$list_items .= '</li>';
 		}
 	}
@@ -79,4 +60,3 @@ ___HTML;
 
 	echo $list;
 }
-
