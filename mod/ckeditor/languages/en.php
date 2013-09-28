3c34f80c2ac6104ade39aa4dd2a4fd3110764184
Cash Costello <cash.costello@gmail.com>
2013-09-28 11:20:33 -0400

    Fixes #6093 removes image uploading from ckeditor

--- mod/ckeditor/languages/en.php
+++ mod/ckeditor/languages/en.php
@@ -6,21 +6,7 @@
  */
 
 return array(
-	'admin:administer_utilities:uploads' => 'CKEditor Uploads',
-	'ckeditor:uploads:none' => 'No uploads',
-	'item:object:ckeditor_upload' => 'CKEditor uploads',
-	'ckeditor:upload:owner' => 'User: %s',
-	'ckeditor:upload:admin' => 'Upload admin',
-
 	'ckeditor:html' => "Edit HTML",
 	'ckeditor:visual' => "Visual editor",
 	'ckeditor:word_count' => 'Word count: ',
-
-	'ckeditor:success:delete' => 'Delete succeeded',
-	'ckeditor:failure:upload' => 'Image upload failed',
-	'ckeditor:failure:resize' => 'Failed to resize the uploaded image',
-	'ckeditor:failure:permissions' => 'Server configuration error',
-	'ckeditor:failure:too_big' => 'The image is too large. Please resize before uploading',
-	'ckeditor:failure:missing' => 'Cannot find the file',
-	'ckeditor:failure:delete' => 'Delete failed',
 );
