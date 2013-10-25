79e2deb4afa0b0d829f8aaeb79171c9cf52e42ef
Steve Clay <steve@mrclay.org>
2013-10-24 23:13:26 -0400

    Fixes #4695: Passes more info to input/access and access hooks

--- views/default/input/access.php
+++ views/default/input/access.php
@@ -8,30 +8,55 @@
  * @uses $vars['name']           The name of the input field
  * @uses $vars['entity']         Optional. The entity for this access control (uses access_id)
  * @uses $vars['class']          Additional CSS class
+ *
+ * @uses $vars['entity_type']    Optional. Type of the entity
+ * @uses $vars['entity_subtype'] Optional. Subtype of the entity
+ * @uses $vars['container_guid'] Optional. Container GUID of the entity
+ *
  */
 
-if (isset($vars['class'])) {
-	$vars['class'] = "elgg-input-access {$vars['class']}";
-} else {
-	$vars['class'] = "elgg-input-access";
+$vars['class'] = trim("elgg-input-access " . elgg_extract('class', $vars, ''));
+
+// will be passed to plugin hooks ['access:collections:write', 'user'] and ['default', 'access']
+$params = array();
+
+$keys = array(
+	'entity' => null,
+	'entity_type' => null,
+	'entity_subtype' => null,
+	'container_guid' => null,
+	'purpose' => 'read',
+);
+foreach ($keys as $key => $default_value) {
+	$params[$key] = elgg_extract($key, $vars, $default_value);
+	unset($vars[$key]);
+}
+
+/* @var ElggEntity $entity */
+$entity = $params['entity'];
+
+if ($entity) {
+	$params['entity_type'] = $entity->type;
+	$params['entity_subtype'] = $entity->getSubtype();
+	$params['container_guid'] = $entity->container_guid;
+}
+
+$container = elgg_get_page_owner_entity();
+if (!$params['container_guid'] && $container) {
+	$params['container_guid'] = $container->guid;
 }
 
 $defaults = array(
 	'disabled' => false,
-	'value' => get_default_access(),
-	'options_values' => get_write_access_array(),
+	'value' => get_default_access(null, $params),
+	'options_values' => get_write_access_array(0, 0, false, $params),
 );
 
-/* @var ElggEntity $entity */
-$entity = elgg_extract('entity', $vars);
-unset($vars['entity']);
-
 // should we tell users that public/logged-in access levels will be ignored?
-$container = elgg_get_page_owner_entity();
 if (($container instanceof ElggGroup)
 		&& $container->getContentAccessMode() === ElggGroup::CONTENT_ACCESS_MODE_MEMBERS_ONLY
 		&& !elgg_in_context('group-edit')
-		&& !($entity && $entity instanceof ElggGroup)) {
+		&& !($entity instanceof ElggGroup)) {
 	$show_override_notice = true;
 } else {
 	$show_override_notice = false;
@@ -44,15 +69,17 @@ if ($entity) {
 $vars = array_merge($defaults, $vars);
 
 if ($vars['value'] == ACCESS_DEFAULT) {
-	$vars['value'] = get_default_access();
+	$vars['value'] = get_default_access(null, $params);
 }
 
-if (is_array($vars['options_values']) && sizeof($vars['options_values']) > 0) {
-	if ($show_override_notice) {
-		$vars['data-group-acl'] = $container->group_acl;
-	}
-	echo elgg_view('input/select', $vars);
-	if ($show_override_notice) {
-		echo "<p class='elgg-text-help'>" . elgg_echo('access:overridenotice')  .  "</p>";
-	}
+if (empty($vars['options_values'])) {
+	return;
+}
+
+if ($show_override_notice) {
+	$vars['data-group-acl'] = $container->group_acl;
+}
+echo elgg_view('input/select', $vars);
+if ($show_override_notice) {
+	echo "<p class='elgg-text-help'>" . elgg_echo('access:overridenotice')  .  "</p>";
 }
