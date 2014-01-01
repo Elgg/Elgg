82b30f63043eba9c18999bd2a15301d62ead4a76
Paweł Sroka <srokap@gmail.com>
2014-01-01 12:42:10 +0100

    Preparing 1.8.17 release

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 
 // YYYYMMDD = Elgg Date
 // XX = Interim incrementer
-$version = 2013052900;
+$version = 2014010100;
 
 // Human-friendly version name
-$release = '1.8.16';
+$release = '1.8.17';
