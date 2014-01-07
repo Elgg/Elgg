Elgg/Elgg

commit 63db31c0237d24247b3b75b8365d610be936c283

Handle case if get_user_notification_settings() returns false

engine/lib/notification.php
@@ -110,12 +110,15 @@ function notify_user($to, $from, $subject, $message, array $params = NULL, $meth
 			// Are we overriding delivery?
 			$methods = $methods_override;
 			if (!$methods) {
-				$tmp = (array)get_user_notification_settings($guid);
+				$tmp = get_user_notification_settings($guid);
 				$methods = array();
-				foreach ($tmp as $k => $v) {
-					// Add method if method is turned on for user!
-					if ($v) {
-						$methods[] = $k;
+				// $tmp may be false. don't cast
+				if (is_array($tmp)) {
+					foreach ($tmp as $k => $v) {
+						// Add method if method is turned on for user!
+						if ($v) {
+							$methods[] = $k;
+						}
 					}
 				}
 			}

commit 2634e94664045e08b3960dc83487d6357ec081a7

Fixes #6309: Fixes notify_user() broken by 63db31c

engine/lib/notification.php
@@ -113,7 +113,7 @@ function notify_user($to, $from, $subject, $message, array $params = NULL, $meth
 				$tmp = get_user_notification_settings($guid);
 				$methods = array();
 				// $tmp may be false. don't cast
-				if (is_array($tmp)) {
+				if (is_object($tmp)) {
 					foreach ($tmp as $k => $v) {
 						// Add method if method is turned on for user!
 						if ($v) {
@@ -168,7 +168,7 @@ function notify_user($to, $from, $subject, $message, array $params = NULL, $meth
  *
  * @param int $user_guid The user id
  *
- * @return stdClass
+ * @return stdClass|false
  */
 function get_user_notification_settings($user_guid = 0) {
 	$user_guid = (int)$user_guid;