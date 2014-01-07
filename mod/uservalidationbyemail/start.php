776acf8343cca3109c5055e5b7cf6c9859f27f13
Centillien <gkanters@gmail.com>
2014-01-07 14:34:16 +0100

    Correct check during login with email address
    When a user is not validated and tries to login with his email addres will be handled correctly.
    
    Reference issue: https://github.com/Elgg/Elgg/issues/6294

--- mod/uservalidationbyemail/start.php
+++ mod/uservalidationbyemail/start.php
@@ -145,6 +145,11 @@ function uservalidationbyemail_check_auth_attempt($credentials) {
 	// See if the user exists and isn't validated
 	$access_status = access_get_show_hidden_status();
 	access_show_hidden_entities(TRUE);
+	
+	// check if logging in with email address
+        if (strpos($username, '@') !== false && ($users = get_user_by_email($username))) {
+        $username = $users[0]->username;
+        }
 
 	$user = get_user_by_username($username);
 	if ($user && isset($user->validated) && !$user->validated) {
