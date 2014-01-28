50d3ff6a23428285f8705cd913211180a0eef075
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-01-28 12:16:23 +0100

    chore(blog): replaces underscores with hyphens in id and for attributes
    Ensuring we comply with coding standards
    
    Fixes #6365

--- mod/blog/views/default/forms/blog/save.php
+++ mod/blog/views/default/forms/blog/save.php
@@ -45,21 +45,21 @@ $action_buttons = $save_button . $preview_button . $delete_link;
 $title_label = elgg_echo('title');
 $title_input = elgg_view('input/text', array(
 	'name' => 'title',
-	'id' => 'blog_title',
+	'id' => 'blog-title',
 	'value' => $vars['title']
 ));
 
 $excerpt_label = elgg_echo('blog:excerpt');
 $excerpt_input = elgg_view('input/text', array(
 	'name' => 'excerpt',
-	'id' => 'blog_excerpt',
+	'id' => 'blog-excerpt',
 	'value' => _elgg_html_decode($vars['excerpt'])
 ));
 
 $body_label = elgg_echo('blog:body');
 $body_input = elgg_view('input/longtext', array(
 	'name' => 'description',
-	'id' => 'blog_description',
+	'id' => 'blog-description',
 	'value' => $vars['description']
 ));
 
@@ -74,7 +74,7 @@ if ($vars['guid']) {
 $status_label = elgg_echo('status');
 $status_input = elgg_view('input/select', array(
 	'name' => 'status',
-	'id' => 'blog_status',
+	'id' => 'blog-status',
 	'value' => $vars['status'],
 	'options_values' => array(
 		'draft' => elgg_echo('status:draft'),
@@ -85,7 +85,7 @@ $status_input = elgg_view('input/select', array(
 $comments_label = elgg_echo('comments');
 $comments_input = elgg_view('input/select', array(
 	'name' => 'comments_on',
-	'id' => 'blog_comments_on',
+	'id' => 'blog-comments-on',
 	'value' => $vars['comments_on'],
 	'options_values' => array('On' => elgg_echo('on'), 'Off' => elgg_echo('off'))
 ));
@@ -93,14 +93,14 @@ $comments_input = elgg_view('input/select', array(
 $tags_label = elgg_echo('tags');
 $tags_input = elgg_view('input/tags', array(
 	'name' => 'tags',
-	'id' => 'blog_tags',
+	'id' => 'blog-tags',
 	'value' => $vars['tags']
 ));
 
 $access_label = elgg_echo('access');
 $access_input = elgg_view('input/access', array(
 	'name' => 'access_id',
-	'id' => 'blog_access_id',
+	'id' => 'blog-access-id',
 	'value' => $vars['access_id']
 ));
 
@@ -116,39 +116,39 @@ echo <<<___HTML
 $draft_warning
 
 <div>
-	<label for="blog_title">$title_label</label>
+	<label for="blog-title">$title_label</label>
 	$title_input
 </div>
 
 <div>
-	<label for="blog_excerpt">$excerpt_label</label>
+	<label for="blog-excerpt">$excerpt_label</label>
 	$excerpt_input
 </div>
 
 <div>
-	<label for="blog_description">$body_label</label>
+	<label for="blog-description">$body_label</label>
 	$body_input
 </div>
 
 <div>
-	<label for="blog_tags">$tags_label</label>
+	<label for="blog-tags">$tags_label</label>
 	$tags_input
 </div>
 
 $categories_input
 
 <div>
-	<label for="blog_comments_on">$comments_label</label>
+	<label for="blog-comments-on">$comments_label</label>
 	$comments_input
 </div>
 
 <div>
-	<label for="blog_access_id">$access_label</label>
+	<label for="blog-access-id">$access_label</label>
 	$access_input
 </div>
 
 <div>
-	<label for="blog_status">$status_label</label>
+	<label for="blog-status">$status_label</label>
 	$status_input
 </div>
 
