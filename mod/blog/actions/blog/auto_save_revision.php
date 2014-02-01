acc05a239e9237b3f18d9b2f9b3c035c9ac01535
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-02-01 14:35:37 +0100

    chore(blog): refactor auto save action and adds missing container references
    Refactors the auto save action to be more readable, adds action description, adds container logic, removes unused variables
    
    Refs #6368

--- mod/blog/actions/blog/auto_save_revision.php
+++ mod/blog/actions/blog/auto_save_revision.php
@@ -1,95 +1,106 @@
 <?php
 
 /**
- * Action called by AJAX periodic auto saving when editing.
+ * Action for periodic auto saving of changes to the blog body via AJAX
+ * For new blogs, it creates a new blog object and sets the 'status'
+ * metadata to 'unsaved_draft'
+ * For existing blogs, revisions are stored in 'blog_auto_save' annotation,
+ * which is then deleted, when the blog is saved
  *
  * @package Blog
  */
-$guid = get_input('guid');
-$user = elgg_get_logged_in_user_entity();
 $title = htmlspecialchars(get_input('title', '', false), ENT_QUOTES, 'UTF-8');
 $description = get_input('description');
 $excerpt = get_input('excerpt');
 
-// because get_input() doesn't use the default if the input is ''
-if (empty($excerpt)) {
-	$excerpt = $description;
+if (empty($title) || empty($description)) {
+	register_error(elgg_echo('blog:error:missing:description'));
+	forward(REFERER);
 }
 
-// store errors to pass along
-$error = FALSE;
+$guid = get_input('guid');
 
-if ($title && $description) {
+$container_guid = get_input('container_guid');
+$container = get_entity($container_guid);
 
-	if ($guid) {
-		$entity = get_entity($guid);
-		if (elgg_instanceof($entity, 'object', 'blog') && $entity->canEdit()) {
-			$blog = $entity;
-		} else {
-			$error = elgg_echo('blog:error:post_not_found');
-		}
-	} else {
-		$blog = new ElggBlog();
-		$blog->subtype = 'blog';
+if (elgg_instanceof($container) && !$container->canWriteToContainer(0, 'object', 'blog')) {
+	register_error(elgg_echo('blog:error:cannot_write_to_container'));
+	forward(REFERER);
+}
 
-		// force draft and private for autosaves.
-		$blog->status = 'unsaved_draft';
-		$blog->access_id = ACCESS_PRIVATE;
-		$blog->title = $title;
-		$blog->description = $description;
-		$blog->excerpt = elgg_get_excerpt($excerpt);
+if ($guid) {
+	$entity = get_entity($guid);
 
-		// mark this as a brand new post so we can work out the
-		// river / revision logic in the real save action.
-		$blog->new_post = TRUE;
+	if (!elgg_instanceof($entity, 'object', 'blog')) {
+		register_error(elgg_echo('blog:error:post_not_found'));
+		forward(REFERER);
+	} else if (!$entity->canEdit()) {
+		register_error(elgg_echo('blog:error:cannot_edit_post'));
+		forward(REFERER);
+	}
+	$blog = $entity;
+} else {
 
-		if (!$blog->save()) {
-			$error = elgg_echo('blog:error:cannot_save');
-		}
+	$blog = new ElggBlog();
+	$blog->subtype = 'blog';
+
+	if (elgg_instanceof($container)) {
+		$blog->container_guid = $container->getGUID();
 	}
 
-	// create draft annotation
-	if (!$error) {
-		// annotations don't have a "time_updated" so
-		// we have to delete everything or the times are wrong.
-		// don't save if nothing changed
-		$auto_save_annotations = $blog->getAnnotations(array(
-			'annotation_name' => 'blog_auto_save',
-			'limit' => 1,
+	// force draft and private for autosaves.
+	$blog->status = 'unsaved_draft';
+	$blog->access_id = ACCESS_PRIVATE;
+	$blog->title = $title;
+	$blog->description = $description;
+
+	if (empty($excerpt)) {
+		$excerpt = $description;
+	}
+	$blog->excerpt = elgg_get_excerpt($excerpt);
+
+	// mark this as a brand new post so we can work out the
+	// river / revision logic in the real save action.
+	$blog->new_post = TRUE;
+
+	if (!$blog->save()) {
+		register_error(elgg_echo('blog:error:cannot_save'));
+		forward(REFERER);
+	}
+}
+
+// create auto save annotation
+// annotations don't have a "time_updated" so
+// we have to delete everything or the times are wrong.
+// don't save if nothing changed
+$auto_save_annotations = $blog->getAnnotations(array(
+	'annotation_name' => 'blog_auto_save',
+	'limit' => 1,
 		));
-		if ($auto_save_annotations) {
-			$auto_save = $auto_save_annotations[0];
-		} else {
-			$auto_save = FALSE;
-		}
 
-		if (!$auto_save) {
-			$annotation_id = $blog->annotate('blog_auto_save', $description);
-		} elseif ($auto_save instanceof ElggAnnotation && $auto_save->value != $description) {
-			$blog->deleteAnnotations('blog_auto_save');
-			$annotation_id = $blog->annotate('blog_auto_save', $description);
-		} elseif ($auto_save instanceof ElggAnnotation && $auto_save->value == $description) {
-			// this isn't an error because we have an up to date annotation.
-			$annotation_id = $auto_save->id;
-		}
+$old_description = '';
+$annotation_id = false;
+if ($auto_save_annotations) {
+	$last_auto_save_annotation = $auto_save_annotations[0];
+	$annotation_id = $last_auto_save_annotation->id;
+	$old_description = $last_auto_save_annotation->value;
+}
 
-		if (!$annotation_id) {
-			$error = elgg_echo('blog:error:cannot_auto_save');
-		}
+if ($old_description != $description) {
+	if ($annotation_id) {
+		$blog->deleteAnnotations('blog_auto_save');
 	}
-} else {
-	$error = elgg_echo('blog:error:missing:description');
+	$annotation_id = $blog->annotate('blog_auto_save', $description);
 }
 
-if ($error) {
-	register_error($error);
-} else {
-	//system_message(elgg_echo('blog:auto_saved_revision'));
-	$json = array(
-		'guid' => $blog->getGUID(),
-		'msg' => date('F j, Y @ H:i:s', time())
-	);
-	echo json_encode($json);
+if (!$annotation_id) {
+	register_error(elgg_echo('blog:error:cannot_auto_save'));
+	forward(REFERER);
 }
 
-forward(REFERER);
+$json = array(
+	'guid' => $blog->getGUID(),
+	'msg' => date('F j, Y @ H:i:s', time())
+);
+echo json_encode($json);
+forward($blog->getURL());
