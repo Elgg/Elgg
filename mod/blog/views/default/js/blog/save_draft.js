86db9a5354522cf6a16e0a1b3a3d63ac6ff70be8
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-02-01 14:41:36 +0100

    chore(blog): correct syntax and combine jQuery selects
    Corrects comparison operand, adds missing semincolon, combines jQuery operators, exposes update interval as a variable
    
    Fixes #6368

--- mod/blog/views/default/js/blog/save_draft.js
+++ mod/blog/views/default/js/blog/save_draft.js
@@ -1,31 +1,33 @@
 /**
- * Save draft through ajax
+ * Implements periodic auto saving of changes to the blog body via AJAX
  *
  * @package Blog
  */
 
 elgg.provide('elgg.blog');
 
+elgg.blog.saveDraftInterval = 60000;
+
 elgg.blog.saveDraft = function() {
 
-	var description = $('#blog-post-edit').find('[name=description]').val();
-	var title = $('#blog-post-edit').find('[name=title]').val();
+	var description = $('#blog-post-edit [name=description]').val();
+	var title = $('#blog-post-edit [name=title]').val();
 
-	if (!(description && title) || (description == elgg.blog.oldDescription)) {
+	if (!description ||  !title  || description === elgg.blog.oldDescription) {
 		return;
 	}
 
 	elgg.action('action/blog/auto_save_revision', {
 		data: $('#blog-post-edit').serialize(),
 		beforeSend: function() {
-			elgg.blog.oldDescription = $('#blog-post-edit').find('[name="description"]').val();
+			elgg.blog.oldDescription = $('#blog-post-edit [name="description"]').val();
 		},
 		success: function(response) {
 			if (response.status >= 0) {
-				$('#blog-post-edit').find('[name=guid]').val(response.output.guid);
-				$('#blog-post-edit').find('.blog-save-status-time').html(response.output.msg);
+				$('#blog-post-edit [name=guid]').val(response.output.guid);
+				$('#blog-post-edit .blog-save-status-time').html(response.output.msg);
 			} else {
-				$('#blog-post-edit').find('.blog-save-status-time').html(elgg.echo('error'));
+				$('#blog-post-edit .blog-save-status-time').html(elgg.echo('error'));
 			}
 		}
 	});
@@ -39,9 +41,9 @@ elgg.blog.init = function() {
 	}
 
 	// Store a copy of the blog body to compare before auto save
-	elgg.blog.oldDescription = $('#blog-post-edit').find('[name="description"]').val();
+	elgg.blog.oldDescription = $('#blog-post-edit [name="description"]').val();
 
-	setInterval(elgg.blog.saveDraft, 60000);
+	setInterval(elgg.blog.saveDraft, elgg.blog.saveDraftInterval);
 };
 
 elgg.register_hook_handler('init', 'system', elgg.blog.init);
