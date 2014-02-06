4deeae9e04b3d07719ff4f8169f3934927ab4766
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-02-06 11:35:36 +0100

    chore(invitefriends): form labels
    Format labels according to #6356, adds email label and moves instructions into its own div, removes leading spaces in email notifications
    
    Refs #6400

--- mod/invitefriends/views/default/forms/invitefriends/invite.php
+++ mod/invitefriends/views/default/forms/invitefriends/invite.php
@@ -7,6 +7,7 @@
  */
 $site = elgg_get_site_entity();
 $introduction = elgg_echo('invitefriends:introduction');
+$emails = elgg_echo('invitefriends:emails');
 $message = elgg_echo('invitefriends:message');
 $default = elgg_echo('invitefriends:message:default', array($site->name));
 $emails_textarea = elgg_view('input/plaintext', array(
@@ -23,8 +24,9 @@ $message_textarea = elgg_view('input/plaintext', array(
 $action_button = elgg_view('input/submit', array('value' => elgg_echo('send')));
 
 echo <<< HTML
+<div class="mbm elgg-text-help">$introduction</div>
 <div>
-	<label for="invitefriends-emails">$introduction</label>
+	<label for="invitefriends-emails">$emails</label>
 	$emails_textarea
 </div>
 <div>
