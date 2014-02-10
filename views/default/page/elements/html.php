baf2df9355a5fc63679ad1aa80f363d00a51572b
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-02-10 12:37:40 +0100

    fix(views): check if body_attrs are set before attempting to format them
    Clears a log warning, cleans up the code and adds @uses to docs
    
    Fixes #6298

--- views/default/page/elements/html.php
+++ views/default/page/elements/html.php
@@ -1,21 +1,30 @@
 <?php
 /**
  * Page shell for all HTML pages
+ *
+ * @uses $vars['head']        Parameters for the <head> element
+ * @uses $vars['body_attrs']  Attributes of the <body> tag
+ * @uses $vars['body']        The main content of the page
  */
-
 // Set the content type
 header("Content-type: text/html; charset=UTF-8");
 
 $lang = get_current_language();
-$attrs = elgg_format_attributes($vars['body_attrs']);
 
+$attrs = "";
+if (isset($vars['body_attrs'])) {
+	$attrs = elgg_format_attributes($vars['body_attrs']);
+	if ($attrs) {
+		$attr = " $attrs";
+	}
+}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang; ?>" lang="<?php echo $lang; ?>">
 	<head>
-<?php echo $vars["head"]; ?>
+		<?php echo $vars["head"]; ?>
 	</head>
-	<body<?php if (!empty($attrs)) { echo " $attrs"; } ?>>
-<?php echo $vars["body"]; ?>
+	<body<?php echo $attrs ?>>
+		<?php echo $vars["body"]; ?>
 	</body>
 </html>
