ba907f896fe27552f39319d28e1793b0078f7a95
Sem <sembrestels@riseup.net>
2014-02-11 06:41:35 +0100

    chore(vendors): Upgraded jQueryUI to 1.10.4

--- engine/lib/views.php
+++ engine/lib/views.php
@@ -1549,7 +1549,7 @@ function elgg_views_boot() {
 	elgg_register_js('require', '/vendors/requirejs/require-1.2.10.min.js', 'head');
 	elgg_register_js('jquery', '/vendors/jquery/jquery-1.11.0.min.js', 'head');
 	elgg_register_js('jquery-migrate', '/vendors/jquery/jquery-migrate-1.2.1.min.js', 'head');
-	elgg_register_js('jquery-ui', '/vendors/jquery/jquery-ui-1.10.3.min.js', 'head');
+	elgg_register_js('jquery-ui', '/vendors/jquery/jquery-ui-1.10.4.min.js', 'head');
 	elgg_register_js('jquery.form', array(
 		'src' => '/vendors/jquery/jquery.form.js',
 		'deps' => array('jquery'),
