817582fc956b2a415d834faed273fd17cb54ad32
Sem <sembrestels@riseup.net>
2014-02-16 04:07:57 +0100

    chore(ckeditor): adapted plugin to work with CKEditor 4
    Theme changed to moono, so removed specific BootstrapCK-Skin CSS
    CKEDITOR.basePath wasn't working for plugins, so defined CKEDITOR_BASEPATH
    Fix applied in #5689 is no more required
    Wordcount own code replaced by wordcount plugin
    Set allowedContent to true to do not filter tags (htmlawed already does it)
    Configured new AutoGrow plugin

--- mod/ckeditor/views/default/js/elgg/ckeditor.js
+++ mod/ckeditor/views/default/js/elgg/ckeditor.js
@@ -3,8 +3,6 @@ define(function(require) {
 	var $ = require('jquery'); require('jquery.ckeditor');
 	var CKEDITOR = require('ckeditor');
 
-	CKEDITOR.basePath = elgg.config.wwwroot + 'mod/ckeditor/vendors/ckeditor/';
-
 	var elggCKEditor = {
 
 		/**
@@ -30,43 +28,9 @@ define(function(require) {
 		/**
 		 * Initializes the ckeditor module
 		 *
-		 * Tasks include configuring the live-updating word counter and HTML writer
-		 *
 		 * @return void
 		 */
 		init: function() {
-			var editor = this;
-
-			// #5689
-			editor.dataProcessor.writer.setRules('p', {
-				breakAfterOpen : false
-			});
-
-			if ($('#cke_wordcount_' + editor.name).length == 0) {
-				$('#cke_bottom_' + editor.name).prepend(
-					'<div id="cke_wordcount_' + editor.name + '" class="cke_wordcount">' + 
-						elgg.echo('ckeditor:word_count') + '0' +
-					'</div>'   
-				);
-			}
-
-			editor.document.on('input', function() {
-				elggCKEditor.updateCount(editor);
-			});
-			elggCKEditor.updateCount(editor);
-		},
-
-		/**
-		 * Show the number of words
-		 * 
-		 * @param {Object} editor
-		 * @return void
-		 */
-		updateCount: function(editor) {
-			var words = $.trim(editor.document.getBody().getText());
-			var count = words !== "" ? words.split(/\s+/).length : 0;
-			var text = elgg.echo('ckeditor:word_count') + count + ' ';
-			$('#cke_wordcount_' + editor.name).html(text);
 		},
 
 		/**
@@ -103,7 +67,7 @@ define(function(require) {
 		 * CKEditor configuration
 		 *
 		 * You can find configuration information here:
-		 * http://docs.cksource.com/Talk:CKEditor_3.x/Developers_Guide
+		 * http://docs.ckeditor.com/#!/api/CKEDITOR.config
 		 */
 		config: require('elgg/ckeditor/config')
 
