f0ec2bebcc70881024b54e7c231d0f86e886bdf5
Matt Beckett <beck24@gmail.com>
2014-06-21 02:49:30 -0700

    fix(upgrade): test for ability to connect to localhost if rewrite test fails - Refs #6888

--- upgrade.php
+++ upgrade.php
@@ -51,6 +51,20 @@ if (get_input('upgrade') == 'upgrade') {
 	$rewriteTester = new ElggRewriteTester();
 	$url = elgg_get_site_url() . "__testing_rewrite?__testing_rewrite=1";
 	if (!$rewriteTester->runRewriteTest($url)) {
+		// see if there is a problem accessing the site at all
+		// due to ip restrictions for example
+		if (!$rewriteTester->runLocalhostAccessTest()) {
+			// note: translation may not be available until after upgrade
+			$msg = elgg_echo("installation:htaccess:localhost:connectionfailed");
+			if ($msg === "installation:htaccess:localhost:connectionfailed") {
+				$msg = "Elgg cannot connect to itself to test rewrite rules properly. Check "
+						. "that curl is working and there are no IP restrictions preventing "
+						. "localhost connections.";
+			}
+			echo $msg;
+			exit;
+		}
+		
 		// note: translation may not be available until after upgrade
 		$msg = elgg_echo("installation:htaccess:needs_upgrade");
 		if ($msg === "installation:htaccess:needs_upgrade") {
