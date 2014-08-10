c5bbe591e204dd0808f44bca00dbba22384d7e99
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-08-11 01:01:29 +0300

    chore(i18n): update translations

---
+++ mod/diagnostics/languages/it.php
@@ -0,0 +1,37 @@
+<?php
+return array(
+	'admin:administer_utilities:diagnostics' => 'Diagnostica di sistema',
+	'diagnostics' => 'diagnostica di sistema',
+	'diagnostics:report' => 'Rapporto di diagnostica',
+	'diagnostics:description' => 'Il rapporto di diagnostica seguente può essere utile per diagnosticare problemi con Elgg. Gli sviluppatori di elg possono richiederlo come allegato alla segnalazione di bug.',
+	'diagnostics:header' => '========================================================================
+Rapporto di diagnostica Elgg
+Generato %s da %s
+========================================================================
+
+',
+	'diagnostics:report:basic' => '
+
+Elgg rlascio %s, versione %s
+
+------------------------------------------------------------------------',
+	'diagnostics:report:php' => '
+Informazioni PHP:
+%s
+------------------------------------------------------------------------',
+	'diagnostics:report:plugins' => '
+Plugin installati e dettagli:
+
+%s
+------------------------------------------------------------------------',
+	'diagnostics:report:md5' => '
+Files installati e checksums:
+
+%s
+------------------------------------------------------------------------',
+	'diagnostics:report:globals' => '
+Variabili Globali:
+
+%s
+------------------------------------------------------------------------',
+);
---
+++ mod/pages/languages/pt_BR.php
@@ -0,0 +1,113 @@
+<?php
+return array(
+
+	/**
+	 * Menu items and titles
+	 */
+
+	'pages' => "Páginas",
+	'pages:owner' => "%s páginas",
+	'pages:friends' => "Páginas dos amigos",
+	'pages:all' => "Todas as páginas do site",
+	'pages:add' => "Adicionar uma página",
+
+	'pages:group' => "Páginas do grupo",
+	'groups:enablepages' => 'Ativar páginas do grupo',
+
+	'pages:new' => "Uma nova página",
+	'pages:edit' => "Editar essa página",
+	'pages:delete' => "Excluir essa página",
+	'pages:history' => "Histórico",
+	'pages:view' => "Ver página",
+	'pages:revision' => "Revisão",
+	'pages:current_revision' => "Revisão atual",
+	'pages:revert' => "Reverter",
+
+	'pages:navigation' => "Navegação",
+
+	'pages:notify:summary' => 'Nova página chamada %s',
+	'pages:notify:subject' => "Uma nova página: %s",
+	'pages:notify:body' =>
+'%s adicionou uma nova página: %s
+
+%s
+
+Veja e comente a página:
+%s
+',
+	'item:object:page_top' => 'Páginas mais importantes',
+	'item:object:page' => 'Páginas',
+	'pages:nogroup' => 'Este grupo ainda não possui quaisquer páginas',
+	'pages:more' => 'Mais páginas',
+	'pages:none' => 'Ainda não foi criado páginas',
+
+	/**
+	* River
+	**/
+
+	'river:create:object:page' => '%s criou a página %s',
+	'river:create:object:page_top' => '%s criou a página %s',
+	'river:update:object:page' => '%s atualizou a página %s',
+	'river:update:object:page_top' => '%s atualizou a página %s',
+	'river:comment:object:page' => '%s comentou na página %s',
+	'river:comment:object:page_top' => '%s comentou na página %s',
+
+	/**
+	 * Form fields
+	 */
+
+	'pages:title' => 'Título da página',
+	'pages:description' => 'Texto da página',
+	'pages:tags' => 'Tags',
+	'pages:parent_guid' => 'Página principal',
+	'pages:access_id' => 'Acesso para leitura',
+	'pages:write_access_id' => 'Acesso para escrita',
+
+	/**
+	 * Status and error messages
+	 */
+	'pages:noaccess' => 'Não tem acesso a página',
+	'pages:cantedit' => 'Você não pode editar essa página',
+	'pages:saved' => 'Página salva',
+	'pages:notsaved' => 'A página não pode ser salva',
+	'pages:error:no_title' => 'Você deve especificar um título para essa página.',
+	'pages:delete:success' => 'A página foi excluída com sucesso.',
+	'pages:delete:failure' => 'A página não pode ser excluída.',
+	'pages:revision:delete:success' => 'A revisão da página foi excluída com sucesso.',
+	'pages:revision:delete:failure' => 'A revisão da página não pode ser excluída.',
+	'pages:revision:not_found' => 'Não foi possível encontrar esta revisão.',
+
+	/**
+	 * Page
+	 */
+	'pages:strapline' => 'Última modificação %s por %s',
+
+	/**
+	 * History
+	 */
+	'pages:revision:subtitle' => 'Revisão criada %s por %s',
+
+	/**
+	 * Widget
+	 **/
+
+	'pages:num' => 'Número de páginas a serem exibidas',
+	'pages:widget:description' => "Lista com suas páginas.",
+
+	/**
+	 * Submenu items
+	 */
+	'pages:label:view' => "Ver página",
+	'pages:label:edit' => "Editar página",
+	'pages:label:history' => "Histórico da página",
+
+	/**
+	 * Sidebar items
+	 */
+	'pages:sidebar:this' => "Essa página",
+	'pages:sidebar:children' => "Subpáginas",
+	'pages:sidebar:parent' => "Principal",
+
+	'pages:newchild' => "Criar uma subpágina",
+	'pages:backtoparent' => "Voltar para '%s'",
+);
