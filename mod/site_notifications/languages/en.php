c28eaac764b468b6df526bc155b31e16f2afe879
Jerome Bakker <jeabakker@coldtrick.com>
2014-09-17 16:18:27 +0200

    feature(site_notifications): option to mass delete site notifications …
    When a user has a lot of site notifications he/she has to delete them
    individually. This is very time consuming, so this solves that problem
    
    fixes #6219

--- mod/site_notifications/languages/en.php
+++ mod/site_notifications/languages/en.php
@@ -8,4 +8,7 @@ return array(
 	'site_notifications:no_access' => 'You do not have access to this page. Perhaps you need to log in.',
 	'site_notifications:no_delete' => 'Cannot delete',
 	'site_notifications:empty' => 'No notifications',
+	'site_notifications:toggle_all' => 'Toggle all',
+	'site_notifications:error:notifications_not_selected' => 'No notifications selected',
+	'site_notifications:success:delete' => 'Notifications deleted'
 );
--- mod/site_notifications/views/default/site_notifications/css.php
+++ mod/site_notifications/views/default/site_notifications/css.php
@@ -10,3 +10,9 @@
 #notificationstable td.sitetogglefield a.sitetoggleOn {
 	background: url(<?php echo elgg_get_site_url(); ?>mod/notifications/graphics/icon_notifications_site.gif) no-repeat right -37px;
 }
+.site-notifications-buttonbank {
+	text-align: right;
+}
+.site-notifications-buttonbank input {
+	margin-left: 10px;
+}
