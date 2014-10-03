f7cb4878be4e3bd277410362740e87972f6a114a
Steve Clay <steve@mrclay.org>
2014-10-03 14:59:12 -0400

    feature(navigation): add hook to filter breadcrumbs
    This also makes sure crumb titles are escaped in the view, and moves the
    elgg_get_excerpt call to a hook handler, allowing it to be disabled.
    
    Fixes #6419

--- views/default/navigation/breadcrumbs.php
+++ views/default/navigation/breadcrumbs.php
@@ -9,6 +9,7 @@
  * @uses $vars['class']
  *
  * @see elgg_push_breadcrumb
+ * @see elgg_get_breadcrumbs
  */
 
 if (isset($vars['breadcrumbs'])) {
@@ -26,14 +27,18 @@ if ($additional_class) {
 if (is_array($breadcrumbs) && count($breadcrumbs) > 0) {
 	echo "<ul class=\"$class\">";
 	foreach ($breadcrumbs as $breadcrumb) {
+		// We have to escape text (without double-encoding). Titles in core plugins are HTML escaped
+		// on input, but we can't guarantee that other users of this view and of elgg_push_breadcrumb()
+		// will do so.
 		if (!empty($breadcrumb['link'])) {
 			$crumb = elgg_view('output/url', array(
 				'href' => $breadcrumb['link'],
 				'text' => $breadcrumb['title'],
+				'encode_text' => true,
 				'is_trusted' => true,
 			));
 		} else {
-			$crumb = $breadcrumb['title'];
+			$crumb = htmlspecialchars($breadcrumb['title'], ENT_QUOTES, 'UTF-8', false);
 		}
 		echo "<li>$crumb</li>";
 	}
