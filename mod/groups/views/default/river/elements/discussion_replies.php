b2d55926b53c794aab6fb82ad3ddbf9c596c73d8
Per Jensen <admin@perjensen-online.dk>
2014-10-09 17:37:01 +0200

    feature(aalborg_theme): comments use triangle indicator instead of text label
    Fixes  #7161

--- mod/groups/views/default/river/elements/discussion_replies.php
+++ mod/groups/views/default/river/elements/discussion_replies.php
@@ -15,8 +15,6 @@ $options = array(
 $count = elgg_get_entities($options);
 
 if ($count) {
-	$replies_text = elgg_echo('group:replies');
-	echo "<span class=\"elgg-river-comments-tab\">$replies_text</span>";
 
 	$list_options = array(
 		'order_by' => 'e.time_created desc',
