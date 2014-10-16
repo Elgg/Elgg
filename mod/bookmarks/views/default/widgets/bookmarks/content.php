adb463699a35cbe18c4b48408f554ce7d2395264
Steve Clay <steve@mrclay.org>
2014-10-15 20:46:49 -0400

    fix(bookmarks): fixes more link in group bookmarks widget
    Fixes #6583

--- mod/bookmarks/views/default/widgets/bookmarks/content.php
+++ mod/bookmarks/views/default/widgets/bookmarks/content.php
@@ -5,12 +5,15 @@
  * @package Bookmarks
  */
 
-$max = (int) $vars['entity']->num_display;
+$widget = $vars['entity'];
+/* @var ElggWidget $widget */
+
+$max = (int) $widget->num_display;
 
 $options = array(
 	'type' => 'object',
 	'subtype' => 'bookmarks',
-	'container_guid' => $vars['entity']->owner_guid,
+	'container_guid' => $widget->owner_guid,
 	'limit' => $max,
 	'full_view' => FALSE,
 	'pagination' => FALSE,
@@ -19,14 +22,21 @@ $content = elgg_list_entities($options);
 
 echo $content;
 
-if ($content) {
-	$url = "bookmarks/owner/" . elgg_get_page_owner_entity()->username;
-	$more_link = elgg_view('output/url', array(
-		'href' => $url,
-		'text' => elgg_echo('more'),
-		'is_trusted' => true,
-	));
-	echo "<span class=\"elgg-widget-more\">$more_link</span>";
-} else {
-	echo elgg_echo('bookmarks:none');
+if (!$content) {
+    echo elgg_echo('bookmarks:none');
+    return;
 }
+
+$owner = $widget->getOwnerEntity();
+if ($owner instanceof ElggGroup) {
+    $url = "bookmarks/group/{$owner->guid}/all";
+} else {
+    $url = "bookmarks/owner/{$owner->username}";
+}
+
+$more_link = elgg_view('output/url', array(
+    'href' => $url,
+    'text' => elgg_echo('more'),
+    'is_trusted' => true,
+));
+echo "<span class=\"elgg-widget-more\">$more_link</span>";
