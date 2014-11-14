Elgg/Elgg

commit 4cf113ab60f6e3c5e0d445f70fdd8cd530917642

fix(views): input/userpicker can now remove all users on edit
We must provide an empty input in case all users are removed. Otherwise
no value will be submitted.

Fixes #6982

views/default/input/userpicker.php
@@ -50,6 +50,13 @@ $limit = (int)elgg_extract('limit', $vars, 0);
 		}
 		?>
 	</ul>
+	<?php
+	if (empty($guids)) {
+		echo elgg_view('input/hidden', array(
+			'name' => $vars['name'],
+		));
+	}
+	?>
 </div>
 <script>
 require(['elgg/UserPicker'], function (UserPicker) {

commit e4008c657a1680c47015ce632c47c470f138a562

fix(views): Revert erroneous changes made to input/userpicker
This reverts commit 4cf113ab60f6e3c5e0d445f70fdd8cd530917642 which caused the
friends picker to stop working when there were no existing values selected.

views/default/input/userpicker.php
@@ -50,13 +50,6 @@ $limit = (int)elgg_extract('limit', $vars, 0);
 		}
 		?>
 	</ul>
-	<?php
-	if (empty($guids)) {
-		echo elgg_view('input/hidden', array(
-			'name' => $vars['name'],
-		));
-	}
-	?>
 </div>
 <script>
 require(['elgg/UserPicker'], function (UserPicker) {