d22a64062b989d80ac9016962e977b467d728e88
iionly <iionly@gmx.de>
2014-11-30 13:41:14 +0100

    fix(core): deprecation notices thrown at login/logout even if there's no valid reason

--- engine/classes/Elgg/EventsService.php
+++ engine/classes/Elgg/EventsService.php
@@ -29,7 +29,7 @@ class Elgg_EventsService extends Elgg_HooksRegistrationService {
 			self::OPTION_DEPRECATION_VERSION => '',
 		), $options);
 
-		$events = $this->getOrderedHandlers($event, $type);
+		$events = $this->hasHandler($event, $type);
 		if ($events && $options[self::OPTION_DEPRECATION_MESSAGE]) {
 			elgg_deprecated_notice(
 				$options[self::OPTION_DEPRECATION_MESSAGE],
@@ -38,6 +38,7 @@ class Elgg_EventsService extends Elgg_HooksRegistrationService {
 			);
 		}
 
+		$events = $this->getOrderedHandlers($event, $type);
 		$args = array($event, $type, $object);
 
 		foreach ($events as $callback) {
