347683c1b6dba1740193b69054d9e1ad9dd4f96c
Steve Clay <steve@mrclay.org>
2014-12-24 17:01:55 -0500

    feature(plugins): adds several reported content features
    Ajaxifies the reporting form (form page still BC) and delete/archive
    actions. Sorts un-archived reports at the top of results. More compact
    report display, and clicking reported link opens page in an iframe
    in colorbox.
    
    Fixes #5379, #6082, #5380

--- mod/reportedcontent/views/default/widgets/reportedcontent/content.php
+++ mod/reportedcontent/views/default/widgets/reportedcontent/content.php
@@ -3,12 +3,17 @@
  * List the latest reports
  */
 
-$list = elgg_list_entities(array(
+$list = elgg_list_entities_from_metadata([
 	'type' => 'object',
 	'subtype' => 'reported_content',
 	'limit' => $vars['entity']->num_display,
 	'pagination' => false,
-));
+	'order_by_metadata' => [
+		'name' => 'state',
+		'direction' => 'ASC',
+		'as' => 'text',
+	],
+]);
 if (!$list) {
 	$list = '<p class="mtm">' . elgg_echo('reportedcontent:none') . '</p>';
 }
