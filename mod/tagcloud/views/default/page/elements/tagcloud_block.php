de92b4ec5608e749b01d7a470a8354721057332f
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-01-26 15:43:39 +0100

    feature(views): add container guid support to tagcloud block
    fixes #4839

--- mod/tagcloud/views/default/page/elements/tagcloud_block.php
+++ mod/tagcloud/views/default/page/elements/tagcloud_block.php
@@ -2,23 +2,14 @@
 /**
  * Display content-based tags
  *
- * Generally used in a sidebar. Does not work with groups currently.
- *
- * @uses $vars['subtypes']   Object subtype string or array of subtypes
- * @uses $vars['owner_guid'] The owner of the content being tagged
- * @uses $vars['limit']      The maxinum number of tags to display
+ * @uses $vars['subtypes']       Object subtype string or array of subtypes
+ * @uses $vars['owner_guid']     The owner of the content being tagged
+ * @uses $vars['container_guid'] The container of the content being tagged
+ * @uses $vars['limit']          The maximum number of tags to display
  */
 
 $owner_guid = elgg_extract('owner_guid', $vars, ELGG_ENTITIES_ANY_VALUE);
-if (!$owner_guid) {
-	$owner_guid = ELGG_ENTITIES_ANY_VALUE;
-}
-
-$owner_entity = get_entity($owner_guid);
-if ($owner_entity && elgg_instanceof($owner_entity, 'group')) {
-	// not supporting groups so return
-	return true;
-}
+$container_guid = elgg_extract('container_guid', $vars, ELGG_ENTITIES_ANY_VALUE);
 
 $type = 'object';
 
@@ -26,6 +17,7 @@ $options = array(
 	'type' => $type,
 	'subtype' => elgg_extract('subtypes', $vars, ELGG_ENTITIES_ANY_VALUE),
 	'owner_guid' => $owner_guid,
+	'container_guid' => $container_guid,
 	'threshold' => 0,
 	'limit' => elgg_extract('limit', $vars, 50),
 	'tag_name' => 'tags',
