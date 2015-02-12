fdfafca7f3ca050951b2b21b4d310929a25dff08
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-02-12 10:49:56 +0100

    chore(views): clean up icon views
    Adds instance checks, removes escaping that is done by default in the
    output lib, removes variable duplication, recycles views

--- mod/file/views/default/icon/object/file.php
+++ mod/file/views/default/icon/object/file.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * File icon view
  *
@@ -8,47 +9,19 @@
  * @uses $vars['img_class']  Optional CSS class added to img
  * @uses $vars['link_class'] Optional CSS class added to link
  */
-
-$entity = $vars['entity'];
-
-$sizes = array('small', 'medium', 'large', 'tiny', 'master', 'topbar');
-// Get size
-if (!in_array($vars['size'], $sizes)) {
-	$vars['size'] = "medium";
+$entity = elgg_extract('entity', $vars);
+if (!$entity instanceof \ElggFile) {
+	return;
 }
 
-$title = $entity->title;
-$title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8', false);
-
-$url = $entity->getURL();
-if (isset($vars['href'])) {
-	$url = $vars['href'];
-}
-
-$class = '';
-if (isset($vars['img_class'])) {
-	$class = $vars['img_class'];
-}
+$class = array();
 if ($entity->thumbnail) {
-	$class = "class=\"elgg-photo $class\"";
-} else if ($class) {
-	$class = "class=\"$class\"";
+	$class[] = 'elgg-photo';
+}
+if (isset($vars['img_class'])) {
+	$class[] = $vars['img_class'];
 }
 
-$img_src = $entity->getIconURL($vars['size']);
-$img_src = elgg_format_url($img_src);
-$img = "<img $class src=\"$img_src\" alt=\"$title\" />";
+$vars['img_class'] = implode(' ', $class);
 
-if ($url) {
-	$params = array(
-		'href' => $url,
-		'text' => $img,
-		'is_trusted' => true,
-	);
-	if (isset($vars['link_class'])) {
-		$params['class'] = $vars['link_class'];
-	}
-	echo elgg_view('output/url', $params);
-} else {
-	echo $img;
-}
+echo elgg_view('icon/default', $vars);
