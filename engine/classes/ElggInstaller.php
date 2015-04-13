629af8547ba9f9bb988184ba836616b1b6de921c
Steve Clay <steve@mrclay.org>
2015-04-13 17:47:54 -0400

    No notices checking isHttps()

--- engine/classes/ElggInstaller.php
+++ engine/classes/ElggInstaller.php
@@ -863,7 +863,7 @@ class ElggInstaller {
 	 */
 	private function isHttps() {
 		return (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ||
-			$_SERVER['SERVER_PORT'] == 443;
+			(!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443);
 	}
 
 	/**
