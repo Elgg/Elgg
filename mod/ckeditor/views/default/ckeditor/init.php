2030e74fbda04a953ff687cb103a8d406a039863
Steve Clay <steve@mrclay.org>
2015-05-17 19:27:32 -0400

    feature(javascript): Adds plugin boot modules
    Plugins can provide an AMD module boot/<plugin_id>, which will be automatically
    loaded by a new elgg/booted module. Plugin boot modules are the ideal
    place to register plugin hooks.
    
    The elgg/booted module can be required to make sure all the plugin hooks
    have been registered.
    
    The final system hooks are now triggered in elgg/booted.
    
    This also adds a boot module for likes, and has elgg/ckeditor/config depend
    on elgg/booted.
    
    Fixes #7926

--- mod/ckeditor/views/default/ckeditor/init.php
+++ mod/ckeditor/views/default/ckeditor/init.php
@@ -7,14 +7,9 @@
 
 ?>
 <script>
-require(['elgg'], function (elgg) {
-	// This global variable must be set before the editor script loading.
-	CKEDITOR_BASEPATH = elgg.get_simplecache_url('ckeditor/');
-
-	require(['elgg/ckeditor', 'jquery', 'jquery.ckeditor'], function(elggCKEditor, $) {
-		$('.elgg-input-longtext:not([data-cke-init])')
-			.attr('data-cke-init', true)
-			.ckeditor(elggCKEditor.init, elggCKEditor.config);
-	});
+require(['elgg/ckeditor', 'jquery', 'jquery.ckeditor'], function(elggCKEditor, $) {
+	$('.elgg-input-longtext:not([data-cke-init])')
+		.attr('data-cke-init', true)
+		.ckeditor(elggCKEditor.init, elggCKEditor.config);
 });
 </script>
