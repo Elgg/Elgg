70cfdd01e277915674c7c3bfbd32e1f3eb7c8de7
Steve Clay <steve@mrclay.org>
2015-05-25 12:12:32 -0400

    fix(spinner): elgg/spinner delays a bit before displaying
    Requests served from browser cache are near instantaneously, and in these
    cases the spinner is a distraction. So we wait 20ms before showing it.
    
    Fixes #8361

--- js/tests/ElggSpinnerTest.js
+++ js/tests/ElggSpinnerTest.js
@@ -13,17 +13,53 @@ define(function(require) {
 			expect($('.elgg-spinner').length).toBe(1);
 		});
 
-		it("start() adds the body class", function() {
+		it("start() doesn't add the body class immediately", function() {
 			expect($(visible_selector).length).toBe(0);
 			spinner.start();
-			expect($(visible_selector).length).toBe(1);
+			expect($(visible_selector).length).toBe(0);
+		});
+
+		it("start() adds the body class after 20ms", function() {
+			expect($(visible_selector).length).toBe(0);
+			var flag = false;
+
+			runs(function() {
+				spinner.start();
+
+				setTimeout(function() {
+					flag = true;
+				}, 25);
+			});
+
+			waitsFor(function() {
+				return flag;
+			});
+
+			runs(function() {
+				expect($(visible_selector).length).toBe(1);
+			});
 		});
 
 		it("stop() removes the body class", function() {
-			spinner.start();
-			expect($(visible_selector).length).toBe(1);
-			spinner.stop();
-			expect($(visible_selector).length).toBe(0);
+			var flag = false;
+
+			runs(function() {
+				spinner.start();
+
+				setTimeout(function() {
+					flag = true;
+				}, 25);
+			});
+
+			waitsFor(function() {
+				return flag;
+			});
+
+			runs(function() {
+				expect($(visible_selector).length).toBe(1);
+				spinner.stop();
+				expect($(visible_selector).length).toBe(0);
+			});
 		});
 	});
 });
