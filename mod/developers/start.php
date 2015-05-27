42013a5da449ac293a1dc33768a5566d2883cde6
Evan Winslow <evan.b.winslow@gmail.com>
2015-05-27 09:27:41 +0000

    feature(routing): Render theme_sandbox shell with view

--- mod/developers/start.php
+++ mod/developers/start.php
@@ -204,41 +204,9 @@ function developers_theme_sandbox_controller($page) {
 	if (!isset($page[0])) {
 		forward('theme_sandbox/intro');
 	}
+	set_input('page', $page[0]);
 
-	elgg_load_css('dev.theme_sandbox');
-
-	$pages = array(
-		'buttons',
-		'components',
-		'forms',
-		'grid',
-		'icons',
-		'javascript',
-		'layouts',
-		'modules',
-		'navigation',
-		'typography',
-	);
-	
-	foreach ($pages as $page_name) {
-		elgg_register_menu_item('theme_sandbox', array(
-			'name' => $page_name,
-			'text' => elgg_echo("theme_sandbox:$page_name"),
-			'href' => "theme_sandbox/$page_name",
-		));
-	}
-
-	elgg_require_js('elgg/dev/theme_sandbox');
-
-	$title = elgg_echo("theme_sandbox:{$page[0]}");
-	$body =  elgg_view("theme_sandbox/{$page[0]}");
-
-	$layout = elgg_view_layout('theme_sandbox', array(
-		'title' => $title,
-		'content' => $body,
-	));
-
-	echo elgg_view_page("Theme Sandbox : $title", $layout, 'theme_sandbox');
+	echo elgg_view('resources/theme_sandbox');
 	return true;
 }
 
