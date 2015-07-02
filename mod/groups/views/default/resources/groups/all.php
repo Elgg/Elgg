ba82c6827ad6839b64cabf56de8a08695992bc34
Jerome Bakker <jeabakker@coldtrick.com>
2015-07-02 13:51:26 +0200

    feature(groups): adds alphabetical sorting to all groups page

--- mod/groups/views/default/resources/groups/all.php
+++ mod/groups/views/default/resources/groups/all.php
@@ -47,6 +47,17 @@ switch ($selected_tab) {
 			$content = elgg_echo('groups:nofeatured');
 		}
 		break;
+	case 'alpha':
+		$dbprefix = elgg_get_config('dbprefix');
+		$content = elgg_list_entities(array(
+			'type' => 'group',
+			'joins' => ["JOIN {$dbprefix}groups_entity ge ON e.guid = ge.guid"],
+			'order_by' => 'ge.name',
+			'full_view' => false,
+			'no_results' => elgg_echo('groups:none'),
+			'distinct' => false,
+		));
+		break;
 	case 'newest':
 	default:
 		$content = elgg_list_entities(array(
