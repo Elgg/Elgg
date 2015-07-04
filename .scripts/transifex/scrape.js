967e4909455d6fbd7f390469b893c4bfb7dc4749
Steve Clay <steve@mrclay.org>
2015-07-04 13:03:21 -0400

    chore(i18n): Adds limited support for new languages on Transifex
    Updates release script to normalize language filenames and remove
    unrecognized language files. Adds missing InvalidLocaleException.
    
    Fixes #7540

---
+++ .scripts/transifex/scrape.js
@@ -0,0 +1,46 @@
+/**
+ * This is just a manual process for updating the languages available from Transifex
+ */
+
+// Open https://www.transifex.com/languages/
+
+// Open console
+
+// Run this:
+i18n_codes = (function(){
+	var codes = {};
+	$("#txlanguage-list li").each(function () {
+		var $span = $('span', this);
+		var code = $.trim($span.text())
+			.replace(/[\(\))]/g, "")
+			.toLowerCase()
+			.replace(/[^a-z0-9]/g, '_');
+		$span.remove();
+		var name = $.trim($(this).text());
+		codes[code] = name;
+	});
+
+	var out = "\u0009\u0009return [\u000A";
+	$.each(codes, function (code, name) {
+		out += "\u0009\u0009\u0009\"" + code + "\", // " + name + "\u000A";
+	});
+	out += "\u0009\u0009];";
+
+	$('body').html($("<pre />").text(out));
+
+	return codes;
+})();
+
+// Copy the page into \Elgg\I18n\Translator::getAllLanguageCodes
+
+// Run this:
+!(function () {
+	var out = "";
+	$.each(i18n_codes, function (code, name) {
+		out += "\u0009\"" + code + "\" => \"" + name + "\",\u000A";
+	});
+
+	$('body').html($("<pre />").text(out));
+})();
+
+// Copy the page over the bottom of languages/en.php
