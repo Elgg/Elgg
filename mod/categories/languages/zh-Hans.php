ce0b9adde92621ecae5d9721bca5654fbf5dce65
Evan Winslow <evan.b.winslow@gmail.com>
2015-07-07 06:12:42 +0000

    chore(i18n): update translations

---
+++ mod/categories/languages/zh-Hans.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'categories' => '分类',
+	'categories:settings' => '设置网站分类',
+	'categories:explanation' => '要设置预定义的全站范围适用的分类，在下面分类名，用逗号隔开。当用户创建或编辑内容时，兼容的工具会显示出来。',
+	'categories:save:success' => '网站分类成功创建。',
+	'categories:results' => "这个分类的结果：%s",
+	'categories:on_activate_reminder' => "全站范围的分类直到你添加时才能生效。<a href=\"%s\">现在添加分类</a>",
+);
