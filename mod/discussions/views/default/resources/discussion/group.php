85c67da208f893b9f8c0e1267152b3bc4779794a
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-08-20 16:44:14 +0300

    chore(discussions): List of discussions by owner now works with any entity type
    Earlier the discussion/owner/<guid> page accepted only a group as the
    container of the discussions. Now groups have a separate page
    discussion/group/<guid> (like all other content plugins), and the owner
    page accepts any entity as a container.
    
    Refs: #8757

--- mod/discussions/views/default/resources/discussion/group.php
+++ mod/discussions/views/default/resources/discussion/group.php
@@ -1,20 +1,18 @@
 <?php
+/**
+ * Lists discussions created inside a specific group
+ */
 
 $guid = elgg_extract('guid', $vars);
-
-$target = get_entity($guid);
-
-if ($target instanceof ElggGroup) {
-	// Before Elgg 2.0 only groups could work as containers for discussions.
-	// Back then the URL that listed all discussions within a group was
-	// "discussion/owner/<guid>". Now that any entity can be used as a
-	// container, we use the standard "<content type>/group/<guid>" URL
-	// also with discussions.
-	forward("discussion/group/$guid", '301');
-}
-
 elgg_set_page_owner_guid($guid);
 
+elgg_group_gatekeeper();
+
+$group = get_entity($guid);
+if (!elgg_instanceof($group, 'group')) {
+	forward('', '404');
+}
+elgg_push_breadcrumb($group->name, $group->getURL());
 elgg_push_breadcrumb(elgg_echo('item:object:discussion'));
 
 elgg_register_title_button();
@@ -26,19 +24,11 @@ $options = array(
 	'subtype' => 'discussion',
 	'limit' => max(20, elgg_get_config('default_limit')),
 	'order_by' => 'e.last_action desc',
+	'container_guid' => $guid,
 	'full_view' => false,
 	'no_results' => elgg_echo('discussion:none'),
 	'preload_owners' => true,
 );
-
-if ($target instanceof ElggUser) {
-	// Display all discussions started by the user regardless of
-	// the entity that is working as a container. See #4878.
-	$options['owner_guid'] = $guid;
-} else {
-	$options['container_guid'] = $guid;
-}
-
 $content = elgg_list_entities($options);
 
 $params = array(
