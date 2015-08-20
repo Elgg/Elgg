d9bd5fc7a3276879180ad774be6705fbf3198f24
Steve Clay <steve@mrclay.org>
2015-08-20 16:08:46 -0400

    chore(installer): Update language for new default settings location
    Fixes #8842

--- install/languages/en.php
+++ install/languages/en.php
@@ -40,12 +40,12 @@ If you are ready to proceed, click the Next button.",
 	'install:check:php:register_globals' => 'Register globals must be turned off.',
 	'install:check:php:session.auto_start' => "session.auto_start must be off for Elgg to work. Either change the configuration of your server or add this directive to Elgg's .htaccess file.",
 
-	'install:check:enginedir' => 'Your web server does not have permission to create the settings.php file in the engine directory. You have two choices:
+	'install:check:installdir' => 'Your web server does not have permission to create the settings.php file in your installation directory. You have two choices:
 
-		1. Change the permissions on the engine directory
+		1. Change the permissions on the root directory of your Elgg installation
 
-		2. Copy the file settings.example.php to settings.php and follow the instructions in it for setting your database parameters.',
-	'install:check:readsettings' => 'A settings file exists in the engine directory, but the web server cannot read it. You can delete the file or change the read permissions on it.',
+		2. Copy the file engine/settings.example.php to /settings.php and follow the instructions in it for setting your database parameters.',
+	'install:check:readsettings' => 'A settings file exists in the installation directory, but the web server cannot read it. You can delete the file or change the read permissions on it.',
 
 	'install:check:php:success' => "Your server's PHP satisfies all of Elgg's requirements.",
 	'install:check:rewrite:success' => 'The test of the rewrite rules was successful.',
@@ -125,7 +125,7 @@ If you are ready to proceed, click the Next button.",
 	'install:error:cannotloadtables' => 'Cannot load the database tables',
 	'install:error:tables_exist' => 'There are already Elgg tables in the database. You need to either drop those tables or restart the installer and we will attempt to use them. To restart the installer, remove \'?step=database\' from the URL in your browser\'s address bar and press Enter.',
 	'install:error:readsettingsphp' => 'Unable to read engine/settings.example.php',
-	'install:error:writesettingphp' => 'Unable to write engine/settings.php',
+	'install:error:writesettingphp' => 'Unable to write /settings.php',
 	'install:error:requiredfield' => '%s is required',
 	'install:error:relative_path' => 'We don\'t think "%s" is an absolute path for your data directory',
 	'install:error:datadirectoryexists' => 'Your data directory %s does not exist.',
