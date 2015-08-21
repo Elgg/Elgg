c66778b0ae39dc363930edebe34d8e8875693a66
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-08-21 16:17:18 +0300

    chore(composer): renames PostUpdate class to PostInstall for clarity
    Refs #8823

--- engine/classes/Elgg/Composer/PostInstall.php
+++ engine/classes/Elgg/Composer/PostInstall.php
@@ -6,14 +6,14 @@ use Elgg;
 use Elgg\Filesystem\Directory;
 
 /**
- * A composer command handler to run after composer updates (and installs)
+ * A composer command handler to run after composer install
  */
-class PostUpdate {
+class PostInstall {
 	/**
 	 * Copies files that Elgg expects to be in the root directory.
-	 * 
+	 *
 	 * @param Event $event The Composer event (install/upgrade)
-	 * 
+	 *
 	 * @return void
 	 */
 	public static function execute(Event $event) {
@@ -21,7 +21,7 @@ class PostUpdate {
 		self::copyFromElggToRoot("index.php", "index.php");
 		self::copyFromElggToRoot("install.php", "install.php");
 		self::copyFromElggToRoot("upgrade.php", "upgrade.php");
-		
+
 		$managed_plugins = [
 			'aalborg_theme',
 			'blog',
@@ -58,39 +58,39 @@ class PostUpdate {
 			'uservalidationbyemail',
 			'web_services',
 		];
-		
+
 		foreach ($managed_plugins as $plugin) {
 			self::symlinkPluginFromRootToElgg($plugin);
 		}
 	}
-	
+
 	/**
 	 * Copies a file from the given location in Elgg to the given location in root.
-	 * 
+	 *
 	 * @param string $elggPath Path relative to elgg dir.
 	 * @param string $rootPath Path relative to app root dir.
-	 * 
+	 *
 	 * @return boolean Whether the copy succeeded.
 	 */
 	private static function copyFromElggToRoot($elggPath, $rootPath) {
 		$from = Elgg\Application::elggDir()->getPath($elggPath);
 		$to = Directory\Local::root()->getPath($rootPath);
-		
+
 		return copy($from, $to);
 	}
-	
+
 	/**
 	 * Make it possible for composer-managed Elgg site to recognize plugins
 	 * version-controlled in Elgg core.
-	 * 
+	 *
 	 * @param string $plugin The name of the plugin to symlink
-	 * 
+	 *
 	 * @return bool Whether the symlink succeeded.
 	 */
 	private static function symlinkPluginFromRootToElgg($plugin) {
 		$from = Directory\Local::root()->getPath("mod/$plugin");
 		$to = Elgg\Application::elggDir()->getPath("mod/$plugin");
-		
+
 		return !file_exists($from) && symlink($to, $from);
 	}
 }
