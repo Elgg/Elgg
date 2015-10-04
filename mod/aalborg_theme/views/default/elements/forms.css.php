ff4829e48c173342417fe9381877c174617ebd72
Steve Clay <steve@mrclay.org>
2015-10-04 15:47:21 -0400

    chore(css): provides missing UI class to hide Aria message
    We have a newer jQuery UI version that uses this class, but it wasn't in our
    CSS.
    
    Fixes #8782

--- mod/aalborg_theme/views/default/elements/forms.css.php
+++ mod/aalborg_theme/views/default/elements/forms.css.php
@@ -250,6 +250,16 @@ select {
 	background-color: #EEE;
 	display: block;
 }
+.ui-helper-hidden-accessible {
+	border: 0;
+	clip: rect(0 0 0 0);
+	height: 1px;
+	margin: -1px;
+	overflow: hidden;
+	padding: 0;
+	position: absolute;
+	width: 1px;
+}
 
 /* ***************************************
 	USER PICKER
