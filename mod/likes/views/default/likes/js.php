dae30cb71e8d1900bac8730e594ca8d5ea8d0154
Steve Clay <steve@mrclay.org>
2015-11-25 18:42:11 -0500

    fix(likes): count is updated after liking/unliking
    Also removes focus from the icon so its state is more apparent.
    
    Fixes #9100

--- mod/likes/views/default/likes/js.php
+++ mod/likes/views/default/likes/js.php
@@ -3,6 +3,7 @@
  * Likes JavaScript extension for elgg.js
  */
 ?>
+//<script>
 
 /**
  * Repositions the likes popup
@@ -24,4 +25,39 @@ elgg.ui.likesPopupHandler = function(hook, type, params, options) {
 };
 
 elgg.register_hook_handler('getOptions', 'ui.popup', elgg.ui.likesPopupHandler);
-elgg.ui.registerTogglableMenuItems('likes', 'unlike');
+
+!function() {
+	function setupHandlers(nameA, nameB) {
+		$(document).on('click', '.elgg-menu-item-' + nameA + ' a', function() {
+			var $menu = $(this).closest('.elgg-menu');
+
+			// Be optimistic about success
+			elgg.ui.toggleMenuItems($menu, nameB, nameA);
+
+			$menu.find('.elgg-menu-item-' + nameB + ' a').blur();
+
+			// Send the ajax request
+			elgg.action($(this).attr('href'), {
+				success: function(data) {
+					if (data.system_messages.error.length) {
+						// Something went wrong, so undo the optimistic changes
+						elgg.ui.toggleMenuItems($menu, nameA, nameB);
+					}
+
+					var func_name = data.output.num_likes > 0 ? 'removeClass' : 'addClass';
+					$(data.output.selector).text(data.output.text)[func_name]('hidden');
+				},
+				error: function() {
+					// Something went wrong, so undo the optimistic changes
+					elgg.ui.toggleMenuItems($menu, nameA, nameB);
+				}
+			});
+
+			// Don't want to actually click the link
+			return false;
+		});
+	}
+
+	setupHandlers('likes', 'unlike');
+	setupHandlers('unlike', 'likes');
+}();
