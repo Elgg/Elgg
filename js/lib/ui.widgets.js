cf55856d104d599de7a87be43db792165b2f6e90
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-12-07 23:36:51 +0100

    feature(widgets): widget layout now be set explicitly
    Before this change, page owner was always assumed as the owner of the layout
    and widgets. This change allows plugins to render multiple widget layouts
    on the same page with different owners.
    
    Fixes #7023

--- js/lib/ui.widgets.js
+++ js/lib/ui.widgets.js
@@ -52,10 +52,12 @@ elgg.ui.widgets.add = function(event) {
 		$(this).unbind('click', elgg.ui.widgets.add);
 	}
 
+	var $layout = $(this).closest('.elgg-layout-widgets');
+	var page_owner_guid = $layout.data('pageOwnerGuid') || elgg.get_page_owner_guid();
 	elgg.action('widgets/add', {
 		data: {
 			handler: type,
-			page_owner_guid: elgg.get_page_owner_guid(),
+			page_owner_guid: page_owner_guid,
 			context: $("input[name='widget_context']").val(),
 			show_access: $("input[name='show_access']").val(),
 			default_widgets: $("input[name='default_widgets']").val() || 0
--- views/default/page/layouts/widgets.php
+++ views/default/page/layouts/widgets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Elgg widgets layout
  *
@@ -7,14 +8,31 @@
  * @uses $vars['show_add_widgets'] Display the add widgets button and panel (true)
  * @uses $vars['exact_match']      Widgets must match the current context (false)
  * @uses $vars['show_access']      Show the access control (true)
+ * @uses $vars['owner_guid']       Widget owner GUID (optional, defaults to page owner GUID)
  */
-
 $num_columns = elgg_extract('num_columns', $vars, 3);
 $show_add_widgets = elgg_extract('show_add_widgets', $vars, true);
 $exact_match = elgg_extract('exact_match', $vars, false);
 $show_access = elgg_extract('show_access', $vars, true);
+$owner_guid = elgg_extract('owner_guid', $vars);
 
-$owner = elgg_get_page_owner_entity();
+$page_owner = elgg_get_page_owner_entity();
+
+if ($owner_guid) {
+	$owner = get_entity($owner_guid);
+} else {
+	$owner = $page_owner;
+}
+
+if (!$owner) {
+	return;
+}
+
+// Underlying views and functions assume that the page owner is
+// the owner of the widgets
+if ($owner->guid != $page_owner->guid) {
+	elgg_set_page_owner_guid($owner->guid);
+}
 
 $widget_types = elgg_get_widget_types();
 
@@ -23,7 +41,12 @@ elgg_push_context('widgets');
 
 $widgets = elgg_get_widgets($owner->guid, $context);
 
-echo '<div class="elgg-layout-widgets">';
+$layout_attrs = elgg_format_attributes(array(
+	'class' => 'elgg-layout-widgets',
+	'data-page-owner-guid' => $owner->guid,
+));
+
+echo "<div $layout_attrs>";
 
 if (elgg_can_edit_widget_layout($context)) {
 	if ($show_add_widgets) {
@@ -66,3 +89,8 @@ elgg_pop_context();
 echo elgg_view('graphics/ajax_loader', array('id' => 'elgg-widget-loader'));
 
 echo '</div>';
+
+// Restore original page owner
+if ($owner->guid != $page_owner->guid) {
+	elgg_set_page_owner_guid($page_owner->guid);
+}
