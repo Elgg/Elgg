9971fd3eaf39a48c8d131a5a50a23d6a00fdd602
Juho Jaakkola <juho@elgg.org>
2016-01-09 12:37:53 +0200

    chore(upgrades): converts upgrade related javascript into an AMD module

--- engine/lib/admin.php
+++ engine/lib/admin.php
@@ -243,7 +243,6 @@ function _elgg_admin_init() {
 	elgg_register_action('profile/fields/reorder', '', 'admin');
 
 	elgg_register_simplecache_view('admin.css');
-	elgg_register_js('elgg.upgrades', 'js/lib/upgrades.js');
 	elgg_register_js('jquery.jeditable', elgg_get_simplecache_url('jquery.jeditable.js'));
 
 	// administer
