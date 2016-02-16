d23f7269bbbed8aed1f5a08f9da10d27aedf7a6d
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2016-02-16 15:43:44 +0200

    feature(file): files are now displayed directly in the river

--- mod/file/views/default/river/object/file/create.php
+++ mod/file/views/default/river/object/file/create.php
@@ -10,7 +10,23 @@ $object = $item->getObjectEntity();
 $excerpt = strip_tags($object->description);
 $excerpt = elgg_get_excerpt($excerpt);
 
+$mime = $object->mimetype;
+$base_type = substr($mime, 0, strpos($mime,'/'));
+
+$params = array(
+	'entity' => $object,
+	'full_view' => true,
+);
+
+$attachment = '';
+if (elgg_view_exists("file/specialcontent/$mime")) {
+	$attachment = elgg_view("file/specialcontent/$mime", $params );
+} else if (elgg_view_exists("file/specialcontent/$base_type/default")) {
+	$attachment = elgg_view("file/specialcontent/$base_type/default", $params);
+}
+
 echo elgg_view('river/elements/layout', array(
 	'item' => $item,
 	'message' => $excerpt,
+	'attachments' => $attachment,
 ));
