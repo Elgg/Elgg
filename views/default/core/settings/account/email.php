9537bc35e63a9bf136f7ddf19467122487383b25
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-02-23 16:28:20 +0100

    chore(forms): update user settings forms to use elgg_view_input()

--- views/default/core/settings/account/email.php
+++ views/default/core/settings/account/email.php
@@ -8,12 +8,15 @@
 
 $user = elgg_get_page_owner_entity();
 
-if ($user) {
-	$title = elgg_echo('email:settings');
-	$content = elgg_echo('email:address:label') . ': ';
-	$content .= elgg_view('input/email', array(
-		'name' => 'email',
-		'value' => $user->email,
-	));
-	echo elgg_view_module('info', $title, $content);
+if (!$user instanceof ElggUser) {
+	return;
 }
+
+$title = elgg_echo('email:settings');
+$content = elgg_view_input('email', array(
+	'name' => 'email',
+	'value' => $user->email,
+	'label' => elgg_echo('email:address:label'),
+));
+
+echo elgg_view_module('info', $title, $content);
--- views/default/core/settings/account/password.php
+++ views/default/core/settings/account/password.php
@@ -8,26 +8,31 @@
  */
 $user = elgg_get_page_owner_entity();
 
-if ($user) {
-	$title = elgg_echo('user:set:password');
-
-	// only make the admin user enter current password for changing his own password.
-	$admin = '';
-	if (!elgg_is_admin_logged_in() || elgg_is_admin_logged_in() && $user->guid == elgg_get_logged_in_user_guid()) {
-		$admin .= elgg_echo('user:current_password:label') . ': ';
-		$admin .= elgg_view('input/password', array('name' => 'current_password'));
-		$admin = "<p>$admin</p>";
-	}
-
-	$password = elgg_echo('user:password:label') . ': ';
-	$password .= elgg_view('input/password', array('name' => 'password'));
-	$password = "<p>$password</p>";
-
-	$password2 = elgg_echo('user:password2:label') . ': ';
-	$password2 .= elgg_view('input/password', array('name' => 'password2'));
-	$password2 = "<p>$password2</p>";
-
-	$content = $admin . $password . $password2;
-
-	echo elgg_view_module('info', $title, $content);
+if (!$user instanceof ElggUser) {
+	return;
 }
+
+$title = elgg_echo('user:set:password');
+
+// only make the admin user enter current password for changing his own password.
+$admin = '';
+if (!elgg_is_admin_logged_in() || elgg_is_admin_logged_in() && $user->guid == elgg_get_logged_in_user_guid()) {
+	$admin .= elgg_view_input('password', array(
+		'name' => 'current_password',
+		'label' => elgg_echo('user:current_password:label'),
+	));
+}
+
+$password = elgg_view_input('password', array(
+	'name' => 'password',
+	'label' => elgg_echo('user:password:label'),
+		));
+
+$password2 = elgg_view_input('password', array(
+	'name' => 'password2',
+	'label' => elgg_echo('user:password2:label')
+		));
+
+$content = $admin . $password . $password2;
+
+echo elgg_view_module('info', $title, $content);
