81f05058223df1657bbd87474d8a68906573ea6c
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-04-17 14:16:13 +0200

    feature(entities): adds user capabilities service
    Adds \Elgg\UserCapabilities service and proxies ElggEntity::can*() methods.
    
    PHPUnit tests forthcoming (this must pass existing tests).

--- engine/classes/ElggAnnotation.php
+++ engine/classes/ElggAnnotation.php
@@ -123,30 +123,8 @@ class ElggAnnotation extends \ElggExtender {
 	 * @see elgg_set_ignore_access()
 	 */
 	public function canEdit($user_guid = 0) {
-		try {
-			$user = _elgg_services()->entityTable->getUserForPermissionsCheck($user_guid);
-		} catch (UserFetchFailureException $e) {
-			return false;
-		}
-
-		$result = false;
 		$entity = $this->getEntity();
-
-		if ($user) {
-			// If the owner of annotation is the specified user, they can edit.
-			if ($this->owner_guid == $user->guid) {
-				$result = true;
-			}
-
-			// If the user can edit the entity this is attached to, they can edit.
-			if ($result == false && $entity->canEdit($user->guid)) {
-				$result = true;
-			}
-		}
-
-		// Trigger plugin hook - note that $user may be null
-		$params = array('entity' => $entity, 'user' => $user, 'annotation' => $this);
-		return _elgg_services()->hooks->trigger('permissions_check', 'annotation', $params, $result);
+		return _elgg_services()->userCapabilities->canEditAnnotation($entity, $user_guid, $this);
 	}
 
 	// SYSTEM LOG INTERFACE
