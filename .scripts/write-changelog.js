c1525ca2ca8c12ce987030543c6aeb45144417f8
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-05-27 10:05:18 +0200

    chore(git): extend allowed commit message types
    Adds "feat" as an alias of "feature"
    Adds "fixes" and "fixed" as an alias of "fix"
    
    Fixes #9460
    Refs #9829

--- .scripts/write-changelog.js
+++ .scripts/write-changelog.js
@@ -5,18 +5,26 @@ var fs = require('fs');
 var changelog = require('elgg-conventional-changelog');
 
 changelog({
-  version: pkg.version,
-  repository: 'https://github.com/Elgg/Elgg',
-  types: {
-      feature: 'Features',
-      perf: 'Performance',
-      docs: 'Documentation',
-      fix: 'Bug Fixes',
-      deprecate: 'Deprecations',
-      breaks: 'Breaking Changes',
-  }
-}, function(err, log) {
-  if (err) throw new Error(err);
-  fs.writeFileSync('CHANGELOG.md', log);
+	version: pkg.version,
+	repository: 'https://github.com/Elgg/Elgg',
+	types: {
+		feat: 'Features',
+		feature: 'Features',
+		perf: 'Performance',
+		performance: 'Performance',
+		doc: 'Documentation',
+		docs: 'Documentation',
+		fix: 'Bug Fixes',
+		fixes: 'Bug Fixes',
+		fixed: 'Bug Fixes',
+		deprecate: 'Deprecations',
+		deprecates: 'Deprecations',
+		break: 'Breaking Changes',
+		breaks: 'Breaking Changes'
+	}
+}, function (err, log) {
+	if (err)
+		throw new Error(err);
+	fs.writeFileSync('CHANGELOG.md', log);
 });
 
