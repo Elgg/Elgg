b06dedde75fd3a9ae5ae8d0aa6409aedd9801086
Steve Clay <steve@mrclay.org>
2016-06-04 17:31:10 -0400

    chore(entities): completes removal of exportable interface
    All the `export` and `getExportableValues` methods were to be removed from
    entities in #9422, but these 3 were missed.
    
    Fixes #9865

--- engine/classes/ElggObject.php
+++ engine/classes/ElggObject.php
@@ -177,23 +177,6 @@ class ElggObject extends \ElggEntity {
 		return $object;
 	}
 
-	/*
-	 * EXPORTABLE INTERFACE
-	 */
-
-	/**
-	 * Return an array of fields which can be exported.
-	 *
-	 * @return array
-	 * @deprecated 1.9 Use toObject()
-	 */
-	public function getExportableValues() {
-		return array_merge(parent::getExportableValues(), array(
-			'title',
-			'description',
-		));
-	}
-
 	/**
 	 * Can a user comment on this object?
 	 *
