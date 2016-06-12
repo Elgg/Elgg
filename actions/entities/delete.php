9c447634d3d59592370e12cf154ca45ec5e2b8fb
Steve Clay <steve@mrclay.org>
2016-06-12 17:37:53 -0400

    security(entities): don't allow site/plugin delete via entities/delete action

--- actions/entities/delete.php
+++ actions/entities/delete.php
@@ -9,8 +9,8 @@
 $guid = get_input('guid');
 $entity = get_entity($guid);
 
-if (($entity) && ($entity->canEdit())) {
-	if ($entity->delete()) {
+if ($entity && $entity->canEdit()) {
+	if ($entity->delete() && (!$entity instanceof ElggPlugin) && (!$entity instanceof ElggSite)) {
 		system_message(elgg_echo('entity:delete:success', array($guid)));
 	} else {
 		register_error(elgg_echo('entity:delete:fail', array($guid)));
