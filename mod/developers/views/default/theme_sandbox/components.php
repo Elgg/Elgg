878dbc8e4891a4659d19d4761288fe2b9982964e
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-07-10 09:39:36 +0200

    feature(developers): add object summary listing view to theme sandbox

--- mod/developers/views/default/theme_sandbox/components.php
+++ mod/developers/views/default/theme_sandbox/components.php
@@ -9,6 +9,9 @@ echo elgg_view_module('theme-sandbox-demo', 'Image Block (.elgg-image-block)', $
 $body = elgg_view('theme_sandbox/components/list');
 echo elgg_view_module('theme-sandbox-demo', 'List (.elgg-list)', $body);
 
+$body = elgg_view('theme_sandbox/components/summary_listing');
+echo elgg_view_module('theme-sandbox-demo', 'Summary Listing (object/elements/summary)', $body);
+
 $body = elgg_view('theme_sandbox/components/table', array('class' => 'elgg-table'));
 echo elgg_view_module('theme-sandbox-demo', 'Table (.elgg-table)', $body);
 
