09649f576d111b5668819ee6c89cc2baeb61b149
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-07-10 10:21:21 +0200

    feature(views): object summary listing now accepts an icon
    Allow passing an icon to the object summary listing view in order to wrap content
    consistently with .elgg-list-summary image block

--- mod/developers/views/default/theme_sandbox/components/summary_listing.php
+++ mod/developers/views/default/theme_sandbox/components/summary_listing.php
@@ -7,6 +7,8 @@ $object->title = 'Test Object';
 $object->description = $ipsum;
 $object->tags = ['tag 1', 'tag 2', 'tag 3'];
 
+$icon = elgg_view_entity_icon($object, 'small');
+
 echo elgg_view('object/elements/summary', [
 	'entity' => $object,
 	'subtitle' => 'Listing subtitle',
@@ -17,4 +19,5 @@ echo elgg_view('object/elements/summary', [
 		'class' => 'elgg-menu-hz',
 	]),
 	'content' => elgg_get_excerpt($object->description),
+	'icon' => $icon,
 ]);
--- views/default/object/elements/summary.php
+++ views/default/object/elements/summary.php
@@ -2,6 +2,8 @@
 
 /**
  * Object summary
+ * Passing an 'icon' with the variables will wrap the listing in an image block. In that case,
+ * variables not listed in @uses (e.g. image_alt) will be passed to the image block.
  *
  * @uses $vars['entity']    ElggEntity
  * @uses $vars['title']     Title link (optional) false = no title, '' = default
@@ -9,7 +11,8 @@
  * @uses $vars['subtitle']  HTML for the subtitle (optional)
  * @uses $vars['tags']      HTML for the tags (default is tags on entity, pass false for no tags)
  * @uses $vars['content']   HTML for the entity content (optional)
- * @uses $vars['class']     Class selector
+ * @uses $vars['icon']      Object icon. If set, the listing will be wrapped with an image block
+ * @uses $vars['class']     Class selector for the image block
  */
 $entity = elgg_extract('entity', $vars);
 if (!$entity instanceof ElggEntity) {
@@ -37,4 +40,21 @@ $subtitle = elgg_view('object/elements/summary/subtitle', $vars);
 $extensions = elgg_view('object/summary/extend', $vars);
 $content = elgg_view('object/elements/summary/content', $vars);
 
-echo $metadata . $title . $subtitle . $tags . $extensions . $content;
+$summary = $metadata . $title . $subtitle . $tags . $extensions . $content;
+
+$icon = elgg_extract('icon', $vars);
+if (isset($icon)) {
+	$params = $vars;
+	foreach (['title', 'metadata', 'subtitle', 'tags', 'content', 'icon', 'class'] as $key) {
+		unset($params[$key]);
+	}
+	$class = (array) elgg_extract('class', $vars, []);
+	$class[] = 'elgg-listing-summary';
+	$params['class'] = $class;
+
+	$params['data-guid'] = $entity->guid;
+	
+	echo elgg_view_image_block($icon, $summary, $params);
+} else {
+	echo $summary;
+}
