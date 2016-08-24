33a029d959b939aaaa4f35c1d0a61b96ae89ee07
Steve Clay <steve@mrclay.org>
2016-08-24 12:52:40 -0400

    chore(river): core can use elgg_delete_river for 2.x w/o notices
    Fixes #10115

--- engine/classes/ElggEntity.php
+++ engine/classes/ElggEntity.php
@@ -2003,9 +2003,9 @@ abstract class ElggEntity extends \ElggData implements
 		access_show_hidden_entities($entity_disable_override);
 		elgg_set_ignore_access($ia);
 
-		elgg_delete_river(array('subject_guid' => $guid));
-		elgg_delete_river(array('object_guid' => $guid));
-		elgg_delete_river(array('target_guid' => $guid));
+		_elgg_delete_river(array('subject_guid' => $guid));
+		_elgg_delete_river(array('object_guid' => $guid));
+		_elgg_delete_river(array('target_guid' => $guid));
 		remove_all_private_settings($guid);
 
 		$res = $this->getDatabase()->deleteData("
