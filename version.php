45d7abbdb6d060d96e05794fcf579f7dd6803146
Steve Clay <steve@mrclay.org>
2016-09-01 15:41:29 -0400

    fix(entities): classnames for entity subtypes can be up to 255 chars
    Fixes #6802

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 
 // YYYYMMDD = Elgg Date
 // XX = Interim incrementer
-$version = 2015062900;
+$version = 2016090100;
 
 $composerJson = file_get_contents(dirname(__FILE__) . "/composer.json");
 if ($composerJson === false) {
