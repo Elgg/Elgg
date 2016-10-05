9f8e8dda8a1347074472bf2010ee5520ac4e90cd
Jerome Bakker <jeabakker@coldtrick.com>
2016-10-05 12:57:33 +0200

    fix(core): elgg_get_page_owner_entity will return ElggEntity
    The user still needs to have access to the entity, but the entity isn't
    limited to users or groups
    
    fixes #10361

--- engine/lib/pageowner.php
+++ engine/lib/pageowner.php
@@ -44,7 +44,7 @@ function elgg_get_page_owner_guid($guid = 0) {
 /**
  * Gets the owner entity for the current page.
  *
- * @return \ElggUser|\ElggGroup|false The current page owner or false if none.
+ * @return \ElggEntity|false The current page owner or false if none.
  *
  * @since 1.8.0
  */
@@ -54,12 +54,7 @@ function elgg_get_page_owner_entity() {
 		return false;
 	}
 
-	$owner = get_entity($guid);
-	if ($owner instanceof ElggUser || $owner instanceof ElggGroup) {
-		return $owner;
-	}
-
-	return false;
+	return get_entity($guid);
 }
 
 /**
