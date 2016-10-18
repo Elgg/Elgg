06e7c4992206874c8b01767b30ec41bf8389df85
jdalsem <jdalsem@coldtrick.com>
2016-10-18 17:20:04 +0200

    feat(blog): archive sidebar is now using a menu to show archive links
    fixes #10418

--- mod/blog/views/default/blog/sidebar/archives.php
+++ mod/blog/views/default/blog/sidebar/archives.php
@@ -15,35 +15,6 @@ if ($page == 'friends') {
 	return;
 }
 
-if (elgg_instanceof($page_owner, 'user')) {
-	$url_segment = 'blog/archive/' . $page_owner->username;
-} else {
-	$url_segment = 'blog/group/' . $page_owner->getGUID() . '/archive';
-}
-
-$dates = get_entity_dates('object', 'blog', $page_owner->getGUID());
-if (!$dates) {
-	return;
-}
-
-$dates = array_reverse($dates);
-
-$months = '';
-foreach ($dates as $date) {
-	$timestamplow = mktime(0, 0, 0, substr($date,4,2) , 1, substr($date, 0, 4));
-	$timestamphigh = mktime(0, 0, 0, ((int) substr($date, 4, 2)) + 1, 1, substr($date, 0, 4));
-
-	$link = $url_segment . '/' . $timestamplow . '/' . $timestamphigh;
-	$month = elgg_echo('date:month:' . substr($date, 4, 2), array(substr($date, 0, 4)));
-	
-	$link = elgg_view('output/url', [
-		'href' => $link,
-		'title' => $month,
-		'text' => $month
-	]);
-	$months .= elgg_format_element('li', [], $link);
-}
-
-$content = elgg_format_element('ul', ['class' => 'blog-archives'], $months);
+$content = elgg_view_menu('blog_archive', ['class' => 'elgg-menu-page']);
 
 echo elgg_view_module('aside', elgg_echo('blog:archives'), $content);
