4fcebae7d828685c8580c8e28038b6921fa8628d
Steve Clay <steve@mrclay.org>
2016-12-01 18:06:54 -0500

    WIP for view compiling

---
+++ views/default/output/friendlytime2.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Friendly time
+ * Translates an epoch time into a human-readable time.
+ *
+ * @uses string $vars['time'] Unix-style epoch timestamp
+ */
+
+namespace NS;
+
+use Foo\Bar as Baz;
+use Foo\Bing;
+
+?>BBB<?php
+
+echo elgg_get_friendly_time($vars);
+
