fdbf4fc4aaeaa0d6221df5dd9d2a4d764046b475
blackleg <hectorespertpardo@gmail.com>
2016-12-10 18:02:40 +0100

    Add basque transalations in blog, bookmarks and categories

---
+++ mod/categories/languages/eu.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  'categories' => 'Atalak',
+  'categories:settings' => 'Ezarri guneko atalak',
+  'categories:explanation' => 'Gune osoan erabiliko diren atalak definitzeko, sartu hemen, komaz bananduta. Atal hauek, bateragarriak diren tresnek erakutsiko dituzte erabiltzaileak edukiak sortu edo editatzean dituenean.',
+  'categories:save:success' => 'Guneko atalak ondo gorde dira.',
+  'categories:results' => 'Gune atalarentzat emaitzak: %s',
+  'categories:on_activate_reminder' => 'Gune atalak ez dira bistaratuko atalak sortu ezean. <a href="%s">Gehitu atalak orain.</a>',
+);
