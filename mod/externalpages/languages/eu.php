7a2ca3107af2912bbbd6810fbea75941ce002dfa
blackleg <hectorespertpardo@gmail.com>
2016-12-10 18:11:27 +0100

    Add more basque and catalan translations

---
+++ mod/externalpages/languages/eu.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'expages' => 'Gune orriak',
+  'admin:appearance:expages' => 'Gune orriak',
+  'expages:about' => 'Honi buruz',
+  'expages:terms' => 'Baldintzak',
+  'expages:privacy' => 'Pribatutasuna',
+  'expages:contact' => 'Kontaktua',
+  'expages:notset' => 'Orri honek ez du ezarpenik oraindik.',
+  'expages:posted' => 'Zure orria ongi eguneratu da.',
+  'expages:error' => 'Ezin izan da orri hau gorde.',
+);
