38b388e3bceab0b4aa298634c2f851fb914d3409
Steve Clay <steve@mrclay.org>
2017-01-30 15:40:15 -0500

    WIP test for @iionly

--- mod/ckeditor/views/default/elgg/ckeditor/set-basepath.js
+++
@@ -1,8 +0,0 @@
-/**
- * This view extends elgg.js and sets ckeditor basepath before jquery.ckeditor is loaded
- */
-define('elgg/ckeditor/set-basepath', function (require) {
-	var elgg = require('elgg');
-	// This global variable must be set before the editor script loading.
-	CKEDITOR_BASEPATH = elgg.get_simplecache_url('ckeditor/');
-});
