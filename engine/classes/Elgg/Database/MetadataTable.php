517e0872c505951c2b928e31eff28a6a3a581bbb
raghukul01 <raghukul@iitk.ac.in>
2017-04-10 22:15:57 +0530

    fix(metadata): warn devs when saving data longer than column holds

--- engine/classes/Elgg/Database/MetadataTable.php
+++ engine/classes/Elgg/Database/MetadataTable.php
@@ -41,6 +41,8 @@ class MetadataTable {
 	
 	/** @var string */
 	protected $table;
+	
+	const MYSQL_TEXT_BYTE_LIMIT = 65535;
 
 	/**
 	 * Constructor
@@ -123,6 +125,10 @@ class MetadataTable {
 		}
 	
 		$access_id = (int) $access_id;
+		
+		if (strlen($value) > self::MYSQL_TEXT_BYTE_LIMIT) {
+ 			elgg_log("Metadata '$name' is above the MySQL TEXT size limit and may be truncated.", 'WARNING');
+ 		}
 	
 		$query = "SELECT * FROM {$this->table}
 			WHERE entity_guid = :entity_guid and name = :name LIMIT 1";
@@ -209,6 +215,10 @@ class MetadataTable {
 		if (is_bool($value)) {
 			$value = (int) $value;
 		}
+		
+		if (strlen($value) > self::MYSQL_TEXT_BYTE_LIMIT) {
+ 			elgg_log("Metadata '$name' is above the MySQL TEXT size limit and may be truncated.", 'WARNING');
+ 		}
 	
 		// If ok then add it
 		$query = "UPDATE {$this->table}
