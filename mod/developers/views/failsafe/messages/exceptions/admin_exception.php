39dd766f1bd5de95fa93b8b743eb07d52390c9b8
Steve Clay <steve@mrclay.org>
2017-07-02 15:43:32 -0400

    security(exceptions): show admins less on exceptions
    Exception pages no longer dump the exception state for `DatabaseException`s
    as these may contain credentials. Unless `developers` is enabled, admins are
    not shown exception details at all.
    
    Fixes #10521

---
+++ mod/developers/views/failsafe/messages/exceptions/admin_exception.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Elgg exception (failsafe mode)
+ * Displays a single exception
+ *
+ * @package Elgg
+ * @subpackage Core
+ *
+ * @uses $vars['object'] An exception
+ */
+
+$exception = $vars['object'];
+/* @var \Exception $exception */
+?>
+
+<p class="elgg-messages-exception">
+	<span title="<?= get_class($exception); ?>">
+	<?= nl2br($exception->getMessage()); ?>
+		<br /><br />
+		Log at time <?= $vars['ts']; ?> may have more data.
+	</span>
+</p>
+
+<?php
+if ($exception instanceof \DatabaseException) {
+	// likely contains credentials
+	return;
+}
+?>
+
+<p class="elgg-messages-exception">
+	<?= nl2br(htmlentities(print_r($exception, true), ENT_QUOTES, 'UTF-8')); ?>
+</p>
