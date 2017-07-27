2c139f76244d9aa11622d82a32f6a8666fe5c3d1
Jeroen Dalsem <jdalsem@coldtrick.com>
2017-07-27 15:06:02 +0200

    chore(admin): improved admin interface styling

--- views/default/admin/sidebar.php
+++ views/default/admin/sidebar.php
@@ -3,6 +3,12 @@
  * Admin sidebar -- just outputs the page menus
  */
 
-$content = elgg_view_menu('page', ['sort_by' => 'priority', 'show_section_headers' => true]);
+$menu = elgg_view_menu('page', [
+	'sort_by' => 'priority',
+	'show_section_headers' => true,
+]);
 
-echo elgg_view_module('main', '', $content, ['class' => 'elgg-admin-sidebar-menu', 'id' => 'elgg-admin-nav-collapse']);
+echo elgg_format_element('div', [
+	'class' => 'elgg-admin-sidebar-menu',
+	'id' => 'elgg-admin-nav-collapse'
+], $menu);
