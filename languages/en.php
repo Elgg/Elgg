ce01e5e17419be4834251fc8a90bfcdc5c471f21
Jeroen Dalsem <jdalsem@coldtrick.com>
2017-08-02 14:22:51 +0200

    chore(friends): moved core actions, resource views and translations in

--- languages/en.php
+++ languages/en.php
@@ -239,33 +239,7 @@ return array(
 	'user' => "User",
 	'item:user' => "Users",
 
-/**
- * Friends
- */
-
 	'friends' => "Friends",
-	'friends:yours' => "Your friends",
-	'friends:owned' => "%s's friends",
-	'friend:add' => "Add friend",
-	'friend:remove' => "Remove friend",
-
-	'friends:add:successful' => "You have successfully added %s as a friend.",
-	'friends:add:failure' => "We couldn't add %s as a friend.",
-
-	'friends:remove:successful' => "You have successfully removed %s from your friends.",
-	'friends:remove:failure' => "We couldn't remove %s from your friends.",
-
-	'friends:none' => "No friends yet.",
-	'friends:none:you' => "You don't have any friends yet.",
-
-	'friends:none:found' => "No friends were found.",
-
-	'friends:of:none' => "Nobody has added this user as a friend yet.",
-	'friends:of:none:you' => "Nobody has added you as a friend yet. Start adding content and fill in your profile to let people find you!",
-
-	'friends:of:owned' => "People who have made %s a friend",
-
-	'friends:of' => "Friends of",
 
 	'avatar' => 'Avatar',
 	'avatar:noaccess' => "You're not allowed to edit this user's avatar",
@@ -863,12 +837,6 @@ These changes will only affect new users on the site.',
 	'river:comments:more' => '+%u more',
 	'river:comments:all' => 'View all %u comments',
 	'river:generic_comment' => 'commented on %s %s',
-
-	'friends:widget:description' => "Displays some of your friends.",
-	'friends:num_display' => "Number of friends to display",
-	'friends:icon_size' => "Icon size",
-	'friends:tiny' => "tiny",
-	'friends:small' => "small",
 
 /**
  * Icons
