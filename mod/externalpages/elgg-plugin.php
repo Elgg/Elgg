cdc289680a61676bffb86e5ad63ec086bf696492
Steve Clay <steve@mrclay.org>
2017-08-02 20:28:18 -0400

    fix(expages): don't log notice if pages aren't created

--- mod/externalpages/elgg-plugin.php
+++ mod/externalpages/elgg-plugin.php
@@ -1,6 +1,23 @@
 <?php
 
 return [
+	'entities' => [
+		[
+			'type' => 'object',
+			'subtype' => 'about',
+			'searchable' => false,
+		],
+		[
+			'type' => 'object',
+			'subtype' => 'terms',
+			'searchable' => false,
+		],
+		[
+			'type' => 'object',
+			'subtype' => 'privacy',
+			'searchable' => false,
+		],
+	],
 	'actions' => [
 		'expages/edit' => [
 			'access' => 'admin',
