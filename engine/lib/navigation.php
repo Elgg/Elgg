b6ba04353c027fa7558479e22a03a1d6a5f781ae
jdalsem <jdalsem@coldtrick.com>
2017-08-04 10:44:31 +0200

    feat(navigation): breadcrumbs is now a menu

--- engine/lib/navigation.php
+++ engine/lib/navigation.php
@@ -251,18 +251,19 @@ function elgg_register_title_button($handler = null, $name = 'add', $entity_type
  *
  * See elgg_get_breadcrumbs() and the navigation/breadcrumbs view.
  *
- * @param string $title The title to display. During rendering this is HTML encoded.
- * @param string $href  Optional. The href for the title. During rendering links are
- *                      normalized via elgg_normalize_url().
+ * @param string $text The title to display. During rendering this is HTML encoded.
+ * @param string $href Optional. The href for the title. During rendering links are
+ *                     normalized via elgg_normalize_url().
  *
  * @return void
  * @since 1.8.0
  * @see elgg_get_breadcrumbs()
  */
-function elgg_push_breadcrumb($title, $href = null) {
+function elgg_push_breadcrumb($text, $href = null) {
 	$breadcrumbs = (array) _elgg_config()->breadcrumbs;
+	
 	$breadcrumbs[] = [
-		'title' => $title,
+		'text' => $text,
 		'href' => $href,
 	];
 	
@@ -293,7 +294,7 @@ function elgg_pop_breadcrumb() {
  * <code>
  * [
  *    [
- *       'title' => 'Breadcrumb title',
+ *       'text' => 'Breadcrumb title',
  *       'href' => '/path/to/page',
  *    ]
  * ]
@@ -334,15 +335,24 @@ function elgg_get_breadcrumbs(array $breadcrumbs = null) {
 	}
 	
 	foreach ($breadcrumbs as $key => $breadcrumb) {
-		if (!isset($breadcrumb['link'])) {
-			continue;
+		$text = elgg_extract('text', $breadcrumb, elgg_extract('title', $breadcrumb));
+		if (isset($breadcrumb['link'])) {
+			elgg_deprecated_notice("Breadcrumb [{$text}] requires 'href' instead of 'link' set in the configuration", '3.0');
+			
+			$breadcrumbs[$key]['href'] = $breadcrumb['link'];
+			unset($breadcrumbs[$key]['link']);
+		}
+		if (isset($breadcrumb['title'])) {
+			elgg_deprecated_notice("Breadcrumb [{$text}] requires 'text' instead of 'title' set in the configuration", '3.0');
+			
+			$breadcrumbs[$key]['text'] = $breadcrumb['title'];
+			unset($breadcrumbs[$key]['title']);
 		}
 		
-		elgg_deprecated_notice("Breadcrumb [{$breadcrumb['title']}] requires 'href' instead of 'link' set in the configuration", '3.0');
-		
-		$breadcrumb['href'] = $breadcrumb['link'];
-		unset($breadcrumb['link']);
-		$breadcrumbs[$key] = $breadcrumb;
+		// adds name for usage in menu items
+		if (!isset($breadcrumb['name'])) {
+			$breadcrumbs[$key]['name'] = $key;
+		}
 	}
 
 	return $breadcrumbs;
@@ -367,9 +377,9 @@ function elgg_prepare_breadcrumbs($hook, $type, $breadcrumbs, $params) {
 		array_pop($breadcrumbs);
 	}
 
-	// apply excerpt to titles
+	// apply excerpt to text
 	foreach (array_keys($breadcrumbs) as $i) {
-		$breadcrumbs[$i]['title'] = elgg_get_excerpt($breadcrumbs[$i]['title'], 100);
+		$breadcrumbs[$i]['text'] = elgg_get_excerpt($breadcrumbs[$i]['text'], 100);
 	}
 	return $breadcrumbs;
 }
--- views/default/navigation/breadcrumbs.php
+++ views/default/navigation/breadcrumbs.php
@@ -2,21 +2,18 @@
 /**
  * Displays breadcrumbs.
  *
- * @package Elgg
- * @subpackage Core
- *
  * @uses $vars['breadcrumbs'] Breadcrumbs
  *                            If not set, will use breadcrumbs from the stack
  *                            pushed by elgg_push_breadcrumb()
  *                            <code>
  *                            [
  *                               [
- *                                  'title' => 'Breadcrumb title',
+ *                                  'text' => 'Breadcrumb title',
  *                                  'href' => '/path/to/page',
  *                               ],
  *                            ]
  *                            </code>
- * @uses $vars['class']       Optional class to add to the wrapping <ul>
+ * @uses $vars['class']       Optional class to add to the menu
  *
  * @see elgg_push_breadcrumb
  * @see elgg_get_breadcrumbs
@@ -25,29 +22,8 @@
 $breadcrumbs = elgg_extract('breadcrumbs', $vars);
 $breadcrumbs = elgg_get_breadcrumbs($breadcrumbs);
 
-if (!is_array($breadcrumbs) || empty($breadcrumbs)) {
-	return;
-}
-
-$attrs['class'] = elgg_extract_class($vars, ['elgg-menu', 'elgg-breadcrumbs']);
-$lis = '';
-
-foreach ($breadcrumbs as $breadcrumb) {
-	// We have to escape text (without double-encoding). Titles in core plugins are HTML escaped
-	// on input, but we can't guarantee that other users of this view and of elgg_push_breadcrumb()
-	// will do so.
-	if (!empty($breadcrumb['href'])) {
-		$crumb = elgg_view('output/url', [
-			'href' => $breadcrumb['href'],
-			'text' => $breadcrumb['title'],
-			'encode_text' => true,
-			'is_trusted' => true,
-		]);
-	} else {
-		$crumb = htmlspecialchars($breadcrumb['title'], ENT_QUOTES, 'UTF-8', false);
-	}
-
-	$lis .= "<li>$crumb</li>";
-}
-
-echo elgg_format_element('ul', $attrs, $lis);
+echo elgg_view_menu('breadcrumbs', [
+	'items' => $breadcrumbs,
+	'sort_by' => 'register',
+	'class' => elgg_extract_class($vars, ['elgg-breadcrumbs']),
+]);
