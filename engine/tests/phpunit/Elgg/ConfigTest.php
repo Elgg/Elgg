5e5906b624b54015cb4a27cd520b549bf4feb4c7
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-08-10 09:10:04 +0200

    chore(tests): adds basic config test
    Adds basic config test to make sure tests are running with expected
    config values

---
+++ engine/tests/phpunit/Elgg/ConfigTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @group Config
+ */
+class ConfigTest extends \Elgg\TestCase {
+
+	public function testCanReadValuesFromConfig() {
+
+		$config = $this->getTestingConfigArray();
+
+		$this->assertEquals(elgg_get_site_url(), $config['wwwroot']);
+		$this->assertEquals(realpath(elgg_get_data_path()), realpath($config['dataroot']));
+		$this->assertEquals(realpath(elgg_get_cache_path()), realpath($config['cacheroot']));
+	}
+}
