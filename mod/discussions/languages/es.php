898db1ca6039a37f2089328a60b6529af267b999
dragonjon <jonaltuna@msn.com>
2017-10-07 16:23:18 +0200

    Spanish translation

---
+++ mod/discussions/languages/es.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file was created by Translation Editor v6.0
+ * On 2017-10-07 16:18
+ */
+
+return array (
+  'discussions_tools:widgets:discussion:description' => 'Muestra las últimas discusiones',
+  'discussions_tools:widgets:discussion:settings:group_only' => 'Mostrar sólo las discusiones de los grupos que soy miembro',
+  'discussions_tools:widgets:discussion:more' => 'Ver mas discusiones',
+  'forum:group_tool_option:description' => 'Permitir a los miembros del grupo comenzar una discusión en formato simple foro.',
+  'discussions_tools:widgets:start_discussion:title' => 'Comenzar una discusión',
+  'discussions_tools:widgets:start_discussion:description' => 'Iniciar rápidamente una discusión en un grupo seleccionado',
+  'discussions_tools:widgets:start_discussion:not_enabled' => 'Ninguno de sus grupos tiene habilitada la discusión.',
+  'discussions_tools:forms:discussion:quick_start:group' => 'Seleccionar un grupo para esta discusión',
+  'discussions_tools:widgets:group_forum_topics:description' => 'Ver la ultima discusión',
+  'discussions_tools:discussion:confirm:open' => '¿Está seguro de que desea volver a abrir este tema?',
+  'discussions_tools:discussion:confirm:close' => '¿Estás seguro de que deseas cerrar este tema?',
+  'discussions_tools:widgets:start_discussion:login_required' => 'Para utilizar este widget debes iniciar sesión',
+  'discussions_tools:widgets:start_discussion:membership_required' => 'Debe ser miembro de al menos un grupo para poder utilizar este widget. Puedes encontrar grupos interesantes %shere%s.',
+  'discussions_tools:forms:discussion:quick_start:group:required' => 'Por favor seleccione un grupo',
+  'discussions_tools:action:discussions:toggle_status:success:open' => 'El tema fue reabierto con éxito',
+  'discussions_tools:action:discussions:toggle_status:success:close' => 'El tema se cerró con éxito',
+);
