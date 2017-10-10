0a84bf8be17816882e8917d93330f619af82d42f
Jerome Bakker <jeabakker@coldtrick.com>
2017-10-10 12:27:20 +0200

    chore(core): moved site secret information to security section
    fixes: #10645

--- views/default/forms/admin/security/settings.php
+++ views/default/forms/admin/security/settings.php
@@ -147,6 +147,35 @@ $notifications .= elgg_view('admin/security/settings/extend/notification');
 
 echo elgg_view_module('inline', elgg_echo('admin:security:settings:label:notifications'), $notifications);
 
+// site secret
+$strength = _elgg_get_site_secret_strength();
+$current_strength = elgg_echo('site_secret:current_strength');
+$strength_text = elgg_echo("site_secret:strength:$strength");
+$strength_msg = elgg_echo("site_secret:strength_msg:$strength");
+
+$site_secret = elgg_view('output/longtext', [
+	'value' => elgg_echo('admin:security:settings:site_secret:intro'),
+]);
+$site_secret .= elgg_view_module('main', "$current_strength: $strength_text", $strength_msg, [
+	'class' => ($strength != 'strong') ? 'elgg-message elgg-state-error' : 'elgg-message elgg-state-success',
+]);
+
+$site_secret_link = elgg_view('output/url', [
+	'text' => elgg_echo('admin:security:settings:site_secret:regenerate'),
+	'href' => 'action/admin/security/regenerate_site_secret',
+	'confirm' => true,
+	'class' => 'elgg-button elgg-button-action',
+]);
+
+$site_secret_link .= elgg_view('output/longtext', [
+	'value' => elgg_echo('admin:security:settings:site_secret:regenerate:help'),
+	'class' => 'elgg-subtext',
+]);
+
+$site_secret .= elgg_format_element('div', ['class' => 'mtm'], $site_secret_link);
+
+echo elgg_view_module('inline', elgg_echo('admin:security:settings:label:site_secret'), $site_secret);
+
 // footer
 $footer = elgg_view_field([
 	'#type' => 'submit',
