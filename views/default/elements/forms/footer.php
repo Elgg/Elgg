b823e609ecccdc61dd6dfb40eb942dd8cb0b8a31
Jerome Bakker <jeabakker@coldtrick.com>
2017-11-30 16:57:58 +0100

    feat(forms): form_vars and body_vars are now available in the footer
    fixes #11398

--- views/default/elements/forms/footer.php
+++ views/default/elements/forms/footer.php
@@ -1,10 +1,13 @@
 <?php
-
 /**
  * Wrap form footer
+ *
  * @uses $vars['footer']      Form footer
  * @uses $vars['action_name'] Action name
+ * @uses $vars['body_vars']   Vars used in the contents of the form
+ * @uses $vars['form_vars']   Vars used to format the form
  */
+
 $footer = elgg_extract('footer', $vars);
 if (empty($footer)) {
 	return;
@@ -12,4 +15,4 @@ if (empty($footer)) {
 
 echo elgg_format_element('div', [
 	'class' => 'elgg-foot elgg-form-footer',
-		], $footer);
+], $footer);
