0aec930a7ddd240c5e8864e4820776127975d6df
Jerome Bakker <jeabakker@coldtrick.com>
2017-12-12 10:01:42 +0100

    fix(core): after logging out return to the front page
    fixes #11485

--- actions/logout.php
+++ actions/logout.php
@@ -7,4 +7,4 @@ if (!logout()) {
 	return elgg_error_response(elgg_echo('logouterror'));
 }
 
-return elgg_ok_response('', elgg_echo('logoutok'));
+return elgg_ok_response('', elgg_echo('logoutok'), '');
