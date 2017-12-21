85daaf889c27d37caef53d255222476f59e53820
Steve Clay <steve@mrclay.org>
2017-12-21 16:01:35 -0500

    chore(docs): documents the `usersettings/save` action better

--- actions/usersettings/save.php
+++ actions/usersettings/save.php
@@ -1,6 +1,16 @@
 <?php
 /**
  * Aggregate action for saving settings
+ *
+ * To see the individual action methods, enable the developers plugin, visit Admin > Inspect > Plugin Hooks
+ * and search for "usersettings:save". The default methods are listed below:
+ *
+ * @see _elgg_set_user_language
+ * @see _elgg_set_user_password
+ * @see _elgg_set_user_default_access
+ * @see _elgg_set_user_name
+ * @see _elgg_set_user_username
+ * @see _elgg_set_user_email
  */
 
 elgg_make_sticky_form('usersettings');
