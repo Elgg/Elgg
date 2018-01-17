cbe065f8f526a7873fc7bc1d21230152846d8b3c
Jerome Bakker <jeabakker@coldtrick.com>
2018-01-17 13:25:26 +0100

    chore(livesearch): correctly fallback to input for matching

--- views/default/resources/livesearch.php
+++ views/default/resources/livesearch.php
@@ -1,12 +1,12 @@
 <?php
-
 /**
  * Page handler for autocomplete endpoint
  *
  * /livesearch/<match_on>?q=<query>
  */
 
-$match_on = elgg_extract('match_on', $vars, get_input('match_on'));
+// extract with false in order to replace empty value in $vars with get_input()
+$match_on = elgg_extract('match_on', $vars, get_input('match_on'), false);
 
 if (!elgg_view_exists("resources/livesearch/$match_on")) {
 	throw new \Elgg\PageNotFoundException();
