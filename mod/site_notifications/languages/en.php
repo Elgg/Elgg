9c351fe1abe48ff97df613ceb85f155fa8728d50
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-01-22 10:28:44 +0100

    chore(i18n): add site notification collection label

--- mod/site_notifications/languages/en.php
+++ mod/site_notifications/languages/en.php
@@ -3,7 +3,8 @@ return [
 	'site_notifications' => 'Site Notifications',
 	'notification:method:site' => 'Site',
 	'site_notifications:topbar' => 'Notifications',
-	'item:object:site_notification' => 'Site notifications',
+	'item:object:site_notification' => 'Site notification',
+	'collection:object:site_notification' => 'Site notifications',
 
 	'site_notifications:no_access' => 'You do not have access to this page. Perhaps you need to log in.',
 	'site_notifications:empty' => 'No notifications',
