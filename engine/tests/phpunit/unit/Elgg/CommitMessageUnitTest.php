f43350cf9cc778f912988d61fb5a3f2add5a837f
Jerome Bakker <jeabakker@coldtrick.com>
2018-01-22 15:23:53 +0100

    chore(tests): handle tests run on non-Unix machines
    fixes #7357

--- engine/tests/phpunit/unit/Elgg/CommitMessageUnitTest.php
+++ engine/tests/phpunit/unit/Elgg/CommitMessageUnitTest.php
@@ -107,8 +107,9 @@ Optional body
 Fixes #123, #456
 Refs #789
 ___MSG;
+		$body = $this->convertLineEndings($body);
 		$text = "$title\n$body";
-
+		
 		$msg = new CommitMessage($text);
 
 		$this->assertTrue($msg->isValidFormat());
@@ -138,6 +139,7 @@ This has multiple lines.
 Some of which are not really very long at all.
 Some are.
 ___TEXT;
+		$text3 = $this->convertLineEndings($text3);
 
 		$this->assertSame(array(2), CommitMessage::findLengthyLines($text3, 30));
 	}
@@ -150,6 +152,8 @@ The long line is down here. This line is much longer than the other.
 And this one is short.
 But here we go again with another long line.
 ___MSG;
+		$text = $this->convertLineEndings($text);
+		
 		$msg = new CommitMessage();
 		$msg->setMaxLineLength(40);
 		$msg->setMsg($text);
@@ -172,9 +176,42 @@ These are lines of text
 # and another one.
 And more text
 ___TEXT;
+		$text = $this->convertLineEndings($text);
 
 		$expected = "These are lines of text\nAnd more text";
 		$this->assertSame($expected, CommitMessage::removeComments($text));
 	}
+	
+	public function testReplaceLineEndings() {
+		
+		// Unix style (\n), this is what we want
+		$text = "chore(tests): Some title\nWith a body";
+		$msg = new CommitMessage($text);
+		
+		$this->assertSame($text, $msg->getMsg());
+		
+		// Commodore (\r)
+		$text2 = "chore(tests): Some title\rWith a body";
+		$msg = new CommitMessage($text2);
+		
+		$this->assertNotSame($text2, $msg->getMsg());
+		
+		// Windows (\r\n)
+		$text3 = "chore(tests): Some title\r\nWith a body";
+		$msg = new CommitMessage($text3);
+		
+		$this->assertNotSame($text3, $msg->getMsg());
+	}
 
+	/**
+	 * Convert Windows (\r\n) and Commodore (\r) line endings to Unix (\n) line endings as the CommitMessage class does the same.
+	 * This will prevent tests from failing on Windows machines
+	 *
+	 * @param string $text the commit message text
+	 *
+	 * @return string
+	 */
+	protected function convertLineEndings($text) {
+		return str_replace(["\r\n", "\r"], "\n", $text);
+	}
 }
