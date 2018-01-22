07b87da06fbdc300b83df2d5484395738063b2c0
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-01-22 11:11:09 +0100

    feat(navigation): added view vars to toggle entity/social menu

--- views/default/object/elements/summary/metadata.php
+++ views/default/object/elements/summary/metadata.php
@@ -2,19 +2,26 @@
 
 /**
  * Outputs object metadata
- * @uses $vars['metadata'] Metadata/menu
+ * @uses $vars['metadata']         Metadata/menu
+ * @uses $vars['show_entity_menu'] Show the entity menu (default: true)
+ * @uses $vars['show_social_menu'] Show the social menu (default: true)
  */
 
 $metadata = elgg_extract('metadata', $vars);
 if (!isset($metadata)) {
 	$metadata = '';
-
-	$metadata .= elgg_view_menu('entity', [
-		'entity' => elgg_extract('entity', $vars),
-		'handler' => elgg_extract('handler', $vars),
-	]);
 	
-	if (!elgg_in_context('admin')) {
+	$show_entity_menu = elgg_extract('show_entity_menu', $vars, true);
+	$show_social_menu = elgg_extract('show_social_menu', $vars, true);
+
+	if ($show_entity_menu) {
+		$metadata .= elgg_view_menu('entity', [
+			'entity' => elgg_extract('entity', $vars),
+			'handler' => elgg_extract('handler', $vars),
+		]);
+	}
+	
+	if ($show_social_menu && !elgg_in_context('admin')) {
 		$metadata .= elgg_view_menu('social', [
 			'entity' => elgg_extract('entity', $vars),
 			'handler' => elgg_extract('handler', $vars),
