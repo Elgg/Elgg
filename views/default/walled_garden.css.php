ea8b55995c7d2530eff3b77b3249099d484973a3
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-02-15 15:54:15 +0100

    chore(css): update walled garden styles
    Walled garden now includes core CSS. elgg.css is no longer loaded.
    Markup has been slightly updated to make it easier to work
    with layout elements
    
    Fixes #11717
    Fixes #11681

---
+++ views/default/walled_garden.css.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Walled garden CSS
+ */
+
+echo elgg_view('core.css');
+?>
+
+.elgg-page-walled-garden {
+	margin: 0;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	min-height: 100vh;
+}
+
+.elgg-page-walled-garden-background {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background-image: url('graphics/walled_garden.jpg');
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: 50%;
+	z-index: 1;
+	filter: blur(4px);
+}
+
+.elgg-heading-walled-garden {
+	font-size: 3rem;
+	line-height: 3rem;
+	color: $(text-color-strong);
+	text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
+	font-weight: $(font-bold-weight);
+}
+
+.elgg-page-walled-garden > .elgg-inner {
+	width: 30rem;
+	min-height: 100%;
+	z-index: 2;
+}
+
+.elgg-page-walled-garden > .elgg-inner > div {
+	&.elgg-page-body {
+		padding: 1.5rem 3rem;
+		background: #fff;
+		box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.3), -1px -1px 1px rgba(0, 0, 0, 0.3);
+	}
+	&.elgg-page-header {
+		padding: 3rem 0;
+	}
+	&.elgg-page-footer {
+		background: none;
+		a {
+			color: $(text-color-strong);
+			text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
+			font-weight: $(font-bold-weight);
+		}
+	}
+
+}
+
+.elgg-page-walled-garden .elgg-main {
+	min-height: 0;
+	padding: 0;
+}
+
+.elgg-heading-walled-garden a {
+	text-decoration: none;
+	color: inherit;
+}
+
+.elgg-page-walled-garden .elgg-form-login,
+.elgg-page-walled-garden .elgg-form-account {
+	max-width: none;
+}
+
+@media $(media-mobile-only) {
+	.elgg-page-walled-garden {
+		width: 100%;
+		float: none;
+	}
+}
+
