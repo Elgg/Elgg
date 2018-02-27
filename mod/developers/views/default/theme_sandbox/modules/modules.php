5fe78207dadfdc8ec6e8d11b2ce1dca00d33a003
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-02-27 17:02:36 +0100

    fixed(css): correct alignment of pageheader and module header elements
    fixes #11715

--- mod/developers/views/default/theme_sandbox/modules/modules.php
+++ mod/developers/views/default/theme_sandbox/modules/modules.php
@@ -7,7 +7,11 @@ $ipsum = elgg_view('developers/ipsum');
 	<div class="elgg-col elgg-col-1of2">
 		<div class="pam">
 			<?php
-				echo elgg_view_module('aside', 'Aside (.elgg-module-aside)', $ipsum);
+				echo elgg_view_module('aside', 'Aside (.elgg-module-aside)', $ipsum, [
+					'menu' => elgg_view('output/url', [
+						'href' => false,
+						'text' => elgg_echo('add'),
+					])]);
 				echo elgg_view_module('popup', 'Popup (.elgg-module-popup)', $ipsum);
 			?>
 		</div>
@@ -15,7 +19,7 @@ $ipsum = elgg_view('developers/ipsum');
 	<div class="elgg-col elgg-col-1of2">
 		<div class="pam">
 			<?php
-				echo elgg_view_module('info', 'Info (.elgg-module-info)', $ipsum);
+				echo elgg_view_module('info', 'Info (.elgg-module-info)', $ipsum, ['menu' => elgg_view_icon('plus')]);
 				echo elgg_view_module('featured', 'Featured (.elgg-module-featured)', $ipsum);
 			?>
 		</div>
