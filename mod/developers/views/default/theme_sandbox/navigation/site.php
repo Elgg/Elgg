4a7d20883cf3ec115e7a608341bd04d59e12d8da
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-03-22 22:09:13 +0100

    feat(navbar): properly handle second and third level child menus
    Second and third level child menus can now be displayed without breaking the layout

--- mod/developers/views/default/theme_sandbox/navigation/site.php
+++ mod/developers/views/default/theme_sandbox/navigation/site.php
@@ -1,16 +1,95 @@
 <?php
 
-$params = [];
-$params['menu'] = [];
-$params['menu']['default'] = [];
-for ($i=1; $i<=5; $i++) {
-	$params['menu']['default'][] = new ElggMenuItem($i, "Page $i", "#");
-}
-$params['menu']['default'][2]->setSelected(true);
+elgg_register_menu_item('site', [
+	'name' => 'parent',
+	'text' => 'Parent 1',
+	'href' => '#',
+]);
+
+elgg_register_menu_item('site', [
+	'name' => 'child1',
+	'parent_name' => 'parent',
+	'text' => 'Child 1',
+	'href' => '#',
+]);
+
+elgg_register_menu_item('site', [
+	'name' => 'child2',
+	'parent_name' => 'parent',
+	'text' => 'Child 2',
+	'href' => '#',
+]);
+
+elgg_register_menu_item('site', [
+	'name' => 'grandchild1',
+	'parent_name' => 'child1',
+	'text' => 'Grandchild 1',
+	'href' => '#',
+]);
+
+elgg_register_menu_item('site', [
+	'name' => 'grandchild2',
+	'parent_name' => 'child1',
+	'text' => 'Grandchild 2',
+	'href' => '#',
+]);
+
+elgg_register_menu_item('topbar', [
+	'name' => 'parent',
+	'parent_name' => 'account',
+	'text' => 'Parent 1',
+	'href' => '#',
+	'section' => 'alt',
+]);
+
+elgg_register_menu_item('topbar', [
+	'name' => 'child1',
+	'parent_name' => 'parent',
+	'text' => 'Child 1',
+	'href' => '#',
+	'section' => 'alt',
+]);
+
+elgg_register_menu_item('topbar', [
+	'name' => 'child2',
+	'parent_name' => 'parent',
+	'text' => 'Child 2',
+	'href' => '#',
+	'section' => 'alt',
+]);
+
+elgg_register_menu_item('topbar', [
+	'name' => 'grandchild1',
+	'parent_name' => 'child1',
+	'text' => 'Grandchild 1',
+	'href' => '#',
+	'section' => 'alt',
+]);
+
+elgg_register_menu_item('topbar', [
+	'name' => 'grandchild2',
+	'parent_name' => 'child1',
+	'text' => 'Grandchild 2',
+	'href' => '#',
+	'section' => 'alt',
+]);
 
 ?>
-<div class="elgg-page-header">
-	<div class="elgg-inner ptm">
-<?php echo elgg_view('navigation/menu/site', $params); ?>
+
+<div class="elgg-page-section elgg-page-topbar">
+	<div class="elgg-inner">
+		<div class="elgg-nav-button">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+
+		<div class="elgg-nav-collapse">
+			<?php
+			echo elgg_view_menu('site');
+			echo elgg_view_menu('topbar');
+			?>
+		</div>
 	</div>
 </div>
+
