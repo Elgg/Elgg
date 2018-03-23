892672cf590fef47f3f740ed932c3328ad76da57
Jerome Bakker <jeabakker@coldtrick.com>
2018-03-23 17:27:56 +0100

    fix(rss): correctly list comments
    fixes #11808

--- views/rss/page/elements/comments.php
+++ views/rss/page/elements/comments.php
@@ -2,12 +2,22 @@
 /**
  * RSS comments view
  *
- * @uses $vars['entity']
+ * @uses $vars['entity']        ElggEntity
+ * @uses $vars['limit']         Optional limit value (default is 25)
  */
 
-$options = array(
-	'guid' => $vars['entity']->getGUID(),
-	'annotation_name' => 'generic_comment',
-	'order_by' => 'n_table.time_created desc',
-);
-echo elgg_list_annotations($options);
+$limit = elgg_extract('limit', $vars, get_input('limit', 0));
+if (!$limit) {
+	$limit = elgg_trigger_plugin_hook('config', 'comments_per_page', [], 25);
+}
+
+echo elgg_list_entities([
+	'type' => 'object',
+	'subtype' => 'comment',
+	'container_guid' => $vars['entity']->guid,
+	'reverse_order_by' => true,
+	'full_view' => true,
+	'limit' => $limit,
+	'preload_owners' => true,
+	'distinct' => false,
+]);
