00ab2a3fb3a412ca861d8169b6b774b503255fa5
Jerome Bakker <jeabakker@coldtrick.com>
2018-05-17 15:52:01 +0200

    fix(blog): auto save draft refactored to use Ajax API
    fixes #11814

--- mod/blog/views/default/elgg/blog/save_draft.js
+++ mod/blog/views/default/elgg/blog/save_draft.js
@@ -6,57 +6,53 @@
 define(function(require) {
 	var $ = require('jquery');
 	var elgg = require('elgg');
+	var Ajax = require('elgg/Ajax');
+	
+	var oldDescription = '';
 
-	var saveDraftCallback = function(data, textStatus, XHR) {
-		if (textStatus == 'success' && data.success == true) {
-			var form = $('form[id=blog-post-edit]');
-
+	var saveDraftCallback = function(data) {
+		var $form = $('form.elgg-form-blog-save');
+		
+		if (data.success == true) {
 			// update the guid input element for new posts that now have a guid
-			form.find('input[name=guid]').val(data.guid);
-
-			oldDescription = form.find('textarea[name=description]').val();
-
+			$form.find('input[name=guid]').val(data.guid);
+			
+			oldDescription = $form.find('textarea[name=description]').val();
+			
 			var d = new Date();
 			var mins = d.getMinutes() + '';
 			if (mins.length == 1) {
 				mins = '0' + mins;
 			}
-			$(".blog-save-status-time").html(d.toLocaleDateString() + " @ " + d.getHours() + ":" + mins);
+			$form.find('.blog-save-status-time').html(d.toLocaleDateString() + " @ " + d.getHours() + ":" + mins);
 		} else {
-			$(".blog-save-status-time").html(elgg.echo('error'));
+			$form.find('.blog-save-status-time').html(elgg.echo('error'));
 		}
 	};
 
 	var saveDraft = function() {
-		if (typeof(tinyMCE) != 'undefined') {
-			tinyMCE.triggerSave();
-		}
-
+		var ajax = new Ajax(false);
+		
+		var formData = ajax.objectify('form.elgg-form-blog-save');
+		
+		formData.set('status', 'draft');
+		
 		// only save on changed content
-		var form = $('form[id=blog-post-edit]');
-		var description = form.find('textarea[name=description]').val();
-		var title = form.find('input[name=title]').val();
-
+		var description = formData.get('description');
+		var title = formData.get('title');
 		if (!(description && title) || (description == oldDescription)) {
 			return false;
 		}
-
-		var draftURL = elgg.config.wwwroot + "action/blog/auto_save_revision";
-		var postData = form.serializeArray();
-
-		// force draft status
-		$(postData).each(function(i, e) {
-			if (e.name == 'status') {
-				e.value = 'draft';
-			}
+		
+		ajax.action('blog/auto_save_revision', {
+			data: formData,
+			success: saveDraftCallback
 		});
-
-		$.post(draftURL, postData, saveDraftCallback, 'json');
 	};
 
 	var init = function() {
 		// get a copy of the body to compare for auto save
-		oldDescription = $('form[id=blog-post-edit]').find('textarea[name=description]').val();
+		oldDescription = $('form.elgg-form-blog-save').find('textarea[name=description]').val();
 
 		setInterval(saveDraft, 60000);
 	};
