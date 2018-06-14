4f00e31e28bb03ab82f7c118e38d2c2f36a00414
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-06-14 13:36:31 +0200

    perf(migrations): improved performance of migrations

--- engine/schema/migrations/20170728040000_change_table_engine.php
+++ engine/schema/migrations/20170728040000_change_table_engine.php
@@ -1,6 +1,5 @@
 <?php
 
-use Phinx\Db\Adapter\MysqlAdapter;
 use Phinx\Migration\AbstractMigration;
 
 class ChangeTableEngine extends AbstractMigration {
@@ -14,47 +13,14 @@ class ChangeTableEngine extends AbstractMigration {
 		'entities',
 		'entity_relationships',
 		'entity_subtypes',
-		'groups_entity',
 		'metadata',
 		'metastrings',
-		'objects_entity',
 		'private_settings',
 		'queue',
 		'river',
-		'sites_entity',
 		'system_log',
-		'users_entity',
 		'users_remember_me_cookies',
 		'users_sessions',
-	];
-
-	private $full_text_indexes = [
-		'groups_entity' => [
-			'name_2' => [
-				'name',
-				'description'
-			],
-		],
-		'objects_entity' => [
-			'title' => [
-				'title',
-				'description'
-			],
-		],
-		'sites_entity' => [
-			'name' => [
-				'name',
-				'description',
-				'url'
-			],
-		],
-		'users_entity' => [
-			'name' => ['name'],
-			'name2' => [
-				'name',
-				'username'
-			],
-		],
 	];
 
 	/**
@@ -69,28 +35,10 @@ class ChangeTableEngine extends AbstractMigration {
 				continue;
 			}
 
-			$table = $this->table($table);
-
-			if (!empty($this->full_text_indexes[$table->getName()])) {
-				$indexes = $this->full_text_indexes[$table->getName()];
-				foreach ($indexes as $index => $columns) {
-					try {
-						$this->execute("
-							ALTER TABLE {$prefix}{$table->getName()}
-							DROP KEY `{$index}`
-						");
-					} catch (Exception $e) {
-						// though schema defines them, some of these keys do not seem to exist
-					}
-				}
-			}
-
-			$result = $this->execute("
-				ALTER TABLE {$prefix}{$table->getName()}
+			$this->execute("
+				ALTER TABLE {$prefix}{$table}
 				ENGINE=InnoDB
 			");
-
-			$table->save();
 		}
 	}
 
@@ -106,28 +54,10 @@ class ChangeTableEngine extends AbstractMigration {
 				continue;
 			}
 
-			$table = $this->table($table);
-
 			$this->execute("
-				ALTER TABLE {$prefix}{$table->getName()}
+				ALTER TABLE {$prefix}{$table}
 				ENGINE=MyISAM
 			");
-
-			if (!empty($this->full_text_indexes[$table->getName()])) {
-				$indexes = $this->full_text_indexes[$table->getName()];
-				foreach ($indexes as $index => $columns) {
-					$columns = implode(',', array_map(function ($e) {
-						return "'$e'";
-					}, $columns));
-
-					$this->execute("
-						ALTER TABLE {$prefix}{$table->getName()}
-						ADD FULLTEXT INDEX {$index} ($columns)
-					");
-				}
-			}
-
-			$table->save();
 		}
 
 	}
