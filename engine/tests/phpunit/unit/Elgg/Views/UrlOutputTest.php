af8a273674047bae678e9ac834d74b097acadc70
Jerome Bakker <jeabakker@coldtrick.com>
2018-07-10 15:03:36 +0200

    fix(views): correctly handle text input for output/url
    fixes #12080

--- engine/tests/phpunit/unit/Elgg/Views/UrlOutputTest.php
+++ engine/tests/phpunit/unit/Elgg/Views/UrlOutputTest.php
@@ -34,4 +34,48 @@ class UrlOutputTest extends ViewRenderingTestCase {
 			'href' => $href,
 		]);
 	}
+	
+	/**
+	 * @dataProvider anchorTextProvider
+	 */
+	public function testCanRenderAnchorWithText($text_input, $text_output) {
+		$href = 'http://example.com';
+
+		$output = elgg_format_element('a', [
+			'class' => 'elgg-anchor',
+			'href' => $href,
+			'rel' => 'nofollow',
+		], $text_output);
+
+		$this->assertViewOutput($output, 'output/url', [
+			'text' => $text_input,
+			'href' => $href,
+		]);
+	}
+	
+	public function anchorTextProvider() {
+		
+		return [
+			[
+				'text_input' => 'sample text',
+				'text_output' => elgg_format_element('span', ['class' => 'elgg-anchor-label',], 'sample text'),
+			],
+			[
+				'text_input' => '0',
+				'text_output' => elgg_format_element('span', ['class' => 'elgg-anchor-label',], '0'),
+			],
+			[
+				'text_input' => 0,
+				'text_output' => elgg_format_element('span', ['class' => 'elgg-anchor-label',], 0),
+			],
+			[
+				'text_input' => '',
+				'text_output' => '',
+			],
+			[
+				'text_input' => false,
+				'text_output' => '',
+			],
+		];
+	}
 }
