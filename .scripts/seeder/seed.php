542af01af4bb0a5ebcaac284c3eb37f6366b130b
Jerome Bakker <jeabakker@coldtrick.com>
2018-10-29 16:11:19 +0100

    chore(seeder): use local images for seeding

--- .scripts/seeder/seed.php
+++ .scripts/seeder/seed.php
@@ -23,6 +23,11 @@ if (elgg_is_logged_in()) {
 	throw new LogicException("Seeds should not be run with a logged in user");
 }
 
+// disable mailer
 _elgg_services()->setValue('mailer', new \Zend\Mail\Transport\InMemory());
 
+// set seeder directory
+elgg_set_config('seeder_local_image_folder', __DIR__ . '/images/');
+
+// seed database
 _elgg_services()->seeder->seed();
