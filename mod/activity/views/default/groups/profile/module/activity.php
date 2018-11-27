13ac5617498e22419e658e48a07fa8585ac6d55f
Jerome Bakker <jeabakker@coldtrick.com>
2018-11-27 15:04:33 +0100

    fix(activity): correctly select group activity

--- mod/activity/views/default/groups/profile/module/activity.php
+++ mod/activity/views/default/groups/profile/module/activity.php
@@ -1,8 +1,37 @@
 <?php
 /**
  * Groups latest activity
- *
- * @todo add people joining group to activity
  */
 
-echo elgg_view('activity/group_module', $vars);
+use Elgg\Activity\GroupRiverFilter;
+
+$group = elgg_extract('entity', $vars);
+if (!$group instanceof \ElggGroup) {
+	return;
+}
+
+$all_link = elgg_view('output/url', [
+	'href' => elgg_generate_url('collection:river:group', [
+		'guid' => $group->guid,
+	]),
+	'text' => elgg_echo('link:view:all'),
+	'is_trusted' => true,
+]);
+
+elgg_push_context('widgets');
+
+$content = elgg_list_river([
+	'limit' => 4,
+	'pagination' => false,
+	'wheres' => [
+		new GroupRiverFilter($group),
+	],
+	'no_results' => elgg_echo('river:none'),
+]);
+elgg_pop_context();
+
+echo elgg_view('groups/profile/module', [
+	'title' => elgg_echo('collection:river:group'),
+	'content' => $content,
+	'all_link' => $all_link,
+]);
