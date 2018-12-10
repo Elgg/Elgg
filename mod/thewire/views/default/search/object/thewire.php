5f68cbcd5a9e30d81d3d15782d364ef55d4d55c6
Jerome Bakker <jeabakker@coldtrick.com>
2018-12-10 15:01:42 +0100

    chore(thewire): set search url to wire thread

--- mod/thewire/views/default/search/object/thewire.php
+++ mod/thewire/views/default/search/object/thewire.php
@@ -10,7 +10,19 @@ if (!$entity instanceof ElggWire) {
 	return;
 }
 
+// set a title, so we get a clickable link and no duplicate information
+// as ->getDisplayName() defaults to an excerpt of description which is already shown
 $entity->setVolatileData('search_matched_title', elgg_echo('item:object:thewire'));
+
+// link to the thread, not the entity
+$thread_id = (int) $entity->wire_thread;
+$search_url = elgg_generate_url('collection:object:thewire:thread', [
+	'guid' => $thread_id,
+]);
+// add # to jump directly to this post
+$search_url .= "#elgg-object-{$entity->guid}";
+
+$entity->setVolatileData('search_url', $search_url);
 
 $vars['entity'] = $entity;
 
