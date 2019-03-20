04ccc6a787ceddd52d8c68c49449225298bac5c7
Jerome Bakker <jeabakker@coldtrick.com>
2019-03-20 16:22:22 +0100

    chore(database): replace deprecated wheres usage
    fixes #12400

--- mod/developers/views/default/theme_sandbox/javascript/user_hover_menu.php
+++ mod/developers/views/default/theme_sandbox/javascript/user_hover_menu.php
@@ -1,12 +1,18 @@
 <?php
 
+use Elgg\Database\QueryBuilder;
+
 $me = elgg_get_logged_in_user_entity();
 echo elgg_view_entity_icon($me);
 
 // show another user if available
 $users = elgg_get_entities([
 	'type' => 'user',
-	'wheres' => ["guid != {$me->getGUID()}"],
+	'wheres' => [
+		function (QueryBuilder $qb, $main_alias) use ($me) {
+			return $qb->compare("{$main_alias}.guid", '!=', $me->guid, ELGG_VALUE_GUID);
+		},
+	],
 	'limit' => 1
 ]);
 
