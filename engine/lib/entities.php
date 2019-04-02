9e785825a93ac8004787d6d5a0e27268b0899c31
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-04-02 20:52:26 +0200

    fix(rss): register rss link in a more logical way and provide control
    fixes #12340

--- engine/lib/entities.php
+++ engine/lib/entities.php
@@ -576,8 +576,6 @@ function elgg_get_entities(array $options = []) {
  */
 function elgg_list_entities(array $options = [], $getter = 'elgg_get_entities', $viewer = 'elgg_view_entity_list') {
 
-	elgg_register_rss_link();
-
 	$offset_key = isset($options['offset_key']) ? $options['offset_key'] : 'offset';
 
 	$defaults = [
@@ -592,6 +590,11 @@ function elgg_list_entities(array $options = [], $getter = 'elgg_get_entities', 
 	];
 
 	$options = array_merge($defaults, $options);
+	
+	$options['register_rss_link'] = elgg_extract('register_rss_link', $options, elgg_extract('pagination', $options));
+	if ($options['register_rss_link']) {
+		elgg_register_rss_link();
+	}
 	
 	if ($options['no_results'] === true) {
 		$options['no_results'] = elgg_echo('notfound');
--- engine/lib/views.php
+++ engine/lib/views.php
@@ -833,8 +833,9 @@ function elgg_view_menu_item(\ElggMenuItem $item, array $vars = []) {
  *
  * @param \ElggEntity $entity The entity to display
  * @param array       $vars   Array of variables to pass to the entity view.
- *      'full_view'        Whether to show a full or condensed view. (Default: true)
- *      'item_view'        Alternative view used to render this entity
+ *      'full_view'           Whether to show a full or condensed view. (Default: true)
+ *      'item_view'           Alternative view used to render this entity
+ *      'register_rss_link'   Register the rss link availability (default: depending on full_view)
  *
  * @return string HTML to display or false
  * @todo The annotation hook might be better as a generic plugin hook to append content.
@@ -846,13 +847,15 @@ function elgg_view_entity(\ElggEntity $entity, array $vars = []) {
 		return false;
 	}
 
-	elgg_register_rss_link();
-
 	$defaults = [
 		'full_view' => true,
 	];
 
 	$vars = array_merge($defaults, $vars);
+	
+	if (elgg_extract('register_rss_link', $vars, elgg_extract('full_view', $vars))) {
+		elgg_register_rss_link();
+	}
 
 	$vars['entity'] = $entity;
 
@@ -946,8 +949,6 @@ function elgg_view_entity_icon(\ElggEntity $entity, $size = 'medium', $vars = []
  * @return string/false Rendered annotation
  */
 function elgg_view_annotation(\ElggAnnotation $annotation, array $vars = []) {
-	elgg_register_rss_link();
-
 	$defaults = [
 		'full_view' => true,
 	];
@@ -1239,7 +1240,8 @@ function elgg_view_message($type, $body, array $vars = []) {
  *
  * @param \ElggRiverItem $item A river item object
  * @param array          $vars An array of variables for the view
- *      'item_view'  Alternative view to render the item
+ *      'item_view'         Alternative view to render the item
+ *      'register_rss_link' Register the rss link availability (default: false)
  * @return string returns empty string if could not be rendered
  */
 function elgg_view_river_item($item, array $vars = []) {
@@ -1256,6 +1258,10 @@ function elgg_view_river_item($item, array $vars = []) {
 	if (!$subject || !$object) {
 		// subject is disabled or subject/object deleted
 		return '';
+	}
+	
+	if (elgg_extract('register_rss_link', $vars)) {
+		elgg_register_rss_link();
 	}
 
 	$vars['item'] = $item;
@@ -1603,10 +1609,6 @@ function _elgg_has_rss_link() {
 		return false;
 	}
 
-	if (isset($GLOBALS['autofeed']) && is_bool($GLOBALS['autofeed'])) {
-		elgg_deprecated_notice('Do not set the global $autofeed. Use elgg_register_rss_link()', '2.1');
-		return $GLOBALS['autofeed'];
-	}
 	return (bool) _elgg_config()->_elgg_autofeed;
 }
 
