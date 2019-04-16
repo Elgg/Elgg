5bab843fe85c511a589ee50b0a3d82ff5a10bcee
Jerome Bakker <jeabakker@coldtrick.com>
2019-04-16 16:46:07 +0200

    chore(i18n): update translations

---
+++ mod/blog/languages/ro_ro.php
@@ -0,0 +1,64 @@
+<?php
+return array(
+	'item:object:blog' => 'Blog',
+	'collection:object:blog' => 'Bloguri',
+	'collection:object:blog:all' => 'Toate blogurile site-ului',
+	'collection:object:blog:owner' => 'Bloguri %s',
+	'collection:object:blog:group' => 'Bloguri de grup',
+	'collection:object:blog:friends' => 'Blogurile prietenilor',
+	'add:object:blog' => 'Adaugă postare de blog',
+	'edit:object:blog' => 'Editează postarea de blog',
+
+	'blog:revisions' => 'Revizuiri',
+	'blog:archives' => 'Arhive',
+
+	'groups:tool:blog' => 'Activează blogul de grup',
+	'blog:write' => 'Scrie o postare de blog',
+
+	// Editing
+	'blog:excerpt' => 'Sumar',
+	'blog:body' => 'Conținut',
+	'blog:save_status' => 'Ultima salvare:',
+
+	'blog:revision' => 'Revizuire',
+	'blog:auto_saved_revision' => 'Revizuire salvată automat',
+
+	// messages
+	'blog:message:saved' => 'Postare de blog salvată.',
+	'blog:error:cannot_save' => 'Nu am putut salva postarea de blog.',
+	'blog:error:cannot_auto_save' => 'Nu am putut salva automat postarea de blog.',
+	'blog:error:cannot_write_to_container' => 'Acces insuficient pentru a salva blogul către grup.',
+	'blog:messages:warning:draft' => 'Există o schiță nesalvată a acestei postări!',
+	'blog:edit_revision_notice' => '(Versiune veche)',
+	'blog:message:deleted_post' => 'Postarea de blog a fost ștearsă.',
+	'blog:error:cannot_delete_post' => 'Nu am putut șterge postarea de blog.',
+	'blog:none' => 'Nu există postări de blog.',
+	'blog:error:missing:title' => 'Te rugăm să introduci un titlu de blog!',
+	'blog:error:missing:description' => 'Te rugăm să introduci conținutul blogului tău!',
+	'blog:error:cannot_edit_post' => 'Această postare s-ar putea să nu existe sau nu ai permisiunea de a o edita.',
+	'blog:error:post_not_found' => 'Nu am putit găsi postarea de blog specificată.',
+	'blog:error:revision_not_found' => 'Nu am putut găsi această revizuire.',
+
+	// river
+	'river:object:blog:create' => '%s a publicat o postare de blog %s',
+	'river:object:blog:comment' => '%s a adăugat un comentariu la blogul %s',
+
+	// notifications
+	'blog:notify:summary' => 'Postare de blog nouă numită %s',
+	'blog:notify:subject' => 'Postare de blog nouă: %s',
+	'blog:notify:body' =>
+'
+%s a publicat o nouă postare de blog: %s
+
+%s
+
+Vezi și adaugă comentariu la postarea de blog:
+%s
+',
+
+	// widget
+	'widgets:blog:name' => 'Postări de blog',
+	'widgets:blog:description' => 'Afișează-ți ultimele postări de blog',
+	'blog:moreblogs' => 'Mai multe postări de blog',
+	'blog:numbertodisplay' => 'Numărul de postări de blog pentru afișare',
+);
---
+++ mod/ckeditor/languages/ro_ro.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+	'ckeditor:html' => "Editează HTML",
+	'ckeditor:visual' => "Editor vizual",
+	'ckeditor:blockimagepaste' => "Lipirea directă a imaginilor nu este permisă.",
+);
