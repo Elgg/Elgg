1630a29a45669ab3ed3e7216fcde605cfc577dc3
Jerome Bakker <jeabakker@coldtrick.com>
2019-05-15 14:42:05 +0200

    chore(i18n): update translations

---
+++ mod/bookmarks/languages/ro_ro.php
@@ -0,0 +1,71 @@
+<?php
+return array(
+
+	/**
+	 * Menu items and titles
+	 */
+	'item:object:bookmarks' => 'Marcaj',
+	'collection:object:bookmarks' => 'Marcaje',
+	'collection:object:bookmarks:group' => 'Marcaje de grup',
+	'collection:object:bookmarks:all' => "Toate marcajele site-ului",
+	'collection:object:bookmarks:owner' => "Marcaje %s",
+	'collection:object:bookmarks:friends' => "Marcajele prietenilor",
+	'add:object:bookmarks' => "Adaugă un marcaj",
+	'edit:object:bookmarks' => "Editează marcajul",
+
+	'bookmarks:this' => "Marchează această pagină",
+	'bookmarks:this:group' => "Marcaj în %s",
+	'bookmarks:bookmarklet' => "Preia marcatorul",
+	'bookmarks:bookmarklet:group' => "Preia marcatorul de grup",
+	'bookmarks:address' => "Adresa marcajului",
+	'bookmarks:none' => 'Nu sunt marcaje',
+
+	'bookmarks:notify:summary' => 'Marcaj nou numit %s',
+	'bookmarks:notify:subject' => 'Marcaj nou: %s',
+	'bookmarks:notify:body' =>
+'%s a adăugat un marcaj nou: %s 
+
+Adresă: %s
+
+%s
+
+Vezi și comentează marcajul:
+%s
+',
+
+	'bookmarks:numbertodisplay' => 'Numărul de marcaje pentru afișare',
+
+	'river:object:bookmarks:create' => '%s a marcat %s',
+	'river:object:bookmarks:comment' => '%s a comentat la un marcaj %s',
+
+	'groups:tool:bookmarks' => 'Activează marcajele de grup',
+	
+	/**
+	 * Widget and bookmarklet
+	 */
+	'widgets:bookmarks:name' => 'Marcaje',
+	'widgets:bookmarks:description' => "Afișează-ți ultimele marcaje.",
+
+	'bookmarks:bookmarklet:description' =>
+			"Un marcator este un buton special pe care î-l salvezi în bara de legături a navigatorului tău. Acest lucru îți permite să salvezi orice resursă găsită pe internet la marcajele tale. Pentru a-l seta, trage butonul de mai jos în bara de legături a navigatorului tău:",
+
+	'bookmarks:bookmarklet:descriptionie' =>
+			"Dacă folosești Internet Explorer, va trebui să dai clic dreapta pe pictograma de marcator, selectează apoi 'adaugă la favorite', și apoi bara de Legături.",
+
+	'bookmarks:bookmarklet:description:conclusion' =>
+			"Poți apoi să marchezi oricând orice pagină pe care o vizitezi apăsând pe butonul din navigatorul tău.",
+
+	/**
+	 * Status messages
+	 */
+
+	'bookmarks:save:success' => "Elementul tău a fost marcat cu succes.",
+	'entity:delete:object:bookmarks:success' => "Marcajul a fost șters.",
+
+	/**
+	 * Error messages
+	 */
+
+	'bookmarks:save:failed' => "Marcajul tău nu a putut fi salvat. Asigură-te că ai introdus un titlu și o adresă apoi încearcă din nou.",
+	'bookmarks:unknown_bookmark' => 'Nu s-a putut găsi marcajul specificat',
+);
