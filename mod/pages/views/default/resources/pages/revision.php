3c91022d4c57c13bed61908df766f55890a8d01b
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-05-27 12:02:54 +0200

    fix(pages): no longer show history sidebar on revision page
    fixes #12413

--- mod/pages/views/default/resources/pages/revision.php
+++ mod/pages/views/default/resources/pages/revision.php
@@ -20,24 +20,19 @@ elgg_set_page_owner_guid($page->getContainerGUID());
 
 $title = "{$page->getDisplayName()}: " . elgg_echo('pages:revision');
 
-elgg_push_collection_breadcrumbs('object', 'page', $container);
+elgg_push_collection_breadcrumbs('object', 'page', $page->getContainerEntity());
 
 pages_prepare_parent_breadcrumbs($page);
 elgg_push_breadcrumb($page->getDisplayName(), $page->getURL());
-elgg_push_breadcrumb(elgg_echo('pages:revision'));
+elgg_push_breadcrumb(elgg_echo('pages:history'), elgg_generate_url('history:object:page', ['guid' => $page->guid]));
 
 $content = elgg_view_entity($page, [
 	'revision' => $annotation,
 ]);
 
-$sidebar = elgg_view('pages/sidebar/history', [
-	'page' => $page,
-]);
-
 $body = elgg_view_layout('default', [
 	'content' => $content,
 	'title' => $title,
-	'sidebar' => $sidebar,
 ]);
 
 echo elgg_view_page($title, $body);
