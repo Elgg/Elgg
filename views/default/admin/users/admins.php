78027dda9ebc940a8a9a8728af172f36924d8b5b
Rohit Gupta <rohit_1290@yahoo.com>
2019-05-27 20:37:42 +0530

    feat(admin): add admins directly from the administrators page

--- views/default/admin/users/admins.php
+++ views/default/admin/users/admins.php
@@ -1,3 +1,7 @@
 <?php
 
-echo elgg_list_entities([], 'elgg_get_admins');
+$make_admin_form = elgg_view_form('admin/user/makeadmin');
+echo elgg_view_module('info', elgg_echo('admin:users:searchuser'), $make_admin_form);
+
+$body = elgg_list_entities([], 'elgg_get_admins');
+echo elgg_view_module('info', elgg_echo('admin:users:existingadmins'), $body);
