7bd0f0da6e040471a588de11ac4bda8f9340492c
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-06-11 16:55:20 +0200

    feat(views): show_add_form view var is now supported in responses
    fixes #12277

--- views/default/object/elements/full/responses.php
+++ views/default/object/elements/full/responses.php
@@ -3,7 +3,8 @@
 /**
  * Outputs responses
  *
- * @uses $vars['responses'] Responses HTML
+ * @uses $vars['responses']     Responses HTML
+ * @uses $vars['show_add_form'] Boolean to control if a comment add form is allowed to show (default: true)
  */
 
 if (!elgg_extract('show_responses', $vars, true)) {
@@ -14,7 +15,7 @@ $entity = elgg_extract('entity', $vars);
 
 $responses = elgg_extract('responses', $vars);
 if ($responses === null && ($entity instanceof \ElggEntity)) {
-	$responses = elgg_view_comments($entity);
+	$responses = elgg_view_comments($entity, (bool) elgg_extract('show_add_form', $vars, true));
 }
 
 if (!$responses) {
