0afb29d8fbf6b89e2082285b19f645ea128a84c4
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-07-16 09:43:32 +0200

    fix(core): always show success message when upgrade has finished
    fixes #11581

--- engine/classes/Elgg/UpgradeService.php
+++ engine/classes/Elgg/UpgradeService.php
@@ -446,8 +446,6 @@ class UpgradeService {
 		$dbversion = (int) $this->config->version;
 
 		if ($this->upgradeCode($dbversion)) {
-			$this->system_messages->addSuccessMessage($this->translator->translate('upgrade:core'));
-
 			return true;
 		}
 
