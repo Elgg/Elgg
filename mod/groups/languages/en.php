6f21a8026699a654fdf891cb0650d1cd46b02f39
Joe Bordes <joe@tsolucio.com>
2019-07-25 01:19:14 +0200

    i18n(modGroups) syntax error in message

--- mod/groups/languages/en.php
+++ mod/groups/languages/en.php
@@ -176,7 +176,7 @@ or click below to view the group's join requests:
 	/**
 	 * Upgrades
 	 */
-	'groups:upgrade:2016101900:title' => 'Transfer group icons to new location',
+	'groups:upgrade:2016101900:title' => 'Transfer group icons to a new location',
 	'groups:upgrade:2016101900:description' => 'New entity icon API stores icons in a predictable location on the filestore
-relative to the entity\'s filestore directory. This upgrade aligns will align group plugin with the requirements of the new API.',
+relative to the entity\'s filestore directory. This upgrade will align the group plugin with the requirements of the new API.',
 );
