bb4a789479683402d4dfbe53facdfeb4a8c24d16
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-10-09 16:48:51 +0200

    fix(js): stop refreshing tokens if session changed
    fixes #12127

--- views/default/core/js/security.js
+++ views/default/core/js/security.js
@@ -86,6 +86,7 @@ elgg.security.refreshToken = function () {
 
 				if (elgg.get_logged_in_user_guid() != data.user_guid) {
 					elgg.session.user = null;
+					clearInterval(elgg.security.tokenRefreshTimer);
 					if (data.user_guid) {
 						elgg.register_error(elgg.echo('session_changed_user'));
 					} else {
