9b60eb971be9d8422d7035c4ed3ce33780a70980
Jerome Bakker <jeabakker@coldtrick.com>
2019-12-11 08:54:08 +0100

    chore(groups): use entity gatekeeper on edit page

--- mod/groups/views/default/resources/groups/edit.php
+++ mod/groups/views/default/resources/groups/edit.php
@@ -1,20 +1,21 @@
 <?php
 
+$guid = elgg_extract('guid', $vars);
+elgg_entity_gatekeeper($guid, 'group', 'group', true);
+
+/* @var $group \ElggGroup */
+$group = get_entity($guid);
+
+elgg_set_page_owner_guid($group->getGUID());
+
 elgg_require_js('elgg/groups/edit');
 
 elgg_push_breadcrumb(elgg_echo('groups'), elgg_generate_url('collection:group:group:all'));
+elgg_push_breadcrumb($group->getDisplayName(), $group->getURL());
 
-$guid = elgg_extract('guid', $vars);
-$title = elgg_echo("groups:edit");
-$group = get_entity($guid);
+$title = elgg_echo('groups:edit');
 
-if ($group instanceof ElggGroup && $group->canEdit()) {
-	elgg_set_page_owner_guid($group->getGUID());
-	elgg_push_breadcrumb($group->getDisplayName(), $group->getURL());
-	$content = elgg_view("groups/edit", ['entity' => $group]);
-} else {
-	$content = elgg_echo('groups:noaccess');
-}
+$content = elgg_view('groups/edit', ['entity' => $group]);
 
 $params = [
 	'content' => $content,
