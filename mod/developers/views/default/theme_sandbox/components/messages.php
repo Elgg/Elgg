a242305078fee4e3ca8ed3a894c1505efd36dba1
Jeroen Dalsem <jdalsem@coldtrick.com>
2020-01-07 14:47:14 +0100

    chore(css): links in messages body are now underlined

--- mod/developers/views/default/theme_sandbox/components/messages.php
+++ mod/developers/views/default/theme_sandbox/components/messages.php
@@ -1,10 +1,10 @@
 <?php
 
-echo elgg_view_message('success', 'Success message (.elgg-state-success)');
-echo elgg_view_message('notice', 'Notice message (.elgg-state-notice)');
-echo elgg_view_message('help', 'Notice message (.elgg-state-help)');
-echo elgg_view_message('warning', 'Warning message (.elgg-state-warning)');
-echo elgg_view_message('error', 'Error message (.elgg-state-error)');
+echo elgg_view_message('success', 'Success message (.elgg-state-success) example with a <a href="#">link</a>.');
+echo elgg_view_message('notice', 'Notice message (.elgg-state-notice) example with a <a href="#">link</a>');
+echo elgg_view_message('help', 'Notice message (.elgg-state-help) example with a <a href="#">link</a>');
+echo elgg_view_message('warning', 'Warning message (.elgg-state-warning) example with a <a href="#">link</a>');
+echo elgg_view_message('error', 'Error message (.elgg-state-error) example with a <a href="#">link</a>');
 
 echo elgg_view_message('success', 'Success message (.elgg-state-success) without title', ['title' => false]);
 echo elgg_view_message('notice', 'Notice message (.elgg-state-notice) without title', ['title' => false]);
