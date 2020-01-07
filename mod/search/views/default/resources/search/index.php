28cd2eadf1620ca0a13c397d7948f1fc3a46ce54
Jeroen Dalsem <jdalsem@coldtrick.com>
2020-01-07 11:24:33 +0100

    fix(search): do not try a search with invalid query
    fixes #12359

--- mod/search/views/default/resources/search/index.php
+++ mod/search/views/default/resources/search/index.php
@@ -23,17 +23,15 @@ if ($container_guid && !is_array($container_guid)) {
 
 $query = elgg_extract('query', $params);
 
-$highlighted_query = $service->getHighlighter()->highlightWords($query);
-
-$title = elgg_echo('search:results', ["\"$highlighted_query\""]);
-
 $form = elgg_view_form('search', [
 	'action' => elgg_generate_url('default:search'),
 	'method' => 'get',
 	'disable_security' => true,
 ], $params);
 
-if (elgg_is_empty($query)) {
+if (!preg_match('/[\pL\pN]+/', $query)) {
+	$form .= elgg_view_message('warning', elgg_echo('search:empty_query'), ['title' => false]);
+	
 	// display a search form if there is no query
 	$layout = elgg_view_layout('content', [
 		'title' => elgg_echo('search'),
@@ -45,6 +43,9 @@ if (elgg_is_empty($query)) {
 
 	return;
 }
+
+$highlighted_query = $service->getHighlighter()->highlightWords($query);
+$title = elgg_echo('search:results', ["\"$highlighted_query\""]);
 
 $use_type = function ($search_type, $type = null, $subtype = null) use ($params) {
 
