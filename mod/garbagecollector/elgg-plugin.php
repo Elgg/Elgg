36df204194bdce5ed6fffd27525b0f1bec1c5698
Jeroen Dalsem <jdalsem@coldtrick.com>
2020-01-09 09:56:01 +0100

    chore(garbagecollector): no need for bootstrap class

--- mod/garbagecollector/elgg-plugin.php
+++ mod/garbagecollector/elgg-plugin.php
@@ -1,11 +1,21 @@
 <?php
 
+use Elgg\GarbageCollector\CronRunner;
+
+require_once(__DIR__ . '/lib/deprecated.php');
+
 return [
-	'bootstrap' => \Elgg\GarbageCollector\Bootstrap::class,
 	'settings' => [
 		'period' => 'monthly',
 	],
 	'cli_commands' => [
 		\Elgg\GarbageCollector\OptimizeCommand::class,
+	],
+	'hooks' => [
+		'cron' => [
+			'all' => [
+				CronRunner::class => [],
+			],
+		],
 	],
 ];
