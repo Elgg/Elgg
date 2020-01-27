8af6356fc3c8764e0858fcba79d7a2e2436e3f6f
RiverVanRain <river.vanrain@gmail.com>
2020-01-27 22:59:02 +0300

    Added dot and dash

--- engine/classes/Elgg/Users/Accounts.php
+++ engine/classes/Elgg/Users/Accounts.php
@@ -271,7 +271,7 @@ class Accounts {
 			}
 		}
 		
-		$whitelist = '/^([A-Za-z_0-9]+)$/i';
+		$whitelist = '/^([A-Za-z_0-9.-]+)$/i';
 		if (!preg_match($whitelist, $username)) {
 		    throw new RegistrationException($this->translator->translate('registration:badchars'));
 		}
