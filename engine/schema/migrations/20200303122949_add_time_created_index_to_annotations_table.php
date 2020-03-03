af34641aa70060d4c9878019b63f182d133e199c
Jerome Bakker <jeabakker@coldtrick.com>
2020-03-03 14:13:57 +0100

    perf(db): add index to annotations table
    fixes #13124

---
+++ engine/schema/migrations/20200303122949_add_time_created_index_to_annotations_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddTimeCreatedIndexToAnnotationsTable extends AbstractMigration {
+    
+	/**
+     * Adds an index to the annotations table for the time_created column
+     */
+	public function change() {
+		if (!$this->hasTable('annotations')) {
+			return;
+		}
+		
+		$table = $this->table('annotations');
+		if ($table->hasIndex(['time_created'])) {
+			return;
+		}
+		
+		$table->addIndex(['time_created'], [
+			'name' => 'time_created',
+			'unique' => false,
+		]);
+		
+		$table->save();
+    }
+}
