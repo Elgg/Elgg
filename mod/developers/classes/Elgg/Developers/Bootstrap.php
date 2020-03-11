e22361d44aa70abd3048779d80f5e8f7c1cd3c70
Jerome Bakker <jeabakker@coldtrick.com>
2020-03-11 14:43:58 +0100

    chore(developers): rewrite internal boot sequence

--- mod/developers/classes/Elgg/Developers/Bootstrap.php
+++ mod/developers/classes/Elgg/Developers/Bootstrap.php
@@ -25,7 +25,11 @@ class Bootstrap extends DefaultPluginBootstrap {
 	 * @return void
 	 */
 	protected function processSettings() {
-		$settings = elgg_get_plugin_from_id('developers')->getAllSettings();
+		$elgg = $this->elgg();
+		$hooks = $elgg->hooks;
+		$events = $elgg->events;
+		
+		$settings = $this->plugin()->getAllSettings();
 	
 		ini_set('display_errors', (int) !empty($settings['display_errors']));
 	
@@ -36,21 +40,21 @@ class Bootstrap extends DefaultPluginBootstrap {
 				// Write to JSON file to not take up memory See #11886
 				$uid = substr(hash('md5', uniqid('', true)), 0, 10);
 				$log_file = \Elgg\Project\Paths::sanitize(elgg_get_config('dataroot') . "logs/screen/$uid.html", false);
-				elgg()->config->log_cache = $log_file;
+				$elgg->config->log_cache = $log_file;
 	
 				$handler = new \Monolog\Handler\StreamHandler(
 					$log_file,
-					elgg()->logger->getLevel()
+					$elgg->logger->getLevel()
 				);
 	
 				$formatter = new \Elgg\Developers\ErrorLogHtmlFormatter();
 				$handler->setFormatter($formatter);
 	
-				elgg()->logger->pushHandler($handler);
+				$elgg->logger->pushHandler($handler);
 	
 				$handler->pushProcessor(new \Elgg\Logger\BacktraceProcessor());
 	
-				elgg_register_plugin_hook_handler('view_vars', 'page/elements/html', function(\Elgg\Hook $hook)  use ($handler) {
+				$hooks->registerHandler('view_vars', 'page/elements/html', function(\Elgg\Hook $hook)  use ($handler) {
 					$handler->close();
 					
 					$vars = $hook->getValue();
@@ -59,11 +63,11 @@ class Bootstrap extends DefaultPluginBootstrap {
 					return $vars;
 				});
 	
-				elgg_register_event_handler('shutdown', 'system', function() use ($handler) {
+				$events->registerHandler('shutdown', 'system', function() use ($handler, $elgg) {
 					// Prevent errors in cli
 					$handler->close();
 					
-					$log_file = elgg()->config->log_cache;
+					$log_file = $elgg->config->log_cache;
 					if (is_file($log_file)) {
 						unlink($log_file);
 					}
@@ -73,8 +77,8 @@ class Bootstrap extends DefaultPluginBootstrap {
 	
 		if (!empty($settings['show_strings'])) {
 			// Beginning and end to make sure both early-rendered and late-loaded translations get included
-			elgg_register_event_handler('init', 'system', 'developers_decorate_all_translations', 1);
-			elgg_register_event_handler('init', 'system', 'developers_decorate_all_translations', 1000);
+			$events->registerHandler('init', 'system', 'developers_decorate_all_translations', 1);
+			$events->registerHandler('init', 'system', 'developers_decorate_all_translations', 1000);
 		}
 	
 		if (!empty($settings['show_modules'])) {
@@ -82,12 +86,12 @@ class Bootstrap extends DefaultPluginBootstrap {
 		}
 	
 		if (!empty($settings['wrap_views'])) {
-			elgg_register_plugin_hook_handler('view', 'all', 'developers_wrap_views', 600);
+			$hooks->registerHandler('view', 'all', 'developers_wrap_views', 600);
 		}
 	
 		if (!empty($settings['log_events'])) {
-			elgg_register_event_handler('all', 'all', 'developers_log_events', 1);
-			elgg_register_plugin_hook_handler('all', 'all', 'developers_log_events', 1);
+			$events->registerHandler('all', 'all', 'developers_log_events', 1);
+			$hooks->registerHandler('all', 'all', 'developers_log_events', 1);
 		}
 	
 		if (!empty($settings['show_gear']) && elgg_is_admin_logged_in() && !elgg_in_context('admin')) {
@@ -95,23 +99,16 @@ class Bootstrap extends DefaultPluginBootstrap {
 			elgg_register_ajax_view('developers/gear_popup');
 			elgg_register_simplecache_view('elgg/dev/gear.html');
 	
-			$handler = [Hooks::class, 'alterMenuSectionVars'];
-			elgg_register_plugin_hook_handler('view_vars', 'navigation/menu/elements/section', $handler);
-	
-			$handler = [Hooks::class, 'alterMenuSections'];
-			elgg_register_plugin_hook_handler('view', 'navigation/menu/elements/section', $handler);
-	
-			$handler = [Hooks::class, 'alterMenu'];
-			elgg_register_plugin_hook_handler('view', 'navigation/menu/default', $handler);
+			$hooks->registerHandler('view_vars', 'navigation/menu/elements/section', __NAMESPACE__ . '\Hooks::alterMenuSectionVars');
+			$hooks->registerHandler('view', 'navigation/menu/elements/section', __NAMESPACE__ . '\Hooks::alterMenuSections');
+			$hooks->registerHandler('view', 'navigation/menu/default', __NAMESPACE__ . '\Hooks::alterMenu');
 		}
 		
 		if (!empty($settings['block_email'])) {
-			$handler = [Hooks::class, 'blockOutgoingEmails'];
-			elgg_register_plugin_hook_handler('transport', 'system:email', $handler);
+			$hooks->registerHandler('transport', 'system:email', __NAMESPACE__ . '\Hooks::blockOutgoingEmails');
 			
 			if (!empty($settings['forward_email'])) {
-				$handler = [Hooks::class, 'setForwardEmailAddress'];
-				elgg_register_plugin_hook_handler('prepare', 'system:email', $handler);
+				$hooks->registerHandler('prepare', 'system:email', __NAMESPACE__ . '\Hooks::setForwardEmailAddress');
 			}
 		}
 	
@@ -132,7 +129,7 @@ class Bootstrap extends DefaultPluginBootstrap {
 			$handler->pushProcessor(new \Monolog\Processor\WebProcessor());
 			$handler->pushProcessor(new \Elgg\Logger\BacktraceProcessor());
 	
-			elgg()->logger->pushHandler($handler);
+			$elgg->logger->pushHandler($handler);
 		}
 	}
 }
