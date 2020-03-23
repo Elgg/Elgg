efdb95f92e33c7e67cb22552d2a03e29e324191a
Jerome Bakker <jeabakker@coldtrick.com>
2020-03-23 15:32:50 +0100

    chore(seeder): use correct params for random access_id

--- engine/classes/Elgg/Database/Seeds/Seeding.php
+++ engine/classes/Elgg/Database/Seeds/Seeding.php
@@ -519,7 +519,7 @@ trait Seeding {
 			'container_guid' => $container ? $container->guid : null,
 		];
 
-		$access_array = get_write_access_array($user->guid, null, null, $params);
+		$access_array = get_write_access_array($user->guid, null, false, $params);
 
 		return array_rand($access_array, 1);
 	}
