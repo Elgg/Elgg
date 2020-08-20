bf07170cc6bdb8cc513169964e72d6bc8d192c44
Jeroen Dalsem <jdalsem@coldtrick.com>
2020-08-20 12:39:27 +0200

    chore(docs): simplified grunt configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,41 @@ module.exports = function(grunt) {
 
   require('load-grunt-config')(grunt);
 
+  grunt.initConfig({
+	  clean: {
+		  docs: ['docs/_build']
+	  },
+	  connect: {
+		  docs: {
+			  options: {
+				  	hostname: "*",
+			  		port: 1919,
+		  			base: 'docs/_build',
+		  			livereload: true
+			  }
+		  }
+	  },
+	  exec: {
+		  build_docs: {
+			  cmd: 'sphinx-build docs docs/_build'
+		  }
+	  },
+	  open: {
+		  docs: {
+			  path: 'http://localhost:1919/index.html',
+		  }
+	  },
+	  watch: {
+		  docs: {
+			  files: ['**/*.rst'],
+			  tasks: ['exec:build_docs'],
+			  options: {
+				  livereload: true
+			  }
+		  }
+	  }
+  });
+  
   grunt.registerTask('build', [
     'clean:docs',
     'exec:build_docs'
