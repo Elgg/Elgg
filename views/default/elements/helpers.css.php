d9c045524ed7aa3a11996c0e78c87c076d5e7af2
Nikolai Shcherbin <support@wzm.me>
2020-11-20 16:32:46 +0300

    Fix #1

--- views/default/elements/helpers.css.php
+++ views/default/elements/helpers.css.php
@@ -83,9 +83,47 @@
 	border-right: 1px solid $(border-color-soft);
 }
 
+/* Flexbox */
+.flex {display: flex !important}
+.flex-row {flex-direction: row !important}
+.flex-column {flex-direction: column !important}
+.flex-row-reverse {flex-direction: row-reverse !important}
+.flex-column-reverse {flex-direction: column-reverse !important}
+.flex-wrap {flex-wrap: wrap !important}
+.flex-nowrap {flex-wrap: nowrap !important}
+.flex-wrap-reverse {flex-wrap: wrap-reverse !important}
+.justify-content-start {justify-content: flex-start !important}
+.justify-content-end {justify-content: flex-end !important}
+.justify-content-center {justify-content: center !important}
+.justify-content-between {justify-content: space-between !important}
+.justify-content-around {justify-content: space-around !important}
+.align-items-start {align-items: flex-start !important}
+.align-items-end {align-items: flex-end !important}
+.align-items-center {align-items: center !important}
+.align-items-baseline {align-items: baseline !important}
+.align-items-stretch {align-items: stretch !important}
+.align-content-start {align-content: flex-start !important}
+.align-content-end {align-content: flex-end !important}
+.align-content-center {align-content: center !important}
+.align-content-between {align-content: space-between !important}
+.align-content-around {align-content: space-around !important}
+.align-content-stretch {align-content: stretch !important}
+.align-self-auto {align-self: auto !important}
+.align-self-start {align-self: flex-start !important}
+.align-self-end {align-self: flex-end !important}
+.align-self-center {align-self: center !important}
+.align-self-baseline {align-self: baseline !important}
+.align-self-stretch {align-self: stretch !important}
+
 /* ***************************************
 	Spacing (from OOCSS)
 *************************************** */
+.maa{margin:auto}
+.mra{margin-right:auto}
+.mla{margin-left:auto}
+.mta{margin-top:auto}
+.mba{margin-bottom:auto}
+
 <?php
 /**
  * Spacing classes
@@ -154,40 +192,3 @@ echo <<<CSS
 .mbl, .mvl{margin-bottom:$large}
 CSS;
 
-.maa{margin:auto}
-.mra{margin-right:auto}
-.mla{margin-left:auto}
-.mta{margin-top:auto}
-.mba{margin-bottom:auto}
-
-/* Flexbox */
-.flex {display: flex !important}
-.flex-row {flex-direction: row !important}
-.flex-column {flex-direction: column !important}
-.flex-row-reverse {flex-direction: row-reverse !important}
-.flex-column-reverse {flex-direction: column-reverse !important}
-.flex-wrap {flex-wrap: wrap !important}
-.flex-nowrap {flex-wrap: nowrap !important}
-.flex-wrap-reverse {flex-wrap: wrap-reverse !important}
-.justify-content-start {justify-content: flex-start !important}
-.justify-content-end {justify-content: flex-end !important}
-.justify-content-center {justify-content: center !important}
-.justify-content-between {justify-content: space-between !important}
-.justify-content-around {justify-content: space-around !important}
-.align-items-start {align-items: flex-start !important}
-.align-items-end {align-items: flex-end !important}
-.align-items-center {align-items: center !important}
-.align-items-baseline {align-items: baseline !important}
-.align-items-stretch {align-items: stretch !important}
-.align-content-start {align-content: flex-start !important}
-.align-content-end {align-content: flex-end !important}
-.align-content-center {align-content: center !important}
-.align-content-between {align-content: space-between !important}
-.align-content-around {align-content: space-around !important}
-.align-content-stretch {align-content: stretch !important}
-.align-self-auto {align-self: auto !important}
-.align-self-start {align-self: flex-start !important}
-.align-self-end {align-self: flex-end !important}
-.align-self-center {align-self: center !important}
-.align-self-baseline {align-self: baseline !important}
-.align-self-stretch {align-self: stretch !important}
