3acbe9765a7cf97125e8d0e4f8e7b19a5a8c83af
Jerome Bakker <jeabakker@coldtrick.com>
2020-12-18 11:01:13 +0100

    chore(i18n): update translations

---
+++ mod/friends/languages/ro_ro.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Translation file
+ *
+ * Note: don't change the return array to short notation because Transifex can't handle those during `tx push -s`
+ */
+
+return array(
+	
+	'relationship:friendrequest' => "%s a cerut prietenia cu %s",
+	'relationship:friendrequest:pending' => "%s dorește să-ți fie prieten/ă",
+	'relationship:friendrequest:sent' => "Ai cerut prietenia cu %s",
+	
+	// plugin settings
+	'friends:settings:request:description' => "Implicit orice utilizator poate fi prieten/ă cu orice alt utilizator, e ca și urmărirea activității altor utilizatori.
+După activarea cererilor de prietenie, atunci când utilizatorul A dorește să fie prieten/ă cu utilizatorul B, utilizatorul B trebuie să aprobe cererea. După aprobare, utilizatorul A va fi prieten/ă cu utilizatorul B și utilizatorul B va fi prieten/ă cu utilizatorul A.",
+	'friends:settings:request:label' => "Activează cererile de prietenie",
+	'friends:settings:request:help' => "Utilizatorii trebuie să aprobe cererea de prietenie iar prieteniile vor deveni bidirecționale",
+	
+	'friends:owned' => "Prieteni cu %s",
+	'friend:add' => "Adaugă prieten/ă",
+	'friend:remove' => "Îndepărtează prietenia",
+	'friends:menu:request:status:pending' => "Cerere de prietenie în așteptare",
+
+	'friends:add:successful' => "Ai adăugat cu succes pe %s ca și prieten/ă",
+	'friends:add:duplicate' => "Ești deja prieten/ă cu %s",
+	'friends:add:failure' => "Nu am putut adăuga pe %s ca prieten/ă",
+	'friends:request:successful' => 'O cerere de prietenie a fost trimisă către %s',
+	'friends:request:error' => 'O eroare a apărut la procesarea prieteniei tale cu %s',
+
+	'friends:remove:successful' => "Ai îndepărtat cu succes pe %s de la prieteni",
+	'friends:remove:no_friend' => "Tu și %s nu sunteți prieteni",
+	'friends:remove:failure' => "Nu am putut îndepărta pe %s de la prieteni.",
+
+	'friends:none' => "Nu sunt prieteni momentan.",
+	'friends:of:owned' => "Persoane care au făcut pe %s prieten/ă",
+
+	'friends:of' => "Prieteni cu",
+	
+	'friends:request:pending' => "Cereri de prietenie în așteptare",
+	'friends:request:pending:none' => "Nu au fost găsite cereri de prietenie în așteptare.",
+	'friends:request:sent' => "Cereri de prietenie trimise",
+	'friends:request:sent:none' => "Nu s-au trimis cereri de prietenie.",
+	
+	'friends:num_display' => "Numărul de prieteni de afișat",
+	
+	'widgets:friends:name' => "Prieteni",
+	'widgets:friends:description' => "Afișează-ți câțiva prieteni.",
+	
+	'friends:notification:request:subject' => "%s dorește să-ți fie prieten/ă!",
+	'friends:notification:request:message' => "Salutare %s,
+
+%s a cerut să-ți fie prieten/ă pe %s.
+
+Pentru a vedea cererea de prietenie, apasă aici:
+%s",
+	
+	'friends:notification:request:decline:subject' => "%s a respins cererea ta de prietenie",
+	'friends:notification:request:decline:message' => "Salutare %s,
+
+%s a respins cererea ta de prietenie.",
+	
+	'friends:notification:request:accept:subject' => "%s a acceptat cererea ta de prietenie",
+	'friends:notification:request:accept:message' => "Salutare %s,
+
+%s a acceptat cererea ta de prietenie.",
+	
+	'friends:action:friendrequest:revoke:fail' => "O eroare a apărut la revocarea cererii de prietenie, te rugăm să încerci din nou",
+	'friends:action:friendrequest:revoke:success' => "Cererea de prietenie a fost revocată",
+	
+	'friends:action:friendrequest:decline:fail' => "O eroare a apărut la respingerea cererii de prietenie, te rugăm să încerci din nou",
+	'friends:action:friendrequest:decline:success' => "Cererea de prietenie a fost respinsă",
+	
+	'friends:action:friendrequest:accept:fail' => "O eroare a apărut la acceptarea cererii de prietenie, te rugăm să încerci din nou",
+	'friends:action:friendrequest:accept:success' => "Cererea de prietenie a fost acceptată",
+);
---
+++ mod/messages/languages/ro_ro.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Translation file
+ *
+ * Note: don't change the return array to short notation because Transifex can't handle those during `tx push -s`
+ */
+
+return array(
+	/**
+	* Menu items and titles
+	*/
+
+	'messages' => "Mesaje",
+	'messages:unreadcount' => "%s necitite",
+	'messages:user' => "Inbox %s",
+	'messages:inbox' => "Primite",
+	'messages:sent' => "Trimise",
+	'messages:message' => "Mesaj",
+	'messages:title' => "Subiect",
+	'messages:to:help' => "Scrie numele de utilizator al destinatarului aici",
+	'messages:inbox' => "Primite",
+	'messages:sendmessage' => "Trimite un mesaj",
+	'messages:add' => "Compune un mesaj",
+	'messages:sentmessages' => "Mesaje trimise",
+	'messages:toggle' => 'Comută toate',
+	'messages:markread' => 'Marchează ca citit',
+
+	'notification:method:site' => 'Site',
+
+	'messages:error' => 'A fost o problemă la salvarea mesajului tău. Te rugăm să încerci din nou.',
+
+	'item:object:messages' => 'Mesaj',
+	'collection:object:messages' => 'Mesaje',
+
+	/**
+	* Status messages
+	*/
+
+	'messages:posted' => "Mesajul tău a fost trimis cu succes.",
+	'messages:success:delete:single' => 'Mesajul a fost șters',
+	'messages:success:delete' => 'Mesaj șters',
+	'messages:success:read' => 'Mesaje marcate ca și citite',
+	'messages:error:messages_not_selected' => 'Nici un mesaj selectat',
+	'messages:error:delete:single' => 'Nu s-a putut șterge mesajul',
+
+	/**
+	* Email messages
+	*/
+
+	'messages:email:subject' => 'Ai un mesaj nou!',
+	'messages:email:body' => "Ai un mesaj nou de la %s.
+
+Scrie:
+
+%s
+
+Pentru a vedea mesajul, apasă aici:
+%s
+
+Pentru a-i trimite un mesaj utilizatorului %s, apasă aici:
+%s",
+
+	/**
+	* Error messages
+	*/
+
+	'messages:blank' => "Scuze; trebuie să adaugi ceva în corpul mesajului înainte să îl putem salva.",
+	'messages:nomessages' => "Nu există mesaje.",
+	'messages:user:nonexist' => "Nu am putut găsi destinatarul în baza de date.",
+	'messages:user:blank' => "Nu ai selectat pe cineva pentru a trimite.",
+	'messages:user:self' => "Nu-ți poți trimite un mesaj ție.",
+
+	'messages:deleted_sender' => 'Utilizator șters',
+
+);
