948927a31e65979991c3d0c332f2587650602003
Jeroen Dalsem <jdalsem@coldtrick.com>
2021-07-30 14:42:12 +0200

    fix(plugins): position and required state of plugins are now asserted
    fixes #13520

--- engine/tests/test_files/mod/dependent_plugin/elgg-plugin.php
+++ engine/tests/test_files/mod/dependent_plugin/elgg-plugin.php
@@ -4,7 +4,13 @@ return [
 		'name' => 'Dependent Plugin',
 		'version' => '1.9',
 		'dependencies' => [
-			'parent_plugin' => [],
+			'parent_plugin' => [
+				'position' => 'after',
+			],
+			'non_existing_plugin' => [
+				'must_be_active' => false,
+				'position' => 'before',
+			],
 		],
 	],
 ];
