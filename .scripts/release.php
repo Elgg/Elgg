d25f4701cdc7039511b626654442f0f29f5013e0
Jerôme Bakker <jeabakker@coldtrick.com>
2021-11-05 14:32:44 +0100

    chore(scripts): international docs build moved to language pull
    After fetching the new translations from Transifex it's a good time to
    build the international docs.
    This will also makte the release commit cleaner as no docs build is
    present.

--- .scripts/release.php
+++ .scripts/release.php
@@ -1,7 +1,14 @@
 <?php
+/**
+ * Make the release commit this will:
+ * - Write the changelog
+ * - Update the version in composer.json
+ *
+ * Usage: php .scripts/release.php <semver>
+ */
 
 if (!isset($argv[1]) || $argv[1] == '--help') {
-	echo "Usage: php .scripts/release.php <semver>\n";
+	echo 'Usage: php .scripts/release.php <semver>' . PHP_EOL;
 	exit;
 }
 
@@ -11,9 +18,10 @@ $version = $argv[1];
 // Performing check according to: https://getcomposer.org/doc/04-schema.md#version
 $regexp = '/^[0-9]+\.[0-9]+\.[0-9]+(?:-(?:alpha|beta|rc)\.[0-9]+)?$/';
 
+$matches = [];
 if (!preg_match($regexp, $version, $matches)) {
-	echo "Bad version format. You must follow the format of X.Y.Z with an optional suffix of"
-	    . " -alpha.N, -beta.N, or -rc.N (where N is a number).\n";
+	echo 'Bad version format. You must follow the format of X.Y.Z with an optional suffix of'
+	    . ' -alpha.N, -beta.N, or -rc.N (where N is a number).' . PHP_EOL;
 	exit(1);
 }
 
@@ -21,10 +29,12 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 function run_commands($commands) {
 	foreach ($commands as $command) {
-		echo "$command\n";
+		echo $command . PHP_EOL;
+
+		$return_val = null;
 		passthru($command, $return_val);
 		if ($return_val !== 0) {
-			echo "Error executing command! Interrupting!\n";
+			echo 'Error executing command! Interrupting!' . PHP_EOL;
 			exit(2);
 		}
 	}
@@ -32,23 +42,23 @@ function run_commands($commands) {
 
 $elgg_path = dirname(__DIR__);
 
-$branch = "release-$version";
+$branch = "release-{$version}";
 
-// Setup. Version checks are here so we fail early if any deps are missing
+// Setup
 run_commands([
-	"git --version",
-	"yarn --version",
-	"node --version",
-	"sphinx-build --version",
+	// Version checks are here so we fail early if any deps are missing
+	'git --version',
+	'yarn --version',
+	'node --version',
 
-	"cd $elgg_path",
-	"git checkout -B $branch",
+	"cd {$elgg_path}",
+	"git checkout -B {$branch}",
 ]);
 
 // Update version in composer.json
 $encoding = new \Elgg\Json\EmptyKeyEncoding();
 
-$composer_path = "$elgg_path/composer.json";
+$composer_path = "{$elgg_path}/composer.json";
 $composer_config = $encoding->decode(file_get_contents($composer_path));
 $composer_config->version = $version;
 $json = $encoding->encode($composer_config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
@@ -57,13 +67,13 @@ file_put_contents($composer_path, $json);
 // make the new release
 run_commands(array(
 	// update hash in composer.lock, because version was updated and now there is a mismatch between .json and .lock
-	"composer update --lock",
+	'composer update --lock',
+
 	// Generate changelog
-	"sphinx-build -b gettext docs docs/locale/pot",
-	"sphinx-intl build --locale-dir=docs/locale/",
-	"yarn install",
-	"node .scripts/write-changelog.js",
-	// commit everything to github
-	"git add .",
-	"git commit -am \"chore(release): v$version\"",
+	'yarn install',
+	'node .scripts/write-changelog.js',
+
+	// commit everything to GitHub
+	'git add .',
+	'git commit -am \'chore(release): v$version\'',
 ));
