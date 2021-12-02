54bbf7c0ee1adcc8b1ea9ab13c72d85ae7bbf5bc
Jeroen Dalsem <jdalsem@coldtrick.com>
2021-12-02 09:54:50 +0100

    feat(custom_index): added settings to control homepage modules

--- mod/custom_index/elgg-plugin.php
+++ mod/custom_index/elgg-plugin.php
@@ -11,4 +11,15 @@ return [
 			],
 		],
 	],
+	'settings' => [
+		'module_about_enabled' => 1,
+		'module_register_enabled' => 1,
+		'module_login_enabled' => 0,
+		'module_activity_enabled' => 1,
+		'module_blog_enabled' => 1,
+		'module_bookmarks_enabled' => 1,
+		'module_file_enabled' => 1,
+		'module_groups_enabled' => 1,
+		'module_users_enabled' => 1,
+	],
 ];
