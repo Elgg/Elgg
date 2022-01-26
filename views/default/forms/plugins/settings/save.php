bee2f1e2556597abb0715465a9857db33a135af9
Jerôme Bakker <jeabakker@coldtrick.com>
2022-01-26 12:10:48 +0100

    fix(plugins): don't show plugin (user)settings form if no content

--- views/default/forms/plugins/settings/save.php
+++ views/default/forms/plugins/settings/save.php
@@ -9,13 +9,17 @@ $user_guid = elgg_extract('user_guid', $vars, elgg_get_logged_in_user_guid());
 
 // Do we want to show admin settings or user settings
 $type = elgg_extract('type', $vars, '');
-
 if ($type != 'user') {
 	$type = '';
 }
 
-if (elgg_view_exists("plugins/$plugin_id/{$type}settings")) {
-	echo elgg_view("plugins/$plugin_id/{$type}settings", $vars);
+$settings = '';
+if (elgg_view_exists("plugins/{$plugin_id}/{$type}settings")) {
+	$settings = elgg_view("plugins/{$plugin_id}/{$type}settings", $vars);
+}
+
+if (empty($settings)) {
+	return;
 }
 
 echo elgg_view_field([
@@ -29,6 +33,8 @@ echo elgg_view_field([
 	'name' => 'user_guid',
 	'value' => $user_guid,
 ]);
+
+echo $settings;
 
 $footer = elgg_view_field([
 	'#type' => 'submit',
