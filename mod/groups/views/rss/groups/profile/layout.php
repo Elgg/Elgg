54353859cacfdefe368afe63df23d59acd9c2233
Jeroen Dalsem <jdalsem@coldtrick.com>
2022-06-15 13:33:10 +0200

    fix(groups): groupprofile rss feed should show recent searchable content

--- mod/groups/views/rss/groups/profile/layout.php
+++ mod/groups/views/rss/groups/profile/layout.php
@@ -7,8 +7,7 @@
  * @uses $vars['entity'] ElggGroup object
  */
 
-$entities = elgg_get_config('registered_entities');
-
+$entities = elgg_entity_types_with_capability('searchable');
 if (!empty($entities['object'])) {
 	echo elgg_list_entities([
 		'type' => 'object',
