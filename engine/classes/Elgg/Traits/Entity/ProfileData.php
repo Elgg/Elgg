7be4ca64eac049bc184edc99a052b9859c3a1469
Jeroen Dalsem <jdalsem@coldtrick.com>
2022-06-20 15:00:32 +0200

    feat(user): added internal cache for profile data
    fixes #11502

--- engine/classes/Elgg/Traits/Entity/ProfileData.php
+++ engine/classes/Elgg/Traits/Entity/ProfileData.php
@@ -9,6 +9,11 @@ namespace Elgg\Traits\Entity;
  * @since 3.1
  */
 trait ProfileData {
+	
+	/**
+	 * @var array all profile data
+	 */
+	protected $_profile_data = [];
 
 	/**
 	 * Store profile data
@@ -37,6 +42,8 @@ trait ProfileData {
 			$this->annotate("profile:{$profile_field_name}", $v, $access_id, $this->guid);
 		}
 		
+		$this->_profile_data = null;
+		
 		// for BC, keep storing fields in MD, but we'll read annotations only
 		$this->$profile_field_name = $value;
 	}
@@ -49,10 +56,16 @@ trait ProfileData {
 	 * @return null|mixed null if no profile data was found
 	 */
 	public function getProfileData(string $profile_field_name) {
-		$annotations = $this->getAnnotations([
-			'annotation_name' => "profile:{$profile_field_name}",
-			'limit' => false,
-		]);
+		
+		if (empty($this->guid)) {
+			// no way to return all temp annotations for an unsaved entity
+			$annotations = $this->getAnnotations([
+				'annotation_name' => "profile:{$profile_field_name}",
+				'limit' => false,
+			]);
+		} else {
+			$annotations = elgg_extract("profile:{$profile_field_name}", $this->getAllProfileAnnotations());
+		}
 		
 		if (empty($annotations)) {
 			return null;
@@ -81,6 +94,37 @@ trait ProfileData {
 	}
 	
 	/**
+	 * Returns all profile annotations
+	 *
+	 * @return array
+	 */
+	protected function getAllProfileAnnotations(): array {
+		// store logged in user guid to prevent unwanted access to annotations when switching logged in user during script run (e.g. ElggCoreUserTest)
+		$logged_in_user_guid = elgg_get_logged_in_user_guid();
+		if (!isset($this->_profile_data[$logged_in_user_guid])) {
+			$annotations = $this->getAnnotations([
+				'limit' => false,
+				'wheres' => function(\Elgg\Database\QueryBuilder $qb, $main_alias) {
+					return $qb->compare("{$main_alias}.name", 'LIKE', 'profile:%', ELGG_VALUE_STRING);
+				},
+			]);
+			
+			$profile_data = [];
+			foreach ($annotations as $annotation) {
+				if (!isset($profile_data[$annotation->name])) {
+					$profile_data[$annotation->name] = [];
+				}
+				
+				$profile_data[$annotation->name][] = $annotation;
+			}
+			
+			$this->_profile_data[$logged_in_user_guid] = $profile_data;
+		}
+		
+		return $this->_profile_data[$logged_in_user_guid];
+	}
+	
+	/**
 	 * Remove profile data
 	 *
 	 * @param string $profile_field_name the profile field name to remove
@@ -90,6 +134,8 @@ trait ProfileData {
 	public function deleteProfileData(string $profile_field_name) {
 		$result = $this->deleteAnnotations("profile:{$profile_field_name}");
 		$result &= $this->deleteMetadata($profile_field_name);
+		
+		$this->_profile_data = null;
 		
 		return $result;
 	}
