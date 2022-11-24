97d657c410500640c8a44c2a9cf9fa44f9de4b8e
Jeroen Dalsem <jdalsem@coldtrick.com>
2022-11-24 14:33:39 +0100

    feat(widgets): target column and position can now be configured

--- views/default/resources/widgets/add_panel.js
+++ views/default/resources/widgets/add_panel.js
@@ -23,15 +23,23 @@ define(['jquery', 'elgg/Ajax'], function($, Ajax) {
 		ajax.path(href).done(function(output) {
 			var query_parts = elgg.parse_url(href, 'query', true);
 			var selector = '';
+			var context = query_parts['context'];
+			var page_owner_guid = query_parts['page_owner_guid'];
+			var new_widget_column = query_parts['new_widget_column'] || 1;
+			var new_widget_position = query_parts['new_widget_position'] || 'top';
 
-			if (query_parts['context'] && query_parts['page_owner_guid']) {
+			if (context && page_owner_guid) {
 				// target the correct widget layout
-				selector = '.elgg-layout-widgets-' + query_parts['context'] + '[data-page-owner-guid="' + query_parts['page_owner_guid'] + '"] #elgg-widget-col-1';
+				selector = '.elgg-layout-widgets-' + context + '[data-page-owner-guid="' + page_owner_guid + '"] #elgg-widget-col-' + new_widget_column;
 			} else {
-				selector = '#elgg-widget-col-1';
+				selector = '#elgg-widget-col-' + new_widget_column;
 			}
 
-			$(selector).prepend(output);
+			if (new_widget_position === 'top') {
+				$(selector).prepend(output);
+			} else {
+				$(selector).append(output);
+			}
 		});
 	};
 	
