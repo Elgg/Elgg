772160ae3855e83d0566d5745c3ef235ffe8be5a
Jeroen Dalsem <jdalsem@coldtrick.com>
2023-02-23 13:41:01 +0100

    chore(views): small rewrite and cleanup of some code

--- views/default/resources/widgets/add_panel.js
+++ views/default/resources/widgets/add_panel.js
@@ -1,5 +1,4 @@
-define(['jquery', 'elgg/Ajax'], function($, Ajax) {
-	var ajax = new Ajax();
+define(['jquery', 'elgg', 'elgg/Ajax'], function($, elgg, Ajax) {
 	
 	/**
 	 * Adds a new widget
@@ -18,8 +17,10 @@ define(['jquery', 'elgg/Ajax'], function($, Ajax) {
 		if (!$item.is('.elgg-widget-multiple')) {
 			$item.toggleClass('elgg-state-unavailable elgg-state-available');
 		}
-
+		
 		var href = $(this).attr('href');
+		var ajax = new Ajax();
+		
 		ajax.path(href).done(function(output) {
 			var query_parts = elgg.parse_url(href, 'query', true);
 			var selector = '';
@@ -27,14 +28,14 @@ define(['jquery', 'elgg/Ajax'], function($, Ajax) {
 			var page_owner_guid = query_parts['page_owner_guid'];
 			var new_widget_column = query_parts['new_widget_column'] || 1;
 			var new_widget_position = query_parts['new_widget_position'] || 'top';
-
+			
 			if (context && page_owner_guid) {
 				// target the correct widget layout
 				selector = '.elgg-layout-widgets-' + context + '[data-page-owner-guid="' + page_owner_guid + '"] #elgg-widget-col-' + new_widget_column;
 			} else {
 				selector = '#elgg-widget-col-' + new_widget_column;
 			}
-
+			
 			if (new_widget_position === 'top') {
 				$(selector).prepend(output);
 			} else {
