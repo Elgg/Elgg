24e31246cc22c876766999ec1f2a0ed7511257f3
Jerôme Bakker <jeabakker@coldtrick.com>
2023-03-24 13:14:25 +0100

    chore(thewire): fixed multiple forms on the same page

--- mod/thewire/views/default/forms/thewire/add.js
+++ mod/thewire/views/default/forms/thewire/add.js
@@ -4,33 +4,36 @@ define(['jquery'], function ($) {
 	 * Update the number of characters left with every keystroke
 	 *
 	 * @param {Object}  textarea
-	 * @param {Object}  status
+	 * @param {Object}  wrapper
 	 * @param {integer} limit
 	 * @return void
 	 */
-	var textCounter = function(textarea, status, limit) {
-
+	function textCounter(textarea, wrapper, limit) {
 		var remaining_chars = limit - $(textarea).val().length;
-		status.html(remaining_chars);
-		var $submit = $(textarea).closest('form').find('#thewire-submit-button');
+		var $container = $(wrapper).find('> div');
+		var $form = $(textarea).closest('form');
+		
+		$container.find('> span').text(remaining_chars);
+		
+		var $submit = $form.find('button[type="submit"]');
 
 		if (remaining_chars < 0) {
-			status.parent().addClass("thewire-characters-remaining-warning");
+			$container.addClass("thewire-characters-remaining-warning");
 			$submit.prop('disabled', true);
 			$submit.addClass('elgg-state-disabled');
 		} else {
-			status.parent().removeClass("thewire-characters-remaining-warning");
+			$container.removeClass("thewire-characters-remaining-warning");
 			$submit.prop('disabled', false);
 			$submit.removeClass('elgg-state-disabled');
 		}
 	};
 
-	var checkMaxLength = function() {
+	function checkMaxLength() {
 		var maxLength = $(this).data('max-length');
 		if (maxLength) {
-			textCounter(this, $(this).closest('form').find("#thewire-characters-remaining span"), maxLength);
+			textCounter(this, $(this).closest('form').find(".thewire-characters-wrapper"), maxLength);
 		}
 	};
 
-	$(document).on('input propertychange', "#thewire-textarea", checkMaxLength);
+	$(document).on('input propertychange', ".thewire-textarea", checkMaxLength);
 });
