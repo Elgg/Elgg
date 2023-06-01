5af4787c465e92ecf13cefb316bb2df7f3e24287
Finn Thomas <Finn.Thomas@topicus.nl>
2023-06-01 17:14:50 +0200

    feature elggEntity created softDelete/restore methods, relinked entity deletions to this

--- engine/classes/Elgg/Database/EntityTable.php
+++ engine/classes/Elgg/Database/EntityTable.php
@@ -440,7 +440,38 @@ class EntityTable {
 		return $user;
 	}
 
-	/**
+    /**
+     * soft delete entity
+     *
+     * @param \ElggEntity $entity Entity to soft delete
+     *
+     * @return bool
+     */
+    public function softDelete(\ElggEntity $entity): bool {
+        $qb = Update::table(self::TABLE_NAME);
+        $qb->set('soft_deleted', $qb->param('yes', ELGG_VALUE_STRING))
+            ->where($qb->compare('guid', '=', $entity->guid, ELGG_VALUE_GUID));
+
+        return $this->db->updateData($qb);
+    }
+
+    /**
+     * restore entity
+     *
+     * @param \ElggEntity $entity Entity to restore
+     *
+     * @return bool
+     */
+    public function restore(\ElggEntity $entity): bool {
+        $qb = Update::table(self::TABLE_NAME);
+        $qb->set('soft_deleted', $qb->param('no', ELGG_VALUE_STRING))
+            ->where($qb->compare('guid', '=', $entity->guid, ELGG_VALUE_GUID));
+
+        return $this->db->updateData($qb);
+    }
+
+
+    /**
 	 * Enables entity
 	 *
 	 * @param \ElggEntity $entity Entity to enable
