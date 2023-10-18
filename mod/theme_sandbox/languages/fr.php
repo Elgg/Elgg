0652d030a390b63aec580483a4f6ac22958d5693
Jerôme Bakker <jeabakker@coldtrick.com>
2023-10-18 11:50:52 +0200

    chore(i18n): update translations

---
+++ mod/theme_sandbox/languages/fr.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Translation file
+ *
+ * Note: don't change the return array to short notation because Transifex can't handle those during `tx push -s`
+ */
+
+return array(
+	'admin:information:theme_sandbox' => 'Bac à sable du thème',
+	
+	'theme_sandbox:intro' => 'Introduction',
+	'theme_sandbox:breakout' => 'Sortir de l\'iframe',
+	'theme_sandbox:buttons' => 'Boutons',
+	'theme_sandbox:components' => 'Composants',
+	'theme_sandbox:email' => 'E-mail',
+	'theme_sandbox:forms' => 'Formulaires',
+	'theme_sandbox:grid' => 'Grille',
+	'theme_sandbox:icons' => 'Icônes',
+	'theme_sandbox:javascript' => 'JavaScript',
+	'theme_sandbox:layouts' => 'Mises en page',
+	'theme_sandbox:modules' => 'Modules',
+	'theme_sandbox:typography' => 'Typographie',
+
+	'theme_sandbox:icons:blurb' => 'Utilisez <em>elgg_view_icon($name)</em> pour afficher des icônes. ',
+	
+	'theme_sandbox:test_email:button' => "Envoyer un e-mail de test",
+	'theme_sandbox:test_email:success' => "E-mail de test envoyé à : %s",
+);
---
+++ mod/theme_sandbox/languages/nl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Translation file
+ *
+ * Note: don't change the return array to short notation because Transifex can't handle those during `tx push -s`
+ */
+
+return array(
+	'admin:information:theme_sandbox' => 'Theme Sandbox',
+	
+	'theme_sandbox:intro' => 'Introductie',
+	'theme_sandbox:breakout' => 'Verlaat het iframe',
+	'theme_sandbox:buttons' => 'Knoppen',
+	'theme_sandbox:components' => 'Componenten',
+	'theme_sandbox:email' => 'Email',
+	'theme_sandbox:forms' => 'Formulieren',
+	'theme_sandbox:grid' => 'Raster',
+	'theme_sandbox:icons' => 'Iconen',
+	'theme_sandbox:javascript' => 'JavaScript',
+	'theme_sandbox:layouts' => 'Lay-outs',
+	'theme_sandbox:modules' => 'Modules',
+	'theme_sandbox:navigation' => 'Navigatie',
+	'theme_sandbox:typography' => 'Typografie',
+
+	'theme_sandbox:icons:blurb' => 'Gebruik <em>elgg_view_icon($name)</em> om iconen weer te geven.',
+	
+	'theme_sandbox:test_email:button' => "Stuur test email",
+	'theme_sandbox:test_email:success' => "Test email verzonden naar: %s",
+);
