441c0f442f87f43eef5c277708316f33a3a8091d
Jeroen Dalsem <jdalsem@coldtrick.com>
2023-10-23 10:00:04 +0200

    feat(ckeditor): updated to v40.0.0

--- mod/ckeditor/vendors/ckeditor5/webpack.config.js
+++ mod/ckeditor/vendors/ckeditor5/webpack.config.js
@@ -10,8 +10,7 @@
 const path = require( 'path' );
 const webpack = require( 'webpack' );
 const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const TerserWebpackPlugin = require( 'terser-webpack-plugin' );
+const { CKEditorTranslationsPlugin } = require( '@ckeditor/ckeditor5-dev-translations' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 module.exports = {
@@ -30,25 +29,9 @@ module.exports = {
 		libraryExport: 'default'
 	},
 
-	optimization: {
-		minimizer: [
-			new TerserWebpackPlugin( {
-				sourceMap: true,
-				terserOptions: {
-					output: {
-						// Preserve CKEditor 5 license comments.
-						comments: /^!/
-					}
-				},
-				extractComments: false
-			} )
-		]
-	},
-
 	plugins: [
-		new CKEditorWebpackPlugin( {
-			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
+		new CKEditorTranslationsPlugin( {
+			// See https://ckeditor.com/docs/ckeditor5/latest/features/ui-language.html
 			language: 'en',
 			additionalLanguages: 'all'
 		} ),
