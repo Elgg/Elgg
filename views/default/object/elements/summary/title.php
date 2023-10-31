385475469ea96ca3f7034811305fbbde866fc1af
Jerôme Bakker <jeabakker@coldtrick.com>
2023-10-31 15:17:11 +0100

    fix(views): don't output a summary title if no display name

--- views/default/object/elements/summary/title.php
+++ views/default/object/elements/summary/title.php
@@ -6,17 +6,27 @@
  * @uses $vars['title']     Title link (optional) false = no title, '' = default
  */
 
-$title = elgg_extract('title', $vars, '');
+$title = elgg_extract('title', $vars);
 if ($title === false) {
 	return;
 }
 
+$title = (string) $title;
 $entity = elgg_extract('entity', $vars);
-if ($title === '' && $entity instanceof ElggEntity) {
-	$title = elgg_view('output/url', [
-		'text' => elgg_get_excerpt($entity->getDisplayName(), 100),
-		'href' => $entity->getURL() ?: false,
-	]);
+if ($title === '' && $entity instanceof \ElggEntity) {
+	$title = elgg_get_excerpt($entity->getDisplayName(), 100);
+	if (elgg_is_empty($title)) {
+		return;
+	}
+	
+	$href = $entity->getURL();
+	if (!empty($href)) {
+		$title = elgg_view_url($href, $title);
+	}
+}
+
+if (elgg_is_empty($title)) {
+	return;
 }
 
 echo elgg_format_element('div', [
