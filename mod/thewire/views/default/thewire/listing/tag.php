87dccb1c0c7ece4fdd4b0eead9ea5d8f5143353b
Jerôme Bakker <jeabakker@coldtrick.com>
2024-02-27 15:23:25 +0100

    chore(thewire): improved listing views

--- mod/thewire/views/default/thewire/listing/tag.php
+++ mod/thewire/views/default/thewire/listing/tag.php
@@ -2,20 +2,24 @@
 /**
  * Show a listing of Wire posts with a given tag
  *
+ * @uses $vars['options'] Additional listing options
  * @uses $vars['tag'] the tag to show
  */
 
+$options = (array) elgg_extract('options', $vars);
 $tag = elgg_extract('tag', $vars);
 if (elgg_is_empty($tag)) {
 	return;
 }
 
-$vars['options'] = [
+$tag_options = [
 	'metadata_name_value_pairs' => [
 		'name' => 'tags',
 		'value' => $tag,
 		'case_sensitive' => false,
 	],
 ];
+
+$vars['options'] = array_merge($options, $tag_options);
 
 echo elgg_view('thewire/listing/all', $vars);
