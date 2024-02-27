e87b3b64e052cbd2c4e144a08dc06df8783f3295
Jerôme Bakker <jeabakker@coldtrick.com>
2024-02-27 15:00:23 +0100

    chore(groups): improved listing views

--- mod/groups/views/default/groups/listing/all.php
+++ mod/groups/views/default/groups/listing/all.php
@@ -3,10 +3,20 @@
  * Renders a list of groups (default sorted by creation date)
  *
  * Note: this view has a corresponding view in the rss view type, changes should be reflected
+ *
+ * @uses $vars['options'] Additional listing options
+ * @uses $vars['getter']  Function to get entities (default: 'elgg_get_entities')
  */
 
-echo elgg_list_entities([
+$defaults = [
 	'type' => 'group',
 	'full_view' => false,
 	'no_results' => elgg_echo('groups:none'),
-]);
+];
+
+$options = (array) elgg_extract('options', $vars, []);
+$options = array_merge($defaults, $options);
+
+$getter = (string) elgg_extract('getter', $vars, 'elgg_get_entities');
+
+echo elgg_list_entities($options, $getter);
