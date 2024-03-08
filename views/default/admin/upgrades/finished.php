adc8b9028049996e2b0a5b243a2402dbe3eb009b
Jerôme Bakker <jeabakker@coldtrick.com>
2024-03-08 11:58:56 +0100

    fix(upgrades): show correct count in the admin upgrade listing
    Hidden entities weren't counted in the admin listing, but where counted
    during execution.
    
    fixes #14520

--- views/default/admin/upgrades/finished.php
+++ views/default/admin/upgrades/finished.php
@@ -16,10 +16,13 @@ $count = count($upgrades);
 
 $items = array_slice($upgrades, $offset, $limit);
 
-echo elgg_view_entity_list($items, [
-	'offset' => $offset,
-	'limit' => $limit,
-	'count' => $count,
-	'pagination_behaviour' => 'ajax-replace',
-	'no_results' => true,
-]);
+// make sure to use the same options as in \Elgg\UpgradeService::executeUpgrade()
+echo elgg_call(ELGG_IGNORE_ACCESS | ELGG_SHOW_DISABLED_ENTITIES, function() use ($items, $offset, $limit, $count) {
+	return elgg_view_entity_list($items, [
+		'offset' => $offset,
+		'limit' => $limit,
+		'count' => $count,
+		'pagination_behaviour' => 'ajax-replace',
+		'no_results' => true,
+	]);
+});
