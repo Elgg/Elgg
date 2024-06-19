415e9ed270298dc3709618d423eab813fa1e6c2a
Jerôme Bakker <jeabakker@coldtrick.com>
2024-06-19 11:08:03 +0200

    chore(composer): update phpunit

---
+++ engine/tests/classes/Elgg/Mocks/Database/Result.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Elgg\Mocks\Database;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver\Result as DriverResult;
+
+class Result extends \Doctrine\DBAL\Result {
+	
+	public function __construct(DriverResult $result = null, Connection $connection = null, protected array $results, protected int $row_count) {
+	
+	}
+	
+	public function fetchAssociative(): array|false {
+		$result = array_shift($this->results);
+		return isset($result) ? (array) $result : false;
+	}
+	
+	public function fetchAllAssociative(): array {
+		return $this->results;
+	}
+	
+	public function rowCount(): int|string {
+		return $this->row_count;
+	}
+}
