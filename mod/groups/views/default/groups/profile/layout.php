e15098825fea9f56c78056ea608c277105aa531d
Jeroen Dalsem <jdalsem@coldtrick.com>
2024-07-01 15:13:49 +0200

    chore(groups): moved closed membership notice to the top of the summary

--- mod/groups/views/default/groups/profile/layout.php
+++ mod/groups/views/default/groups/profile/layout.php
@@ -6,23 +6,12 @@
  */
 
 $group = elgg_extract('entity', $vars);
-
-if (!$group instanceof ElggGroup) {
+if (!$group instanceof \ElggGroup) {
 	return;
 }
 
 echo elgg_view('groups/profile/summary', $vars);
 
 if ($group->canAccessContent()) {
-	if (!$group->isPublicMembership() && !$group->isMember()) {
-		echo elgg_view('groups/profile/closed_membership');
-	}
-
 	echo elgg_view('groups/profile/widgets', $vars);
-} else {
-	if ($group->isPublicMembership()) {
-		echo elgg_view('groups/profile/membersonly_open');
-	} else {
-		echo elgg_view('groups/profile/membersonly_closed');
-	}
 }
