cdf0e7bd62a9cd66242f5659873aad290950f5e2
Jeroen Dalsem <jdalsem@coldtrick.com>
2024-07-09 15:59:23 +0200

    chore(notifications): improved notifications form

--- views/default/notifications/settings/delayed_email.php
+++ views/default/notifications/settings/delayed_email.php
@@ -6,19 +6,19 @@
  */
 
 $user = elgg_extract('entity', $vars);
-if (!$user instanceof ElggUser || !(bool) elgg_get_config('enable_delayed_email')) {
+if (!$user instanceof \ElggUser || !(bool) elgg_get_config('enable_delayed_email')) {
 	return;
 }
 
 echo elgg_view_field([
-	'#type' => 'select',
+	'#type' => 'radio',
 	'#label' => elgg_echo('usersettings:delayed_email:interval'),
 	'#help' => elgg_echo('usersettings:delayed_email:interval:help'),
-	'#class' => 'ptl',
 	'name' => 'delayed_email_interval',
+	'align' => 'horizontal',
 	'options_values' => [
 		'daily' => elgg_echo('interval:daily'),
 		'weekly' => elgg_echo('interval:weekly'),
 	],
-	'value' => $user->delayed_email_interval,
+	'value' => $user->delayed_email_interval ?: 'daily',
 ]);
