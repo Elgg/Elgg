7d3b282b8ef8fa473903c722066fb27f73bfe7fd
Jeroen Dalsem <jdalsem@coldtrick.com>
2024-10-21 14:00:12 +0200

    feat(forms): double submit prevention support multiple submit buttons

--- views/default/input/form-double-submit.mjs
+++ views/default/input/form-double-submit.mjs
@@ -2,4 +2,9 @@ import 'jquery';
 
 $(document).on('submit', '.elgg-form-prevent-double-submit', function (e) {
 	$(this).find('button[type="submit"]').prop('disabled', true);
+	
+	var $submitter = $(e.originalEvent.submitter);
+	if ($submitter.attr('name') !== undefined) {
+		$(this).append('<input type="hidden" name="' + $submitter.attr('name') + '" value="' + $submitter.attr('value') + '"/>');
+	}
 });
