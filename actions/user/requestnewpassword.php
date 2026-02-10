e5cb56a8bfeb859a2db9cd4563e84ed5f7da6e1f
Jeroen Dalsem <jdalsem@coldtrick.com>
2026-02-10 15:54:47 +0100

    fix(account): always report success when requesting a new password
    fixes #15000

--- actions/user/requestnewpassword.php
+++ actions/user/requestnewpassword.php
@@ -10,10 +10,9 @@ if (empty($username)) {
 
 // also allow by email addresses
 $user = elgg_get_user_by_username($username, true);
-if (!$user instanceof \ElggUser) {
-	return elgg_error_response(elgg_echo('user:username:notfound', [$username]));
+if ($user instanceof \ElggUser) {
+	elgg_request_new_password($user);
 }
 
-elgg_request_new_password($user);
-
+// always report success, do not want to inform requester about account existence
 return elgg_ok_response('', elgg_echo('user:password:changereq:success'), '');
