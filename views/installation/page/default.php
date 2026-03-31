ac0f4b6ead47ff06ad3d5df00aa965abbaccb658
Jeroen Dalsem <jdalsem@coldtrick.com>
2026-03-31 09:55:08 +0200

    chore(install): always use default color scheme in installer

--- views/installation/page/default.php
+++ views/installation/page/default.php
@@ -4,6 +4,9 @@
  */
 
 echo elgg_view('page/elements/html', [
+	'html_attrs' => [
+		'data-color-scheme' => 'default', // always use default color scheme in installer
+	],
 	'head' => elgg_view('page/elements/head', $vars),
 	'body' => elgg_view('page/elements/body', $vars),
 ]);
