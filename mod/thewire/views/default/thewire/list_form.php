a13cfb54a703353ee4adb631801f3e441c058841
Jerôme Bakker <jeabakker@coldtrick.com>
2026-04-17 10:47:18 +0200

    chore(thewire): list form moved from controller to view

---
+++ mod/thewire/views/default/thewire/list_form.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Prepend the listing of TheWire with an add form
+ *
+ * @uses $vars['page']     the current listing page (all, owner, friends, etc.)
+ * @uses $vars['add_form'] should the form be added
+ */
+
+$page = elgg_extract('page', $vars);
+$user = elgg_get_logged_in_user_entity();
+
+$add_form = false;
+if ($page === 'all') {
+	$add_form = ($user instanceof \ElggUser);
+} elseif (in_array($page, ['friends', 'owner'])) {
+	$add_form = $user?->guid === elgg_get_page_owner_guid();
+}
+
+$add_form = (bool) elgg_extract('add_form', $vars, $add_form);
+if (!$add_form) {
+	return;
+}
+
+echo elgg_view_form('thewire/add', [
+	'class' => 'thewire-form',
+]);
