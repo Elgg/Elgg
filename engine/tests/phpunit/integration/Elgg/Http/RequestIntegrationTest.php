739b1c9456c7018d1aa03c6693fb8ce7bb3a8507
Jerôme Bakker <jeabakker@coldtrick.com>
2026-05-18 17:01:26 +0200

    chore(tests): make request input validation test is more robust

--- engine/tests/phpunit/integration/Elgg/Http/RequestIntegrationTest.php
+++ engine/tests/phpunit/integration/Elgg/Http/RequestIntegrationTest.php
@@ -1,8 +1,20 @@
 <?php
 
+namespace Elgg\Http;
+
 use Elgg\IntegrationTestCase;
 
 class RequestIntegrationTest extends IntegrationTestCase {
+	
+	public function up() {
+		_elgg_services()->events->backup();
+		_elgg_services()->events->registerHandler('sanitize', 'input', \Elgg\Input\ValidateInputHandler::class, 1);
+		_elgg_services()->events->registerHandler('attributes', 'htmlawed', '\Elgg\Input\ValidateInputHandler::sanitizeStyles');
+	}
+	
+	public function down() {
+		_elgg_services()->events->restore();
+	}
 	
 	public function testGetParamFiltersInput() {
 		$request = $this->prepareHttpRequest('action/foo', 'GET', ['foo' => 'very<script>alert("welcome");</script><b>bold</b>text'], 0, false);
