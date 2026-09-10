9418446ad6bcb000774268407e9d3b0ef33f0289
Jerôme Bakker <jeabakker@coldtrick.com>
2026-09-10 09:40:17 +0200

    perf(db): add index to metadata table for entity_guid name

---
+++ engine/schema/migrations/20260910072856_add_metadata_entity_guid_name_index.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+final class AddMetadataEntityGuidNameIndex extends AbstractMigration {
+ 
+	/**
+	 * Add an index to the metadata table for the columns ('entity_guid', 'name')
+	 *
+	 * @return void
+	 */
+	public function change(): void {
+		if (!$this->hasTable('metadata')) {
+			return;
+		}
+		
+		$table = $this->table('metadata');
+		if ($table->hasIndexByName('entity_guid_name')) {
+			return;
+		}
+		
+		$table->addIndex(['entity_guid', 'name'], [
+			'name' => 'entity_guid_name',
+			'unique' => false,
+			'limit' => [
+				'name' => MysqlAdapter::INT_TINY,
+			],
+		]);
+		
+		$table->save();
+	}
+}
